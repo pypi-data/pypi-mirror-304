747f0dece555d107c348de90466fa283a38cefd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:54 +0000

    Add package gv100ad-py/0.1.2/gv100ad_py-0.1.2.tar.gz

---
+++ packages/gv100ad-py/gv100ad_py-0.1.2.tar.gz/gv100ad_py-0.1.2/src/gv100ad/entities/federale_state.py
@@ -0,0 +1,21 @@
+##
+# Copyright (c) STÜBER SYSTEMS GmbH
+# Licensed under the MIT License, Version 2.0. 
+##
+
+from gv100ad.entities.base_record import BaseRecord
+
+class FederalState(BaseRecord):
+    def __init__(self, line):
+        """
+        Initializes a new instance of the FederalState class.
+        :param line: A text row with Satzart 10.
+        """
+        super().__init__(line)
+
+        self.regional_code = line[10:12]
+        self.seat_of_government = line[72:122].rstrip()
+
+    def __repr__(self):
+        return (f"FederalState(Name={self.name}, RegionalCode={self.regional_code}, "
+                f"SeatOfGovernment={self.seat_of_government}, TimeStamp={self.timestamp})")
