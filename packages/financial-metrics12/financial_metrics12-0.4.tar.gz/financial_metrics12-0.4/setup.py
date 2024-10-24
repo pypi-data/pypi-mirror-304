af64fbefbf0edc8039c55d369dfc54e94c13bf15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:11 +0000

    Add package financial-metrics12/0.4/financial_metrics12-0.4.tar.gz

---
+++ packages/financial-metrics12/financial_metrics12-0.4.tar.gz/financial_metrics12-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='financial_metrics12',
+    version='0.4',
+    description='Package for calculating net profit and ROI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Nikita1207',
+    author_email='dorogin345@gmail.com',
+    url='',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'financial_metrics1=financial_metrics1:main',
+        ],
+    },
+)
