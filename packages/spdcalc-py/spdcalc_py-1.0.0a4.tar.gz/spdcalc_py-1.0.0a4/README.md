377656a9fffc65cf958f869b6a0da4c460b1c21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 05:03:42 +0000

    Add package spdcalc-py/1.0.0a4/spdcalc_py-1.0.0a4.tar.gz

---
+++ packages/spdcalc-py/spdcalc_py-1.0.0a4.tar.gz/spdcalc_py-1.0.0a4/README.md
@@ -0,0 +1,10 @@
+# spdcalc-py
+
+[![Documentation](https://img.shields.io/badge/spdcalc-blue?style=flat-square&logo=readthedocs&logoColor=white)](https://spdcalc-py.readthedocs.io)
+[![PyPI](https://img.shields.io/pypi/v/spdcalc-py.svg?logo=python&style=flat-square)](https://pypi.org/project/spdcalc-py)
+
+Python bindings for spdcalc
+
+## Getting Started
+
+See the [Quickstart](docs/source/quickstart.md) guide for a quick introduction to using spdcalc-py.
---
+++ packages/spdcalc-py/spdcalc_py-1.0.0a4.tar.gz/spdcalc_py-1.0.0a4/py-examples/simple.py
@@ -0,0 +1,76 @@
+from spdcalc import *
+import plotly.graph_objects as go
+import numpy as np
+import pathlib
+import time
+basedir = pathlib.Path(__file__).parent.resolve()
+
+def get_spdc():
+    path = basedir / "settings.yaml"
+    f = open(path, "r")
+    config = f.read()
+    return SPDC.from_yaml(config)
+
+print(SPDC.default())
+# help(SPDC)
+# print(get_all_crystal_meta())
+spdc = get_spdc()
+# spdc.crystal_kind = 'BBO_1'
+# print(get_crystal_meta(spdc.crystal_kind))
+
+spdc.apodization = {
+  'kind': 'gaussian',
+  'parameter': {
+    'fwhm_um': 0.1
+  }
+}
+print(spdc)
+print(spdc.signal_waist_um)
+
+spdc.crystal_kind = """
+no = sqrt(2.7359+0.01878/(l^2-0.01822)-0.01354*l^2) - 9.3e-6 * T
+ne = sqrt(2.3753+0.01224/(l^2-0.01667)-0.01516*l^2) - 16.6e-6 * T
+"""
+
+spdc.to_optimum()
+
+print(spdc.to_yaml())
+
+grid = spdc.optimum_range(100)
+# tests
+# print(grid)
+# print(get_jsi(spdc, spdc.optimum_range(100).to_wavelength_space()))
+# print(get_jsi.__doc__)
+# print(spdc.delta_k(spdc.signal_frequency_hz, spdc.idler_frequency_hz))
+spectrum = spdc.joint_spectrum()
+print("shmidt", spectrum.schmidt_number(grid))
+print("HOM 2 source visibilities")
+print(spdc.hom_two_source_visibilities(grid.set_resolution(10), Integrator.default()))
+
+print("HOM Series")
+spdc = SPDC.default()
+time_steps = [x * 1e-15 for x in range(-400, 800, 50)]
+start_time = time.time()
+rates = spdc.hom_rate_series(time_steps, grid.set_resolution(500))
+print("took:", time.time() - start_time, "s")
+print(rates)
+
+grid = spdc.optimum_range(100).to_wavelength_space()
+jsi = get_jsi(spdc, grid)
+jsi = np.reshape(jsi, (100, 100))
+x_values = np.array(grid.x_values()) * 1e9
+y_values = np.array(grid.y_values()) * 1e9
+
+fig = go.Figure(data=go.Heatmap(
+  z=jsi,
+  x=x_values,
+  y=y_values
+))
+
+fig.update_layout(
+  title='Joint Spectrum Intensity',
+  xaxis_title='Signal Frequency (nm)',
+  yaxis_title='Idler Frequency (nm)',
+)
+
+fig.show()
