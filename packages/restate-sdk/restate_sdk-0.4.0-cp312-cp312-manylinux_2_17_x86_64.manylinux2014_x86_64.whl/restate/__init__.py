140be5c0b31a44f1cb7bb725852acc04941f1061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:01 +0000

    Add package restate-sdk/0.4.0/restate_sdk-0.4.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/restate-sdk/restate_sdk-0.4.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/restate/__init__.py
@@ -0,0 +1,24 @@
+#
+#  Copyright (c) 2023-2024 - Restate Software, Inc., Restate GmbH
+#
+#  This file is part of the Restate SDK for Python,
+#  which is released under the MIT license.
+#
+#  You can find a copy of the license in file LICENSE in the root
+#  directory of this repository or package, or at
+#  https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+#
+"""
+Restate SDK for Python
+"""
+
+from .service import Service
+from .object import VirtualObject
+from .workflow import Workflow
+
+# types
+from .context import Context, ObjectContext, ObjectSharedContext
+from .context import WorkflowContext, WorkflowSharedContext
+from .context import DurablePromise
+
+from .endpoint import app
