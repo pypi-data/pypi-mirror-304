b307845ccfc50866e1af45bd6fdb1824cf31eb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:15 +0000

    Add package wah/1.7.38/wah-1.7.38.tar.gz

---
+++ packages/wah/wah-1.7.38.tar.gz/wah-1.7.38/src/wah/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+
+from . import (
+    classification,
+    path,
+    plot,
+    riemann,
+    tensor,
+    utils,
+)
+
+__all__ = [
+    "classification",
+    "np",
+    "path",
+    "plot",
+    "riemann",
+    "tensor",
+    "torch",
+    "utils",
+]
