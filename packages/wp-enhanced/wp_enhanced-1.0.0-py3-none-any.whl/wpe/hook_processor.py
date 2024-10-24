5b3ca28d1d0061e1d252049516473bd2306e35b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:05 +0000

    Add package wp-enhanced/1.0.0/wp_enhanced-1.0.0-py3-none-any.whl

---
+++ packages/wp-enhanced/wp_enhanced-1.0.0-py3-none-any.whl/wpe/hook_processor.py
@@ -0,0 +1,55 @@
+import logging
+import os.path as osp
+
+import kkpyutil as util
+
+from wpe.pathman import PathMan
+
+
+@util.SingletonDecorator
+class HookProcessor:
+    def __init__(self):
+        self.pathMan = None
+        self.args = None
+        self.targetHooks = None
+
+    def lazy_init(self, args):
+        self.pathMan = PathMan(cwd=args.root)
+        self.args = args
+        self.targetHooks = args.withHooks
+
+    def register(self, command):
+        """
+        return a decorator that wraps the function to run pre&post hooks
+        """
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                self.lazy_init(args[0])
+                self.process_pre_hook(command)
+                res = func(*args, **kwargs)
+                self.process_post_hook(command)
+                return res
+            return wrapper
+        return decorator
+
+    def process_pre_hook(self, command):
+        self._process_hook('pre', command)
+
+    def process_post_hook(self, command):
+        self._process_hook('post', command)
+
+    def _process_hook(self, phase, command):
+        def should_execute(_hook_name):
+            if not self.targetHooks or _hook_name in self.targetHooks:
+                return osp.isfile(osp.join(self.pathMan.hooksDir, f'{hook_name}.py'))
+
+        hook_name = f'{phase}_{command}'
+        if not should_execute(hook_name):
+            return
+
+        hook_module = util.safe_import_module(hook_name, self.pathMan.hooksDir)
+        logging.info(f'Running hook: {hook_name}')
+        hook_module.main(proj_root=self.pathMan.root,
+                         plugin_name=self.pathMan.pluginName,
+                         **self.args.__dict__
+                         )
---
+++ packages/wp-enhanced/wp_enhanced-1.0.0-py3-none-any.whl/wpe/pathman.py
@@ -0,0 +1,61 @@
+import os
+import os.path as osp
+import re
+
+import kkpyutil as util
+
+
+from wpe.wp_wrapper import WpWrapper
+
+
+class PathMan:
+    def __init__(self, cwd=None):
+        self.templatesDir = osp.join(osp.dirname(__file__), 'templates')
+        self.testUtilDir = osp.join(osp.dirname(__file__), 'test', 'util')
+        self.premakePluginLua = self.find_premake_plugin_lua_in_ancestor_and_update_root(cwd or os.getcwd())
+        self.root = osp.dirname(self.premakePluginLua)
+        os.chdir(self.root)
+        self.pluginName = self.parse_plugin_name()
+        self.pluginConfigHeader = osp.join(self.root, f'{self.pluginName}Config.h')
+        self.pluginId = self.parse_plugin_id()
+        self.configDir = osp.join(self.root, '.wpe')
+        self.projConfig = osp.join(self.configDir, 'wpe_project.toml')
+        # compatible with old version
+        self.parameterConfig = osp.join(self.configDir, 'wpe_parameters.toml')
+        self.docsDir = osp.join(self.root, 'WwisePlugin/res/Md')
+        self.htmlDocsDir = osp.join(self.root, 'WwisePlugin/res/Html')
+        self.distDir = osp.join(self.root, 'dist')
+        self.testDir = osp.join(self.root, 'test')
+        self.hooksDir = osp.join(self.configDir, 'hooks')
+
+    @staticmethod
+    def find_premake_plugin_lua_in_ancestor_and_update_root(cwd):
+        premake_script_filename = 'PremakePlugin.lua'
+        while cwd:
+            lua = osp.join(cwd, premake_script_filename)
+            if osp.isfile(lua):
+                return lua
+            parent = osp.dirname(cwd)
+            if parent == cwd:
+                break
+            cwd = parent
+        raise FileNotFoundError(f'Can not find PremakePlugin.lua in ancestor directories. '
+                                f'This command must be executed under a plugin directory. cwd: {os.getcwd()}')
+
+    def parse_plugin_name(self):
+        lines = util.load_lines(self.premakePluginLua, rmlineend=True)
+        name_define_pattern = r'Plugin.name = ".*"'
+        for line in lines:
+            if matched := re.match(name_define_pattern, line):
+                return matched.group().split('"')[1]
+
+    def parse_plugin_id(self):
+        lines = util.load_lines(self.pluginConfigHeader, rmlineend=True)
+        prefix = '    static const unsigned short PluginID = '
+        name_define_pattern = rf'{prefix}\d+'
+        for line in lines:
+            if matched := re.match(name_define_pattern, line):
+                return int(matched.group().lstrip(prefix))
+
+    def refresh_paths(self, cwd=None):
+        self.__init__(cwd)
