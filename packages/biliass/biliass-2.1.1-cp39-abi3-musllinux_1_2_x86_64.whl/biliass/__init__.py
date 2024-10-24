9aac950056676aa3c73595b355294b8e51c5dffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 04:05:08 +0000

    Add package biliass/2.1.1/biliass-2.1.1-cp39-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/biliass/biliass-2.1.1-cp39-abi3-musllinux_1_2_x86_64.whl/biliass/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from biliass._core import (
+    enable_tracing as enable_tracing,
+    get_danmaku_meta_size as get_danmaku_meta_size,
+)
+
+from .biliass import (
+    BlockOptions as BlockOptions,
+    Danmaku2ASS as Danmaku2ASS,
+    convert_to_ass as convert_to_ass,
+)
