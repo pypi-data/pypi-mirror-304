70ca2fb21c9c8d8c106360bf3b3c9f6d2c2629d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:03:44 +0000

    Add package LiteJsonDb/3.4.0/litejsondb-3.4.0.tar.gz

---
+++ packages/LiteJsonDb/litejsondb-3.4.0.tar.gz/litejsondb-3.4.0/tests/test.py
@@ -0,0 +1,7 @@
+import LiteJsonDb
+
+# Initialize the database with encryption enabled
+db = LiteJsonDb.JsonDB(crypted=True)
+
+
+db.backup_to_telegram("6684463779:AAHWqj03Xa4tIy1jylTo5oHfti6bUOWOXW", "1869143336")
