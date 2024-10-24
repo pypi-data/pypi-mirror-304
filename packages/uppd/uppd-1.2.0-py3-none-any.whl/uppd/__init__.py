d584c8b1779fec6615d8827d687c2f248833b43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:43 +0000

    Add package uppd/1.2.0/uppd-1.2.0-py3-none-any.whl

---
+++ packages/uppd/uppd-1.2.0-py3-none-any.whl/uppd/__init__.py
@@ -0,0 +1,27 @@
+
+# Copyright (c) 2024, Janus Heide.
+# All rights reserved.
+#
+# Distributed under the "BSD 3-Clause License", see LICENSE.txt.
+
+"""Import uppd functions."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from uppd.uppd import (
+    find_latest_version, get_package_info, set_version, set_versions,
+    upgrade_requirements,
+)
+
+try:
+    __version__ = version("uppd")
+except PackageNotFoundError:
+    pass
+
+__all__ = (
+    "set_version",
+    "set_versions",
+    "get_package_info",
+    "find_latest_version",
+    "upgrade_requirements",
+)
