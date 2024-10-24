fa2dae734ec70a3f6a28d703d789886328b1401b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 04:05:32 +0000

    Add package OTCFinUtils/0.0.9/otcfinutils-0.0.9.tar.gz

---
+++ packages/OTCFinUtils/otcfinutils-0.0.9.tar.gz/otcfinutils-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from os import getenv
+
+setup(
+    name="OTCFinUtils",
+      version="0.0.9",
+      description="Useful functions to interact with dataverse and sharepoint",
+      packages=["OTCFinUtils"],
+      author="Shomoos Aldujaily, Petar Kasapinov",
+      author_email="saldujaily@otcfin.com,pkasapinov@otcfin.com",
+      zip_safe=False,
+      install_requires=[
+            "msal",
+            "python-dotenv",
+            "pandas",
+            "azure-identity",
+            "azure-keyvault-secrets",
+            "openpyxl",
+      ],
+)
