7e9237ac095fe3a8cc706754c16f692788e9dcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 04:05:33 +0000

    Add package XMZ/1.5.0/XMZ-1.5.0-py3-none-any.whl

---
+++ packages/XMZ/XMZ-1.5.0-py3-none-any.whl/HWNLP/__init__.py
@@ -0,0 +1,6 @@
+from .mt_service import MTService
+from .nlg_service import NLGService
+from .nlpf_service import NLPFService
+from .nlu_service import NLUService
+
+__all__ = ["MTService", "NLGService", "NLPFService", "NLUService"]
