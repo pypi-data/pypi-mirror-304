1d8112d094bdf17d848d173903ee5f6f69e433d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:04 +0000

    Add package bio-shark/1.2.2/bio_shark-1.2.2-py3-none-any.whl

---
+++ packages/bio-shark/bio_shark-1.2.2-py3-none-any.whl/bio_shark/settings.py
@@ -0,0 +1,13 @@
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+DATA_DIR = os.path.join(here, 'data')
+SUBS_MATRIX_DIR = os.path.join(DATA_DIR, 'substitution_matrix_grantham.tsv')
+DIVE_MODEL_PKL_PATH = os.path.join(DATA_DIR, 'dive_pred_model_v2.pkl')
+
+ROUND_OFF_DP = 10
+
+MASKING_CHARACTER = '*'
+
+CANONICAL_AAS = 'ARNDCQEGHILKMFPSTWYV' + MASKING_CHARACTER
