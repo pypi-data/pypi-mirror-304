7878caa575c2836320b5bf5769701739260ca7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 06:04:35 +0000

    Add package juv/0.2.5/juv-0.2.5-py3-none-any.whl

---
+++ packages/juv/juv-0.2.5-py3-none-any.whl/juv/_nbconvert.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import jupytext
+import nbformat.v4.nbbase as nb
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def code_cell(source: str, *, hidden: bool = False) -> dict:
+    return nb.new_code_cell(
+        source,
+        metadata={"jupyter": {"source_hidden": hidden}},
+    )
+
+
+def new_notebook(cells: list[dict]) -> dict:
+    return nb.new_notebook(cells=cells)
+
+
+def write_ipynb(nb: dict, file: Path) -> None:
+    file.write_text(jupytext.writes(nb, fmt="ipynb"))
