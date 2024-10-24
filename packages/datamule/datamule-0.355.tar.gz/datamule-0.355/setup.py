a11976fc12640330ee788238b017c5bfd5e90ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:04:24 +0000

    Add package datamule/0.355/datamule-0.355.tar.gz

---
+++ packages/datamule/datamule-0.355.tar.gz/datamule-0.355/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("../readme.md").read_text(encoding='utf-8')
+
+extras = {
+    "filing_viewer": ["lxml"],
+    "mulebot": ['openai'],
+    "mulebot_server": ['flask']
+}
+
+all_dependencies = set(dep for extra_deps in extras.values() for dep in extra_deps)
+extras["all"] = list(all_dependencies)
+
+setup(
+    name="datamule",
+    author="John Friedman",
+    version="0.355",
+    description="Making it easier to use SEC filings.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url="https://github.com/john-friedman/datamule-python",
+    install_requires=[
+        'aiohttp',
+        'aiolimiter',
+        'tqdm',
+        'requests',
+        'nest_asyncio',
+        'aiofiles',
+        'polars',
+        'setuptools'
+    ],
+    extras_require=extras,
+    package_data={
+        "datamule": ["data/*.csv"],
+        "datamule.mulebot.mulebot_server": [
+            "templates/*.html",
+            "static/css/*.css",
+            "static/scripts/*.js"
+        ],
+    },
+    include_package_data=True,
+)
