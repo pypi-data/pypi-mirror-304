faddc45f1ce93ce92e5cc0e37cb3576fd0da837b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:54 +0000

    Add package doweb/0.0.1/doweb-0.0.1-py3-none-any.whl

---
+++ packages/doweb/doweb-0.0.1-py3-none-any.whl/doweb/browser.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from fastapi import Depends, FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import ValidationError
+
+from . import config
+from .api.browser import router as browser_router
+from .api.viewer import router as viewer_router
+from .layout_server import LayoutViewServerEndpoint
+
+
+def get_app(fileslocation: Path | str | None = None, editable: bool = False) -> FastAPI:
+    if fileslocation is None:
+        try:
+            _settings = config.Config()
+        except ValidationError:
+            raise ValueError(
+                "To start the Kweb please set the environment "
+                "variable DOWEB_FILESLOCATION to a path in your filesystem."
+                " Alternatively, you can set the filepath in the get_app function."
+            )
+
+    else:
+        _settings = config.Config(fileslocation=fileslocation, editable=editable)
+
+    def settings() -> config.Config:
+        return _settings
+
+    staticfiles = StaticFiles(directory=Path(__file__).parent / "static")
+
+    # app = FastAPI(routes=[WebSocketRoute("/ws", endpoint=LayoutViewServerEndpoint)])
+
+    app = FastAPI()
+    viewer_router.dependencies.insert(0, Depends(settings))
+    browser_router.dependencies.insert(0, Depends(settings))
+
+    _settings = settings()
+
+    class BrowserLayoutViewServerEndpoint(
+        LayoutViewServerEndpoint,
+        root=_settings.fileslocation,
+        editable=editable,
+        add_missing_layers=_settings.add_missing_layers,
+        meta_splitter=_settings.meta_splitter,
+    ):
+        pass
+
+    app.add_websocket_route("/ws", BrowserLayoutViewServerEndpoint)
+    viewer_router.dependencies.insert(0, Depends(settings))
+    app.include_router(viewer_router)
+    app.include_router(browser_router)
+    app.mount("/static", staticfiles, name="doweb_static")
+
+    return app
