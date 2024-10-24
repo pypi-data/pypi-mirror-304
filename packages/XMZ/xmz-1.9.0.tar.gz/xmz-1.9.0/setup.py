907655af74c75be07408ed445c96536a972ac875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:00 +0000

    Add package XMZ/1.9.0/xmz-1.9.0.tar.gz

---
+++ packages/XMZ/xmz-1.9.0.tar.gz/xmz-1.9.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="XMZ",
+    version="1.9.0",
+    description="米粥SDK",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="祁筱欣",
+    author_email="mzapi@x.mizhoubaobei.top",
+    url="https://github.com/xiaomizhoubaobei/XMZAPI",  # 正确的 URL
+    packages=["HWNLP"],
+    install_requires=["huaweicloudsdkcore", "huaweicloudsdknlp"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
