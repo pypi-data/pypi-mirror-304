6b7de2e3972691cc9cb2285b95f47dc619000078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 05:03:54 +0000

    Add package DEODR/0.2.3/DEODR-0.2.3-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/DEODR/DEODR-0.2.3-cp313-cp313-macosx_11_0_arm64.whl/deodr/laplacian_rigid_energy.py
@@ -0,0 +1,41 @@
+"""Implementation of an as-rigid-as-possible energy based on the difference of laplacian with a reference shape."""
+
+import copy
+from typing import Tuple
+
+import numpy as np
+import scipy
+import scipy.sparse
+
+from deodr.triangulated_mesh import TriMesh
+
+
+class LaplacianRigidEnergy:
+    """As-rigid-as-possible energy based on the difference of laplacian with a reference shape."""
+
+    def __init__(self, mesh: TriMesh, vertices: np.ndarray, cregu: float):
+        self.cT = scipy.sparse.kron(
+            mesh.adjacencies.laplacian.T * mesh.adjacencies.laplacian,
+            scipy.sparse.eye(3),
+        ).tocsr()
+        self.vertices_ref = copy.copy(vertices)
+        self.mesh = mesh
+        self.cregu = cregu
+        self.approx_hessian = self.cregu * self.cT
+        n_components, _ = scipy.sparse.csgraph.connected_components(
+            csgraph=self.mesh.adjacencies.adjacency_vertices,
+            directed=False,
+            return_labels=True,
+        )
+        if n_components > 1:
+            raise (BaseException("You have more than one connected component in your mesh."))
+
+    def evaluate(
+        self,
+        vertices: np.ndarray,
+    ) -> Tuple[float, np.ndarray, scipy.sparse.csr_matrix]:
+        diff = (vertices - self.vertices_ref).flatten()
+        grad_vertices = self.cregu * (self.cT * diff).reshape((vertices.shape[0], 3))
+        energy = 0.5 * diff.dot(grad_vertices.flatten())
+
+        return energy, grad_vertices, self.approx_hessian
