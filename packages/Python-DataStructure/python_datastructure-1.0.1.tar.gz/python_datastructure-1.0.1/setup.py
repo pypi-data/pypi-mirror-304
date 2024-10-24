2216a2e3290dddb519624d0c18e7783a6557d993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:04 +0000

    Add package Python-DataStructure/1.0.1/python_datastructure-1.0.1.tar.gz

---
+++ packages/Python-DataStructure/python_datastructure-1.0.1.tar.gz/python_datastructure-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Python_DataStructure",  
+    version="1.0.1",
+    author="Mehrdad Hasanzade",
+    author_email="mehrdadt75m@gmail.com",
+    description="A Python package for data structures and algorithms",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
