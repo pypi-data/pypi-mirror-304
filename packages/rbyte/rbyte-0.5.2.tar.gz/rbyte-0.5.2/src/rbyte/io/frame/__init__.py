0db611f4692c9872812f766daf9330f93522d819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:38 +0000

    Add package rbyte/0.5.2/rbyte-0.5.2.tar.gz

---
+++ packages/rbyte/rbyte-0.5.2.tar.gz/rbyte-0.5.2/src/rbyte/io/frame/__init__.py
@@ -0,0 +1,25 @@
+from .directory import DirectoryFrameReader
+
+__all__ = ["DirectoryFrameReader"]
+
+
+try:
+    from .mcap import McapFrameReader
+except ImportError:
+    pass
+else:
+    __all__ += ["McapFrameReader"]
+
+try:
+    from .hdf5 import Hdf5FrameReader
+except ImportError:
+    pass
+else:
+    __all__ += ["Hdf5FrameReader"]
+
+try:
+    from .video.ffmpeg_reader import FfmpegFrameReader
+except ImportError:
+    pass
+else:
+    __all__ += ["FfmpegFrameReader"]
