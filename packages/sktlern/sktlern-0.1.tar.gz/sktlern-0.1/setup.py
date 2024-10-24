6c8aa5476cac4bf994c21ffa6e4e7239d48b60db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:05 +0000

    Add package sktlern/0.1/sktlern-0.1.tar.gz

---
+++ packages/sktlern/sktlern-0.1.tar.gz/sktlern-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sktlern',
+      version='0.1',
+      description='sktlern',
+      install_requires=[
+            'g4f',  # Добавьте g4f как зависимость
+      ],
+      packages=['sktlern'],
+      author_email='gotkayasielkashy@gmail.com',
+      zip_safe=False)
