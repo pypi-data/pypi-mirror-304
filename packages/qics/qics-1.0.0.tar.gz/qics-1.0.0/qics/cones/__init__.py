0f0c3d4a4d36f38f7ad9fd039aff94933787b204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:04:16 +0000

    Add package qics/1.0.0/qics-1.0.0.tar.gz

---
+++ packages/qics/qics-1.0.0.tar.gz/qics-1.0.0/qics/cones/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, Kerry He, James Saunderson, and Hamza Fawzi
+
+# This Python package QICS is licensed under the MIT license; see LICENSE.md
+# file in the root directory or at https://github.com/kerry-he/qics
+
+# __init__.py
+from qics.cones.nonnegorthant import NonNegOrthant  # noqa
+from qics.cones.possemidefinite import PosSemidefinite  # noqa
+from qics.cones.secondorder import SecondOrder  # noqa
+
+from qics.cones.classentr import ClassEntr  # noqa
+from qics.cones.classrelentr import ClassRelEntr  # noqa
+
+from qics.cones.quantentr import QuantEntr  # noqa
+from qics.cones.quantrelentr import QuantRelEntr  # noqa
+from qics.cones.quantcondentr import QuantCondEntr  # noqa
+from qics.cones.quantkeydist import QuantKeyDist  # noqa
+
+from qics.cones.opperspectr import OpPerspecTr  # noqa
+from qics.cones.opperspecepi import OpPerspecEpi  # noqa
