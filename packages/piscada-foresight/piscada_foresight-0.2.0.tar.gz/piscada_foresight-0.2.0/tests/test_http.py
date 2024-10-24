55a52ea60728647cadade35b0e2ede966c5802ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:35 +0000

    Add package piscada-foresight/0.2.0/piscada_foresight-0.2.0.tar.gz

---
+++ packages/piscada-foresight/piscada_foresight-0.2.0.tar.gz/piscada_foresight-0.2.0/tests/test_http.py
@@ -0,0 +1,9 @@
+"""Tests for the `http` module."""
+
+from piscada_foresight import http
+
+
+def test_get_state_file():  # noqa: D103
+    state_file = http._get_state_file("test")
+    assert str(state_file).endswith("/.test_state")
+    assert str(state_file).startswith("/")
