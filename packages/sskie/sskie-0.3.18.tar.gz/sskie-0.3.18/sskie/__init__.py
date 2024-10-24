19111d418a0c908445b3a3f04afe29a68f121d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:49 +0000

    Add package sskie/0.3.18/sskie-0.3.18.tar.gz

---
+++ packages/sskie/sskie-0.3.18.tar.gz/sskie-0.3.18/sskie/__init__.py
@@ -0,0 +1,12 @@
+from .spatial_analysis import Spatial_Clustering
+
+__version__ = '0.3.18'
+
+# 아래 할때는 build, dist, egg 지워야됨 + __version__ 수정
+# python setup.py sdist bdist_wheel
+
+# python -m twine upload dist/*
+
+# pypi-AgEIcHlwaS5vcmcCJDY4NTg2NzlhLTQxY2ItNDdiOC04MGE3LWI4N2E4NjY2NTBlYQACDVsxLFsic3NraWUiXV0AAixbMixbImM0YTIyMDQ5LTNlNjgtNDcyZC04MTU4LTI1NjU2ZjIxMDAzZCJdXQAABiBJAMT9Ig_LebGjr2bh1cO4x6cGtdh3l15b3LK5OBFORw
+
+
