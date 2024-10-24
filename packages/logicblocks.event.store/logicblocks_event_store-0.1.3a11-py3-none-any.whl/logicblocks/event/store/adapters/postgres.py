f548ea37fb4d0372d55f6ac6a72ff8053954a23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:03:53 +0000

    Add package logicblocks.event.store/0.1.3a11/logicblocks_event_store-0.1.3a11-py3-none-any.whl

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.3a11-py3-none-any.whl/logicblocks/event/store/adapters/postgres.py
@@ -0,0 +1,80 @@
+from collections.abc import Set
+from typing import Sequence, Iterator
+from uuid import uuid4
+
+from psycopg import Connection
+from psycopg.types.json import Jsonb
+from psycopg_pool import ConnectionPool
+
+from logicblocks.event.store.adapters import StorageAdapter
+from logicblocks.event.store.conditions import WriteCondition
+from logicblocks.event.types import NewEvent, StoredEvent
+
+
+class PostgresStorageAdapter(StorageAdapter):
+    def __init__(self, *, connection_pool: ConnectionPool[Connection]):
+        self.connection_pool = connection_pool
+
+    def save(
+        self,
+        *,
+        category: str,
+        stream: str,
+        events: Sequence[NewEvent],
+        conditions: Set[WriteCondition] = frozenset(),
+    ) -> Sequence[StoredEvent]:
+        event = events[0]
+        event_id = uuid4().hex
+        event_position = 0
+
+        with self.connection_pool.connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO events (
+                      id, 
+                      name, 
+                      stream, 
+                      category, 
+                      position, 
+                      payload, 
+                      observed_at, 
+                      occurred_at
+                  )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    """,
+                    (
+                        event_id,
+                        event.name,
+                        stream,
+                        category,
+                        event_position,
+                        Jsonb(event.payload),
+                        event.observed_at,
+                        event.occurred_at,
+                    ),
+                )
+
+        return [
+            StoredEvent(
+                id=event_id,
+                name=event.name,
+                stream=stream,
+                category=category,
+                position=event_position,
+                payload=event.payload,
+                observed_at=event.observed_at,
+                occurred_at=event.occurred_at,
+            )
+        ]
+
+    def scan_stream(
+        self, *, category: str, stream: str
+    ) -> Iterator[StoredEvent]:
+        return iter([])
+
+    def scan_category(self, *, category: str) -> Iterator[StoredEvent]:
+        return iter([])
+
+    def scan_all(self) -> Iterator[StoredEvent]:
+        return iter([])
