a99594e502097c1de9f2a1af98eaaddc5add9363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:03:59 +0000

    Add package blue-geo/4.680.1/blue_geo-4.680.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.680.1.tar.gz/blue_geo-4.680.1/blue_geo/watch/workflow/common.py
@@ -0,0 +1,31 @@
+from typing import Tuple, List
+import glob
+from functools import reduce
+
+from blue_objects import objects
+
+from blue_geo.watch.targets.target import Target
+from blue_geo.catalog.generic.generic.scope import raster_suffix
+from blue_geo.logger import logger
+
+
+def load_watch(object_name: str) -> Tuple[bool, Target, List[str]]:
+    success, target = Target.load(object_name)
+
+    list_of_files = sorted(
+        reduce(
+            lambda x, y: x + y,
+            [
+                glob.glob(
+                    objects.path_of(
+                        f"*{suffix}",
+                        object_name,
+                    )
+                )
+                for suffix in raster_suffix
+            ],
+        )
+    )
+    logger.info("{} file(s) to process.".format(len(list_of_files)))
+
+    return success, target, list_of_files
