77cb39d41bd0757faca531dc516b82622ad35f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:04 +0000

    Add package funcs-aux/0.4.35/funcs_aux-0.4.35-py3-none-any.whl

---
+++ packages/funcs-aux/funcs_aux-0.4.35-py3-none-any.whl/funcs_aux/__init__.py
@@ -0,0 +1,162 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+from .value_explicit import (
+    # BASE
+    Explicit,
+    Default,
+    ValueNotExist,
+    # AUX
+    # TYPES
+    TYPE__EXPLICIT,
+    TYPE__DEFAULT,
+    TYPE__VALUE_NOT_PASSED,
+    # EXX
+)
+from .args import (
+    # BASE
+    # AUX
+    ArgsEmpty,
+    # TYPES
+    TYPE__ARGS_EMPTY,
+    TYPE__ARGS,
+    TYPE__KWARGS,
+    # EXX
+)
+from .ensure import (
+    # BASE
+    args__ensure_tuple,
+    ensure_class,
+    # AUX
+    # TYPES
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .value_unit import (
+    # BASE
+    ValueUnit,
+    # AUX
+    UnitBase,
+    UNIT_MULT__VARIANTS,
+    # TYPES
+    Exx__ValueNotParsed,
+    Exx__ValueUnitsIncompatible,
+    # EXX
+)
+from .value_variants import (
+    # BASE
+    ValueVariants,
+    # AUX
+    # TYPES
+    # EXX
+    Exx__ValueNotInVariants,
+    Exx__VariantsIncompatible,
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .valid_aux import ValidAux
+from .valid import (
+    # BASE
+    Valid,
+    ValidChains,
+    # AUX
+    # TYPES
+    TYPE__EXCEPTION,
+    TYPE__SOURCE_LINK,
+    TYPE__VALIDATE_LINK,
+    TYPE__BOOL_LINK,
+    # EXX
+)
+from .valid_derivatives import (
+    # BASE
+    ValidRetry1,
+    ValidRetry2,
+    ValidFailStop,
+    ValidFailContinue,
+    ValidNoCum,
+    ValidReverse,
+    ValidSleep,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .valid_regexp import (
+    # BASE
+    ValidRegExp,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .result_cum import (
+    # BASE
+    ResultCum,
+    # AUX
+    # TYPES
+    TYPE__RESULT_CUM_STEP,
+    TYPE__RESULT_CUM_STEPS,
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .arrays import array_2d_get_compact_str
+from .iterables import (
+    # BASE
+    Iterables,
+    # AUX
+    # TYPES
+    TYPE__ITERABLE_PATH_KEY,
+    TYPE__ITERABLE_PATH_ORIGINAL,
+    TYPE__ITERABLE_PATH_EXPECTED,
+    TYPE__ITERABLE,
+    # EXX
+)
+from .strings import (
+    # BASE
+    Strings,
+    # AUX
+    # TYPES
+    TYPES_ELEMENTARY_SINGLE,
+    TYPES_ELEMENTARY_COLLECTION,
+    TYPES_ELEMENTARY,
+    TYPE_ELEMENTARY,
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .breeder_str_series import (
+    # BASE
+    BreederStrSeries,
+    # AUX
+    # EXX
+    Exx__IndexOverlayed,
+    Exx__IndexNotSet,
+    Exx__ItemNotExists,
+    Exx__StartOuterNONE_UsedInStackByRecreation,
+)
+from .breeder_str_stack import (
+    # BASE
+    BreederStrStack,
+    # AUX
+    BreederStrStack_Example,
+    BreederStrStack_Example__BestUsage
+    # TYPES
+    # EXX
+)
+from .breeder_objects import (
+    # BASE
+    BreederObjectList,
+    # AUX
+    BreederObjectList_GroupType,
+    # TYPES
+    TYPE__BREED_RESULT__ITEM,
+    TYPE__BREED_RESULT__GROUP,
+    TYPE__BREED_RESULT__GROUPS,
+    # EXX
+    Exx__BreederObjectList_GroupsNotGenerated,
+    Exx__BreederObjectList_GroupNotExists,
+    Exx__BreederObjectList_ObjCantAccessIndex,
+)
+
+
+# =====================================================================================================================
