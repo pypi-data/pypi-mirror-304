095add02946cd8f441580790cb8e760d4a3b3b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:07:50 +0000

    Add package DDownloader/0.1.3/ddownloader-0.1.3.tar.gz

---
+++ packages/DDownloader/ddownloader-0.1.3.tar.gz/ddownloader-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DDownloader',
+    version='0.1.3',
+    description='A library to download HLS and DASH manifests and decrypt media files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Pari Malam',
+    author_email='shafiqsamsuri@serasi.tech',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    package_data={
+        '': ['bin/*'],
+    },
+)
