0feb14e6257ce8387a9f8f2a2e2160fce8e53657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:00 +0000

    Add package easy-kubeflow/0.2.4/easy-kubeflow-0.2.4.tar.gz

---
+++ packages/easy-kubeflow/easy-kubeflow-0.2.4.tar.gz/easy-kubeflow-0.2.4/easy_kubeflow/__init__.py
@@ -0,0 +1,3 @@
+from easy_kubeflow.pipelines import Component, ReuseComponent, EasyPipelines
+from easy_kubeflow._docker import EasyDocker
+from easy_kubeflow.submitjobs import EasyJobs, JobSpec, ReuseJobSpec
