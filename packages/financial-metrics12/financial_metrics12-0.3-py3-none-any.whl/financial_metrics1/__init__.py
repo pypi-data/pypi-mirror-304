b7e4fc162bb677b412a73cbe1a7fd59d365cc1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:09 +0000

    Add package financial-metrics12/0.3/financial_metrics12-0.3-py3-none-any.whl

---
+++ packages/financial-metrics12/financial_metrics12-0.3-py3-none-any.whl/financial_metrics1/__init__.py
@@ -0,0 +1,14 @@
+from financial_metrics.financial_metrics1.metrics import calculate_net_profit, calculate_roi
+
+def main_menu():
+    revenue = float(input("Введите доходы: "))
+    costs = float(input("Введите расходы: "))
+
+    net_profit = calculate_net_profit(revenue, costs)
+    roi = calculate_roi(net_profit, costs)
+
+    print(f"Чистая прибыль: {net_profit:.2f} руб.")
+    print(f"ROI: {roi:.2f}%")
+
+if __name__ == "__main__":
+    main()
