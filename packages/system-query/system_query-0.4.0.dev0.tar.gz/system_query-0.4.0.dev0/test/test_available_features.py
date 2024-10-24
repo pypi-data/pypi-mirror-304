3a777eea56ff4b5319837b4b05bee00aa2482d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:07:51 +0000

    Add package system-query/0.4.0.dev0/system_query-0.4.0.dev0.tar.gz

---
+++ packages/system-query/system_query-0.4.0.dev0.tar.gz/system_query-0.4.0.dev0/test/test_available_features.py
@@ -0,0 +1,21 @@
+"""Tests for available_features module."""
+
+import unittest
+
+from system_query.available_features import CPU, GPU, HDD
+from system_query import query_cpu, query_gpus, query_hdds
+
+
+class Tests(unittest.TestCase):
+
+    @unittest.skipUnless(CPU, 'querying CPU is not supported')
+    def test_cpu(self):
+        self.assertGreaterEqual(len(query_cpu()), 1)
+
+    @unittest.skipUnless(GPU, 'querying GPUs is not supported')
+    def test_gpus(self):
+        self.assertGreaterEqual(len(query_gpus()), 1)
+
+    @unittest.skipUnless(HDD, 'querying HDDs is not supported')
+    def test_hdds(self):
+        self.assertGreaterEqual(len(query_hdds()), 1)
