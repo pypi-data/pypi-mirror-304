b96176157ad315e366ecb6dd84b4984bb7c06610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 05:04:01 +0000

    Add package redlite/0.3.4/redlite-0.3.4-py3-none-any.whl

---
+++ packages/redlite/redlite-0.3.4-py3-none-any.whl/redlite/model/__init__.py
@@ -0,0 +1,4 @@
+from ._ignore_system import IgnoreSystemModel, ConvertSystemToUserModel
+from ._dummy import ParrotModel, CannedModel
+
+__all__ = ["IgnoreSystemModel", "ConvertSystemToUserModel", "ParrotModel", "CannedModel"]
