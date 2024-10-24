2f114ad48756a9b51f6b687c33fb441b6065f31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:03 +0000

    Add package biobb-dna/5.0.0/biobb_dna-5.0.0-py3-none-any.whl

---
+++ packages/biobb-dna/biobb_dna-5.0.0-py3-none-any.whl/biobb_dna/__init__.py
@@ -0,0 +1,10 @@
+from . import dna
+from . import curvesplus
+from . import backbone
+from . import stiffness
+from . import interbp_correlations
+from . import intrabp_correlations
+name = "biobb_dna"
+__all__ = ["dna", "curvesplus", "backbone", "stiffness",
+           "interbp_correlations", "intrabp_correlations"]
+__version__ = "5.0.0"
