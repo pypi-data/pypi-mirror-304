511be6789b5fd7fc03452e672e0cce12a13051ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:07:57 +0000

    Add package netbox-ptov/0.1.26/netbox_ptov-0.1.26.tar.gz

---
+++ packages/netbox-ptov/netbox_ptov-0.1.26.tar.gz/netbox_ptov-0.1.26/netbox_ptov/migrations/__init__.py
@@ -0,0 +1,5 @@
+"""Django migrations for the netbox_ptov plugin"""
+
+"""Django API elements for the netbox_ptov plugin"""
+
+import netbox_ptov.migrations *
---
+++ packages/netbox-ptov/netbox_ptov-0.1.26.tar.gz/netbox_ptov-0.1.26/tests/test_netbox_ptov.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""Tests for `netbox_ptov` package."""
+
+from netbox_ptov import forms
+from netbox_ptov import models
+from netbox_ptov import filtersets
+
