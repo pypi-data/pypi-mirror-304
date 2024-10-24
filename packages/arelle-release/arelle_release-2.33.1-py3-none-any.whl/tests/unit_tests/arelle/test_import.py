71d68327cf5a47e8291778594b5fc15df815f6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 06:03:47 +0000

    Add package arelle-release/2.33.1/arelle_release-2.33.1-py3-none-any.whl

---
+++ packages/arelle-release/arelle_release-2.33.1-py3-none-any.whl/tests/unit_tests/arelle/test_import.py
@@ -0,0 +1,37 @@
+import glob
+import subprocess
+import sys
+
+import pytest
+
+KNOWN_FAILURES = frozenset([
+    'arelle.CntlrProfiler',
+    'arelle.FunctionXfi',
+    'arelle.PrototypeDtsObject',
+    'arelle.ViewCsvRelationshipSet',
+    'arelle.archive.LoadSavePreLbCsv',
+    'arelle.archive.SaveTableToExelle',
+    'arelle.archive.TR3toTR4',
+    'arelle.archive.plugin.loadFromOIM-2018',
+    'arelle.archive.plugin.sphinx.SphinxEvaluator',
+    'arelle.archive.plugin.validate.XFsyntax.xf',
+    'arelle.formula.FormulaEvaluator',
+])
+MODULE_NAMES = [
+    g.replace('/', '.').replace('\\', '.').replace('.py', '')
+    for g in glob.glob('arelle/**/*.py', recursive=True)
+    if not g.startswith(tuple(f'arelle/plugin/{p}/' for p in ['EDGAR', 'iXBRLViewerPlugin', 'xule']))
+]
+TEST_PARAMS = [
+    pytest.param(
+        module_name,
+        id=module_name,
+        marks=[pytest.mark.xfail()] if module_name in KNOWN_FAILURES else []
+    ) for module_name in MODULE_NAMES
+]
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize('module_name', TEST_PARAMS)
+def test(module_name):
+    subprocess.run([sys.executable, '-c', f'import {module_name}'], check=True)
