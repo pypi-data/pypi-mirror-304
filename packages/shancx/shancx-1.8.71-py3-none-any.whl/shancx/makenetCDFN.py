0edcdab91d21b4a603a6860269cdbe3c3ec86361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:03:36 +0000

    Add package shancx/1.8.71/shancx-1.8.71-py3-none-any.whl

---
+++ packages/shancx/shancx-1.8.71-py3-none-any.whl/shancx/makenetCDFN.py
@@ -0,0 +1,13 @@
+import xarray as xr
+import numpy as np
+def convert_longitude(nc_input_path, nc_output_path):
+    ds = xr.open_dataset(nc_input_path)
+    lon = 'lon'
+    if lon not in ds.coords:
+        raise ValueError("输入文件中没有'lon'坐标")
+    ds[lon] = xr.where(ds[lon] > 180, ds[lon] - 360, ds[lon])
+    ds = ds.sortby(lon)
+    ds = ds.sortby('lat', ascending=False)
+    ds.to_netcdf(nc_output_path)
+    print(f"成功将数据转换并保存到 {nc_output_path}")
+convert_longitude('CMORPH2_0.25deg-30min_202410160100.RT.nc', 'CMORPH2_0.25deg-30min_202410160100.RT_N.nc')
