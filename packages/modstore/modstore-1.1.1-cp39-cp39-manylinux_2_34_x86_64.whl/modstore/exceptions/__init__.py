7d32a97b7e90270c41fdd9e31141fd628cd59844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:04:11 +0000

    Add package modstore/1.1.1/modstore-1.1.1-cp39-cp39-manylinux_2_34_x86_64.whl

---
+++ packages/modstore/modstore-1.1.1-cp39-cp39-manylinux_2_34_x86_64.whl/modstore/exceptions/__init__.py
@@ -0,0 +1,34 @@
+from .python import (
+    StackError,
+    StackOverFlow,
+    StackUnderFlow,
+    TypeCastError
+)
+
+from .algorithms import (
+    IterableNotSet,
+    KeyPropertyDeleteError,
+    ReversePropertyDeleteError,
+    CountingSortError,
+    RadixSortError,
+    IterableHasUnsupportedTypeValues,
+    IterableIsNotSupported,
+    TargetCannotBeFound,
+    TargetNotSet,
+)
+
+__all__ = [
+    "StackError",
+    "StackOverFlow",
+    "StackUnderFlow",
+    "TypeCastError",
+    "IterableNotSet",
+    "KeyPropertyDeleteError",
+    "ReversePropertyDeleteError",
+    "CountingSortError",
+    "RadixSortError",
+    "IterableHasUnsupportedTypeValues",
+    "IterableIsNotSupported",
+    "TargetCannotBeFound",
+    "TargetNotSet",
+]
