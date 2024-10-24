be0dfc4bdb2a2ae36c34aa61d67631e87223d9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:07:50 +0000

    Add package xy-work/1.0.5/xy_work-1.0.5-py3-none-any.whl

---
+++ packages/xy-work/xy_work-1.0.5-py3-none-any.whl/xy_work/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+__author__ = "helios"
+__doc__ = "__init__"
+''' 
+ * @Author: yuyanget 845262968@qq.com
+ * @Date: 2024-10-19 20:22:02
+ * @LastEditors: yuyanget 845262968@qq.com
+ * @LastEditTime: 2024-10-21 10:40:36
+ * @FilePath: /xy_work/xy_work/__init__.py
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ '''
+"""
+  * @File    :   __init__.py
+  * @Time    :   2023/04/24 18:29:47
+  * @Author  :   helios
+  * @Version :   1.0
+  * @Contact :   yuyang.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @Desc    :   None
+"""
+
+
+__version__ = "1.0.1"
