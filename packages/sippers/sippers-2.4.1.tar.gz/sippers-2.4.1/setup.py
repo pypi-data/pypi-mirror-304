f764f38069dfce0b95f0055616e4094d5ad7d7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:25 +0000

    Add package sippers/2.4.1/sippers-2.4.1.tar.gz

---
+++ packages/sippers/sippers-2.4.1.tar.gz/sippers-2.4.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sippers',
+    version='2.4.1',
+    packages=find_packages(),
+    url='https://github.com/gisce/sippers',
+    license='GPLv3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='sippers',
+    long_description='sippers',
+    entry_points='''
+        [console_scripts]
+        sippers=sippers.cli:sippers
+    ''',
+    package_data={
+        'sippers': ['data/*']
+    },
+    install_requires=[
+        'raven<6.0.0;python_version<="2.7.18"',
+        'raven;python_version>"2.7.18"',
+        "pymongo<=3.13.0",
+        "osconf",
+        "marshmallow<3.0",
+        "click"
+    ],
+    test_suite='tests',
+)
