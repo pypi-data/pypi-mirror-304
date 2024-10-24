b2b0d3650018ee2d796f88c9d67babbb483ceac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:54 +0000

    Add package gv100ad-py/0.1.2/gv100ad_py-0.1.2-py3-none-any.whl

---
+++ packages/gv100ad-py/gv100ad_py-0.1.2-py3-none-any.whl/gv100ad/entities/district_type.py
@@ -0,0 +1,14 @@
+##
+# Copyright (c) STÜBER SYSTEMS GmbH
+# Licensed under the MIT License, Version 2.0. 
+##
+
+from enum import Enum
+
+class DistrictType(Enum):
+    NONE = 0
+    KREISFREIE_STADT = 41
+    STADTKREIS = 42
+    KREIS = 43
+    LANDKREIS = 44
+    REGIONALVERBAND = 45
