2f20c9fb40ae68d47f25e17803183b594ddb2e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:03:52 +0000

    Add package guigaga/0.0.7/guigaga-0.0.7-py3-none-any.whl

---
+++ packages/guigaga/guigaga-0.0.7-py3-none-any.whl/guigaga/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Wytamma Wirth <wytamma.wirth@me.com>
+#
+# SPDX-License-Identifier: MIT
+from guigaga.guigaga import gui
+from guigaga.types import Upload, Download
+
+__all__ = ["gui", "Upload", "Download"]
