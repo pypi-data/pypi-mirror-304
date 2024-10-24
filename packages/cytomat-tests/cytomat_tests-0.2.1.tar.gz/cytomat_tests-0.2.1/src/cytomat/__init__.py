f0f843a022ac496e43be6cf7d2b1a455d962d98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 04:05:51 +0000

    Add package cytomat-tests/0.2.1/cytomat_tests-0.2.1.tar.gz

---
+++ packages/cytomat-tests/cytomat_tests-0.2.1.tar.gz/cytomat_tests-0.2.1/src/cytomat/__init__.py
@@ -0,0 +1,15 @@
+# import sys
+import time
+# if sys.version_info[:2] < (3, 8):
+#     import importlib_metadata as metadata
+# else:
+#     from importlib import metadata
+from .setup import post_install
+post_install()
+#wait for post install to finish
+time.sleep(0.5)
+from .cytomat import Cytomat
+# __version__ = metadata.version("open-cytomat")
+
+#__all__ = ["__version__", "Cytomat"]
+__all__ = ["Cytomat"]
