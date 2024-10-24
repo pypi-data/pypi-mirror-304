4e7148dc2b2a735423096e6b873505734fb22f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 05:03:45 +0000

    Add package pyflutterflow/0.0.13/pyflutterflow-0.0.13-py3-none-any.whl

---
+++ packages/pyflutterflow/pyflutterflow-0.0.13-py3-none-any.whl/pyflutterflow/dashboard/src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { useAuthStore } from '@/stores/auth.store'; // Adjust the path according to your file structure
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || ''
+});
+
+// Request interceptor for API calls
+api.interceptors.request.use(
+  async (config) => {
+    const authStore = useAuthStore();
+    const token = authStore.accessToken;
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default api;
