bb0965a45948f4dd091145b0862c028e1c39bc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:24 +0000

    Add package tap-sftp/1.2.0/tap-sftp-1.2.0.tar.gz

---
+++ packages/tap-sftp/tap-sftp-1.2.0.tar.gz/tap-sftp-1.2.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-sftp",
+    version="1.2.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_sftp"],
+    install_requires=[
+        "singer-python==5.12.1",
+        'paramiko==2.6.0',
+        'backoff==1.8.0',
+        'singer-encodings==0.1.3',
+        'terminaltables==3.1.0',
+    ],
+    extras_require={
+        'dev': [
+            'ipdb',
+            'pylint',
+            'nose'
+        ],
+        'test': [
+            'paramiko==2.6.0'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-sftp=tap_sftp:main
+    """,
+    packages=["tap_sftp"],
+    package_data = {
+        "schemas": ["tap_sftp/schemas/*.json"]
+    },
+    include_package_data=True,
+)
