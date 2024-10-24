e4e854ed6a834c78a21e1288b67806dda9721eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 16:04:07 +0000

    Add package financial-metrics12/0.8/financial_metrics12-0.8-py3-none-any.whl

---
+++ packages/financial-metrics12/financial_metrics12-0.8-py3-none-any.whl/financial_metrics1/metrics.py
@@ -0,0 +1,7 @@
+def calculate_net_profit(revenue, costs):
+    return revenue - costs
+
+def calculate_roi(net_profit, costs):
+    if costs == 0:
+        return 0.0
+    return (net_profit / costs) * 100
