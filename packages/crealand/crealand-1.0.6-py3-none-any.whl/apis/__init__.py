1fb9b9d3cdc2465041559954fcd541b7560de4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:03:54 +0000

    Add package crealand/1.0.6/crealand-1.0.6-py3-none-any.whl

---
+++ packages/crealand/crealand-1.0.6-py3-none-any.whl/apis/__init__.py
@@ -0,0 +1,9 @@
+# from crealand.apis.ai import *
+# from crealand.apis.control import *
+# from crealand.apis.detect import *
+from crealand.apis.event import *
+# from crealand.apis.interactive import *
+# from crealand.apis.media import *
+# from crealand.apis.object import *
+# from crealand.apis.sensor import *
+
