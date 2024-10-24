4a102d5b1040068316fc45ed0b439d0cb65d1e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 14:04:26 +0000

    Add package AcSecurity/0.7/acsecurity-0.7.tar.gz

---
+++ packages/AcSecurity/acsecurity-0.7.tar.gz/acsecurity-0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AcSecurity',
+    version='0.7',
+    packages=find_packages(),
+    install_requires=[
+        'pip-audit',
+        'pylint',
+
+    ],
+    entry_points={
+        'console_scripts': [
+            'acsecurity=AcSecurity.scanner:main',  # Adjust if your main function is in another file
+        ],
+    },
+    description='A security scanner for applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Austin Cabler',
+    author_email='austin_cabler@icloud.com',
+    url='https://github.com/austincabler13/AcSecurity',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3',
+)
