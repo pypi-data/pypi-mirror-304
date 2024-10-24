43df730e46c861e1666ce6b3e597b17bf64cfa96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 05:05:03 +0000

    Add package polars/1.11.0/polars-1.11.0.tar.gz

---
+++ packages/polars/polars-1.11.0.tar.gz/polars-1.11.0/crates/polars/tests/it/io/avro/read_async.rs
@@ -0,0 +1,51 @@
+use apache_avro::Codec;
+use arrow::io::avro::avro_schema::read_async::{block_stream, read_metadata};
+use arrow::io::avro::read;
+use futures::{pin_mut, StreamExt};
+use polars_error::PolarsResult;
+
+use super::read::{schema, write_avro};
+
+async fn test(codec: Codec) -> PolarsResult<()> {
+    let avro_data = write_avro(codec).unwrap();
+    let (_, expected_schema) = schema();
+
+    let mut reader = &mut &avro_data[..];
+
+    let metadata = read_metadata(&mut reader).await?;
+    let schema = read::infer_schema(&metadata.record)?;
+
+    assert_eq!(schema, expected_schema);
+
+    let blocks = block_stream(&mut reader, metadata.marker).await;
+
+    pin_mut!(blocks);
+    while let Some(block) = blocks.next().await.transpose()? {
+        assert!(block.number_of_rows > 0 || block.data.is_empty())
+    }
+    Ok(())
+}
+
+// Issue with clippy interacting with tokio. See:
+// https://github.com/rust-lang/rust-clippy/issues/13458
+#[allow(clippy::needless_return)]
+#[tokio::test]
+async fn read_without_codec() -> PolarsResult<()> {
+    test(Codec::Null).await
+}
+
+// Issue with clippy interacting with tokio. See:
+// https://github.com/rust-lang/rust-clippy/issues/13458
+#[allow(clippy::needless_return)]
+#[tokio::test]
+async fn read_deflate() -> PolarsResult<()> {
+    test(Codec::Deflate).await
+}
+
+// Issue with clippy interacting with tokio. See:
+// https://github.com/rust-lang/rust-clippy/issues/13458
+#[allow(clippy::needless_return)]
+#[tokio::test]
+async fn read_snappy() -> PolarsResult<()> {
+    test(Codec::Snappy).await
+}
