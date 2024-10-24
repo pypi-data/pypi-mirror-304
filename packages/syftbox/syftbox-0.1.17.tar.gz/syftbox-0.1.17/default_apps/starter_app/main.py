dc1d250c3b82b9c5bfb727ca36364a8609258f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 04:05:33 +0000

    Add package syftbox/0.1.17/syftbox-0.1.17.tar.gz

---
+++ packages/syftbox/syftbox-0.1.17.tar.gz/syftbox-0.1.17/default_apps/starter_app/main.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+default_apps = [
+    "https://github.com/OpenMined/ring",
+    "https://github.com/OpenMined/github_app_updater",
+    "https://github.com/OpenMined/logged_in",
+]
+
+
+def clone_apps():
+    global default_apps
+    # Iterate over the list and clone each repository
+    for url in default_apps:
+        subprocess.run(["git", "clone", url])
+
+
+if __name__ == "__main__":
+    current_directory = Path(os.getcwd())
+
+    apps_directory = current_directory.parent
+    os.chdir(apps_directory)
+    clone_apps()
+    shutil.rmtree(current_directory)
