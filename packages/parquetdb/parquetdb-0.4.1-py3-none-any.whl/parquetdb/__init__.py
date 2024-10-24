ec42730a23a6c086c1e85b24b63170519ec86c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:33 +0000

    Add package parquetdb/0.4.1/parquetdb-0.4.1-py3-none-any.whl

---
+++ packages/parquetdb/parquetdb-0.4.1-py3-none-any.whl/parquetdb/__init__.py
@@ -0,0 +1,6 @@
+from parquetdb._version import __version__
+
+from parquetdb.utils.config import config
+
+from parquetdb.core.parquetdb_manager import ParquetDBManager
+from parquetdb.core.parquetdb import ParquetDB
