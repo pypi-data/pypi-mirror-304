641e0d63e41ac4d7fcf8b2770b81a9f1bbc4d1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 03:08:05 +0000

    Add package NLP-EXPERIMENTS/1.5/NLP_EXPERIMENTS-1.5.tar.gz

---
+++ packages/NLP-EXPERIMENTS/NLP_EXPERIMENTS-1.5.tar.gz/NLP_EXPERIMENTS-1.5/setup.py
@@ -0,0 +1,19 @@
+import os
+import pprint
+from setuptools import setup, find_packages
+
+# Define the setup parameters
+setup(
+    name="NLP_EXPERIMENTS",
+    version="1.5",
+    description="NLP Expts",
+    author="a",
+    author_email="a@gmail.com",
+    packages=find_packages(),
+    package_data={
+        "NLP_EXPERIMENTS": ["*.pdf", "*.py", "*.docx"],
+    },  # Include all files in the CC_EXPTS package
+)
+
+
+# pypi-AgEIcHlwaS5vcmcCJDYyNzAyODU1LWZhMWEtNDFjNy1hN2RjLWE2MWJjNTJkYWUzOAACKlszLCIyNjYzYmUxNy00ZWZkLTRhZjAtOGY5Yy1lM2I4Y2M2OTNiZmMiXQAABiBJbpr91ty6nq70V6hXy9aL_kO1BO7Kw0OJvDUE7T8pVQ
