867b94fb53b005f6dc948effc7d5fd65d556508e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:24 +0000

    Add package marquez-python/0.50.0/marquez-python-0.50.0.tar.gz

---
+++ packages/marquez-python/marquez-python-0.50.0.tar.gz/marquez-python-0.50.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-2023 contributors to the Marquez project
+# SPDX-License-Identifier: Apache-2.0
+#
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "requests",
+    "six",
+    "pyrfc3339",
+    "deprecation",
+]
+
+extras_require = {
+    "tests": ["pytest", "pytest-cov", "mock", "flake8"],
+}
+extras_require["dev"] = set(sum(extras_require.values(), []))
+
+setup(
+    name="marquez-python",
+    version="0.50.0",
+    description="Marquez Python Client",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Marquez Project",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require=extras_require,
+    python_requires=">=3.6",
+    zip_safe=False,
+    keywords="marquez",
+)
