69d7797c8dc73a3e0d691b667877d4b265acf2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 06:03:51 +0000

    Add package styxdefs/0.3.1/styxdefs-0.3.1-py3-none-any.whl

---
+++ packages/styxdefs/styxdefs-0.3.1-py3-none-any.whl/styxdefs/__init__.py
@@ -0,0 +1,35 @@
+""".. include:: ../../README.md"""  # noqa: D415
+
+from .dry_runner import (
+    DryRunner,
+    DummyRunner,
+)
+from .global_state import (
+    get_global_runner,
+    set_global_runner,
+)
+from .local_runner import (
+    LocalRunner,
+)
+from .types import (
+    Execution,
+    InputPathType,
+    Metadata,
+    OutputPathType,
+    Runner,
+    StyxRuntimeError,
+)
+
+__all__ = [
+    "Execution",
+    "InputPathType",
+    "Metadata",
+    "OutputPathType",
+    "Runner",
+    "LocalRunner",
+    "DryRunner",
+    "DummyRunner",
+    "get_global_runner",
+    "set_global_runner",
+    "StyxRuntimeError",
+]
