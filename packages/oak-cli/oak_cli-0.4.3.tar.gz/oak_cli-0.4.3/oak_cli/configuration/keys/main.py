920882c86de07a35a41bbbb2e9b419e78f7a50cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:03:58 +0000

    Add package oak-cli/0.4.3/oak_cli-0.4.3.tar.gz

---
+++ packages/oak-cli/oak_cli-0.4.3.tar.gz/oak_cli-0.4.3/oak_cli/configuration/keys/main.py
@@ -0,0 +1,18 @@
+import typer
+
+from oak_cli.configuration.auxiliary import prompt_for_path
+from oak_cli.configuration.common import check_and_handle_config_file, update_config_value
+from oak_cli.configuration.keys.enums import ConfigurableConfigKey
+
+app = typer.Typer()
+
+
+@app.command("configure", help="Configure a core variable.")
+def configure_config_key(key: ConfigurableConfigKey, value: str = "") -> None:
+    check_and_handle_config_file()
+    if not value:
+        if key.is_path():
+            value = str(prompt_for_path(key.get_pleasant_name()))
+        else:
+            value = typer.prompt(f"What value(s) should '{key.get_pleasant_name()}' have?")
+    update_config_value(key=key, value=value)
