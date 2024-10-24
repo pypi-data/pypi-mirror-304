3328ff27e1b0fb2f7a9cdb5b8fecd5e356df0595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:05:47 +0000

    Add package surrealist/1.0.3/surrealist-1.0.3.tar.gz

---
+++ packages/surrealist/surrealist-1.0.3.tar.gz/surrealist-1.0.3/src/surrealist/__init__.py
@@ -0,0 +1,13 @@
+from .connections import WebSocketConnection, HttpConnection, Connection
+from .enums import Algorithm
+from .errors import *
+from .ql import Database, DatabaseConnectionsPool, Table, Where
+from .result import SurrealResult
+from .surreal import Surreal
+from .utils import DATA_LENGTH_FOR_LOGS, get_uuid, to_surreal_datetime_str, to_datetime, LOG_FORMAT
+
+__all__ = ("Surreal", "SurrealResult", "WebSocketConnection", "HttpConnection", "PySurrealError", "HttpConnectionError",
+           "HttpClientError", "SurrealConnectionError", "WebSocketConnectionError", "WebSocketConnectionClosedError",
+           "ConnectionParametersError", "CompatibilityError", "OperationOnClosedConnectionError", "WrongCallError",
+           "DATA_LENGTH_FOR_LOGS", "Connection", "get_uuid", "Database", "Table", "Where", "DatabaseConnectionsPool",
+           "to_surreal_datetime_str", "to_datetime", "LOG_FORMAT", "Algorithm")
---
+++ packages/surrealist/surrealist-1.0.3.tar.gz/surrealist-1.0.3/src/surrealist/utils.py
@@ -0,0 +1,84 @@
+import datetime
+import re
+import uuid
+from typing import Union
+
+ENCODING = "UTF-8"
+OK = "OK"
+ERR = "ERR"
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+DATE_FORMAT_NS = "%Y-%m-%dT%H:%M:%S.%fZ"
+HTTP_OK = 200  # status code for success
+DEFAULT_TIMEOUT = 15  # timeout in seconds for basic operations
+DATA_LENGTH_FOR_LOGS = 300  # size of data in logs, data will be cropped if bigger than that
+LOG_FORMAT = '%(asctime)s : %(threadName)s : %(name)s : %(levelname)s : %(message)s'  # use it for logs
+NS = "NS"
+DB = "DB"
+AC = "AC"
+
+
+def _set_length(length: int):
+    global DATA_LENGTH_FOR_LOGS
+    DATA_LENGTH_FOR_LOGS = length
+
+
+def get_uuid() -> str:
+    """
+    Helper to generate uuid for records or testing purposes
+
+    :return: uuid string representation
+    """
+    return str(uuid.uuid4())
+
+
+def mask_pass(text: str) -> str:
+    """
+    Mask the passwords in logs. Replace all 'pass':'any_pass' in logs to 'pass':'******'. Works both with ' and "
+    :param text: text before putting it to log
+    :return: text without visible passwords
+    """
+    return re.sub(r"(?ms)(?<=['\"]pass['\"]: ['\"]).*?(?=['\"])", '******', text)
+
+
+def crop_data(data: Union[str, bytes], is_str: bool = True) -> Union[str, bytes]:
+    """
+    Crop the data to maximum size, no actions if data is smaller, work with str and bytes in logs
+
+    :param data: str or bytes of data to put it in the logs
+    :param is_str: a flag to use correct operation
+    :return: data of the same type, but cropped if it is bigger than DATA_LENGTH_FOR_LOGS
+    """
+    if len(data) > DATA_LENGTH_FOR_LOGS:
+        return f"{data[:DATA_LENGTH_FOR_LOGS]}..." if is_str else data[:DATA_LENGTH_FOR_LOGS] + b'...'
+    return data
+
+
+def to_surreal_datetime_str(dt: datetime.datetime) -> str:
+    """
+    Convert datetime to string in Surreal format, for example: d'2024-04-18T11:34:41.665249Z'
+    :param dt: datetime object
+    :return: string representation of the datetime
+    """
+    return f"d'{dt.strftime(DATE_FORMAT_NS)}'"
+
+
+def to_datetime(dt_str: str) -> datetime.datetime:
+    """
+    Convert string in Surreal format to datetime object
+    :param dt_str: string datetime representation in Surreal format (e.g. d'2024-04-18T11:34:41.665249Z')
+    :return: datetime object
+    """
+    if "d'" in dt_str:
+        dt_str = dt_str.rstrip("'").replace("d'", "")
+    elif 'd"' in dt_str:
+        dt_str = dt_str.rstrip('"').replace('d"', '')
+    return datetime.datetime.strptime(dt_str, DATE_FORMAT_NS)
+
+
+def clean_dates(data: str) -> str:
+    """
+    Get surreal dates with d' prefix out of quotes
+    :param data: some Surreal query
+    :return: data with valid Surreal dates
+    """
+    return re.sub(r'["\'](d(["\']).+?)["\']+', r'\1\2', data)
