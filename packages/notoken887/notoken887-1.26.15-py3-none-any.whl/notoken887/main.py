d3ca2150f4b2dc25aefc0c70e11cfc27c80ab68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-24 15:04:20 +0000

    Add package notoken887/1.26.15/notoken887-1.26.15-py3-none-any.whl

---
+++ packages/notoken887/notoken887-1.26.15-py3-none-any.whl/notoken887/main.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+from notoken887.encryptor import TokenCryptor
+
+def encrypt_code(code):
+    cryptor = TokenCryptor()
+    encrypted_lines = []
+    for line in code.splitlines():
+        if line.strip().startswith("import") or line.strip().startswith("from"):
+            encrypted_lines.append(line)
+        else:
+            encrypted_lines.append(cryptor.encrypt(line))
+    encrypted_code = '\n'.join(encrypted_lines)
+    return encrypted_code
+
+def decrypt_code(encrypted_content):
+    cryptor = TokenCryptor()
+    encrypted_code = ''
+    for line in encrypted_content.splitlines():
+        if line.strip().startswith("encrypted_code"):
+            encrypted_code = line.split('=')[1].strip().strip("'''")
+            break
+    decrypted_string = cryptor.decrypt(encrypted_code)
+    return decrypted_string
+
+def process_file(input_path, output_path, mode):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(input_path, 'r', encoding='utf-8') as infile:
+        content = infile.read()
+
+    if mode == 'e':
+        encrypted_code = encrypt_code(content)
+        output_content = f"""from notoken887.encryptor import TokenCryptor
+cryptor = TokenCryptor()
+encrypted_code = '''{encrypted_code}'''
+def run_decrypted_code():
+    decrypted_string = cryptor.decrypt(encrypted_code)
+    exec(decrypted_string, globals())
+if __name__ == "__main__":
+    run_decrypted_code()"""
+    else:
+        decrypted_content = decrypt_code(content)
+        output_content = decrypted_content
+
+    with open(output_path, 'w', encoding='utf-8') as outfile:
+        outfile.write(output_content)
+
+def main():
+    parser = argparse.ArgumentParser(description='Encrypt or decrypt Python files using TokenCryptor.')
+    parser.add_argument('--input', '-i', type=str, required=True, help='Input file path')
+    parser.add_argument('--output', '-o', type=str, required=True, help='Output file path')
+    parser.add_argument('--mode', '-m', choices=['e', 'd'], required=True, help='Mode: e for encrypt, d for decrypt')
+    args = parser.parse_args()
+    process_file(args.input, args.output, args.mode)
+
+if __name__ == "__main__":
+    main()
