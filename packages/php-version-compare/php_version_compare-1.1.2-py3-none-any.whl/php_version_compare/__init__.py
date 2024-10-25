99d750cf7b5a53793254640eaf4595e481f9eb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:11:15 +0000

    Add package php-version-compare/1.1.2/php_version_compare-1.1.2-py3-none-any.whl

---
+++ packages/php-version-compare/php_version_compare-1.1.2-py3-none-any.whl/php_version_compare/__init__.py
@@ -0,0 +1,8 @@
+from php_version_compare.versioning import (
+    canonicalize_version as canonicalize_version,
+    version_compare as version_compare,
+    Compare as Compare,
+    Operator as Operator,
+)
+
+__version__ = "1.1.2"
