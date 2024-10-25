90cc4ea8eb0102a7f60337eb964f3b93891cab41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:56 +0000

    Add package fastapi-observability/0.0.1.dev20922/fastapi_observability-0.0.1.dev20922.tar.gz

---
+++ packages/fastapi-observability/fastapi_observability-0.0.1.dev20922.tar.gz/fastapi_observability-0.0.1.dev20922/README.md
@@ -0,0 +1,2 @@
+
+### A middleware and instrumentation library for FastAPI to enable Prometheus metrics and OpenTelemetry tracing.
---
+++ packages/fastapi-observability/fastapi_observability-0.0.1.dev20922.tar.gz/fastapi_observability-0.0.1.dev20922/fastapi_observability/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+from .middleware import PrometheusMiddleware, metrics
+from .instrumentation import setup_opentelemetry
+
+__all__ = ["PrometheusMiddleware", "metrics", "setup_opentelemetry"]
+
+
+SHARED_LOG_CONFIG = \
+    "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
+
+
+class EndpointFilter(logging.Filter):
+    # Uvicorn endpoint access log filter
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find("GET /metrics") == -1
+
+
+# Filter out /endpoint
+logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
