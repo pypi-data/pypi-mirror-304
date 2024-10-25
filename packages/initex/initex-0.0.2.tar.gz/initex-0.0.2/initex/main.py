d6ce47c4b2fd87ccf4ba9bfa0ab06a9e88f15dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:05:42 +0000

    Add package initex/0.0.2/initex-0.0.2.tar.gz

---
+++ packages/initex/initex-0.0.2.tar.gz/initex-0.0.2/initex/main.py
@@ -0,0 +1,3 @@
+def run_task(task_name):
+    """Run a specified task."""
+    print(f"Running task: {task_name}")
---
+++ packages/initex/initex-0.0.2.tar.gz/initex-0.0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="initex", # distribution name is required
+  version="0.0.2",
+  packages=find_packages(),
+)
