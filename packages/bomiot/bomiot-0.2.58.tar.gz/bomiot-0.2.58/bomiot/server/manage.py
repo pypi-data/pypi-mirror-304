d81a0e027e7ed599939883d787b0e1f31a307113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:04:45 +0000

    Add package bomiot/0.2.58/bomiot-0.2.58.tar.gz

---
+++ packages/bomiot/bomiot-0.2.58.tar.gz/bomiot-0.2.58/bomiot/server/manage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+def manage():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bomiot.server.server.settings')
+    os.environ.setdefault('RUN_MAIN', 'true')
+    try:
+        from django.core.management import execute_from_command_line
+        # start watch media file change
+        from bomiot.server.core.watchfile import run
+        run()
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 3.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    manage()
