47cf0fdbc4a45cb954cf6b3a371a8945cf35443f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:05:41 +0000

    Add package FiinQuant/0.7.45/FiinQuant-0.7.45-py3-none-any.whl

---
+++ packages/FiinQuant/FiinQuant-0.7.45-py3-none-any.whl/FiinQuant/SubscribeCoveredWarrantEvents.pyi
@@ -0,0 +1,10 @@
+class SubscribeCoveredWarrantEvents:
+    def __init__(self, access_token: str, tickers: list, callback: callable) -> None:
+        
+        self.tickers: list
+        self._stop: bool
+        
+    def start(self) -> None: ...
+        
+    def stop(self) -> None: ...
+
