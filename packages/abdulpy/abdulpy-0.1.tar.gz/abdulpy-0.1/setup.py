8f8a9435c70f6e2a9c512704046a10c8463cdce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:04:14 +0000

    Add package abdulpy/0.1/abdulpy-0.1.tar.gz

---
+++ packages/abdulpy/abdulpy-0.1.tar.gz/abdulpy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='abdulpy',
+    version='0.1',
+    description='A simple library which contains all the programs like simple calculations, loops etc.',
+    author='Abdul Ahad',
+    author_email='abdulahad30604@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
