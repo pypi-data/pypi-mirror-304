9bec83d7393667fb7bea2eed3d63d78b45bdd8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:46 +0000

    Add package illallangi-tripit/0.7.0/illallangi_tripit-0.7.0-py3-none-any.whl

---
+++ packages/illallangi-tripit/illallangi_tripit-0.7.0-py3-none-any.whl/illallangi/tripit/models/__init__.py
@@ -0,0 +1,16 @@
+from illallangi.tripit.models.airline import AirlineKey
+from illallangi.tripit.models.airport import AirportKey
+from illallangi.tripit.models.flight import Flight, FlightKey
+from illallangi.tripit.models.profile import Profile, ProfileKey
+from illallangi.tripit.models.trip import Trip, TripKey
+
+__all__ = [
+    "AirlineKey",
+    "AirportKey",
+    "Flight",
+    "FlightKey",
+    "Profile",
+    "ProfileKey",
+    "Trip",
+    "TripKey",
+]
