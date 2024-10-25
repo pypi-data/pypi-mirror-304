5403437e99da81896dd65f1e7dfb0a0abb5a5f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:06:22 +0000

    Add package zepben.evolve/0.41.2/zepben.evolve-0.41.2-py3-none-any.whl

---
+++ packages/zepben.evolve/zepben.evolve-0.41.2-py3-none-any.whl/zepben/evolve/services/customer/customers.py
@@ -0,0 +1,15 @@
+#  Copyright 2024 Zeppelin Bend Pty Ltd
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from zepben.evolve.services.common.base_service import BaseService
+
+__all__ = ["CustomerService"]
+
+
+class CustomerService(BaseService):
+    """
+    Used to store Customer related types.
+    """
+    name: str = "customer"
