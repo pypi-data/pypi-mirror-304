f654ddd7a5d3565131fe5bac84a3cea11ccb4ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:03:54 +0000

    Add package communex/0.1.34.6/communex-0.1.34.6-py3-none-any.whl

---
+++ packages/communex/communex-0.1.34.6-py3-none-any.whl/communex/cli/key.py
@@ -0,0 +1,386 @@
+import json
+import re
+from enum import Enum
+from typing import Any, Optional, cast
+
+import typer
+from substrateinterface import Keypair  # type: ignore
+from typer import Context
+
+from communex._common import BalanceUnit, format_balance
+from communex.cli._common import (get_universal_password, make_custom_context,
+                                  print_table_from_plain_dict,
+                                  print_table_standardize)
+from communex.compat.key import (classic_key_path, classic_store_key,
+                                 local_key_addresses, try_classic_load_key,
+                                 try_load_key)
+from communex.key import check_ss58_address, generate_keypair, is_ss58_address
+from communex.misc import (local_keys_allbalance, local_keys_to_freebalance,
+                           local_keys_to_stakedbalance)
+
+key_app = typer.Typer(no_args_is_help=True)
+
+
+class SortBalance(str, Enum):
+    all = "all"
+    free = "free"
+    staked = "staked"
+
+
+@key_app.command()
+def create(
+    ctx: Context, name: str,
+    password: str = typer.Option(None)
+):
+    """
+    Generates a new key and stores it on a disk with the given name.
+    """
+    context = make_custom_context(ctx)
+
+    keypair = generate_keypair()
+    address = keypair.ss58_address
+
+    context.info(f"Generated key with public address '{address}'.")
+
+    classic_store_key(keypair, name, password)
+
+    context.info(f"Key successfully stored with name '{name}'.")
+
+
+@key_app.command()
+def regen(
+        ctx: Context, name: str, key_input: str, password: Optional[str] = None):
+    """
+    Stores the given key on a disk. Works with private key or mnemonic.
+    """
+    # TODO: secret input from env var and stdin
+    context = make_custom_context(ctx)
+    # Determine the input type based on the presence of spaces.
+    if re.search(r'\s', key_input):
+        # If mnemonic (contains spaces between words).
+        keypair = Keypair.create_from_mnemonic(key_input)
+        key_type = "mnemonic"
+    else:
+        # If private key (assumes no spaces).
+        keypair = Keypair.create_from_private_key(key_input, ss58_format=42)
+        key_type = "private key"
+        # Substrate does not return these.
+        keypair.mnemonic = ""  # type: ignore
+        keypair.seed_hex = ""
+
+    address = keypair.ss58_address
+    context.info(f"Loaded {key_type} with public address `{address}`.")
+
+    classic_store_key(keypair, name, password)
+
+    context.info(f"Key stored with name `{name}` successfully.")
+
+
+@key_app.command()
+def show(
+    ctx: Context, key: str, show_private: bool = False,
+    password: Optional[str] = None
+):
+    """
+    Show information about a key.
+    """
+    context = make_custom_context(ctx)
+
+    path = classic_key_path(key)
+    key_dict_json = try_load_key(path, context, password=password)
+    key_dict = json.loads(key_dict_json)
+
+    if show_private is not True:
+        key_dict["private_key"] = "[SENSITIVE-MODE]"
+        key_dict["seed_hex"] = "[SENSITIVE-MODE]"
+        key_dict["mnemonic"] = "[SENSITIVE-MODE]"
+
+    print_table_from_plain_dict(key_dict, ["Key", "Value"], context.console)
+
+
+@key_app.command()
+def balances(
+    ctx: Context,
+    unit: BalanceUnit = BalanceUnit.joule,
+    sort_balance: SortBalance = SortBalance.all,
+    use_universal_password: bool = typer.Option(
+        False, help="""
+        If you want to use a password to decrypt all keys.
+        This will only work if all encrypted keys uses the same password.
+        If this is not the case, leave it blank and you will be prompted to give
+        every password.
+        """
+    )
+
+):
+    """
+    Gets balances of all keys.
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+    if use_universal_password:
+        universal_password = get_universal_password(context)
+    else:
+        universal_password = None
+
+    local_keys = local_key_addresses(context, universal_password=universal_password)
+    with context.console.status("Getting balances of all keys, this might take a while..."):
+        key2freebalance, key2stake = local_keys_allbalance(client, local_keys)
+    key_to_freebalance = {k: format_balance(
+        v, unit) for k, v in key2freebalance.items()}
+    key_to_stake = {k: format_balance(v, unit) for k, v in key2stake.items()}
+
+    key2balance = {k: v + key2stake[k] for k, v in key2freebalance.items()}
+    key_to_balance = {k: format_balance(v, unit)
+                      for k, v in key2balance.items()}
+
+    if sort_balance == SortBalance.all:
+        sorted_bal = {k: v for k, v in sorted(
+            key2balance.items(), key=lambda item: item[1], reverse=True)}
+    elif sort_balance == SortBalance.free:
+        sorted_bal = {k: v for k, v in sorted(
+            key2freebalance.items(), key=lambda item: item[1], reverse=True)}
+    elif sort_balance == SortBalance.staked:
+        sorted_bal = {k: v for k, v in sorted(
+            key2stake.items(), key=lambda item: item[1], reverse=True)}
+    else:
+        raise ValueError("Invalid sort balance option")
+
+    stake: list[str] = []
+    all_balance: list[str] = []
+    free: list[str] = []
+    keys: list[str] = []
+
+    for key, _ in sorted_bal.items():
+        keys.append(key)
+        free.append(key_to_freebalance[key])
+        stake.append(key_to_stake[key])
+        all_balance.append(key_to_balance[key])
+
+    pretty_dict = {
+        "key": keys,
+        "free": free,
+        "staked": stake,
+        "all": all_balance,
+    }
+
+    general_dict: dict[str, list[Any]] = cast(
+        dict[str, list[Any]], pretty_dict)
+    print_table_standardize(general_dict, context.console)
+
+
+@key_app.command(name='list')
+def inventory(
+    ctx: Context,
+    use_universal_password: bool = typer.Option(
+        False, help="""
+        Password to decrypt all keys.
+        This will only work if all encrypted keys uses the same password.
+        If this is not the case, leave it blank and you will be prompted to give
+        every password.
+        """
+    )
+):
+    """
+    Lists all keys stored on disk.
+    """
+    context = make_custom_context(ctx)
+    if use_universal_password:
+        universal_password = get_universal_password(context)
+    else:
+        universal_password = None
+    key_to_address = local_key_addresses(context, universal_password)
+    general_key_to_address: dict[str, str] = cast(
+        dict[str, str], key_to_address)
+    print_table_from_plain_dict(general_key_to_address, [
+                                "Key", "Address"], context.console)
+
+
+@key_app.command()
+def stakefrom(
+    ctx: Context, key: str,
+    unit: BalanceUnit = BalanceUnit.joule,
+    password: Optional[str] = None,
+):
+    """
+    Gets what keys is key staked from.
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+
+    if is_ss58_address(key):
+        key_address = key
+    else:
+        keypair = try_classic_load_key(key, context, password)
+        key_address = keypair.ss58_address
+        key_address = check_ss58_address(key_address)
+    with context.progress_status(f"Getting stake-from map for {key_address}..."):
+        result = client.get_stakefrom(key=key_address)
+
+    result = {k: format_balance(v, unit) for k, v in result.items()}
+
+    print_table_from_plain_dict(result, ["Key", "Stake"], context.console)
+
+
+@key_app.command()
+def staketo(
+    ctx: Context, key: str,
+    unit: BalanceUnit = BalanceUnit.joule,
+    password: Optional[str] = None,
+):
+    """
+    Gets stake to a key.
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+
+    if is_ss58_address(key):
+        key_address = key
+    else:
+        keypair = try_classic_load_key(key, context, password)
+        key_address = keypair.ss58_address
+        key_address = check_ss58_address(key_address)
+
+    with context.progress_status(f"Getting stake-to of {key_address}..."):
+        result = client.get_staketo(key=key_address)
+
+    result = {k: format_balance(v, unit) for k, v in result.items()}
+
+    print_table_from_plain_dict(result, ["Key", "Stake"], context.console)
+
+
+@key_app.command()
+def total_free_balance(
+    ctx: Context,
+    unit: BalanceUnit = BalanceUnit.joule,
+    use_universal_password: Optional[str] = typer.Option(
+        False, help="""
+        Password to decrypt all keys.
+        This will only work if all encrypted keys uses the same password.
+        If this is not the case, leave it blank and you will be prompted to give
+        every password.
+        """
+    )
+):
+    """
+    Returns total balance of all keys on a disk
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+
+    if use_universal_password:
+        universal_password = get_universal_password(context)
+    else:
+        universal_password = None
+    local_keys = local_key_addresses(context, universal_password)
+    with context.progress_status("Getting total free balance of all keys..."):
+        key2balance: dict[str, int] = local_keys_to_freebalance(client, local_keys)
+
+        balance_sum = sum(key2balance.values())
+
+        context.output(format_balance(balance_sum, unit=unit))
+
+
+@key_app.command()
+def total_staked_balance(
+    ctx: Context, unit: BalanceUnit = BalanceUnit.joule,
+    use_universal_password: bool = typer.Option(
+        False, help="""
+    Password to decrypt all keys.
+    This will only work if all encrypted keys uses the same password.
+    If this is not the case, leave it blank and you will be prompted to give
+    every password.
+    """
+    )
+):
+    """
+    Returns total stake of all keys on a disk
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+
+    if use_universal_password:
+        universal_password = get_universal_password(context)
+    else:
+        universal_password = None
+    local_keys = local_key_addresses(context, universal_password)
+    with context.progress_status("Getting total staked balance of all keys..."):
+        key2stake: dict[str, int] = local_keys_to_stakedbalance(
+            client, local_keys,
+        )
+
+        stake_sum = sum(key2stake.values())
+
+        context.output(format_balance(stake_sum, unit=unit))
+
+
+@key_app.command()
+def total_balance(
+    ctx: Context, unit: BalanceUnit = BalanceUnit.joule,
+    use_universal_password: bool = typer.Option(
+        False, help="""
+    Password to decrypt all keys.
+    This will only work if all encrypted keys uses the same password.
+    If this is not the case, leave it blank and you will be prompted to give
+    every password.
+    """
+    )
+):
+    """
+    Returns total tokens of all keys on a disk
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+
+    if use_universal_password:
+        universal_password = get_universal_password(context)
+    else:
+        universal_password = None
+    local_keys = local_key_addresses(context, universal_password)
+    with context.progress_status("Getting total tokens of all keys..."):
+        key2balance, key2stake = local_keys_allbalance(
+            client, local_keys
+        )
+        key2tokens = {k: v + key2stake[k] for k, v in key2balance.items()}
+        tokens_sum = sum(key2tokens.values())
+
+        context.output(format_balance(tokens_sum, unit=unit))
+
+
+@key_app.command()
+def power_delegation(
+    ctx: Context,
+    key: Optional[str] = None,
+    enable: bool = typer.Option(True, "--disable"),
+    use_universal_password: bool = typer.Option(False)
+):
+    """
+    Gets power delegation of a key.
+    """
+    context = make_custom_context(ctx)
+    client = context.com_client()
+    if key is None:
+        action = "enable" if enable else "disable"
+        confirm_message = (
+            f"Key was not set, this will {action} vote power delegation for all"
+            " keys on disk. Do you want to proceed?"
+        )
+        if not typer.confirm(confirm_message):
+            context.info("Aborted.")
+            exit(0)
+
+        if use_universal_password:
+            universal_password = get_universal_password(context)
+        else:
+            universal_password = None
+        local_keys = local_key_addresses(context, universal_password)
+    else:
+        local_keys = {key: None}
+    for key_name in local_keys.keys():
+        keypair = try_classic_load_key(key_name, context)
+        if enable is True:
+            context.info(f"Enabling vote power delegation on key {key_name} ...")
+            client.enable_vote_power_delegation(keypair)
+        else:
+            context.info(f"Disabling vote power delegation on key {key_name} ...")
+            client.disable_vote_power_delegation(keypair)
---
+++ packages/communex/communex-0.1.34.6-py3-none-any.whl/communex/compat/key.py
@@ -0,0 +1,302 @@
+"""
+Key storage compatible with the classic `commune` library.
+
+WIP
+"""
+
+import json
+import os
+from getpass import getpass
+from pathlib import Path
+from typing import Any, Protocol, cast
+
+from substrateinterface import Keypair  # type: ignore
+
+from communex.compat.storage import COMMUNE_HOME, classic_load, classic_put
+from communex.compat.types import CommuneKeyDict
+from communex.key import check_ss58_address, is_ss58_address
+from communex.types import Ss58Address
+from communex.util import bytes_to_hex, check_str
+
+
+class GenericCtx(Protocol):
+    def info(self, message: str):
+        ...
+
+
+def check_key_dict(key_dict: Any) -> CommuneKeyDict:
+    """
+    Validates a given dictionary as a commune key dictionary and returns it.
+
+    This function checks if the provided dictionary adheres to the structure of
+    a CommuneKeyDict, that is used by the classic `commune` library and returns
+    it if valid.
+
+    Args:
+        key_dict: The dictionary to validate.
+
+    Returns:
+        The validated commune key dictionary. Same as input.
+
+    Raises:
+      AssertionError: If the dictionary does not conform to the expected
+        structure.
+    """
+
+    assert isinstance(key_dict, dict)
+    assert isinstance(key_dict["crypto_type"], int)
+    assert isinstance(key_dict["seed_hex"], str)
+    assert isinstance(key_dict["derive_path"], str | None)
+    assert isinstance(key_dict["path"], str) or key_dict["path"] is None
+    assert isinstance(key_dict["public_key"], str)
+    assert isinstance(key_dict["ss58_format"], int)
+    assert isinstance(key_dict["ss58_address"], str)
+    assert is_ss58_address(key_dict["ss58_address"])
+    assert isinstance(key_dict["private_key"], str)
+    assert isinstance(key_dict["mnemonic"], str)
+    return cast(CommuneKeyDict, key_dict)
+
+
+def classic_key_path(name: str) -> str:
+    """
+    Constructs the file path for a key name in the classic commune format.
+    """
+
+    home = Path.home()
+    root_path = home / '.commune' / "key"
+    name = name + ".json"
+    return str(root_path / name)
+
+
+def from_classic_dict(
+        data: dict[Any, Any],
+        from_mnemonic: bool = False
+) -> Keypair:
+    """
+    Creates a `Key` from a dict conforming to the classic `commune` format.
+
+    Args:
+        data: The key data in a classic commune format.
+        name: The name to assign to the key.
+
+    Returns:
+        The reconstructed `Key` instance.
+
+    Raises:
+        AssertionError: If `data` does not conform to the expected format.
+    """
+
+    data_ = check_key_dict(data)
+
+    ss58_address = data_["ss58_address"]
+    private_key = data_["private_key"]
+    mnemonic_key = data_["mnemonic"]
+    public_key = data_["public_key"]
+    ss58_format = data_["ss58_format"]
+    if from_mnemonic:
+        key = Keypair.create_from_mnemonic(
+            mnemonic_key, ss58_format
+        )
+    else:
+        key = Keypair.create_from_private_key(
+            private_key, public_key, ss58_address, ss58_format
+        )
+
+    return key
+
+
+def to_classic_dict(keypair: Keypair, path: str) -> CommuneKeyDict:
+    """
+    Converts a keypair to a dictionary conforming to the classic commune format.
+
+    Args:
+        keypair: The keypair to convert.
+        path: The path/name of the key file.
+    """
+
+    return {
+        "path": path,
+        "mnemonic": check_str(keypair.mnemonic),
+        "public_key": bytes_to_hex(keypair.public_key),
+        "private_key": bytes_to_hex(keypair.private_key),
+        "ss58_address": check_ss58_address(keypair.ss58_address),
+        "seed_hex": bytes_to_hex(keypair.seed_hex),
+        "ss58_format": keypair.ss58_format,
+        "crypto_type": keypair.crypto_type,
+        "derive_path": keypair.derive_path,
+    }
+
+
+def classic_load_key(
+    name: str,
+    password: str | None = None,
+    from_mnemonic: bool = False,
+) -> Keypair:
+    """
+    Loads the keypair with the given name from a disk.
+    """
+    path = classic_key_path(name)
+    key_dict_json = classic_load(path, password=password)
+    key_dict = json.loads(key_dict_json)
+    return from_classic_dict(key_dict, from_mnemonic=from_mnemonic)
+
+
+def is_encrypted(name: str) -> bool:
+    """
+    Checks if the key with the given name is encrypted.
+    """
+    path = classic_key_path(name)
+    full_path = os.path.expanduser(os.path.join(COMMUNE_HOME, path))
+    with open(full_path, "r") as file:
+        body = json.load(file)
+    return body["encrypted"]
+
+
+def classic_store_key(keypair: Keypair, name: str, password: str | None = None) -> None:
+    """
+    Stores the given keypair on a disk under the given name.
+    """
+    key_dict = to_classic_dict(keypair, name)
+    key_dict_json = json.dumps(key_dict)
+    path = classic_key_path(name)
+    classic_put(path, key_dict_json, password=password)
+
+
+def try_classic_load_key(
+    name: str, context: GenericCtx | None = None,
+    password: str | None = None,
+    from_mnemonic: bool = False,
+) -> Keypair:
+    try:
+        keypair = classic_load_key(
+            name,
+            password=password,
+            from_mnemonic=from_mnemonic
+        )
+    except json.JSONDecodeError:
+        prompt = f"Please provide the password for the key {name}"
+        if context is not None:
+            context.info(prompt)
+        else:
+            print(prompt)
+        password = getpass()
+        keypair = classic_load_key(
+            name,
+            password=password,
+            from_mnemonic=from_mnemonic
+        )
+    return keypair
+
+
+def try_load_key(
+    name: str,
+    context: GenericCtx | None = None,
+    password: str | None = None
+):
+    try:
+        key_dict = classic_load(name, password=password)
+    except json.JSONDecodeError:
+        prompt = f"Please provide the password for the key {name}"
+        if context is not None:
+            context.info(prompt)
+        else:
+            print(prompt)
+        password = getpass()
+        key_dict = classic_load(name, password=password)
+    return key_dict
+
+
+def local_key_addresses(
+    ctx: GenericCtx | None = None,
+    universal_password: str | None = None
+) -> dict[str, Ss58Address]:
+    """
+    Retrieves a mapping of local key names to their SS58 addresses.
+    If password is passed, it will be used to decrypt every key.
+    If password is not passed and ctx is,
+    the user will be prompted for the password.
+    """
+    home = Path.home()
+    key_dir = home / '.commune' / "key"
+
+    key_names = [f.stem for f in key_dir.iterdir() if f.is_file() and not f.name.startswith('.')]
+
+    addresses_map: dict[str, Ss58Address] = {}
+
+    for key_name in key_names:
+        # issue #11 https://github.com/agicommies/communex/issues/12 added check for key2address to stop error from being thrown by wrong key type.
+        if key_name == "key2address":
+            print("key2address is saved in an invalid format. It will be ignored.")
+            continue
+        encrypted = is_encrypted(key_name)
+        if encrypted:
+            if universal_password:
+                password = universal_password
+            elif ctx:
+                ctx.info(f"Please provide the password for the key '{key_name}'")
+                password = getpass()
+            else:
+                print(f"Please provide the password for the key '{key_name}'")
+                password = getpass()
+        else:
+            password = None
+        key_dict = classic_load_key(key_name, password=password)
+        addresses_map[key_name] = check_ss58_address(key_dict.ss58_address)
+
+    return addresses_map
+
+
+def resolve_key_ss58(key: Ss58Address | Keypair | str) -> Ss58Address:
+    """
+    Resolves a keypair or key name to its corresponding SS58 address.
+
+    If the input is already an SS58 address, it is returned as is.
+    """
+
+    if isinstance(key, Keypair):
+        return key.ss58_address  # type: ignore
+
+    if is_ss58_address(key):
+        return key
+
+    try:
+        keypair = classic_load_key(key)
+    except FileNotFoundError:
+        raise ValueError(
+            f"Key is not a valid SS58 address nor a valid key name: {key}")
+
+    address = keypair.ss58_address
+
+    return check_ss58_address(address)
+
+
+def resolve_key_ss58_encrypted(
+        key: Ss58Address | Keypair | str, context: GenericCtx,
+        password: str | None = None
+
+) -> Ss58Address:
+    """
+    Resolves a keypair or key name to its corresponding SS58 address.
+
+    If the input is already an SS58 address, it is returned as is.
+    """
+
+    if isinstance(key, Keypair):
+        return check_ss58_address(key.ss58_address, key.ss58_format)
+
+    if is_ss58_address(key):
+        return key
+
+    try:
+        keypair = classic_load_key(key, password=password)
+    except json.JSONDecodeError:
+        context.info(f"Please provide the password for the key {key}")
+        password = getpass()
+        keypair = classic_load_key(key, password=password)
+    except FileNotFoundError:
+        raise ValueError(
+            f"Key is not a valid SS58 address nor a valid key name: {key}")
+
+    address = keypair.ss58_address
+
+    return check_ss58_address(address, keypair.ss58_format)
