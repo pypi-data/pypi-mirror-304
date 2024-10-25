3f583a0537676f1277030069c775113dc1908105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:07 +0000

    Add package movoid-config/1.2.6/movoid_config-1.2.6-py3-none-any.whl

---
+++ packages/movoid-config/movoid_config-1.2.6-py3-none-any.whl/movoid_config/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/1 11:52
+# Description   : 
+"""
+from .config import Config
+from .loop_run import CmdLoopRun
