3e2c0d2bff44507693b7b3c3a00f2e094f21d7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:35 +0000

    Add package devart-firebird-connector/1.2.0/devart_firebird_connector-1.2.0-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/devart-firebird-connector/devart_firebird_connector-1.2.0-cp310-cp310-macosx_10_9_universal2.whl/devart/firebird/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python Connector for Firebird is a connectivity solution for accessing
+Firebird databases from Python applications. It fully implements the Python DB
+API 2.0 specification. The connector is distributed as a wheel package
+for Windows, macOS, and Linux.
+ 
+Secure communication
+
+    The connector supports the Over-the-Wire (OTW) encryption feature of Firebird
+    to encrypt data during the transmission process.
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/firebird/
+"""
+from .firebird import *
