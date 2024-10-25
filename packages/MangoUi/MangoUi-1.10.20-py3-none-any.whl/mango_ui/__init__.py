d23d564c46141e54c6f7256375127cfda953565b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:03:56 +0000

    Add package MangoUi/1.10.20/MangoUi-1.10.20-py3-none-any.whl

---
+++ packages/MangoUi/MangoUi-1.10.20-py3-none-any.whl/mango_ui/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-10-08 16:09
+# @Author : 毛鹏
+from .components import *
+from .enums.enums import *
+from .models.models import *
+from .settings.settings import *
+from .widgets import *
+from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout, QFormLayout, QGridLayout, QWidget, QApplication
+from .widget_tool import WidgetTool
