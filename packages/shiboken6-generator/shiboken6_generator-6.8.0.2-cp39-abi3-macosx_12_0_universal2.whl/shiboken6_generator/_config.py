9e0e0a5a9436e814d9ae73e2a9cf7cc541f56f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:43 +0000

    Add package shiboken6-generator/6.8.0.2/shiboken6_generator-6.8.0.2-cp39-abi3-macosx_12_0_universal2.whl

---
+++ packages/shiboken6-generator/shiboken6_generator-6.8.0.2-cp39-abi3-macosx_12_0_universal2.whl/shiboken6_generator/_config.py
@@ -0,0 +1,10 @@
+version = "6.8.0.2"
+version_info = (6, 8, 0.2, "", "")
+
+__build_date__ = '2024-10-23T15:22:17+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.8.0.2'
+__qt_macos_min_deployment_target__ = '12.0'
