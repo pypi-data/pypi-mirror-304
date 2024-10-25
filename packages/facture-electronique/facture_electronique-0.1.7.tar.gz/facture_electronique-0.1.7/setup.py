cc79ae2c8f4aad36d133fbd246666acf725b34b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:43 +0000

    Add package facture-electronique/0.1.7/facture_electronique-0.1.7.tar.gz

---
+++ packages/facture-electronique/facture_electronique-0.1.7.tar.gz/facture_electronique-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='facture_electronique',
+	version='0.1.7',
+	author='Thierry Martin',
+	author_email='thierry.martin.2008@gadz.org',
+	description="Facturation Electronique SDK est une bibliothèque Python qui simplifie l'interaction avec les principales API de facturation électronique en France, notamment **Chorus Pro**, et d'autres partenaires privés. Elle supporte également le format Factur-X pour la création et l'envoi de factures électroniques.",
+	long_description=open('readme.md').read(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/thierryjmartin/facturation_electronique',
+	packages=find_packages(),
+	install_requires=[
+		# List of dependencies (from requirements.txt, for example)
+	],
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	python_requires='>=3.6',  # Specify Python version compatibility
+	include_package_data=True,  # If you have extra files to include (via MANIFEST.in)
+)
