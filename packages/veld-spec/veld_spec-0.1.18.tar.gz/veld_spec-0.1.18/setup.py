a20821fb3c17082bc95c4b675cd434016df93ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:20 +0000

    Add package veld-spec/0.1.18/veld_spec-0.1.18.tar.gz

---
+++ packages/veld-spec/veld_spec-0.1.18.tar.gz/veld_spec-0.1.18/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="veld_spec",
+    version="0.1.18",
+    packages=find_packages(),
+    author="Stefan Resch",
+    author_email="stefan.resch@oeaw.ac.at",
+    description="VELD specification",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/acdh-oeaw/VELD_spec",
+    py_modules=["veld_spec"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "PyYAML>=6.0.2",
+    ],
+    include_package_data=True,
+    package_data={"": ["README.md"]},
+)
+
