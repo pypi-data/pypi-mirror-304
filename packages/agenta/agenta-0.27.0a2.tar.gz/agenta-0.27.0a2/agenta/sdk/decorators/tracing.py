93043254603b911d389ad944b214c51d695db033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:45 +0000

    Add package agenta/0.27.0a2/agenta-0.27.0a2.tar.gz

---
+++ packages/agenta/agenta-0.27.0a2.tar.gz/agenta-0.27.0a2/agenta/sdk/decorators/tracing.py
@@ -0,0 +1,289 @@
+import inspect
+import traceback
+from functools import wraps
+from itertools import chain
+from typing import Callable, Optional, Any, Dict, List, Union
+
+import agenta as ag
+
+
+from agenta.sdk.utils.exceptions import suppress
+
+from agenta.sdk.context.tracing import tracing_context
+from agenta.sdk.tracing.conventions import parse_span_kind
+
+
+class instrument:
+    DEFAULT_KEY = "__default__"
+
+    def __init__(
+        self,
+        kind: str = "task",
+        config: Optional[Dict[str, Any]] = None,
+        ignore_inputs: Optional[bool] = None,
+        ignore_outputs: Optional[bool] = None,
+        max_depth: Optional[int] = 2,
+        # DEPRECATED
+        spankind: Optional[str] = "TASK",
+    ) -> None:
+        self.kind = spankind if spankind is not None else kind
+        self.config = config
+        self.ignore_inputs = ignore_inputs
+        self.ignore_outputs = ignore_outputs
+        self.max_depth = max_depth
+
+    def __call__(self, func: Callable[..., Any]):
+        is_coroutine_function = inspect.iscoroutinefunction(func)
+
+        def parse(*args, **kwargs) -> Dict[str, Any]:
+            inputs = {
+                key: value
+                for key, value in chain(
+                    zip(inspect.getfullargspec(func).args, args),
+                    kwargs.items(),
+                )
+            }
+
+            return inputs
+
+        def redact(
+            io: Dict[str, Any], ignore: Union[List[str], bool] = False
+        ) -> Dict[str, Any]:
+            """
+            Redact user-defined sensitive information from inputs and outputs as defined by the ignore list or boolean flag.
+
+            Example:
+            - ignore = ["password"] -> {"username": "admin", "password": "********"} -> {"username": "admin"}
+            - ignore = True -> {"username": "admin", "password": "********"} -> {}
+            - ignore = False -> {"username": "admin", "password": "********"} -> {"username": "admin", "password": "********"}
+            """
+            io = {
+                key: value
+                for key, value in io.items()
+                if key
+                not in (
+                    ignore
+                    if isinstance(ignore, list)
+                    else io.keys()
+                    if ignore is True
+                    else []
+                )
+            }
+
+            return io
+
+        def patch(result: Any) -> Dict[str, Any]:
+            """
+            Patch the result to ensure that it is a dictionary, with a default key when necessary.
+
+            Example:
+            - result = "Hello, World!" -> {"__default__": "Hello, World!"}
+            - result = {"message": "Hello, World!", "cost": 0.0, "usage": {}} -> {"__default__": "Hello, World!"}
+            - result = {"message": "Hello, World!"} -> {"message": "Hello, World!"}
+            """
+            outputs = (
+                {instrument.DEFAULT_KEY: result}
+                if not isinstance(result, dict)
+                else (
+                    {instrument.DEFAULT_KEY: result["message"]}
+                    if all(key in result for key in ["message", "cost", "usage"])
+                    else result
+                )
+            )
+
+            return outputs
+
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            async def wrapped_func(*args, **kwargs):
+                if not ag.tracing.get_current_span().is_recording():
+                    self.kind = "workflow"
+
+                kind = parse_span_kind(self.kind)
+
+                with ag.tracer.start_as_current_span(func.__name__, kind=kind):
+                    span = ag.tracing.get_current_span()
+
+                    with suppress():
+                        span.set_attributes(
+                            attributes={"node": self.kind},
+                            namespace="type",
+                        )
+
+                        if span.parent is None:
+                            rctx = tracing_context.get()
+
+                            span.set_attributes(
+                                attributes={"configuration": rctx.get("config", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"environment": rctx.get("environment", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"version": rctx.get("version", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"variant": rctx.get("variant", {})},
+                                namespace="meta",
+                            )
+
+                        _inputs = redact(parse(*args, **kwargs), self.ignore_inputs)
+                        span.set_attributes(
+                            attributes={"inputs": _inputs},
+                            namespace="data",
+                            max_depth=self.max_depth,
+                        )
+
+                    result = await func(*args, **kwargs)
+
+                    with suppress():
+                        cost = None
+                        usage = {}
+
+                        if isinstance(result, dict):
+                            cost = result.get("cost", None)
+                            usage = result.get("usage", {})
+
+                        if isinstance(usage, (int, float)):
+                            usage = {"total_tokens": usage}
+
+                        span.set_attributes(
+                            attributes={"total": cost},
+                            namespace="metrics.unit.costs",
+                        )
+                        span.set_attributes(
+                            attributes=(
+                                {
+                                    "prompt": usage.get("prompt_tokens", None),
+                                    "completion": usage.get("completion_tokens", None),
+                                    "total": usage.get("total_tokens", None),
+                                }
+                            ),
+                            namespace="metrics.unit.tokens",
+                        )
+
+                        _outputs = redact(patch(result), self.ignore_outputs)
+                        span.set_attributes(
+                            attributes={"outputs": _outputs},
+                            namespace="data",
+                            max_depth=self.max_depth,
+                        )
+
+                        span.set_status("OK")
+
+                    with suppress():
+                        if hasattr(span, "parent") and span.parent is None:
+                            tracing_context.set(
+                                tracing_context.get()
+                                | {
+                                    "root": {
+                                        "trace_id": span.get_span_context().trace_id,
+                                        "span_id": span.get_span_context().span_id,
+                                    }
+                                }
+                            )
+
+                    return result
+
+            return await wrapped_func(*args, **kwargs)
+
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            def wrapped_func(*args, **kwargs):
+                if not ag.tracing.get_current_span().is_recording():
+                    self.kind = "workflow"
+
+                kind = parse_span_kind(self.kind)
+
+                with ag.tracer.start_as_current_span(func.__name__, kind=kind):
+                    span = ag.tracing.get_current_span()
+
+                    with suppress():
+                        span.set_attributes(
+                            attributes={"node": self.kind},
+                            namespace="type",
+                        )
+
+                        if span.parent is None:
+                            rctx = tracing_context.get()
+
+                            span.set_attributes(
+                                attributes={"configuration": rctx.get("config", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"environment": rctx.get("environment", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"version": rctx.get("version", {})},
+                                namespace="meta",
+                            )
+                            span.set_attributes(
+                                attributes={"variant": rctx.get("variant", {})},
+                                namespace="meta",
+                            )
+
+                        _inputs = redact(parse(*args, **kwargs), self.ignore_inputs)
+                        span.set_attributes(
+                            attributes={"inputs": _inputs},
+                            namespace="data",
+                            max_depth=self.max_depth,
+                        )
+
+                    result = func(*args, **kwargs)
+
+                    with suppress():
+                        cost = None
+                        usage = {}
+                        if isinstance(result, dict):
+                            cost = result.get("cost", None)
+                            usage = result.get("usage", {})
+
+                        if isinstance(usage, (int, float)):
+                            usage = {"total_tokens": usage}
+
+                        span.set_attributes(
+                            attributes={"total": cost},
+                            namespace="metrics.unit.costs",
+                        )
+                        span.set_attributes(
+                            attributes=(
+                                {
+                                    "prompt": usage.get("prompt_tokens", None),
+                                    "completion": usage.get("completion_tokens", None),
+                                    "total": usage.get("total_tokens", None),
+                                }
+                            ),
+                            namespace="metrics.unit.tokens",
+                        )
+
+                        _outputs = redact(patch(result), self.ignore_outputs)
+                        span.set_attributes(
+                            attributes={"outputs": _outputs},
+                            namespace="data",
+                            max_depth=self.max_depth,
+                        )
+
+                        span.set_status("OK")
+
+                    with suppress():
+                        if hasattr(span, "parent") and span.parent is None:
+                            tracing_context.set(
+                                tracing_context.get()
+                                | {
+                                    "root": {
+                                        "trace_id": span.get_span_context().trace_id,
+                                        "span_id": span.get_span_context().span_id,
+                                    }
+                                }
+                            )
+
+                    return result
+
+            return wrapped_func(*args, **kwargs)
+
+        return async_wrapper if is_coroutine_function else sync_wrapper
---
+++ packages/agenta/agenta-0.27.0a2.tar.gz/agenta-0.27.0a2/agenta/sdk/tracing/tracing.py
@@ -0,0 +1,229 @@
+from typing import Optional, Any, Dict
+from enum import Enum
+
+from httpx import get as check
+
+from opentelemetry.trace import (
+    get_current_span,
+    set_tracer_provider,
+    get_tracer_provider,
+    Status,
+    StatusCode,
+)
+from opentelemetry.sdk.trace import Span, Tracer, TracerProvider
+from opentelemetry.sdk.resources import Resource
+
+from agenta.sdk.utils.singleton import Singleton
+from agenta.sdk.utils.exceptions import suppress
+from agenta.sdk.utils.logging import log
+
+from agenta.sdk.tracing.processors import TraceProcessor
+from agenta.sdk.tracing.exporters import ConsoleExporter, InlineExporter, OTLPExporter
+from agenta.sdk.tracing.spans import CustomSpan
+from agenta.sdk.tracing.inline import parse_inline_trace
+
+from agenta.sdk.tracing.conventions import Reference, is_valid_attribute_key
+
+
+class Tracing(metaclass=Singleton):
+    VERSION = "0.1.0"
+
+    Status = Status
+    StatusCode = StatusCode
+
+    def __init__(
+        self,
+        url: str,
+    ) -> None:
+        # ENDPOINT (OTLP)
+        self.otlp_url = url
+        # AUTHENTICATION (OTLP)
+        self.project_id: Optional[str] = None
+        # AUTHORIZATION (OTLP)
+        self.api_key: Optional[str] = None
+        # HEADERS (OTLP)
+        self.headers: Dict[str, str] = dict()
+        # REFERENCES
+        self.references: Dict[str, str] = dict()
+
+        # TRACER PROVIDER
+        self.tracer_provider: Optional[TracerProvider] = None
+        # TRACER
+        self.tracer: Optional[Tracer] = None
+        # INLINE SPANS for INLINE TRACES (INLINE PROCESSOR)
+        self.inline_spans: Dict[str, Any] = dict()
+
+    # PUBLIC
+
+    def configure(
+        self,
+        project_id: Optional[str] = None,
+        api_key: Optional[str] = None,
+        #
+        app_id: Optional[str] = None,
+    ):
+        # AUTHENTICATION (OTLP)
+        self.project_id = project_id  # "f7943e42-ec69-498e-bf58-8db034b9286e"
+        self.app_id = app_id
+        # AUTHORIZATION (OTLP)
+        self.api_key = api_key
+        # HEADERS (OTLP)
+        self.headers = {}
+        if project_id:
+            self.headers.update(**{"AG-PROJECT-ID": project_id})
+        if app_id:
+            self.headers.update(**{"AG-APP-ID": app_id})
+        if api_key:
+            self.headers.update(**{"Authorization": self.api_key})
+        # REFERENCES
+        self.references = {"application.id": app_id}
+
+        # TRACER PROVIDER
+        self.tracer_provider = TracerProvider(
+            resource=Resource(attributes={"service.name": "agenta-sdk"})
+        )
+        # TRACE PROCESSORS -- CONSOLE
+        # _console = TraceProcessor(
+        #    ConsoleExporter(),
+        #    references=self.references,
+        # )
+        # self.tracer_provider.add_span_processor(_console)
+        # TRACE PROCESSORS -- INLINE
+        self.inline = TraceProcessor(
+            InlineExporter(registry=self.inline_spans),
+            references=self.references,
+        )
+        self.tracer_provider.add_span_processor(self.inline)
+        # TRACE PROCESSORS -- OTLP
+        try:
+            log.info(f"Connecting to the remote trace receiver at {self.otlp_url}...")
+
+            check(self.otlp_url, headers=self.headers, timeout=1)
+
+            log.info(f"Connection established.")
+
+            _otlp = TraceProcessor(
+                OTLPExporter(endpoint=self.otlp_url, headers=self.headers),
+                references=self.references,
+            )
+
+            self.tracer_provider.add_span_processor(_otlp)
+        except Exception as e:
+            log.error(e)
+            log.warning(f"Connection failed.")
+            log.warning(
+                f"Warning: Your traces will not be exported since {self.otlp_url} is unreachable."
+            )
+        # GLOBAL TRACER PROVIDER -- INSTRUMENTATION LIBRARIES
+        set_tracer_provider(self.tracer_provider)
+        # TRACER
+        self.tracer: Tracer = self.tracer_provider.get_tracer("agenta.tracer")
+
+    def get_current_span(
+        self,
+    ):
+        _span = None
+
+        with suppress():
+            _span = get_current_span()
+
+            if _span.is_recording():
+                return CustomSpan(_span)
+
+        return _span
+
+    def store_internals(
+        self,
+        attributes: Dict[str, Any],
+        span: Optional[Span] = None,
+    ):
+        with suppress():
+            if span is None:
+                span = self.get_current_span()
+
+            span.set_attributes(attributes={"internals": attributes}, namespace="data")
+
+    def store_refs(
+        self,
+        refs: Dict[str, str],
+        span: Optional[Span] = None,
+    ):
+        with suppress():
+            if span is None:
+                span = self.get_current_span()
+
+            for key in refs.keys():
+                if key in Reference:
+                    # ADD REFERENCE TO THIS SPAN
+                    span.set_attribute(
+                        key.value if isinstance(key, Enum) else key,
+                        refs[key],
+                        namespace="refs",
+                    )
+                    # AND TO ALL SPANS CREATED AFTER THIS ONE
+                    self.references[key] = refs[key]
+                    # TODO: THIS SHOULD BE REPLACED BY A TRACE CONTEXT !!!
+
+    def store_meta(
+        self,
+        meta: Dict[str, Any],
+        span: Optional[Span] = None,
+    ):
+        with suppress():
+            if span is None:
+                span = self.get_current_span()
+
+            for key in meta.keys():
+                if is_valid_attribute_key(key):
+                    span.set_attribute(key, meta[key], namespace="meta")
+
+    def store_metrics(
+        self,
+        metrics: Dict[str, Any],
+        span: Optional[Span] = None,
+    ):
+        with suppress():
+            if span is None:
+                span = self.get_current_span()
+
+            for key in metrics.keys():
+                if is_valid_attribute_key(key):
+                    span.set_attribute(key, metrics[key], namespace="metrics")
+
+    def is_inline_trace_ready(
+        self,
+        trace_id: Optional[int] = None,
+    ) -> bool:
+        is_ready = True
+
+        with suppress():
+            if trace_id is not None:
+                is_ready = self.inline.is_ready(trace_id)
+
+        return is_ready
+
+    def get_inline_trace(
+        self,
+        trace_id: Optional[int] = None,
+    ) -> Dict[str, Any]:
+        _inline_trace = {}
+
+        with suppress():
+            is_ready = self.inline.is_ready(trace_id)
+
+            if is_ready is True:
+                otel_spans = self.inline.fetch(trace_id)
+
+                if otel_spans:
+                    _inline_trace = parse_inline_trace(
+                        self.project_id or self.app_id, otel_spans
+                    )
+
+        return _inline_trace
+
+
+def get_tracer(tracing: Tracing) -> Tracer:
+    if tracing is None or tracing.tracer is None or tracing.tracer_provider is None:
+        return get_tracer_provider().get_tracer("default.tracer")
+
+    return tracing.tracer
