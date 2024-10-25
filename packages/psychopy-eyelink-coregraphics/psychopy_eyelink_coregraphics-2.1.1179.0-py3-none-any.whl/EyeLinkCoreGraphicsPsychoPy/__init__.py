00f01efceba49b2834afe8a9f0676870b2cc78de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:04:46 +0000

    Add package psychopy-eyelink-coregraphics/2.1.1179.0/psychopy_eyelink_coregraphics-2.1.1179.0-py3-none-any.whl

---
+++ packages/psychopy-eyelink-coregraphics/psychopy_eyelink_coregraphics-2.1.1179.0-py3-none-any.whl/EyeLinkCoreGraphicsPsychoPy/__init__.py
@@ -0,0 +1,5 @@
+import importlib.metadata
+__version__ = importlib.metadata.version("psychopy-eyelink-coregraphics")
+_last_updated = '09/19/2024'
+
+from .EyeLinkCoreGraphicsPsychoPy import EyeLinkCoreGraphicsPsychoPy
