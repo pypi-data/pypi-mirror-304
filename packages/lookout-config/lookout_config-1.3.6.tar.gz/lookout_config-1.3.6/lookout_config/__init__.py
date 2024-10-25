6586e278f90317e0458834ebff91a743546e4fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:06:09 +0000

    Add package lookout-config/1.3.6/lookout_config-1.3.6.tar.gz

---
+++ packages/lookout-config/lookout_config-1.3.6.tar.gz/lookout_config-1.3.6/lookout_config/__init__.py
@@ -0,0 +1,70 @@
+# IMPORTANT
+# After changing this file, run `python3 -m lookout_config.generate_schemas`
+# To re-generate the json schemas
+import yaml
+import os
+from typing import Any
+from pathlib import Path
+
+from lookout_config.types import (
+    LookoutConfig,
+    Mode,
+    LogLevel,
+    Network,
+    GeolocationMode,
+    Point,
+    Polygon,
+    CameraExtended,
+)
+
+
+LOOKOUT_CONFIG_FILE_NAME = "lookout.yml"
+LOOKOUT_SCHEMA_URL = "https://greenroom-robotics.github.io/lookout/schemas/lookout.schema.json"
+
+
+def find_config() -> Path:
+    """Returns the path to the .config/greenroom directory"""
+    return Path.home().joinpath(".config/greenroom")
+
+
+def get_path():
+    return find_config() / LOOKOUT_CONFIG_FILE_NAME
+
+
+def parse(config: dict[str, Any]) -> LookoutConfig:
+    return LookoutConfig(**config)
+
+
+def read() -> LookoutConfig:
+    path = get_path()
+    with open(path) as stream:
+        return parse(yaml.safe_load(stream))
+
+
+def write(config: LookoutConfig):
+    path = get_path()
+    # Make the parent dir if it doesn't exist
+    os.makedirs(path.parent, exist_ok=True)
+    with open(path, "w") as stream:
+        print(f"Writing: {path}")
+        headers = f"# yaml-language-server: $schema={LOOKOUT_SCHEMA_URL}"
+        data = "\n".join([headers, yaml.dump(config.model_dump(mode="json"))])
+        stream.write(data)
+
+
+__all__ = [
+    "find_config",
+    "get_path",
+    "parse",
+    "read",
+    "write",
+    "LOOKOUT_SCHEMA_URL",
+    "LookoutConfig",
+    "Mode",
+    "LogLevel",
+    "Network",
+    "GeolocationMode",
+    "Point",
+    "Polygon",
+    "CameraExtended",
+]
