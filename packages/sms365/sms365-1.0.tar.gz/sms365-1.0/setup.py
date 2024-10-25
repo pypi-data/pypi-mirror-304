3d7cfe22f49c9c3a53164767807468c395c9d59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:05:08 +0000

    Add package sms365/1.0/sms365-1.0.tar.gz

---
+++ packages/sms365/sms365-1.0.tar.gz/sms365-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="sms365",
+      version="1.0",
+      description="365SMS API Client",
+      packages=["sms365"],
+      author_email="ermalajsonga@gmail.com",
+      zip_safe=False)
