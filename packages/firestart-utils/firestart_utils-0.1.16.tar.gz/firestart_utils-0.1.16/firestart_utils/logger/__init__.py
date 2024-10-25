be5ed31c04b81da56b4622cef8325bbaa15fd83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:18 +0000

    Add package firestart-utils/0.1.16/firestart_utils-0.1.16.tar.gz

---
+++ packages/firestart-utils/firestart_utils-0.1.16.tar.gz/firestart_utils-0.1.16/firestart_utils/logger/__init__.py
@@ -0,0 +1,8 @@
+from .logger import Logger, LakeHouseLogger
+
+
+def get_logger(dd_api_key, dd_customer, environment, workspace_name, log_level):
+    return Logger(dd_api_key, dd_customer, environment, workspace_name, log_level)
+
+def get_lakehouse_logger(environment, location):
+    return LakeHouseLogger(environment, location)
