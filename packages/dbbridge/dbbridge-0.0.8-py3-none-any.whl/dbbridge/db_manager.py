9a2015ba66d68067fb5e08f481eac63fd8190427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:48 +0000

    Add package dbbridge/0.0.8/dbbridge-0.0.8-py3-none-any.whl

---
+++ packages/dbbridge/dbbridge-0.0.8-py3-none-any.whl/dbbridge/db_manager.py
@@ -0,0 +1,19 @@
+from .aws_db import AWSRDSConnection
+from .local_db import LocalDBConnection
+
+def create_db_connection(service, host, user, password, database, region=None, port=3306):
+    if not service or service not in ['local', 'aws', 'azure', 'gcp']:
+        raise ValueError(f"Unsupported cloud provider: '{service}'. Supported providers: aws, azure, gcp, local")
+
+    if service == "local":
+        if not all([host, user, password, database]):
+            raise ValueError("Missing parameters for local database connection: host, user, password, database, and region are required.")
+        return LocalDBConnection(host, user, password, database, port)
+
+    elif service == "aws":
+        if not all([host, user, database, region]):
+            raise ValueError("Missing parameters for AWS database connection: host, user, database, and region are required.")
+        return AWSRDSConnection(host, user, database, region, port)
+
+    else:
+        raise ValueError("Unsupported db_type. Use 'local', 'aws', 'azure', 'gcp'.")
