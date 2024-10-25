94526264f874008e5a7f5d38dbee1be7bfa96499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:46 +0000

    Add package openspg-knext/0.5b1/openspg_knext-0.5b1.tar.gz

---
+++ packages/openspg-knext/openspg_knext-0.5b1.tar.gz/openspg_knext-0.5b1/knext/command/sub_command/reasoner.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 OpenSPG Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+import os
+import sys
+import click
+
+from knext.reasoner.client import ReasonerClient
+
+
+@click.option("--file", help="Path of DSL file.")
+@click.option("--dsl", help="DSL string enclosed in double quotes.")
+@click.option("--output", help="Output file.")
+def execute_reasoner_job(file, dsl, output=None):
+    """
+    Submit asynchronous reasoner jobs to server by providing DSL file or string.
+    """
+    with_server = eval(os.getenv("KAG_PROJECT_WITH_SERVER", "False"))
+    if not with_server:
+        click.secho(
+            "ERROR: Reasoner must be executed with SPG Server.", fg="bright_red"
+        )
+        sys.exit()
+    client = ReasonerClient()
+    if file and not dsl:
+        with open(file, "r") as f:
+            dsl_content = f.read()
+    elif not file and dsl:
+        dsl_content = dsl
+    else:
+        click.secho("ERROR: Please choose either --file or --dsl.", fg="bright_red")
+        sys.exit()
+    client.execute(dsl_content, output)
