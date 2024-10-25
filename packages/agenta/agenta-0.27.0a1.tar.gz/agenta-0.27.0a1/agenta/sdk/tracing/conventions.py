8af9792fd239a3efe68975917185d8da5cff9d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:45 +0000

    Add package agenta/0.27.0a1/agenta-0.27.0a1.tar.gz

---
+++ packages/agenta/agenta-0.27.0a1.tar.gz/agenta-0.27.0a1/agenta/sdk/tracing/conventions.py
@@ -0,0 +1,47 @@
+from opentelemetry.trace import SpanKind
+
+from enum import Enum
+
+from re import fullmatch
+
+_PATTERN = r"[A-Za-z0-9._-]+"
+
+
+def is_valid_attribute_key(string):
+    return bool(fullmatch(_PATTERN, string))
+
+
+class Reference(str, Enum):
+    #
+    VARIANT_ID = "variant.id"
+    VARIANT_SLUG = "variant.slug"
+    VARIANT_VERSION = "variant.version"
+    #
+    ENVIRONMENT_ID = "environment.id"
+    ENVIRONMENT_SLUG = "environment.slug"
+    ENVIRONMENT_VERSION = "environment.version"
+    #
+    APPLICATION_ID = "application.id"
+    APPLICATION_SLUG = "application.slug"
+    #
+
+
+def parse_span_kind(type: str) -> SpanKind:
+    kind = SpanKind.INTERNAL
+    if type in [
+        "agent",
+        "chain",
+        "workflow",
+    ]:
+        kind = SpanKind.SERVER
+    elif type in [
+        "tool",
+        "embedding",
+        "query",
+        "completion",
+        "chat",
+        "rerank",
+    ]:
+        kind = SpanKind.CLIENT
+
+    return kind
