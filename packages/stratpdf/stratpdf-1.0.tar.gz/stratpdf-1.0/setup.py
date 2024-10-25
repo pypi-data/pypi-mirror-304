ac7310cdb49eb4d915afe884b9144682a327a5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:21 +0000

    Add package stratpdf/1.0/stratpdf-1.0.tar.gz

---
+++ packages/stratpdf/stratpdf-1.0.tar.gz/stratpdf-1.0/setup.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import setuptools
+
+
+setuptools.setup(
+    name="stratpdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)
