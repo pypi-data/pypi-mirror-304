08e1746e0fcb07e2b281cf59a7950291cff03e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:04:12 +0000

    Add package mtc-base/0.0.3/mtc-base-0.0.3.tar.gz

---
+++ packages/mtc-base/mtc-base-0.0.3.tar.gz/mtc-base-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(
+    name="mtc-base",
+    version="0.0.3",
+    description="触控-base",
+    author="KateTsengK",
+    author_email="cu2171505467@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="Auto Script Testing",
+    project_urls={},
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        # 如果你的bin文件在包下，可以这样指定
+        # 'minifw.touch': ['bin/**/*'],
+        # 如果bin文件不在包内，也可以直接指定路径
+        # '': ['bin/*'],
+    },
+    install_requires=[],
+    python_requires=">=3",
+)
