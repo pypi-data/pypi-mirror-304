413a114e3d1d647e8d268befebea1c7fa6d94eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:05:05 +0000

    Add package devart-bigquery-connector/1.2.0/devart_bigquery_connector-1.2.0-cp39-cp39-win_amd64.whl

---
+++ packages/devart-bigquery-connector/devart_bigquery_connector-1.2.0-cp39-cp39-win_amd64.whl/devart/bigquery/__init__.py
@@ -0,0 +1,21 @@
+"""
+Python Connector for Google BigQuery is a connectivity solution for accessing
+the Google BigQuery data warehouse from Python applications. It fully
+implements the Python DB API 2.0 specification. The connector is distributed
+as a wheel package for Windows and Windows Server.
+ 
+GoogleSQL for BigQuery
+
+    The connector fully supports the BigQuery SQL dialects, data types, and query
+    syntax, including functions, operators, and conditional expressions.
+    Examples of supported BigQuery functions:
+    - Aggregate functions
+    - Array functions
+    - Datetime functions
+    - Navigation functions
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/bigquery/
+"""
+from .bigquery import *
