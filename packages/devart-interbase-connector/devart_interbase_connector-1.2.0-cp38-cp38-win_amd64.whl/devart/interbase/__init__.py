29ee07ac174efecc3d8e049ced013354d716d09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:48 +0000

    Add package devart-interbase-connector/1.2.0/devart_interbase_connector-1.2.0-cp38-cp38-win_amd64.whl

---
+++ packages/devart-interbase-connector/devart_interbase_connector-1.2.0-cp38-cp38-win_amd64.whl/devart/interbase/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python Connector for InterBase is a connectivity solution for accessing
+InterBase databases from Python applications. It fully implements the Python
+DB API 2.0 specification. The connector is distributed as a wheel package
+for Windows, macOS, and Linux.
+ 
+Secure communication
+
+    The connector supports the Over-the-Wire (OTW) encryption feature of InterBase
+    to encrypt data during the transmission process.
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/interbase/
+"""
+from .interbase import *
