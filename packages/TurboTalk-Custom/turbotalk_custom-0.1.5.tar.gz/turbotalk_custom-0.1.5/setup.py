cc06a5f00f92780585386c8521e04854496442e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:04:24 +0000

    Add package TurboTalk-Custom/0.1.5/turbotalk_custom-0.1.5.tar.gz

---
+++ packages/TurboTalk-Custom/turbotalk_custom-0.1.5.tar.gz/turbotalk_custom-0.1.5/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TurboTalk_Custom',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'g4f'  # Include any dependencies here
+    ],
+)
