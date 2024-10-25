3934d3646eda02a8e13532cad17e931dce0ccc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:11:04 +0000

    Add package pyfirecrest/2.7.0/pyfirecrest-2.7.0-py3-none-any.whl

---
+++ packages/pyfirecrest/pyfirecrest-2.7.0-py3-none-any.whl/firecrest/__init__.py
@@ -0,0 +1,40 @@
+#
+#  Copyright (c) 2019-2023, ETH Zurich. All rights reserved.
+#
+#  Please, refer to the LICENSE file in the root directory.
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+import sys
+
+
+__version__ = "2.7.0"
+__app_name__ = "firecrest"
+MIN_PYTHON_VERSION = (3, 7, 0)
+
+# Check python version
+if sys.version_info[:3] < MIN_PYTHON_VERSION:
+    sys.stderr.write(
+        "Unsupported Python version: "
+        "Python >= %d.%d.%d is required\n" % MIN_PYTHON_VERSION
+    )
+    sys.exit(1)
+
+from firecrest.BasicClient import Firecrest
+from firecrest.AsyncClient import AsyncFirecrest
+from firecrest.ExternalStorage import ExternalDownload, ExternalUpload, ExternalStorage
+from firecrest.AsyncExternalStorage import (
+    AsyncExternalDownload,
+    AsyncExternalUpload,
+    AsyncExternalStorage,
+)
+from firecrest.Authorization import ClientCredentialsAuth
+from firecrest.FirecrestException import (
+    ClientsCredentialsException,
+    FirecrestException,
+    HeaderException,
+    NotImplementedOnAPIversion,
+    StorageDownloadException,
+    StorageUploadException,
+    UnauthorizedException,
+    UnexpectedStatusException,
+)
