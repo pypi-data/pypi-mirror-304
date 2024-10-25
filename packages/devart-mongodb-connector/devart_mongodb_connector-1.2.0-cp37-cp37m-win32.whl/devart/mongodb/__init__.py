bd5a207e0f02b6f55bac0a84b2c2a62b69d3b2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:51 +0000

    Add package devart-mongodb-connector/1.2.0/devart_mongodb_connector-1.2.0-cp37-cp37m-win32.whl

---
+++ packages/devart-mongodb-connector/devart_mongodb_connector-1.2.0-cp37-cp37m-win32.whl/devart/mongodb/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python Connector for MongoDB is a connectivity solution for accessing MongoDB
+databases from Python applications. It fully implements the Python DB API 2.0
+specification. The connector is distributed as a wheel package for Windows,
+macOS, and Linux.
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/mongodb/
+"""
+from .mongodb import *
