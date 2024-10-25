2da92a7c14c72c73f45acb868d698ad287ddf922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:45 +0000

    Add package nista-library/6.0.0/nista_library-6.0.0-py3-none-any.whl

---
+++ packages/nista-library/nista_library-6.0.0-py3-none-any.whl/data_point_client/models/en_data_point_status.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class EnDataPointStatus(str, Enum):
+    ERROR = "Error"
+    INCREATION = "InCreation"
+    READY = "Ready"
+
+    def __str__(self) -> str:
+        return str(self.value)
