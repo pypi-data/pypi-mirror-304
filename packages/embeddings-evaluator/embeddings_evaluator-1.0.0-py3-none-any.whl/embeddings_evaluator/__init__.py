1eac9c3bec68c498b7c208c6aa624196cd24a282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:47 +0000

    Add package embeddings-evaluator/1.0.0/embeddings_evaluator-1.0.0-py3-none-any.whl

---
+++ packages/embeddings-evaluator/embeddings_evaluator-1.0.0-py3-none-any.whl/embeddings_evaluator/__init__.py
@@ -0,0 +1,14 @@
+from .metrics import (
+    pairwise_similarities,
+    get_all_metrics
+)
+
+from .comparison import (
+    plot_model_comparison
+)
+
+__all__ = [
+    'pairwise_similarities',
+    'get_all_metrics',
+    'plot_model_comparison'
+]
