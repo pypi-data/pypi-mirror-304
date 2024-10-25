0a87c435e13a24bca302e8332fd44db4cc2bca01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:04:33 +0000

    Add package collinear/0.1.9/collinear-0.1.9-py3-none-any.whl

---
+++ packages/collinear/collinear-0.1.9-py3-none-any.whl/collinear/model/types.py
@@ -0,0 +1,18 @@
+import uuid
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class ModelTypeEnum(Enum):
+    openai = "openai"
+    anthropic = "anthropic"
+
+
+class ModelDTO(BaseModel):
+    id: uuid.UUID
+    name: str
+    nickname: str
+    base_url: str
+    api_key: str
+    type: ModelTypeEnum
