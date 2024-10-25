1dc3bb1373b4b8695c80037f832fc9e0f821ede3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:04:01 +0000

    Add package mtc-adb/0.0.2/mtc-adb-0.0.2.tar.gz

---
+++ packages/mtc-adb/mtc-adb-0.0.2.tar.gz/mtc-adb-0.0.2/mtc/adb.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+from adbutils import adb
+
+from mtc.touch import Touch
+
+
+class ADBTouch(Touch):
+    def __init__(self, serial) -> None:
+        """
+        __init__ ADB 操作方式
+
+        Args:
+            serial (str): 设备id
+        """
+        self.__adb = adb.device(serial)
+
+    def click(self, point: Tuple[int, int], duration: int = 100):
+        adb_command = ["input", "touchscreen", "swipe"]
+        x, y = point
+        adb_command.extend([str(x), str(y), str(x), str(y), str(duration)])
+        self.__adb.shell(adb_command)
+
+    def swipe(self, points: List[Tuple[int, int]], duration: int = 500):
+        start_x, start_y = points[0]
+        end_x, end_y = points[-1]
+        self.__adb.swipe(start_x, start_y, end_x, end_y, duration / 1000)
