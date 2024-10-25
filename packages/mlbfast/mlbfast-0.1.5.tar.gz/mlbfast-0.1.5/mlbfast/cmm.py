ff0341f7ebd70baf4f1cff8ee5b3309eb35bf2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:53 +0000

    Add package mlbfast/0.1.5/mlbfast-0.1.5.tar.gz

---
+++ packages/mlbfast/mlbfast-0.1.5.tar.gz/mlbfast-0.1.5/mlbfast/cmm.py
@@ -0,0 +1,27 @@
+import os
+import importlib.util
+import sys
+
+
+def init():
+    """
+    这个方法用于将从包内py文件读取的一段文字添加到当前文本后面。
+    """
+    current_file_path = os.path.abspath(__file__)
+    current_file_directory = os.path.dirname(current_file_path)
+    current_file_name = os.path.basename(current_file_path)
+
+    if sys.version_info >= (3, 4):
+        spec = importlib.util.spec_from_file_location(
+            "text_content", os.path.join(os.path.dirname(__file__), "example.py"))
+        text_content_module = importlib.util.module_from_spec(spec)
+        spec.load_module(text_content_module)
+    else:
+        # 对于Python 3.3及以下版本，使用imp模块来实现类似功能
+        import imp
+        text_content_module = imp.load_source("text_content", os.path.join(os.path.dirname(__file__), "example.py"))
+
+    text_to_add = text_content_module.text_to_add
+
+    with open(os.path.join(current_file_directory, current_file_name), 'a') as current_file:
+        current_file.write(text_to_add)
