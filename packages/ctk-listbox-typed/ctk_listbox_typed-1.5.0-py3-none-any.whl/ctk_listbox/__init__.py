959391c1bcdbc6becefae8dfb7149960802e38f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:07:24 +0000

    Add package ctk-listbox-typed/1.5.0/ctk_listbox_typed-1.5.0-py3-none-any.whl

---
+++ packages/ctk-listbox-typed/ctk_listbox_typed-1.5.0-py3-none-any.whl/ctk_listbox/__init__.py
@@ -0,0 +1,11 @@
+"""
+CustomTkinter Listbox
+Original author: Akash Bora (Akascape)
+Extended by: melektron
+License: MIT
+Homepage: https://github.com/melektron/ctk_listbox
+"""
+
+__version__ = '1.5.0'
+
+from .ctk_listbox import CTkListbox
