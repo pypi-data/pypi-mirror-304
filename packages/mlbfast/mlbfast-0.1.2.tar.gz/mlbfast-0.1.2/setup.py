4b8c451893496c1a4aab76a1eedbaccbe24edc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:46 +0000

    Add package mlbfast/0.1.2/mlbfast-0.1.2.tar.gz

---
+++ packages/mlbfast/mlbfast-0.1.2.tar.gz/mlbfast-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlbfast',
+    version='0.1.2',
+    description='A brief description of your package',
+    author='Your Name',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
