52fa3d76fc3d0fda534bb9fb5c7655a4aeb21948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:05:35 +0000

    Add package p1-bank/0.5.0/p1_bank-0.5.0-py3-none-any.whl

---
+++ packages/p1-bank/p1_bank-0.5.0-py3-none-any.whl/bank/factories.py
@@ -0,0 +1,12 @@
+from builtins import object
+import factory
+
+
+class BankFactory(factory.django.DjangoModelFactory):
+    short_name = factory.Sequence(lambda n: 'BANK X%02d' % n)
+    long_name = factory.Sequence(lambda n: 'PT BANK X%02d' % n)
+    bank_code = factory.Sequence(lambda n: 'X%02d' % n)
+    branch_code = factory.Sequence(lambda n: 'X%03d' % n)
+
+    class Meta(object):
+        model = 'bank.Bank'
