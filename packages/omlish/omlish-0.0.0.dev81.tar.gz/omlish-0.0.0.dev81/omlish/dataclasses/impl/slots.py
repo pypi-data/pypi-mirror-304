63f8f9a26446bf4ddaea1f11c2fee5d4d305c5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:35 +0000

    Add package omlish/0.0.0.dev81/omlish-0.0.0.dev81.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev81.tar.gz/omlish-0.0.0.dev81/omlish/dataclasses/impl/slots.py
@@ -0,0 +1,80 @@
+import dataclasses as dc
+import itertools
+
+
+MISSING = dc.MISSING
+
+
+def _dataclass_getstate(self):
+    return [getattr(self, f.name) for f in dc.fields(self)]
+
+
+def _dataclass_setstate(self, state):
+    for field, value in zip(dc.fields(self), state):
+        object.__setattr__(self, field.name, value)
+
+
+def _get_slots(cls):
+    match cls.__dict__.get('__slots__'):
+        # A class which does not define __slots__ at all is equivalent to a class defining
+        #   __slots__ = ('__dict__', '__weakref__')
+        # `__dictoffset__` and `__weakrefoffset__` can tell us whether the base type has dict/weakref slots, in a way
+        # that works correctly for both Python classes and C extension types. Extension types don't use `__slots__` for
+        # slot creation
+        case None:
+            slots = []
+            if getattr(cls, '__weakrefoffset__', -1) != 0:
+                slots.append('__weakref__')
+            if getattr(cls, '__dictrefoffset__', -1) != 0:
+                slots.append('__dict__')
+            yield from slots
+        case str(slot):
+            yield slot
+        # Slots may be any iterable, but we cannot handle an iterator because it will already be (partially) consumed.
+        case iterable if not hasattr(iterable, '__next__'):
+            yield from iterable
+        case _:
+            raise TypeError(f"Slots of '{cls.__name__}' cannot be determined")
+
+
+def add_slots(
+        cls: type,
+        is_frozen: bool,
+        weakref_slot: bool,
+) -> type:
+    if '__slots__' in cls.__dict__:
+        raise TypeError(f'{cls.__name__} already specifies __slots__')
+
+    cls_dict = dict(cls.__dict__)
+    field_names = tuple(f.name for f in dc.fields(cls))  # noqa
+
+    inherited_slots = set(itertools.chain.from_iterable(map(_get_slots, cls.__mro__[1:-1])))
+
+    cls_dict['__slots__'] = tuple(
+        itertools.filterfalse(
+            inherited_slots.__contains__,
+            itertools.chain(
+                field_names,
+                ('__weakref__',) if weakref_slot else (),
+            ),
+        ),
+    )
+
+    for field_name in field_names:
+        cls_dict.pop(field_name, None)
+
+    cls_dict.pop('__dict__', None)
+    cls_dict.pop('__weakref__', None)
+
+    qualname = getattr(cls, '__qualname__', None)
+    cls = type(cls)(cls.__name__, cls.__bases__, cls_dict)
+    if qualname is not None:
+        cls.__qualname__ = qualname
+
+    if is_frozen:
+        if '__getstate__' not in cls_dict:
+            cls.__getstate__ = _dataclass_getstate  # type: ignore
+        if '__setstate__' not in cls_dict:
+            cls.__setstate__ = _dataclass_setstate  # type: ignore
+
+    return cls
---
+++ packages/omlish/omlish-0.0.0.dev81.tar.gz/omlish-0.0.0.dev81/omlish/formats/json/json.py
@@ -0,0 +1,47 @@
+"""
+TODO:
+ - backend abstr
+ - streaming
+"""
+import functools
+import json as _json
+import typing as ta
+
+
+##
+
+
+dump = _json.dump
+dumps = _json.dumps
+
+detect_encoding = _json.detect_encoding
+
+load = _json.load
+loads = _json.loads
+
+
+##
+
+
+PRETTY_INDENT = 2
+
+PRETTY_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=PRETTY_INDENT,
+)
+
+dump_pretty: ta.Callable[..., bytes] = functools.partial(dump, **PRETTY_KWARGS)  # type: ignore
+dumps_pretty: ta.Callable[..., str] = functools.partial(dumps, **PRETTY_KWARGS)
+
+
+##
+
+
+COMPACT_SEPARATORS = (',', ':')
+
+COMPACT_KWARGS: ta.Mapping[str, ta.Any] = dict(
+    indent=None,
+    separators=COMPACT_SEPARATORS,
+)
+
+dump_compact: ta.Callable[..., bytes] = functools.partial(dump, **COMPACT_KWARGS)  # type: ignore
+dumps_compact: ta.Callable[..., str] = functools.partial(dumps, **COMPACT_KWARGS)
