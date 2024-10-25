43a21ceee97ab34a74c0980aee8a0e0ac53108ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:48 +0000

    Add package aitermis/1.2.4/aitermis-1.2.4.tar.gz

---
+++ packages/aitermis/aitermis-1.2.4.tar.gz/aitermis-1.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aitermis",
+    version="1.2.4",
+    packages=find_packages(),
+    include_package_data=True,
+    description="An ai powered terminal assistant to help you find the command you need!",
+    author="Naya Singhania",
+    author_email="naya.singhania@gmail.com",
+    url="https://github.com/raspberri05/aitermis",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "a=aitermis.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
