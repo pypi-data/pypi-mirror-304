28844986c3d0c25ae4d5219d28243e31e69a4c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:56 +0000

    Add package lion-core/0.5.3/lion_core-0.5.3.tar.gz

---
+++ packages/lion-core/lion_core-0.5.3.tar.gz/lion_core-0.5.3/lion_core/__init__.py
@@ -0,0 +1,31 @@
+"""lion-core."""
+
+import logging
+
+from .log_manager import LogManager
+from .setting import BASE_LION_FIELDS, DEFAULT_LION_ID_CONFIG, LN_UNDEFINED
+from .version import __version__
+
+event_log_manager = LogManager(
+    persist_dir="./data/logs",
+    subfolder="events",
+    file_prefix="event_",
+)
+
+message_log_manager = LogManager(
+    persist_dir="./data/logs",
+    subfolder="messages",
+    file_prefix="message_",
+)
+
+__all__ = [
+    "BASE_LION_FIELDS",
+    "DEFAULT_LION_ID_CONFIG",
+    "LN_UNDEFINED",
+    "LogManager",
+    "event_log_manager",
+    "__version__",
+]
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
