1c1b23b898f52a2fa58b1b35aa708675d8d13d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:27 +0000

    Add package vizly-notebook/0.1.28/vizly_notebook-0.1.28.tar.gz

---
+++ packages/vizly-notebook/vizly_notebook-0.1.28.tar.gz/vizly_notebook-0.1.28/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+# Determine the directory containing setup.py and get its parent directory
+this_directory = os.path.abspath(os.path.dirname(__file__))
+parent_directory = os.path.abspath(os.path.join(this_directory, os.pardir))
+
+# Read the content of README.md from the parent directory
+with open(os.path.join(parent_directory, "README.md"), "r") as fh:
+    long_description = fh.read()
+
+# Replace all mentions of 'public/' with 'vizly-notebook/static/'
+long_description = long_description.replace(
+    'public/', 'vizly_notebook/static/')
+
+setup(
+    name="vizly_notebook",
+    version="0.1.28",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"vizly_notebook": ["static/**/*"]},
+    install_requires=[
+        "jupyter-server>=2.0",
+        "jupyter",
+    ],
+    entry_points={
+        "console_scripts": [
+            "jupyter-vizly-notebook = vizly_notebook:launch_instance",
+            "jupyter-thread = vizly_notebook:launch_instance",
+            "thread = vizly_notebook:launch_instance",
+            "vizly-notebook = vizly_notebook:launch_instance",
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
