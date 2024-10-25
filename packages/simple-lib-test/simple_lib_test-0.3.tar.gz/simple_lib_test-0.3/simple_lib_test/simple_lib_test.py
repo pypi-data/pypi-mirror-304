9a775f77c1975ea0f69cd5a70f0e33d0047c65ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:04:50 +0000

    Add package simple-lib-test/0.3/simple_lib_test-0.3.tar.gz

---
+++ packages/simple-lib-test/simple_lib_test-0.3.tar.gz/simple_lib_test-0.3/simple_lib_test/simple_lib_test.py
@@ -0,0 +1,17 @@
+# simplelib/simplelib.py
+
+def fetch_data(code):
+    if code == 1:
+        return '1'
+    elif code == 2:
+        return '2'
+    elif code == 3:
+        return '3'
+    elif code == 4:
+        return '4'
+    elif code == 5:
+        return '5'
+    elif code == 6:
+        return '6'
+    else:
+        return 'null'
