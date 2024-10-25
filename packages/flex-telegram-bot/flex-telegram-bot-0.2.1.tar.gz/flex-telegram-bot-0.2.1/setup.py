7400d46be5ade5de77d7268efa8ebdc3866b8075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:57 +0000

    Add package flex-telegram-bot/0.2.1/flex-telegram-bot-0.2.1.tar.gz

---
+++ packages/flex-telegram-bot/flex-telegram-bot-0.2.1.tar.gz/flex-telegram-bot-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flex-telegram-bot',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    description='Esnek Türkçe Telegram bot kütüphanesi.',
+    author='FlexN01',
+    author_email='v.flexn01@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
