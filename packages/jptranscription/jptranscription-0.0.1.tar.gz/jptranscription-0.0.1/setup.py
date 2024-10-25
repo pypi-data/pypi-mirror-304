887044939d1ab4e9170d601fa362317d644365a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:04:41 +0000

    Add package jptranscription/0.0.1/jptranscription-0.0.1.tar.gz

---
+++ packages/jptranscription/jptranscription-0.0.1.tar.gz/jptranscription-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='jptranscription',
+   version='0.0.1',
+   description='Transcribes German words into Katakana using IPA',
+   author='Philipp Schmitz',
+   author_email='schmxtz@gmail.com',
+   packages=['jptranscription', 'jptranscription.phonetics', 'jptranscription.transcription'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
