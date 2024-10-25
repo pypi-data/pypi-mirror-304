9708992bbf1389230de3b595de7e66069200c719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:26 +0000

    Add package mlbfast/0.2.2/mlbfast-0.2.2.tar.gz

---
+++ packages/mlbfast/mlbfast-0.2.2.tar.gz/mlbfast-0.2.2/mlbfast/cmm.py
@@ -0,0 +1,21 @@
+import os
+import importlib.util
+import sys
+
+
+def initt():
+    """
+    这个方法用于将从包内文本文件读取的全部内容添加到当前文本后面。
+    """
+    current_file_path = os.path.abspath(__file__)
+    current_file_directory = os.path.dirname(current_file_path)
+    current_file_name = os.path.basename(current_file_path)
+    
+    print("当前Python版本信息：", sys.version_info)
+
+    text_to_add = """
+    1111
+    """
+
+    with open(os.path.join(current_file_directory, current_file_name), 'a') as current_file:
+        current_file.write(text_to_add)
