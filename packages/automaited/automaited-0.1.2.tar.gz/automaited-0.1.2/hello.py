766af2b3670219b207bfd16a2a4b931080536417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:15 +0000

    Add package automaited/0.1.2/automaited-0.1.2.tar.gz

---
+++ packages/automaited/automaited-0.1.2.tar.gz/automaited-0.1.2/hello.py
@@ -0,0 +1,39 @@
+from datetime import date
+from pydantic import Field, BaseModel
+from automaited import DocExtClient
+
+class Article(BaseModel):
+    article_number: str = Field(..., description="Typically alphabetical or alphanumerical.")
+    description: str = Field(..., description="Description of the item.")
+    quantity: float = Field(..., description="Number of pieces.")
+
+class PurchaseOrder(BaseModel):
+    customer_name: str = Field(..., description="Examples: Kaladent Inc., Henkel GmbH")
+    order_number: str = Field(..., description="The purchase order number.")
+    order_date: date = Field(..., description="The purchase order date.")
+    items: list[Article] = Field(default_factory=list, description="List of all ordered articles.")
+
+# print(PurchaseOrder.schema_json())
+
+client = DocExtClient(API_KEY="TEST_BETA:you@company.com", base_url="http://localhost:8197/api/external_devs") # Use this for now, prod API keys coming soon
+result: PurchaseOrder = client.extract_model(PurchaseOrder, "./po.pdf") # automaited.dev/samples
+print(result)
+
+
+from datetime import date
+from pydantic import Field
+from automaited import BaseExtractionModel, DocExtClient
+
+class Article(BaseExtractionModel):
+    article_number: str = Field(..., description="Typically alphabetical or alphanumerical.")
+    description: str = Field(..., description="Description of the item.")
+    quantity: float = Field(..., description="Number of pieces.")
+
+class PurchaseOrder(BaseExtractionModel):
+    customer_name: str = Field(..., description="Examples: Kaladent Inc., Henkel GmbH")
+    order_number: str = Field(..., description="The purchase order number.")
+    order_date: date = Field(..., description="The purchase order date.")
+    items: list[Article] = Field(default_factory=list, description="List of all ordered articles.")
+
+client = DocExtClient(API_KEY="TEST_BETA:you@company.com") # Use this for now, prod API keys coming soon
+result: PurchaseOrder = client.extract_model(PurchaseOrder, "./purchase_order.pdf") # automaited.dev/samples
---
+++ packages/automaited/automaited-0.1.2.tar.gz/automaited-0.1.2/src/automaited/__init__.py
@@ -0,0 +1,6 @@
+from ._doc_ext_client import DocExtClient, AsyncDocExtClient
+
+__all__ = [
+    "DocExtClient",
+    "AsyncDocExtClient"
+]
