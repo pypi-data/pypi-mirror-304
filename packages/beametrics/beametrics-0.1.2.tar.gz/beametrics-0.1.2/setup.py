db936de54b2f8e77cf2b50ea1c9299cd503c0c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:05:37 +0000

    Add package beametrics/0.1.2/beametrics-0.1.2.tar.gz

---
+++ packages/beametrics/beametrics-0.1.2.tar.gz/beametrics-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beametrics",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "apache-beam[gcp]>=2.60.0",
+        "google-cloud-monitoring>=2.22.2",
+        "protobuf>=4.21.6",
+    ],
+)
