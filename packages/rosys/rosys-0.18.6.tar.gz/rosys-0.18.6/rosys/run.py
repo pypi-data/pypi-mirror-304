a541302ef43ccba95047eff17d3afa97486bb7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:04:21 +0000

    Add package rosys/0.18.6/rosys-0.18.6.tar.gz

---
+++ packages/rosys/rosys-0.18.6.tar.gz/rosys-0.18.6/rosys/run.py
@@ -0,0 +1,138 @@
+import asyncio
+import logging
+import os
+import shlex
+import signal
+import subprocess
+import uuid
+from collections.abc import Callable, Generator
+from contextlib import contextmanager
+from functools import wraps
+from pathlib import Path
+from typing import Any
+
+from nicegui import run
+
+from .helpers import is_stopping
+
+running_cpu_bound_processes: list[str] = []  # NOTE is used in tests to advance time slower until computation is done
+running_sh_processes: list[subprocess.Popen] = []
+log = logging.getLogger('rosys.run')
+
+
+async def io_bound(callback: Callable, *args: Any, **kwargs: Any):
+    if is_stopping():
+        return
+    try:
+        return await run.io_bound(callback, *args, **kwargs)
+    except RuntimeError as e:
+        if 'cannot schedule new futures after shutdown' not in str(e):
+            raise
+    except asyncio.exceptions.CancelledError:
+        pass
+
+
+def awaitable(func: Callable) -> Callable:
+    """decorator to wrap a normal function into an asyncio coroutine"""
+    @wraps(func)
+    async def inner(*args, **kwargs):
+        return await io_bound(func, *args, **kwargs)
+    return inner
+
+
+async def cpu_bound(callback: Callable, *args: Any):
+    if is_stopping():
+        return
+    with cpu():
+        try:
+            return await run.cpu_bound(callback, *args)
+        except RuntimeError as e:
+            if 'cannot schedule new futures after shutdown' not in str(e):
+                raise
+        except asyncio.exceptions.CancelledError:
+            pass
+
+
+@contextmanager
+def cpu() -> Generator[None, None, None]:
+    id_ = str(uuid.uuid4())
+    running_cpu_bound_processes.append(id_)
+    try:
+        yield
+    finally:
+        running_cpu_bound_processes.remove(id_)
+
+
+async def sh(command: list[str] | str, *,
+             timeout: float | None = 1,
+             shell: bool = False,
+             working_dir: Path | None = None) -> str:
+    """executes a shell command
+
+    Args:
+        command: a sequence of program arguments as subprocess.Popen requires or full string
+        shell: whether a subshell should be launched (default is `False`, for speed, use `True` if you need file globbing or other features)
+
+    Returns:
+        stdout
+    """
+    def popen() -> str:
+        cmd_list = command if isinstance(command, list) else shlex.split(command)
+        if timeout is not None:
+            cmd_list = ['timeout', '--signal=SIGTERM', str(timeout), *cmd_list]
+        cmd = ' '.join(cmd_list) if shell else cmd_list
+        try:
+            with subprocess.Popen(
+                cmd,
+                cwd=working_dir,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=shell,
+                start_new_session=True,
+            ) as proc:
+                running_sh_processes.append(proc)
+                stdout, stderr = proc.communicate()
+                assert proc.stdout is not None
+                assert proc.stderr is not None
+                proc.stdout.close()
+                proc.stderr.close()
+                _kill(proc)
+                running_sh_processes.remove(proc)
+                return stdout.decode('utf-8') if proc.returncode == 0 else stderr.decode('utf-8')
+        except Exception:
+            log.exception('failed to run command "%s"', cmd)
+            if 'proc' in locals() and proc:
+                _kill(proc)
+            return ''
+
+    if is_stopping():
+        return ''
+    try:
+        return await asyncio.wait_for(io_bound(popen), timeout) or ''
+    except asyncio.TimeoutError:
+        log.warning('Command "%s" timed out after %s seconds.', command, timeout)
+        return ''
+
+
+def _kill(proc: subprocess.Popen) -> None:
+    try:
+        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+        log.info('sent SIGTERM to %s', proc.pid)
+        try:
+            proc.wait(timeout=5)  # wait for 5 seconds
+        except subprocess.TimeoutExpired:
+            os.killpg(os.getpgid(proc.pid), signal.SIGKILL)  # force kill if process didn't terminate
+            log.info('sent SIGKILL to %s', proc.pid)
+            proc.wait()  # ensure the process is reaped
+    except ProcessLookupError:
+        pass
+    except Exception:
+        log.exception('Failed to kill and/or wait for process %s', proc.pid)
+
+
+def tear_down() -> None:
+    log.info('teardown shell processes...')
+    for process in running_sh_processes:
+        _kill(process)
+    running_sh_processes.clear()
+    log.info('teardown complete.')
