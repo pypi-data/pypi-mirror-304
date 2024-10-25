cd0d5f3630548766f4ba6fe800f4873acc8ad4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:37 +0000

    Add package importfa/0.1/importfa-0.1.tar.gz

---
+++ packages/importfa/importfa-0.1.tar.gz/importfa-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="importfa",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="wywzxxz",
+    author_email="wywzxxz@163.com",
+    description="add parent/ancestor folder path into sys.path when import",
+    license="MIT",
+    keywords="sample setuptools development",
+    url="https://github.com/yourusername/mypackage",
+)
