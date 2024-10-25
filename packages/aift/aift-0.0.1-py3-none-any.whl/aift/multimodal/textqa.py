90c5d5422747d10efb86be483259ed8f3c946d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:02 +0000

    Add package aift/0.0.1/aift-0.0.1-py3-none-any.whl

---
+++ packages/aift/aift-0.0.1-py3-none-any.whl/aift/multimodal/textqa.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from aift.setting.setting import get_api_key, PACKAGE_NAME
+
+def generate(instruction:str, 
+    system_prompt:str="You are Pathumma LLM, created by NECTEC. Your are a helpful assistant.", 
+    max_new_tokens:int=256,
+    temperature:float=0.4,
+    return_json:bool=True):
+
+    api_key = get_api_key()
+    headers = {'Apikey':api_key, 'X-lib':PACKAGE_NAME}
+    url = 'https://api.aiforthai.in.th/textqa/completion'
+    payload = {
+        'instruction': instruction,
+        'system_prompt': system_prompt,
+        'max_new_tokens':max_new_tokens,
+        'temperature': temperature,
+        'return_json': return_json
+    }
+
+    res = requests.post(url, headers=headers, data=payload)
+    if return_json == False:
+        return res.json()['content']
+    else:
+        return res.json()
