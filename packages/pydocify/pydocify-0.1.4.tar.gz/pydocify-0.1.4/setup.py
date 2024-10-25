8e74a3fa276f60b6d99df89a58381055c19a3752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:04:21 +0000

    Add package pydocify/0.1.4/pydocify-0.1.4.tar.gz

---
+++ packages/pydocify/pydocify-0.1.4.tar.gz/pydocify-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Read the contents of requirements.txt
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+# Read the contents of README.md for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pydocify",
+    version="0.1.4",
+    description="LLM-based library to auto-generate docstrings for Python scripts and modules.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Rahul Bhoyar",
+    author_email="rahulbhoyaroffice@gmail.com",
+    url="https://github.com/rahulbhoyar1995/pydocify.git",
+    packages=['pydocify'],
+    install_requires=required_packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,  # Ensures additional files like requirements.txt are included
+)
