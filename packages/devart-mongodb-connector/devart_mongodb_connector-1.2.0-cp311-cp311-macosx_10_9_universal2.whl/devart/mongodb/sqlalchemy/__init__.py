2658cf7f0dfb7cc893d6c448ba31ddf42c6f542c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:50 +0000

    Add package devart-mongodb-connector/1.2.0/devart_mongodb_connector-1.2.0-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/devart-mongodb-connector/devart_mongodb_connector-1.2.0-cp311-cp311-macosx_10_9_universal2.whl/devart/mongodb/sqlalchemy/__init__.py
@@ -0,0 +1,33 @@
+from sqlalchemy.dialects import registry
+from sqlalchemy.dialects.sqlite.base import SQLiteDialect
+
+registry.register('devart.mongodb', 'devart.mongodb.sqlalchemy', 'DevartMongoDialect')
+
+class DevartMongoDialect(SQLiteDialect):
+    driver = "devart"
+    name = "mongodb"
+
+    supports_statement_cache = False
+    default_paramstyle = "named"
+
+    @classmethod
+    def import_dbapi(cls):
+        import devart.access
+        access_package = devart.access
+        setattr(access_package, "sqlite_version_info", (3, 39, 2))
+        return access_package
+
+    def get_isolation_level(self, dbapi_connection):
+        return "SERIALIZABLE"
+
+    def _get_table_pragma(self, connection, pragma, table_name, schema=None):
+        quote = self.identifier_preparer.quote_identifier
+        qtable = quote(table_name)
+        statement = "PRAGMA {}({})".format(pragma, qtable)
+        cursor = connection.exec_driver_sql(statement)
+        if not cursor._soft_closed:
+            result = cursor.fetchall()
+        else:
+            result = []
+        if result:
+            return result
