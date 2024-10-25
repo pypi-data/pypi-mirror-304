09fefe17ad41afca3db0b22767aa0b5a079fcaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:37 +0000

    Add package zenonfox/1.6.0/zenonfox-1.6.0-py3-none-any.whl

---
+++ packages/zenonfox/zenonfox-1.6.0-py3-none-any.whl/zenonfox/__init__.py
@@ -0,0 +1,2 @@
+from .base import base
+from .banner import banner
---
+++ packages/zenonfox/zenonfox-1.6.0-py3-none-any.whl/zenonfox/banner.py
@@ -0,0 +1,41 @@
+from colorama import Fore, Style
+
+class banner:
+    def __init__(self):
+        self.blue = Fore.BLUE
+        self.green = Fore.GREEN
+        self.red = Fore.RED
+        self.yellow = Fore.YELLOW
+        self.white = Fore.WHITE
+        self.cyan = Fore.CYAN
+        self.magenta = Fore.MAGENTA
+        self.black = Fore.BLACK
+        self.reset = Style.RESET_ALL
+    def create_banner(self, game_name: str):
+        # Create banner with game name
+        banner = f"""
+        
+\n\n{self.cyan}┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃  {self.white}ZENON FOX {self.red}✚{self.white} MINIAPP BOT {self.cyan}┃         {self.white}COPYRIGHT BY Zennon{self.cyan}
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━┫  {self.white}⟨⟨⟨{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.white}⟩⟩⟩{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀ {self.cyan}┃     {self.blue} GITHUB    {self.cyan}➤ {self.white}github.com/foxZenonn{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠙⠻⢶⣄⡀⠀⠀⠀⢀⣤⠶⠛⠛⡇ {self.cyan}┃
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⠀⠀⣙⣿⣦⣤⣴⣿⣁⠀⠀⣸⠇ {self.cyan}┃      {self.blue}TG OWNER  {self.cyan}➤{self.white} t.me/FoxZenon{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣡⣾⣿⣿⣿⣿⣿⣿⣿⣷⣌⠋⠀ {self.cyan}┃⠀
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣷⣄⡈⢻⣿⡟⢁⣠⣾⣿⣦⠀ {self.cyan}┃      {self.blue}TG GROUP  {self.cyan}➤{self.white} t.me/zzenonFox{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠘⣿⠃⣿⣿⣿⣿⡏⠀ {self.cyan}┃  {self.white}⟨⟨⟨{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.white}⟩⟩⟩{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠈⠛⣰⠿⣆⠛⠁⠀⡀⠀⠀ {self.cyan}┃             {self.white}YOUR PLAY GAME{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣦⠀⠘⠛⠋⠀⣴⣿⠁⠀⠀ {self.cyan}┃  {self.white}⟨⟨⟨{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.white}⟩⟩⟩{self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣿⡇⠀⠀⠀⢸⣿⣏⠀⠀ ⠀{self.cyan}┃      {self.blue}GAME{self.cyan} ➤{self.white} {game_name} {self.cyan}
+┃{self.yellow}⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠀⠀⠀⠾⢿⣿⠀⠀ ⠀{self.cyan}┃            {self.red}●   {self.yellow}●   {self.blue}●   {self.white}●   {self.cyan}
+┃{self.yellow}⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠟⠋⣁⣠⣤⣤⡶⠶⠶⣤⣄⠈⠀⠀ ⠀{self.cyan}┃          ┏━━━━━━━━━━━━━━━┓
+┃{self.yellow}⠀⢰⣿⣿⣮⣉⣉⣉⣤⣴⣶⣿⣿⣋⡥⠄⠀⠀⠀⠀⠉⢻⣄⠀ ⠀{self.cyan}┃          ┃{self.magenta}╔═╗╔═╗╔╗╔╔═╗╔╗╔{self.cyan}┃
+┃{self.yellow}⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣋⣁⣤⣀⣀⣤⣤⣤⣤⣄⣿⡄⠀ {self.cyan}┃          ┃{self.magenta}╔═╝║╣ ║║║║║║║║║{self.cyan}┃
+┃{self.yellow}⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠋⠉⠁⠀⠀⠀⠀⠈⠛⠃⠀ {self.cyan}┃          ┃{self.magenta}╚═╝╚═╝╝╚╝╚═╝╝╚╝{self.cyan}┃
+┃{self.yellow}⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ {self.cyan}┃          ┗━━━━━━━━━━━━━━━┛
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛  {self.white}⟨⟨⟨{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.blue}━{self.red}━{self.white}━{self.yellow}{self.red}━{self.white}━{self.yellow}━{self.green}━{self.magenta}━{self.black}━{self.white}⟩⟩⟩{self.cyan}
+  {self.reset}
+"""
+        return banner
+
+banner = banner()
