6478b34a16b60251a5b733c34689304a5abdd5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:56 +0000

    Add package devart-mysql-connector/1.2.0/devart_mysql_connector-1.2.0-cp37-cp37m-manylinux_2_34_x86_64.whl

---
+++ packages/devart-mysql-connector/devart_mysql_connector-1.2.0-cp37-cp37m-manylinux_2_34_x86_64.whl/devart/mysql/__init__.py
@@ -0,0 +1,25 @@
+"""
+Python Connector for MySQL is a connectivity solution for accessing MySQL
+databases from Python applications. It fully implements the Python DB API 2.0
+specification. The connector is distributed as a wheel package for Windows,
+macOS, and Linux.
+ 
+Direct connection
+
+    The connector enables you to establish a direct connection to MySQL from
+    a Python application via TCP/IP, eliminating the need for the database client
+    library. A direct connection increases the speed of data transmission between
+    the application and MySQL database server. It also streamlines the deployment
+    process since you don't have to distribute any client libraries with
+    the application.
+ 
+Secure communication
+
+    The connector supports encrypted communications using SSL/TLS, SSH tunneling,
+    and HTTP/HTTPS tunneling.
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/mysql/
+"""
+from .mysql import *
