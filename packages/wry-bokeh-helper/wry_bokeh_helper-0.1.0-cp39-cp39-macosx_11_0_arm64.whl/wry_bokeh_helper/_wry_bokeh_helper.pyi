03f2a713dd05268c5253a704dbbdc9d7c1e232da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:04:52 +0000

    Add package wry-bokeh-helper/0.1.0/wry_bokeh_helper-0.1.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/wry-bokeh-helper/wry_bokeh_helper-0.1.0-cp39-cp39-macosx_11_0_arm64.whl/wry_bokeh_helper/_wry_bokeh_helper.pyi
@@ -0,0 +1,7 @@
+from typing import Literal
+
+ResourceType = Literal["cdn", "local"]
+
+def render_bokeh(json: str, resource: tuple[ResourceType, str] | None = None) -> str:
+    """Render Bokeh JSON to a PNG or SVG image."""
+    ...
