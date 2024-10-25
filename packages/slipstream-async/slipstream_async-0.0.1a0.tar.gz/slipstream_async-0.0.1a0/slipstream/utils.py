9e6545b874c9925bf9cb5ee4bcccc812f2a35203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:26 +0000

    Add package slipstream-async/0.0.1a0/slipstream_async-0.0.1a0.tar.gz

---
+++ packages/slipstream-async/slipstream_async-0.0.1a0.tar.gz/slipstream_async-0.0.1a0/slipstream/utils.py
@@ -0,0 +1,42 @@
+"""Slipstream utilities."""
+
+import logging
+from asyncio import iscoroutinefunction
+from inspect import signature
+from typing import Any, Dict
+
+logger = logging.getLogger(__name__)
+
+
+def iscoroutinecallable(o: Any) -> bool:
+    """Check whether function is coroutine."""
+    return iscoroutinefunction(o) or (
+        hasattr(o, '__call__')
+        and iscoroutinefunction(o.__call__)
+    )
+
+
+def get_params_names(o: Any):
+    """Return function parameters."""
+    parameters = signature(o).parameters.values()
+    return getattr(parameters, 'mapping')
+
+
+class Singleton(type):
+    """Maintain a single instance of a class."""
+
+    _instances: Dict['Singleton', Any] = {}
+
+    def __init__(cls, name, bases, dct):
+        """Perform checks before instantiation."""
+        if '__update__' not in dct:
+            raise TypeError('Expected __update__.')
+
+    def __call__(cls, *args, **kwargs):
+        """Apply metaclass singleton action."""
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        instance = cls._instances[cls]
+        instance.__update__(*args, **kwargs)
+        return instance
