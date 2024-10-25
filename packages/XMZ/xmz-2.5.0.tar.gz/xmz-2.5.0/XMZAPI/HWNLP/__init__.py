f2914769edc7b83c6fdf3e9a68e5fe216152154c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:33 +0000

    Add package XMZ/2.5.0/xmz-2.5.0.tar.gz

---
+++ packages/XMZ/xmz-2.5.0.tar.gz/xmz-2.5.0/XMZAPI/HWNLP/__init__.py
@@ -0,0 +1,6 @@
+from XMZAPI.HWNLP.mt_service import MTService
+from XMZAPI.HWNLP.nlg_service import NLGService
+from XMZAPI.HWNLP.nlpf_service import NLPFService
+from XMZAPI.HWNLP.nlu_service import NLUService
+
+__all__ = ["MTService", "NLGService", "NLPFService", "NLUService"]
