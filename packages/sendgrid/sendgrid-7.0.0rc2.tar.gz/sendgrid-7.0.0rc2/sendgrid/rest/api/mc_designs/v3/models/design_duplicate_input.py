38ece64f530945fd369b174b6adb17d1cd841e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:07:08 +0000

    Add package sendgrid/7.0.0rc2/sendgrid-7.0.0rc2.tar.gz

---
+++ packages/sendgrid/sendgrid-7.0.0rc2.tar.gz/sendgrid-7.0.0rc2/sendgrid/rest/api/mc_designs/v3/models/design_duplicate_input.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from typing import Optional, Dict, List
+from sendgrid.converters.serialize import to_serializable, from_serializable
+from enum import Enum
+from sendgrid.rest.api.mc_designs.v3.models.editor import Editor
+
+
+
+class DesignDuplicateInput:
+    def __init__(
+            self,
+            name: Optional[str]=None,
+            editor: Optional[Editor]=None
+    ):
+        self.name=name
+        self.editor=editor
+
+    def to_dict(self):
+        return {key: to_serializable(value)
+            for key, value in {
+            "name": self.name,
+            "editor": self.editor
+            }.items() if value is not None}
+
+    @classmethod
+    def from_dict(cls, data):
+        return from_serializable(data, cls)
+
+    @staticmethod
+    def generate_model(payload: Dict[str, object]):
+        return DesignDuplicateInput(
+            name=payload.get('name'),
+            editor=payload.get('editor')
+        ) 
+
---
+++ packages/sendgrid/sendgrid-7.0.0rc2.tar.gz/sendgrid-7.0.0rc2/sendgrid/rest/api/mc_singlesends/v3/models/list_single_send500_response.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from typing import Optional, Dict, List
+from sendgrid.converters.serialize import to_serializable, from_serializable
+from enum import Enum
+from sendgrid.rest.api.mc_singlesends.v3.models.list_single_send500_response_errors_inner import ListSingleSend500ResponseErrorsInner
+
+
+
+class ListSingleSend500Response:
+    def __init__(
+            self,
+            errors: Optional[List[ListSingleSend500ResponseErrorsInner]]=None
+    ):
+        self.errors=errors
+
+    def to_dict(self):
+        return {key: to_serializable(value)
+            for key, value in {
+            "errors": self.errors
+            }.items() if value is not None}
+
+    @classmethod
+    def from_dict(cls, data):
+        return from_serializable(data, cls)
+
+    @staticmethod
+    def generate_model(payload: Dict[str, object]):
+        return ListSingleSend500Response(
+            errors=payload.get('errors')
+        ) 
+
