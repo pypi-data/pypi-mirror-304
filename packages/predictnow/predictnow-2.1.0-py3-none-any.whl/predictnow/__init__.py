8d1ead1fd06c6af2872fcf00fd59bd5546e2eb7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:41 +0000

    Add package predictnow/2.1.0/predictnow-2.1.0-py3-none-any.whl

---
+++ packages/predictnow/predictnow-2.1.0-py3-none-any.whl/predictnow/__init__.py
@@ -0,0 +1,12 @@
+cert = {  
+  "type": "service_account",  
+  "project_id": "predictnow-2a4bf",  
+  "private_key_id": "ac2089ba9f843bb0e65ab9f3a11d2aaa607452ef",  
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDT3redbyzrk012\nkyKoQ6RouOfn1LbRRc6wVUz1XJnzabuREtg0MviDcskPVFgQGMez0mWLCdU8D9EU\nUb3NSOAdrYDjFAjkuOsTmnc2xarwNHXqe7JqaVWj746N9JKvWSKzpZpSlpjzZESY\n8CNHqv8aujmuVTUaIlukX5p3AVy0LEonOwFI9Jx1weBh/dDbqTR7x1CIpz4m9ZPJ\nb051BE/DPVWPQScXJC54aj95CHBp39MzcoAEek7sd/mouW044lhm84RO288CMsiw\nuRYuC/Z2XMY/c2dsibU7ccxEf9/vHvAwqeDzUyns5HECda/Wfu6f6XR/ALbCnpjJ\nQTid23jbAgMBAAECggEABjgJBF+pXjd8/u2uBxRdNLm2DOcAgBS1eWriOb2AbgBS\nLMhJ0lZrVv29NcGlssCXdAlz85MpUCcCsxRR0ZRsXP1PxYU8Bu1VRnEH7h1P5Cxr\nXaOd3A5qy3ISVCEJVojzl3mqlaFYAfo/zTbB7QOcPf5DXSuhGxBGclpgCqgz6Wc/\nB0IcsFkeImRW+7qWn3Jlu4le2pwdP/i+vTnzMBFw85pH6/rJFQ0B/vr5ulHRKpN4\nHCdu/5K2KKsRmD3A+XV2idoDhE5yFTFp0NpXT6X5UaoB4JxbJIUCBma0sUNau8/R\nzn+I9aFDjMcPjuPkJPD50DJHyRs0NrUcy/MPel/RSQKBgQD6hTqMIDUEkM9ZD98S\nnuwS8+DfYZkzF5lsBmxTGpCtz6l9YsGnFWWHyU2m/GHaK+C7X9k832hKuiOh1f0O\nc3QjjYflrlEVCNYVB15K9y4LIelMUbhOb0nuvVIJw/4DemSMCUxVR/EH4hvc5tRT\nJFcNWd17pYNnWbCAX304i1ZYLQKBgQDYgRF0WARrd35BlAHGFZEDdrgfcc4jtmtd\nZX+T3BNp61cL0XdrFz9JwIEnVJDUj0SDcvSIyefG+Vdeb5b9mFjeqSwXglaB85Ld\nJZXb8Hly5aQzLmuuoZT2WMO+x6dvLQN1R1kBHLFV5KQ/YyrIsJZS8dbrlDNz+V3F\nOvwxCDByJwKBgAr8cLbYarrKhu1G7udoJL4oB8KECy3caRP9rB5w2ctlge9Ht2pW\nW996inc6NihVKEPPTj2HJpGwXAOq2LAbjApLGJyP6mpbutetPQtYj6JZWFvKcaSu\n6LUZhFF9UNRiN6qUdc5/myhXWBV14fXLcC3btQTJG1wa6MyYY2tpYN35AoGAAYeP\n5wGELnouoyn3hT27w4mALYCxUyFDyuofp5zDalkmSUgWjuQgJ1Y701pOl6HXexJB\n1gGPKKG0U094gpwj7JbHX/ckw1BENlnAoEgno38p1M68JgSwN7lS0LM6ZJet0ZNK\ndq6MNi4iVuxNbhOJTFa3DP0EDAqz9eO/ylzjcrsCgYAM/zrpz9MNO5SEp/M7bzcl\nnKsF5krcLLeFVEMkH6t4zBh9VcXGOzjawHA5qXoEMyLZpmDJ9LOhHHXy2Le21QQO\nNZ1W/VXsNWtCcO5wM1lgKZrqqKuYh7lmkmA45yIfrJA3mp0i9IlMqXii0ZmcDazx\nFZDmNmHPz4i5eO+Wgnr3vg==\n-----END PRIVATE KEY-----\n",  
+  "client_email": "firebase-adminsdk-t8p41@predictnow-2a4bf.iam.gserviceaccount.com",  
+  "client_id": "113900166751454816008",  
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",  
+  "token_uri": "https://oauth2.googleapis.com/token",  
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",  
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t8p41%40predictnow-2a4bf.iam.gserviceaccount.com"
+}
