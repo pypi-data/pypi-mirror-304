b0adbedc2f76a30a8469d8d82c5f3abb6804c922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:30 +0000

    Add package XMZ/2.40/xmz-2.40.tar.gz

---
+++ packages/XMZ/xmz-2.40.tar.gz/xmz-2.40/setup.py
@@ -0,0 +1,39 @@
+# from setuptools import setup, find_packages
+#
+#
+# setup(
+#     name="XMZ",
+#     version="2.3.0",
+#     description="米粥SDK",
+#     long_description=open("README.md", encoding="utf-8").read(),
+#     long_description_content_type="text/markdown",
+#     author="祁筱欣",
+#     author_email="mzapi@x.mizhoubaobei.top",
+#     url="https://github.com/xiaomizhoubaobei/XMZAPI",
+#     packages=find_packages(),
+#     install_requires=["huaweicloudsdkcore", "huaweicloudsdknlp"],
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+# )
+from setuptools import setup, find_packages
+
+setup(
+    name="XMZ",
+    version="2.40",
+    description="米粥SDK",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="祁筱欣",
+    author_email="mzapi@x.mizhoubaobei.top",
+    url="https://github.com/xiaomizhoubaobei/XMZAPI",
+    packages=find_packages(where="XMZAPI"),
+    install_requires=["huaweicloudsdkcore", "huaweicloudsdknlp"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
