0c3286693920eddbdd95214ccf85fae59f056e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:30 +0000

    Add package boost-flask/0.0.1.dev6/boost_flask-0.0.1.dev6.tar.gz

---
+++ packages/boost-flask/boost_flask-0.0.1.dev6.tar.gz/boost_flask-0.0.1.dev6/example/your_project/view/hello.py
@@ -0,0 +1,19 @@
+__author__ = 'deadblue'
+
+from typing import Any
+
+from boostflask.view import JsonView
+
+
+class HelloView(JsonView):
+
+    def __init__(self) -> None:
+        super().__init__(
+            url_rule='/hello',
+            methods=('GET', )
+        )
+    
+    def handle(self, name: str = 'world') -> Any:
+        return {
+            'hello': name
+        }
