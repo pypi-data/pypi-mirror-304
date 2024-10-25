04866bdd1bb7faae9a738f9365c82c05d3926959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:26 +0000

    Add package solace-pubsubplus/1.9.0/solace_pubsubplus-1.9.0-py36-none-manylinux_2_12_x86_64.whl

---
+++ packages/solace-pubsubplus/solace_pubsubplus-1.9.0-py36-none-manylinux_2_12_x86_64.whl/solace/messaging/core/_dynamic_version.py
@@ -0,0 +1,25 @@
+# pubsubplus-python-client
+#
+# Copyright 2021-2024 Solace Corporation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file is automatically overwritten at build time through a setup.py
+# extension with a dynamically generated file containing the version
+# information, which that conforms to this same interface.
+
+# Several values in this file are replaced at build time with the correct values.
+# pylint: disable=missing-module-docstring
+SOLACE_PYTHON_CLIENT_VERSION = "1.9.0"
+SOLACE_PYTHON_CLIENT_COMMIT_DATE = "Oct 23 2024 16:15:20"
+SOLACE_PYTHON_CLIENT_BUILD_REV = "d5e816a671ce436f03288fd36730abcabd0ea661"
