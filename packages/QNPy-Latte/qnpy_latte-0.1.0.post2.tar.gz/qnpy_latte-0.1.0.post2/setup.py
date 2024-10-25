c807b00c75e565f61ccf43f651ba758d5c1f0996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:47 +0000

    Add package QNPy-Latte/0.1.0.post2/qnpy_latte-0.1.0.post2.tar.gz

---
+++ packages/QNPy-Latte/qnpy_latte-0.1.0.post2.tar.gz/qnpy_latte-0.1.0.post2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='QNPy_Latte',
+    version='0.1.0.post2',
+    packages=find_packages(),
+    install_requires=[
+        'astropy>=5.3.4',
+        'cycler>=0.11.0',
+        'dill>=0.3.4',
+        'matplotlib>=3.5.2',
+        'MiniSom>=2.3.1',
+        'numpy>=1.21.5',
+        'pandas>=1.4.4',
+        'plotly>=5.9.0',
+        'scikit_learn>=1.0.2',
+        'scipy>=1.9.1',
+        'seaborn>=0.12.2',
+        'torch>=1.13.0',
+        'tqdm>=4.64.1',
+        'tslearn>=0.6.2'],
+    authors = 'Aman Raju',
+    author_email='rajuaman@gmail.com',
+    description='Latent ATTEntive Neural Processes for Quasar Light Curves with parametric recovery',
+    long_description= long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rajuaman1/QNPy_Latte',  # Project's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
