07f88d8f7e507c215e83b4089e5d42adeed24ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:24 +0000

    Add package faradaysec/5.8.0/faradaysec-5.8.0.tar.gz

---
+++ packages/faradaysec/faradaysec-5.8.0.tar.gz/faradaysec-5.8.0/faraday/__init__.py
@@ -0,0 +1,8 @@
+"""
+Faraday Penetration Test IDE
+Copyright (C) 2013  Infobyte LLC (https://faradaysec.com/)
+See the file 'doc/LICENSE' for the license information
+"""
+
+__version__ = '5.8.0'
+__license_version__ = __version__
