1fcf854732fb34373ec2b55f40ee61794551cd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:04:18 +0000

    Add package beautifull-terminal/1.8.1/beautifull_terminal-1.8.1-py3-none-any.whl

---
+++ packages/beautifull-terminal/beautifull_terminal-1.8.1-py3-none-any.whl/beautifull_terminal/__init__.py
@@ -0,0 +1,3 @@
+from beautifull_terminal.terminal import BeautifulTerminal
+from beautifull_terminal.update.requests import notify_user_if_update_available
+notify_user_if_update_available('beautifull-terminal')
