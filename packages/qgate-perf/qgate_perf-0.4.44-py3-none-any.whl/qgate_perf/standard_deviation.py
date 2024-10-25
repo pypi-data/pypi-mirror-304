f8e056eb4b8cb5d02ce7f244b96226028e802938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:12 +0000

    Add package qgate-perf/0.4.44/qgate_perf-0.4.44-py3-none-any.whl

---
+++ packages/qgate-perf/qgate_perf-0.4.44-py3-none-any.whl/qgate_perf/standard_deviation.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+
+class StandardDeviation:
+    """
+    Welford's alg for incrementally calculation of standard deviation
+
+    NOTE: You can see the same outputs as in functions 'STDEVPA' and 'STDEV.P' in Microsoft Excel
+    """
+    def __init__(self, ddof = 1):
+        self.ddof, self.n, self.mean, self.M2 = ddof, 0, 0.0, 0.0
+
+    def include(self, data):
+        self.n += 1
+        self.delta = data - self.mean
+        self.mean += self.delta / self.n
+        self.M2 += self.delta * (data - self.mean)
+
+    @property
+    def variance(self):
+        return self.M2 / (self.n - self.ddof) if self.n > 0 else 0
+
+    @property
+    def std(self):
+        """ Standard deviation """
+        return sqrt(self.variance)
