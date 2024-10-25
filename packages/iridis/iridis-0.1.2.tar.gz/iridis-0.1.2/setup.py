4ccd59fd1e1c8b0248fa9e39403ef71498dbdaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:30 +0000

    Add package iridis/0.1.2/iridis-0.1.2.tar.gz

---
+++ packages/iridis/iridis-0.1.2.tar.gz/iridis-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iridis",
+    version="0.1.2",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Tobias Filgas",
+    author_email="tobias.filgas@student.mgvsetin.cz",
+    url="https://github.com/realtobi999/Python_PrettyPrint",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    python_requires=">=3.6",
+)
