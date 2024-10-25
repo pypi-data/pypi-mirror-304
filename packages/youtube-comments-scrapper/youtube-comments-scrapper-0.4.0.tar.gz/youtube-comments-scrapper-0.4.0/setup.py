974bb84eda2313f3ea3b6a09f459923aef8d4ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:45 +0000

    Add package youtube-comments-scrapper/0.4.0/youtube-comments-scrapper-0.4.0.tar.gz

---
+++ packages/youtube-comments-scrapper/youtube-comments-scrapper-0.4.0.tar.gz/youtube-comments-scrapper-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='youtube-comments-scrapper',  # Replace with your package name
+    version='0.4.0',
+    description='A Python package to scrape YouTube comments using Selenium and BeautifulSoup',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Abhishek Kumar',
+    author_email='abhiop.dev@gmail.com',
+    url='https://github.com/Abhi9868/Youtube-Comment-Scraper',  # Replace with your repository URL
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'selenium>=4.25.0',
+        'webdriver-manager>=4.0.2',
+        'beautifulsoup4>=4.12.3',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
