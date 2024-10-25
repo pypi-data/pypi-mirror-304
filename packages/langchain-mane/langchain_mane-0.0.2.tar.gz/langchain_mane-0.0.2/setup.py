e4fac621a27c564d6024520ad204e35d2f7fac7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 03:10:06 +0000

    Add package langchain-mane/0.0.2/langchain_mane-0.0.2.tar.gz

---
+++ packages/langchain-mane/langchain_mane-0.0.2.tar.gz/langchain_mane-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="langchain-mane",
+    version="0.0.2",
+    description="PYPI",
+    author="mane5880",
+    author_email="gjtjqkr5880@gmail.com",
+    url="https://github.com/teddylee777/teddynote",
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=["langchain", "mane", "python langchain", "langchain util"],
+    python_requires=">=3.8",
+    package_data={},
+    zip_safe=False,
+)
