c4262b66ecc74c888d840c9964253b42639a16e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 17:05:40 +0000

    Add package crealand/1.0.12/crealand-1.0.12.tar.gz

---
+++ packages/crealand/crealand-1.0.12.tar.gz/crealand-1.0.12/crealand/__init__.py
@@ -0,0 +1,56 @@
+__version__ = '1.0.12'
+
+from ._apis import (
+    ai as AI, 
+    detect as Detect, 
+    event as Event, 
+    interactive as Interactive, 
+    object as Object
+)
+
+from ._apis.interactive import (
+    Dialogue, 
+    HelpPanel, 
+    TaskPanel, 
+    Speak, 
+    Interactive
+)
+
+from ._apis.media import (
+    Sound, 
+    Video, 
+    Image
+)
+
+from ._apis.sensor import (
+    Ultrasonic, 
+    Auditory, 
+    Visual, 
+    Temperature, 
+    Humidity, 
+    Gravity
+)
+
+from ._utils.logger_setup import (
+    enable_logger, 
+    disable_logger, 
+    setup_logger
+)
+
+def __initialize():
+    try:
+        import threading
+        from ._core.websocket.websocket_client import ws_connect, get_ws_client, init_callback
+
+        logger = setup_logger()
+        threading.Thread(target=ws_connect, args=()).start()
+        while True:
+            ws_client = get_ws_client()
+            if ws_client and ws_client.session_id:
+                break
+        init_callback()
+    except Exception as e:
+        logger.error(f'An error occurred: {e}')
+
+__initialize()
+
