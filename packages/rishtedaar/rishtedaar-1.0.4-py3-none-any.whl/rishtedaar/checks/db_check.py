cca6fe6db18f3126dba5cb4ea9bd2cdc563b6275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:21 +0000

    Add package rishtedaar/1.0.4/rishtedaar-1.0.4-py3-none-any.whl

---
+++ packages/rishtedaar/rishtedaar-1.0.4-py3-none-any.whl/rishtedaar/checks/db_check.py
@@ -0,0 +1,10 @@
+from django.db import connection
+
+def check_db():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+            cursor.fetchall()  # Fetch results to ensure the query executes
+        return True, "Database connection is healthy"
+    except Exception as e:
+        return False, f"Database error: {str(e)}"
