7420f0b1b98dc5b5e234d5b5a269c2f58e27c439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:05:21 +0000

    Add package pyStreamRead/3.0.3/pystreamread-3.0.3.tar.gz

---
+++ packages/pyStreamRead/pystreamread-3.0.3.tar.gz/pystreamread-3.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyStreamRead',
+    version='3.0.3',
+    keywords=("rtsp", "rtmp", "video", "stream", "python", "video_stream"),
+    license="MIT Licence",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["ffmpeg-python == 0.2.0", "future == 1.0.0", "numpy == 2.1.2", "opencv-python == 4.10.0.84"],
+    # Metadata
+    author='rui.li',
+    author_email='jiangsulirui@gmail.com',
+    description="video stream reading",
+    long_description="video stream reading for rtsp or rtmp, not use opencv video capture but use ffmpeg",
+    # url='https://github.com/your_username/your_package',
+    python_requires=">=3.12",
+)
