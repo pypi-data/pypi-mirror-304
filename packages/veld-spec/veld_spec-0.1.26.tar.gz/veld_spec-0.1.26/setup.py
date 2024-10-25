5bcd9c5963e561b8b9effdefb49cf37cf805011a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:08 +0000

    Add package veld-spec/0.1.26/veld_spec-0.1.26.tar.gz

---
+++ packages/veld-spec/veld_spec-0.1.26.tar.gz/veld_spec-0.1.26/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="veld_spec",
+    version="0.1.26",
+    packages=["veld_spec_f"],
+    author="Stefan Resch",
+    author_email="stefan.resch@oeaw.ac.at",
+    description="VELD specification",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/acdh-oeaw/VELD_spec",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "PyYAML>=6.0.2",
+    ],
+    include_package_data=True,
+    # package_data={"veld_spec_f": ["README.md"]},
+)
+
---
+++ packages/veld-spec/veld_spec-0.1.26.tar.gz/veld_spec-0.1.26/tests/test.py
@@ -0,0 +1,42 @@
+import unittest
+
+import jsonschema
+import yaml
+
+from veld_spec import validate
+
+
+TEST_FILES_PATH = "tests/veld_yaml_files/"
+
+
+class TestVeldMetadata(unittest.TestCase):
+    
+    def test_chain_barebone_valid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_barebone_valid.yaml")
+        print(result[1])
+        self.assertTrue(result[0])
+    
+    def test_chain_barebone_invalid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_barebone_invalid.yaml")
+        print(result[1])
+        self.assertFalse(result[0])
+    
+    def test_code_barebone_valid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/code_barebone_valid.yaml")
+        print(result[1])
+        self.assertTrue(result[0])
+    
+    def test_code_barebone_invalid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/code_barebone_invalid.yaml")
+        print(result[1])
+        self.assertFalse(result[0])
+    
+    def test_data_barebone_valid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/data_barebone_valid.yaml")
+        print(result[1])
+        self.assertTrue(result[0])
+    
+    def test_data_barebone_invalid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/data_barebone_invalid.yaml")
+        print(result[1])
+        self.assertFalse(result[0])
