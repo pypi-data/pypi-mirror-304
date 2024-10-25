4ebef43bc014e90748ed6df04cbbfd15193e29d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:57 +0000

    Add package OpenZerox/0.1.16/openzerox-0.1.16.tar.gz

---
+++ packages/OpenZerox/openzerox-0.1.16.tar.gz/openzerox-0.1.16/OpenZerox/__init__.py
@@ -0,0 +1,4 @@
+from .openzerox import OpenZeroxPipeline
+from .utils import resize_image, encode_image_to_base64, create_image_message
+
+__all__ = ['OpenZeroxPipeline', 'resize_image', 'encode_image_to_base64', 'create_image_message']
