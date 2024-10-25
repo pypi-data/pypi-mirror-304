abf52cbe4dfa106e342e296cee45d520925075dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 14:04:20 +0000

    Add package cobang/0.1.0/cobang-0.1.0-py3-none-any.whl

---
+++ packages/cobang/cobang-0.1.0-py3-none-any.whl/hello.py
@@ -0,0 +1,8 @@
+def main():
+    print(
+        "Please install CoBang from your OS package manager. More detail at https://github.com/hongquan/CoBang/"
+    )
+
+
+if __name__ == "__main__":
+    main()
