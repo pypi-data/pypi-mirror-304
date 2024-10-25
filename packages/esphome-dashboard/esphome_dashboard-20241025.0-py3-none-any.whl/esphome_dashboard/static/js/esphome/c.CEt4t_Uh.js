82ff5d06ed3bf61b22750b942d9597db30eaee60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:04:36 +0000

    Add package esphome-dashboard/20241025.0/esphome_dashboard-20241025.0-py3-none-any.whl

---
+++ packages/esphome-dashboard/esphome_dashboard-20241025.0-py3-none-any.whl/esphome_dashboard/static/js/esphome/c.CEt4t_Uh.js
@@ -0,0 +1,10 @@
+import{b as e,c as o,n as s,s as i,x as t}from"./index-DxTkLEpY.js";import"./c.CG9mkB5F.js";import"./c.CrXQjN4Z.js";let a=class extends i{render(){return t`
+      <esphome-process-dialog
+        .heading=${`Clean MQTT discovery topics for ${this.configuration}`}
+        .type=${"clean-mqtt"}
+        .spawnParams=${{configuration:this.configuration}}
+        @closed=${this._handleClose}
+      >
+      </esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};e([o()],a.prototype,"configuration",void 0),a=e([s("esphome-clean-mqtt-dialog")],a);
+//# sourceMappingURL=c.CEt4t_Uh.js.map
