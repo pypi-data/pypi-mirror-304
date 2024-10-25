83c0365f0e486373959f049a794b944e49498b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 07:04:26 +0000

    Add package devart-snowflake-connector/1.2.0/devart_snowflake_connector-1.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/devart-snowflake-connector/devart_snowflake_connector-1.2.0-cp311-cp311-win_amd64.whl/devart/snowflake/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python Connector for Snowflake is a connectivity solution for accessing
+the Snowflake data cloud from Python applications. It fully implements
+the Python DB API 2.0 specification. The connector is distributed as a wheel
+package for Windows and Windows Server.
+
+Version: 1.2.0 
+
+Homepage: https://www.devart.com/python/snowflake/
+"""
+from .snowflake import *
