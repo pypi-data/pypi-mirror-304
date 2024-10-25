3e90a731642cb2cb10aa06446000b2791877df1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 05:04:02 +0000

    Add package pharmpy-core/1.3.0/pharmpy_core-1.3.0-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-1.3.0-py3-none-any.whl/pharmpy/reporting/conf.py
@@ -0,0 +1,16 @@
+# See issue #20
+# extensions = ['altair.sphinxext.altairplot']
+extensions = [
+    'pharmpy.reporting.altairplot',
+    'pharmpy.reporting.jupyter_sphinx',
+]  # TODO: Remove altairplot
+html_sidebars = {
+    '**': ['localtoc.html'],
+}
+html_static_path = ['.']
+html_css_files = [
+    'custom.css',
+]
+html_theme_options = {'body_max_width': 'none'}
+html_title = 'Results'
+master_doc = 'results'
