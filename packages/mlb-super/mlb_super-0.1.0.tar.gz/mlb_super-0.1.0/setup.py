cb31062570d35c4760cd4e74074b731ac6e8a1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:05:47 +0000

    Add package mlb-super/0.1.0/mlb_super-0.1.0.tar.gz

---
+++ packages/mlb-super/mlb_super-0.1.0.tar.gz/mlb_super-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlb_super',
+    version='0.1.0',
+    description='A brief description of your package',
+    author='Your Name',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
