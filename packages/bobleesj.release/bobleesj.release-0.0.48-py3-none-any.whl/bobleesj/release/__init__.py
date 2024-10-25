65b9acb58a17ba291583836c3a6bfdec48b6a00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 04:05:08 +0000

    Add package bobleesj.release/0.0.48/bobleesj.release-0.0.48-py3-none-any.whl

---
+++ packages/bobleesj.release/bobleesj.release-0.0.48-py3-none-any.whl/bobleesj/release/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/bobleesj-test-org/bobleesj.release/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""Python package for doing science."""
+
+# package version
+from bobleesj.release.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
