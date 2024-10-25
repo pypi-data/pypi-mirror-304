814325de6b3f8b4a329df7b6856e5fcd1daa8dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 15:05:40 +0000

    Add package imkernel/1.7.2/imkernel-1.7.2-py3-none-any.whl

---
+++ packages/imkernel/imkernel-1.7.2-py3-none-any.whl/imkernel/__init__.py
@@ -0,0 +1,4 @@
+from .core import Model
+from .utils.tree_utils import tree_to_df, df_to_tree, create_tree, tree_to_json, tree_ele, find_node_by_tag, sys_add_branch_nodes
+# from .utils.tree_utils import tree_sys
+from .core.model_new import tree_sys
