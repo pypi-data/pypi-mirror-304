6f6b0e5dff2cc5034279ae9582d95d0023df726d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 16:04:56 +0000

    Add package Arbeitszeitrechner/0.3/arbeitszeitrechner-0.3.tar.gz

---
+++ packages/Arbeitszeitrechner/arbeitszeitrechner-0.3.tar.gz/arbeitszeitrechner-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Arbeitszeitrechner',
+    version='0.3',
+    description='Arbeitszeitrechner',
+    author='Julian Egger, Marc Liesch',
+    author_email='julian.elias.egger@outlook.com',
+    packages=find_packages(),
+    install_requires=[
+        'customtkinter'
+    ],
+    entry_points={
+        'gui_scripts': [
+            'Arbeitszeitrechner=LucaZeitRechner.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
