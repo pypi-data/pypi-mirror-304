da370fbd17aec03ff73caa0a20c727626d6d811d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-25 06:05:06 +0000

    Add package veld-spec/0.1.32/veld_spec-0.1.32.tar.gz

---
+++ packages/veld-spec/veld_spec-0.1.32.tar.gz/veld_spec-0.1.32/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="veld_spec",
+    version="0.1.32",
+    author="Stefan Resch",
+    author_email="stefan.resch@oeaw.ac.at",
+    description="VELD specification",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/acdh-oeaw/VELD_spec",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["PyYAML>=6.0.2"],
+    packages=["veld_spec"],
+    package_data={"veld_spec": ["README.md"]},
+)
