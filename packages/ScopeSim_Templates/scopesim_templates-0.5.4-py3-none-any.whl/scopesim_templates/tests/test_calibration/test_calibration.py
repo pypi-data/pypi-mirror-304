065ba3f5e3c1f073eaca80247b0520ef931bd473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:33 +0000

    Add package ScopeSim_Templates/0.5.4/scopesim_templates-0.5.4-py3-none-any.whl

---
+++ packages/ScopeSim_Templates/scopesim_templates-0.5.4-py3-none-any.whl/scopesim_templates/tests/test_calibration/test_calibration.py
@@ -0,0 +1,15 @@
+from astropy.table import Table
+from synphot import SourceSpectrum
+
+from scopesim_templates import calibration
+from scopesim_templates.rc import Source
+
+
+class TestEmptySky:
+    def test_empty_sky_returns_source_object(self):
+        sky = calibration.calibration.empty_sky()
+        assert isinstance(sky, Source)
+        assert isinstance(sky.spectra[0], SourceSpectrum)
+        assert (isinstance(getattr(sky.fields[0], "field", sky.fields[0]), Table)
+                or isinstance(sky.fields[0], Table))
+        assert sky.fields[0]["ref"][0] == 0
