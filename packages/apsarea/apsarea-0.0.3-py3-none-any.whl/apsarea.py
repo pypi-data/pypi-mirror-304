8def332d251580c91815d19268072c0420b743b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 03:07:29 +0000

    Add package apsarea/0.0.3/apsarea-0.0.3-py3-none-any.whl

---
+++ packages/apsarea/apsarea-0.0.3-py3-none-any.whl/apsarea.py
@@ -0,0 +1,13 @@
+def square(a):
+    return a**2
+def rectangle(l,w):
+    return l*w
+def circle(r):
+    return 3.14*r**2
+def trapezoid(a,b,h):
+    t=1/2 *(a + b)*h
+    return t
+def ellipse(a,b):
+    a=3.14*a*b
+    return a
+
