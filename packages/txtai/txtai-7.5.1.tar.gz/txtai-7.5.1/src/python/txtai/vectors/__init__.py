063007fd60b32a99d1737a19b9c72bce104f1beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 03:06:57 +0000

    Add package txtai/7.5.1/txtai-7.5.1.tar.gz

---
+++ packages/txtai/txtai-7.5.1.tar.gz/txtai-7.5.1/src/python/txtai/vectors/__init__.py
@@ -0,0 +1,11 @@
+"""
+Vectors import
+"""
+
+from .base import Vectors
+from .external import External
+from .factory import VectorsFactory
+from .huggingface import HFVectors
+from .litellm import LiteLLM
+from .llama import LlamaCpp
+from .words import WordVectors
