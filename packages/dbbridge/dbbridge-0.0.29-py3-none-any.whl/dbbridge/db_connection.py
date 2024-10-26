403ca6512c5a01b4df426edb85c04c7400b2ee99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:38 +0000

    Add package dbbridge/0.0.29/dbbridge-0.0.29-py3-none-any.whl

---
+++ packages/dbbridge/dbbridge-0.0.29-py3-none-any.whl/dbbridge/db_connection.py
@@ -0,0 +1,15 @@
+from .db_manager import create_db_connection
+
+def get_db_connection(service, **config):
+    """
+    Get a database connection based on the provided configuration.
+
+    :param service: Type of the database ('aws' or 'local').
+    :param config: Configuration parameters for the database connection.
+    :return: A database connection object.
+    """
+    try:
+        return create_db_connection(service=service, **config)
+    except Exception as e:
+        print(f"Error getting DB connection: {e}")
+        return
