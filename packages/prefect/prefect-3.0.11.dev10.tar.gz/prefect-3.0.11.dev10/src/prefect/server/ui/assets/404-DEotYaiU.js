33883ba261dc243fbf27c25bb222f5c6fe617358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:05:03 +0000

    Add package prefect/3.0.11.dev10/prefect-3.0.11.dev10.tar.gz

---
+++ packages/prefect/prefect-3.0.11.dev10.tar.gz/prefect-3.0.11.dev10/src/prefect/server/ui/assets/404-DEotYaiU.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,l as a,o as t,de as r}from"./index-CibiDZQv.js";import{u as s}from"./usePageTitle-DYAZWMh1.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(t(),o(a(r)))}});export{m as default};
+//# sourceMappingURL=404-DEotYaiU.js.map
