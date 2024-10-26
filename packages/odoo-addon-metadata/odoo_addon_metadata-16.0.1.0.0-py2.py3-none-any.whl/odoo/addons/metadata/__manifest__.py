52d5da1ffb0af56069eba9062bad606f8b07a0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:45 +0000

    Add package odoo-addon-metadata/16.0.1.0.0/odoo_addon_metadata-16.0.1.0.0-py2.py3-none-any.whl

---
+++ packages/odoo-addon-metadata/odoo_addon_metadata-16.0.1.0.0-py2.py3-none-any.whl/odoo/addons/metadata/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "metadata",
+
+    'summary': """
+        Save custom metadata on any model""",
+
+    'author': "Coopdevs Treball SCCL",
+    'website': "",
+
+    'category': 'server',
+    'version': '16.0.1.0.0',
+    "license": "AGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+}
