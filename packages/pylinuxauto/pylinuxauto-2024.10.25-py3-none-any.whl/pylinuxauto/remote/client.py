00caced521d1520e54974f18621c015b11cd7c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:04:44 +0000

    Add package pylinuxauto/2024.10.25/pylinuxauto-2024.10.25-py3-none-any.whl

---
+++ packages/pylinuxauto/pylinuxauto-2024.10.25-py3-none-any.whl/pylinuxauto/remote/client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-License-Identifier: GPL-2.0-only
+
+from xmlrpc.client import ServerProxy
+from xmlrpc.client import Fault
+
+from funnylog2 import logger
+from pylinuxauto.config import config
+from pylinuxauto.remote.guard import guard_rpc
+
+
+@guard_rpc
+def client(
+        user: str,
+        ip: str,
+        password: str,
+        auto_restart: bool = False,
+) -> ServerProxy:
+    try:
+        server = ServerProxy(f"http://{ip}:{config.RPC_PORT}", allow_none=True)
+        server.ping()
+        logger.info(f"Successfully connected to {ip}:{config.RPC_PORT}")
+        return server
+    except Fault as e:
+        logger.error(f"XML-RPC Fault: {e.faultString}")
+        raise RuntimeError(f"XML-RPC Fault: {e.faultString}")
+    except ConnectionError as e:
+        logger.error(f"Connection Error: {e}")
+        raise RuntimeError(f"Connection Error: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}")
