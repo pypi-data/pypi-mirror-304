b9c2761d5dfd9fbbcb373d6173b12b150a6df332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:26 +0000

    Add package trendyol-crawler/0.9/trendyol-crawler-0.9.tar.gz

---
+++ packages/trendyol-crawler/trendyol-crawler-0.9.tar.gz/trendyol-crawler-0.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trendyol-crawler",
+    version="0.9",
+    packages=find_packages(),
+    description="TRENDYOL CRAWLER",
+    author="Esat",
+    author_email="esatyilmaz3500@gmail.com",
+)
