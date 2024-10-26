d4916a3fe398ff575dffa83fe7125db1c285d16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:06:09 +0000

    Add package bauplan/0.0.3a240/bauplan-0.0.3a240-py3-none-macosx_11_0_arm64.whl

---
+++ packages/bauplan/bauplan-0.0.3a240-py3-none-macosx_11_0_arm64.whl/bauplan/_protobufs/runner_service_pb2.pyi
@@ -0,0 +1,15 @@
+import bauplan_pb2 as _bauplan_pb2
+import execution_plan_pb2 as _execution_plan_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class FlightStream(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class QueryRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
