293b74d98af0901f8d0c77d563d36d389821b658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:06:48 +0000

    Add package boto3-stubs-full/1.35.49/boto3_stubs_full-1.35.49-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.49-py3-none-any.whl/mypy_boto3_mediapackagev2/__init__.py
@@ -0,0 +1,41 @@
+"""
+Main interface for mediapackagev2 service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_mediapackagev2 import (
+        Client,
+        ListChannelGroupsPaginator,
+        ListChannelsPaginator,
+        ListOriginEndpointsPaginator,
+        Mediapackagev2Client,
+    )
+
+    session = Session()
+    client: Mediapackagev2Client = session.client("mediapackagev2")
+
+    list_channel_groups_paginator: ListChannelGroupsPaginator = client.get_paginator("list_channel_groups")
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_origin_endpoints_paginator: ListOriginEndpointsPaginator = client.get_paginator("list_origin_endpoints")
+    ```
+"""
+
+from .client import Mediapackagev2Client
+from .paginator import (
+    ListChannelGroupsPaginator,
+    ListChannelsPaginator,
+    ListOriginEndpointsPaginator,
+)
+
+Client = Mediapackagev2Client
+
+
+__all__ = (
+    "Client",
+    "ListChannelGroupsPaginator",
+    "ListChannelsPaginator",
+    "ListOriginEndpointsPaginator",
+    "Mediapackagev2Client",
+)
