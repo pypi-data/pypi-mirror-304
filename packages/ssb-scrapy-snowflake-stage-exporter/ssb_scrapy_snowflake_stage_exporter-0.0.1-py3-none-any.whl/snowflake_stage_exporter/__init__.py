9f36876783558d38756da26b1924b7e4949b7432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:26 +0000

    Add package ssb-scrapy-snowflake-stage-exporter/0.0.1/ssb_scrapy_snowflake_stage_exporter-0.0.1-py3-none-any.whl

---
+++ packages/ssb-scrapy-snowflake-stage-exporter/ssb_scrapy_snowflake_stage_exporter-0.0.1-py3-none-any.whl/snowflake_stage_exporter/__init__.py
@@ -0,0 +1,262 @@
+import json
+import logging
+import os
+import time
+import typing
+from contextlib import AbstractContextManager
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+from typing import Any, Collection, Dict, Iterable, Literal, Set
+
+import snowflake.connector  # type: ignore
+from itemadapter import ItemAdapter  # type: ignore
+
+from .utils import chunk, normalize_identifier
+
+logger = logging.getLogger(__name__)
+
+
+ExporterEvent = Literal["finish", "flush", "never"]
+
+
+class SnowflakeStageExporterError(Exception):
+    pass
+
+
+class SnowflakeStageExporter(AbstractContextManager):
+
+    typemap = {
+        dict: "OBJECT",
+        list: "ARRAY",
+        int: "NUMBER",
+        float: "FLOAT",
+        bool: "BOOLEAN",
+        str: "VARCHAR",
+    }
+    multitype = "VARIANT"
+
+    def __init__(
+        self,
+        user: str,
+        password: str,
+        account: str,
+        table_path: str,
+        connection_kwargs: Dict[str, Any] = None,
+        stage: str = "@~",
+        stage_path: str = "{table_path}/{instance_ms}_{batch_n}.jl",
+        max_file_size: int = 2**30,
+        predefined_column_types: Dict[str, Dict[str, str]] = None,
+        ignore_unexpected_fields: bool = True,
+        allow_varying_value_types: bool = False,
+        create_tables_on: ExporterEvent = "finish",
+        populate_tables_on: ExporterEvent = "finish",
+        clear_stage_on: ExporterEvent = "never",
+    ):
+        self._max_file_size = max_file_size
+        self._table_path = table_path
+        self._stage = stage
+        self._stage_path = stage_path
+        self._create_tables_on = create_tables_on
+        self._populate_tables_on = populate_tables_on
+        self._clear_stage_on = clear_stage_on
+        self._allow_varying_value_types = allow_varying_value_types
+        self._predefined_column_types = predefined_column_types or {}
+        self._ignore_unexpected_fields = ignore_unexpected_fields
+        self._instance_ms = int(time.time() * 1000)
+
+        exporter_events = typing.get_args(ExporterEvent)
+        for attr in ("create_tables_on", "populate_tables_on", "clear_stage_on"):
+            val = getattr(self, "_" + attr)
+            if val not in exporter_events:
+                raise ValueError(f"unexpected value ({val!r}) for {attr!r}, expected one of {exporter_events!r}")
+
+        if any(not v for v in self._predefined_column_types.values()):
+            raise ValueError("values of 'predefined_column_types' can't be empty")
+
+        if not stage.startswith("@") or "/" in stage:
+            raise ValueError('"stage" must begin with @ and have no path elements')
+
+        self.conn = self.create_connection(
+            user=user,
+            password=password,
+            account=account,
+            client_session_keep_alive=True,
+            **(connection_kwargs or {}),
+        )
+        self._table_buffers: Dict = {}  # {table_path: tmp_buffer_file}
+        self._exported_fpaths: Dict = {}  # {table_path: [exported_fpath_in_stage, ...]}
+        self._recorded_coltypes: Dict = {}  # {table_path: {field: set({cls, ...})}}
+        self._created_tables_for: Set[str] = set()  # {table_path, ...}
+        self._copied_fpaths: Set[str] = set()  # {copied_fpath_in_stage, ...}
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def create_connection(self, **connection_kwargs):
+        return snowflake.connector.connect(**connection_kwargs)
+
+    def close(self) -> None:
+        self.conn.close()
+
+    def export_item(self, item: Any, **extra_params) -> str:
+        item_dict: Dict = ItemAdapter(item).asdict()
+
+        table_path = self.table_for_item(item_dict, **extra_params)
+        if table_path not in self._table_buffers:
+            logger.info("Creating buffer for %r", table_path)
+            self._table_buffers[table_path] = NamedTemporaryFile("wb")
+
+        line = json.dumps(item_dict) + "\n"
+        self._table_buffers[table_path].write(line.encode("utf8"))
+        if self._should_flush_buffer_for(table_path):
+            self.flush_table_buffer(table_path)
+
+        self._record_field_types(table_path, item_dict)
+
+        return table_path
+
+    def _should_flush_buffer_for(self, table_path: str) -> bool:
+        return self._table_buffers[table_path].tell() >= self._max_file_size
+
+    def _record_field_types(self, table_path: str, item_dict: Dict) -> None:
+        if table_path in self._created_tables_for:
+            return
+        recorded = self._recorded_coltypes.setdefault(table_path, {})
+        for k, v in item_dict.items():
+            if v is not None:
+                recorded.setdefault(k, set()).add(self.typemap[type(v)])
+
+    def table_for_item(self, item_dict: Dict, **extra_params) -> str:
+        table_path = self._table_path.format(**extra_params, item=item_dict)
+        table_path = ".".join(map(normalize_identifier, table_path.split(".", 2)))
+        return table_path
+
+    def fpath_for_table(self, table_path: str, batch_n: int) -> str:
+        return self._stage_path.format(
+            table_path=table_path,
+            instance_ms=self._instance_ms,
+            batch_n=batch_n,
+        )
+
+    def flush_table_buffer(self, table_path: str) -> None:
+        # HACK: there is no straightforward way to provide a filename via PUT
+        # statement, so as a workaround we symlink our file into a temporary
+        # directory with the desired filename.
+        tmp_file = self._table_buffers[table_path]
+        tmp_file.flush()
+        batch_n = len(self._exported_fpaths.setdefault(table_path, [])) + 1
+        with TemporaryDirectory() as tempdir:
+            fpath = self.fpath_for_table(table_path, batch_n)
+            logger.info("Flushing buffer of %r to %r (batch %d)", table_path, fpath, batch_n)
+            prefix, fname = fpath.rsplit("/", 1)
+            symlink_path = os.path.join(tempdir, fname)
+            os.symlink(tmp_file.name, symlink_path)
+            fpath = self._put_file(symlink_path, self._stage, prefix)
+        tmp_file.close()
+        self._table_buffers.pop(table_path)
+        self._exported_fpaths[table_path].append(fpath)
+
+        if self._create_tables_on == "flush":
+            self.create_table(table_path)
+        if self._populate_tables_on == "flush":
+            self.populate_table(table_path)
+        if self._clear_stage_on == "flush":
+            self.clear_stage([fpath])
+
+    def _put_file(self, fpath: str, stage: str, prefix: str) -> str:
+        """Uploads a file to stage and returns resulted fpath in stage.
+        Due to compression the resulting filename in stage can have an extra prefix (e.g. ".gz").
+        """
+        cursor = self.conn.cursor().execute(f"PUT 'file://{fpath}' '{stage}/{prefix}'")
+        return prefix + "/" + cursor.fetchone()[1]
+
+    def flush_all_table_buffers(self) -> None:
+        for table_path in list(self._table_buffers):
+            self.flush_table_buffer(table_path)
+
+    def get_column_types(self, table_path: str) -> Dict[str, str]:
+        columns = self._predefined_column_types.get(table_path, {})
+        if columns and self._ignore_unexpected_fields:
+            return columns
+
+        recorded_coltypes = self._recorded_coltypes.get(table_path, {})
+        for colname, coltypes in recorded_coltypes.items():
+            if colname in columns:
+                continue
+            if len(coltypes) == 1:
+                coltype = next(iter(coltypes))
+            else:
+                if self._allow_varying_value_types:
+                    coltype = self.multitype
+                else:
+                    logger.error(
+                        "Table %r, field %r: skipping. Multiple coltypes encountered on values: %r.",
+                        table_path,
+                        colname,
+                        coltypes,
+                    )
+                    continue
+            columns[colname] = coltype
+
+        if not columns:
+            raise SnowflakeStageExporterError(
+                f"no valid columns are found for table {table_path!r}, recorded columns: {recorded_coltypes!r}",
+            )
+
+        return columns
+
+    def create_table(self, table_path: str) -> None:
+        if table_path in self._created_tables_for:
+            return
+        logger.info("Creating table %r", table_path)
+        cols = ", ".join(
+            [f"{normalize_identifier(col)} {coltype}" for col, coltype in self.get_column_types(table_path).items()]
+        )
+        self.conn.cursor().execute(f"CREATE TABLE IF NOT EXISTS {table_path} ({cols})")
+        self._created_tables_for.add(table_path)
+
+    def populate_table(self, table_path: str) -> None:
+        logger.info("Populating table %r", table_path)
+        new_fpaths = [fp for fp in self._exported_fpaths[table_path] if fp not in self._copied_fpaths]
+        coltypes = self.get_column_types(table_path)
+        cols = ", ".join(map(normalize_identifier, coltypes))
+        safe_fields = [field.replace('"', '""') for field in coltypes]
+        json_select = ", ".join(f'$1:"{field}"' for field in safe_fields)
+        for fpaths in chunk(new_fpaths, 1000):
+            fpaths_expr = ", ".join(f"'{fpath}'" for fpath in fpaths)
+            self.conn.cursor().execute(
+                f"""
+                COPY INTO {table_path} ({cols})
+                    FROM (SELECT {json_select} FROM {self._stage})
+                    FILE_FORMAT = (TYPE = JSON)
+                    FILES = ({fpaths_expr})
+                """
+            )
+            self._copied_fpaths.update(fpaths)
+
+    def create_all_tables(self) -> None:
+        for table_path in self._exported_fpaths:
+            self.create_table(table_path)
+
+    def populate_all_tables(self) -> None:
+        for table_path in self._exported_fpaths:
+            self.populate_table(table_path)
+
+    def clear_stage(self, fpaths: Iterable[str] = None) -> None:
+        if fpaths is None:
+            fpaths = [fpath for fpaths in self._exported_fpaths.values() for fpath in fpaths]
+            count_msg = f"{len(fpaths)} (all)"
+        else:
+            fpaths = list(fpaths or [])
+            count_msg = str(len(fpaths))
+        logger.info("Removing %s staged files", count_msg)
+        if fpaths:
+            self.conn.cursor().executemany("REMOVE %s", [(f"{self._stage}/{fpath}",) for fpath in fpaths])
+
+    def finish_export(self) -> None:
+        self.flush_all_table_buffers()
+        if self._create_tables_on == "finish":
+            self.create_all_tables()
+        if self._populate_tables_on == "finish":
+            self.populate_all_tables()
+        if self._clear_stage_on == "finish":
+            self.clear_stage()
---
+++ packages/ssb-scrapy-snowflake-stage-exporter/ssb_scrapy_snowflake_stage_exporter-0.0.1-py3-none-any.whl/snowflake_stage_exporter/scrapy_pipeline.py
@@ -0,0 +1,66 @@
+import inspect
+import logging
+import os
+
+from . import SnowflakeStageExporter
+
+logger = logging.getLogger(__name__)
+
+
+class SnowflakePipeline:
+
+    stage_exporter_class = SnowflakeStageExporter
+    setting_getters = {
+        "connection_kwargs": "getdict",
+        "max_file_size": "getint",
+        "predefined_column_types": "getdict",
+        "ignore_unexpected_fields": "getbool",
+        "allow_varying_value_types": "getbool",
+    }
+
+    def __init__(self, settings):
+        job = os.environ.get("SHUB_JOBKEY", "local")
+        kwargs = self._turn_settings_to_kwargs(settings)
+        kwargs.setdefault(
+            "stage_path", "{table_path}/" + job + "/{instance_ms}_{batch_n}.jl"
+        )
+        self.stage_exporter = self.stage_exporter_class(**kwargs)
+
+    def _turn_settings_to_kwargs(self, settings):
+        stage_exporter_kwargs = {}
+        for param_name, param_spec in inspect.signature(
+            self.stage_exporter_class
+        ).parameters.items():
+            settings_key = "SNOWFLAKE_" + param_name.upper()
+            if settings_key not in settings:
+                if param_spec.default == param_spec.empty:
+                    raise ValueError(f"Setting {settings_key!r} is required")
+                continue
+            stage_exporter_kwargs[param_name] = getattr(
+                settings, self.setting_getters.get(param_name, "get")
+            )(settings_key)
+        return stage_exporter_kwargs
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        from scrapy import signals  # type: ignore # pylint: disable=import-error
+
+        instance = cls(crawler.settings)
+        crawler.signals.connect(instance.on_spider_close, signal=signals.spider_closed)
+        return instance
+
+    def process_item(self, item, spider):
+        try:
+            self.stage_exporter.export_item(
+                item, spider=spider, item_type_name=type(item).__name__
+            )
+        except Exception:  # pylint: disable=broad-except
+            logger.exception("Failed to export item: %r", str(item)[:1000])
+        return item
+
+    def on_spider_close(self, spider, reason):
+        # pylint: disable=unused-argument
+        self.stage_exporter.flush_all_table_buffers()
+        if reason == "finished":
+            self.stage_exporter.finish_export()
+        self.stage_exporter.close()
