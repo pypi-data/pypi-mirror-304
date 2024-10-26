0ac1c224c961d7ac9e7c7ef01f34d2f23a7fa16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:49 +0000

    Add package kairo/0.5.0/kairo-0.5.0.tar.gz

---
+++ packages/kairo/kairo-0.5.0.tar.gz/kairo-0.5.0/src/kairo/utils/api.py
@@ -0,0 +1,3 @@
+def dummy_sum(a: int, b: int) -> int:
+    """Dummy function to test the API."""
+    return a + b
---
+++ packages/kairo/kairo-0.5.0.tar.gz/kairo-0.5.0/tests/unit/api_test.py
@@ -0,0 +1,5 @@
+from kairo.utils.api import dummy_sum
+
+
+def test_dummy_sum():
+    assert dummy_sum(1, 2) == 3
