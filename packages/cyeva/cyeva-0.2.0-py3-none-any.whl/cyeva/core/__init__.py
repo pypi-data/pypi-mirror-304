f0949ac8587ffc9bd27976864397cda8434cf1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:04:51 +0000

    Add package cyeva/0.2.0/cyeva-0.2.0-py3-none-any.whl

---
+++ packages/cyeva/cyeva-0.2.0-py3-none-any.whl/cyeva/core/__init__.py
@@ -0,0 +1,6 @@
+from .base import *  # noqa
+from .precip import *  # noqa
+from .statistic import *  # noqa
+from .temp import *  # noqa
+from .weather_code import *  # noqa
+from .wind import *  # noqa
