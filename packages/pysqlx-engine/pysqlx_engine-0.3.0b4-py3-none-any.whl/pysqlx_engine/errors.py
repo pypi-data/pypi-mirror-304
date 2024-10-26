ef220929b01d208aff70ca02809ffee8de8a6630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:05:53 +0000

    Add package pysqlx-engine/0.3.0b4/pysqlx_engine-0.3.0b4-py3-none-any.whl

---
+++ packages/pysqlx-engine/pysqlx_engine-0.3.0b4-py3-none-any.whl/pysqlx_engine/errors.py
@@ -0,0 +1,16 @@
+# new pool errors
+from ._core.errors import AlreadyConnectedError as AlreadyConnectedError
+from ._core.errors import ConnectError as ConnectError
+from ._core.errors import ExecuteError as ExecuteError
+from ._core.errors import IsoLevelError as IsoLevelError
+from ._core.errors import NotConnectedError as NotConnectedError
+from ._core.errors import ParameterInvalidJsonValueError as ParameterInvalidJsonValueError
+from ._core.errors import ParameterInvalidProviderError as ParameterInvalidProviderError
+from ._core.errors import ParameterInvalidValueError as ParameterInvalidValueError
+from ._core.errors import PoolAlreadyStartedError as PoolAlreadyStartedError
+from ._core.errors import PoolClosedError as PoolClosedError
+from ._core.errors import PoolTimeoutError as PoolTimeoutError
+from ._core.errors import PySQLXError as PySQLXError
+from ._core.errors import QueryError as QueryError
+from ._core.errors import RawCmdError as RawCmdError
+from ._core.errors import StartTransactionError as StartTransactionError
