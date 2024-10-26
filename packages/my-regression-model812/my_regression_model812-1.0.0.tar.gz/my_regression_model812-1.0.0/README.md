8deb46a8db035b365e500d12aa25092604cba27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:47 +0000

    Add package my-regression-model812/1.0.0/my_regression_model812-1.0.0.tar.gz

---
+++ packages/my-regression-model812/my_regression_model812-1.0.0.tar.gz/my_regression_model812-1.0.0/README.md
@@ -0,0 +1,8 @@
+# my_regression_model
+
+This package provides a simple linear regression model.
+
+## Installation
+
+```bash
+pip install .
---
+++ packages/my-regression-model812/my_regression_model812-1.0.0.tar.gz/my_regression_model812-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='my_regression_model812',
+   version='1.0.0',
+   author='iZALLA',
+   author_email='i.zalla@esi-sba.dz',
+   packages=find_packages(),
+   url='http://pypi.python.org/pypi/ROCKET/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   #long_description=open('README.md').read(),
+   #long_description_content_type="text/markdown",
+   #install_requires=['math']
+)
