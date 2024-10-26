607bd64028fc6b4aeefdcc021e7e14540a26952d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:46 +0000

    Add package eywa-client/0.2.0/eywa_client-0.2.0.tar.gz

---
+++ packages/eywa-client/eywa_client-0.2.0.tar.gz/eywa_client-0.2.0/src/eywa/__init__.py
@@ -0,0 +1,11 @@
+"""
+Eywa package - manages async operations and more.
+"""
+
+__author__ = "Robert Gersak"
+__email__ = "r.gersak@gmail.com"
+__license__ = "MIT"
+__status__ = "Development"
+__version__ = "0.2.0"
+
+from .eywa import *
