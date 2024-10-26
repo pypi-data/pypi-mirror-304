f9c29ade2679cb8262ec77efe2c4a86d1d6a6289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 03:07:24 +0000

    Add package ctCustomRedis/0.1.1/ctCustomRedis-0.1.1.tar.gz

---
+++ packages/ctCustomRedis/ctCustomRedis-0.1.1.tar.gz/ctCustomRedis-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ctCustomRedis',
+    packages=find_packages(include=['ctCustomRedis']),
+    version='0.1.1',
+    description='Common custom library for Redis',
+    author='credenti',
+)
