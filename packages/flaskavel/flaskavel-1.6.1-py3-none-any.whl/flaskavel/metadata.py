e056045ece28cb343e482d6ef66bc8af46830f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:06:54 +0000

    Add package flaskavel/1.6.1/flaskavel-1.6.1-py3-none-any.whl

---
+++ packages/flaskavel/flaskavel-1.6.1-py3-none-any.whl/flaskavel/metadata.py
@@ -0,0 +1,8 @@
+NAME="flaskavel"
+VERSION="1.6.1"
+AUTHOR="Raul Mauricio Uñate Castro"
+AUTHOR_EMAIL="raulmauriciounate@gmail.com"
+DESCRIPTION="Like in Laravel but with Python"
+SKELETON="https://github.com/flaskavel/skeleton"
+FRAMEWORK="https://github.com/flaskavel/framework"
+PYTHON_REQUIRES='>=3.12'
