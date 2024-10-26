d41f8adb4c5c5bdeeaa0b599a2b600ffd248c25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:38 +0000

    Add package gpustack/0.3.1/gpustack-0.3.1-py3-none-win_amd64.whl

---
+++ packages/gpustack/gpustack-0.3.1-py3-none-win_amd64.whl/gpustack/logging.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+import logging
+
+
+def setup_logging(debug: bool = False):
+    level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+    logging.Formatter.formatTime = (
+        lambda self, record, datefmt=None: datetime.fromtimestamp(
+            record.created, timezone.utc
+        )
+        .astimezone()
+        .isoformat(timespec="seconds")
+    )
+
+    # Disable third-party loggers
+    third_party_logger_names = [
+        "httpcore.connection",
+        "httpcore.http11",
+        "httpcore.proxy",
+        "httpx",
+        "asyncio",
+        "aiosqlite",
+        "urllib3.connectionpool",
+        "multipart.multipart",
+        "apscheduler.scheduler",
+        "apscheduler.executors.default",
+        "tzlocal",
+        "alembic.runtime.migration",
+        "filelock",
+    ]
+
+    for logger_name in third_party_logger_names:
+        logger = logging.getLogger(logger_name)
+        logger.disabled = True
