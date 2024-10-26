6aa28f452297c10160790513aae6568e215c8078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:06:00 +0000

    Add package preliz/0.11.0/preliz-0.11.0-py3-none-any.whl

---
+++ packages/preliz/preliz-0.11.0-py3-none-any.whl/preliz/__init__.py
@@ -0,0 +1,40 @@
+"""
+PreliZ.
+
+Tools to help you pick a prior
+"""
+import logging
+from os import path as os_path
+
+from matplotlib import rcParams
+from matplotlib import style
+
+from .distributions import *
+from .predictive import *
+from .ppls import *
+from .unidimensional import *
+from .multidimensional import *
+
+
+__version__ = "0.11.0"
+
+_log = logging.getLogger("preliz")
+
+if not logging.root.handlers:
+    _log.setLevel(logging.INFO)
+    if len(_log.handlers) == 0:
+        handler = logging.StreamHandler()
+        _log.addHandler(handler)
+
+# Allow legend outside plot in maxent to be included when saving a figure
+# We may want to make this more explicit by having preliz.rcParams
+rcParams["savefig.bbox"] = "tight"
+
+
+# add PreliZ's styles to matplotlib's styles
+_preliz_style_path = os_path.join(os_path.dirname(__file__), "styles")
+style.core.USER_LIBRARY_PATHS.append(_preliz_style_path)
+style.core.reload_library()
+
+# clean namespace
+del logging, os_path, rcParams, _preliz_style_path, _log
---
+++ packages/preliz/preliz-0.11.0-py3-none-any.whl/preliz/unidimensional/__init__.py
@@ -0,0 +1,19 @@
+from .beta_mode import beta_mode
+from .combine import combine
+from .combine_roulette import combine_roulette
+from .maxent import maxent
+from .mle import mle
+from .quartile import quartile
+from .quartile_int import quartile_int
+from .roulette import Roulette
+
+__all__ = [
+    "beta_mode",
+    "combine",
+    "combine_roulette",
+    "maxent",
+    "mle",
+    "Roulette",
+    "quartile",
+    "quartile_int",
+]
