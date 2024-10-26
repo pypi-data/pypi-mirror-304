a261f346cbb32e8650c33f5f3adf4dc59f3488d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 07:04:59 +0000

    Add package api-sl/0.0.2/api_sl-0.0.2.tar.gz

---
+++ packages/api-sl/api_sl-0.0.2.tar.gz/api_sl-0.0.2/README.md
@@ -0,0 +1,2 @@
+# api-sl
+SL Api
---
+++ packages/api-sl/api_sl-0.0.2.tar.gz/api_sl-0.0.2/src/sl/const.py
@@ -0,0 +1,3 @@
+SL_TRANSPORT_URL= "https://transport.integration.sl.se/v1"
+SL_DEVIATIONS_URL= "https://deviations.integration.sl.se/v1"
+SL_ROUTE_PLANNER_URL = "https://journeyplanner.integration.sl.se/v1/TravelplannerV3_1"
