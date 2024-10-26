2c331115eae114bf827cb04278f6d378a1967863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:08:30 +0000

    Add package Ilyass/0.8/ilyass-0.8.tar.gz

---
+++ packages/Ilyass/ilyass-0.8.tar.gz/ilyass-0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Ilyass', 
+    version='0.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    author='ILYASS MOROX',
+    author_email='ilyassvv@gmail.com',
+    description='Best library to check Hotmail email if is available',
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',
+    url='https://t.me/n1z1n',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
