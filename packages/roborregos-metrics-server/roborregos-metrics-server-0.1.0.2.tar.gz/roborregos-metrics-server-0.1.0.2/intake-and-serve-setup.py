c1649ae06ecf3bf163866fa729e7fe7f3daac078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:08 +0000

    Add package roborregos-metrics-server/0.1.0.2/roborregos-metrics-server-0.1.0.2.tar.gz

---
+++ packages/roborregos-metrics-server/roborregos-metrics-server-0.1.0.2.tar.gz/roborregos-metrics-server-0.1.0.2/intake-and-serve-setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roborregos-metrics-server',
+    version='0.1.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'fastapi',
+        'pydantic',
+        'uvicorn',
+        'pymongo',
+        'psutil'
+    ],
+    entry_points={
+        'console_scripts': [
+            'robometrics-server = robometrics.server.IntakeAndServe:main'
+        ]
+    },
+    readme='README.md'
+)
