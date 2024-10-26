3465eb4e3d703e1c3ee8b168a0134bfa763961eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:57 +0000

    Add package OSMsimp/1.0.50/OSMsimp-1.0.50.tar.gz

---
+++ packages/OSMsimp/OSMsimp-1.0.50.tar.gz/OSMsimp-1.0.50/OSMsimp/__init__.py
@@ -0,0 +1,6 @@
+from .extract_OSM import extract_from_PBF
+from .simplification_A import simplification_A
+from .simplification_B import simplification_B
+from .merging_networks import merge_networks
+import logging
+logging.basicConfig(level=logging.INFO)
