c00897fbafa14f6ad30a5646586252b2b4a4a5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:05:50 +0000

    Add package bgc-cloud/1.0.2/bgc_cloud-1.0.2.tar.gz

---
+++ packages/bgc-cloud/bgc_cloud-1.0.2.tar.gz/bgc_cloud-1.0.2/bgc_cloud/transport.py
@@ -0,0 +1,40 @@
+from dataclasses import asdict
+
+import requests
+from ._fn import Credentials
+
+
+class BGCTransport:
+    _PATH: str = "https://apiv2.bobgroganconsulting.com/api/v1"
+
+    def __init__(self, action, trigger):
+        self._cred: Credentials | None = None
+        self._action = action
+        self._trigger = trigger
+
+    @property
+    def credentials(self):
+        return
+
+    @credentials.setter
+    def credentials(self, cred: Credentials):
+        self._cred = cred
+
+    def send(self, data: dict, headers=None):
+        try:
+            res = requests.post(
+                self._PATH,
+                json=self._build_data(data)
+            ).json()
+            return res
+        except Exception as e:
+            print(e)
+            return
+
+    def _build_data(self, data):
+        return {
+            "action": self._action,
+            "trigger": self._trigger,
+            "config": vars(self._cred),
+            "data": dict(data),
+        }
