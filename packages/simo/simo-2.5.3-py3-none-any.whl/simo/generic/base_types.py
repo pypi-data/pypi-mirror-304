c8c08c3c17bafdceed2b80000c938575e9c303d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:44 +0000

    Add package simo/2.5.3/simo-2.5.3-py3-none-any.whl

---
+++ packages/simo/simo-2.5.3-py3-none-any.whl/simo/generic/base_types.py
@@ -0,0 +1,14 @@
+from django.utils.translation import gettext_lazy as _
+
+BASE_TYPES = {
+    'script': _("Script"),
+    'thermostat': _("Thermostat"),
+    'alarm-group': _("Alarm Group"),
+    'ip-camera': _("IP Camera"),
+    'weather-forecast': _("Weather Forecast"),
+    'gate': _("Gate"),
+    'blinds': _("Blinds"),
+    'watering': _("Watering"),
+    'state-select': _("State Select"),
+    'alarm-clock': _("Alarm Clock"),
+}
