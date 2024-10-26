afb58a04a8f06ae858011273cc6ac7f5d7278719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:07:32 +0000

    Add package radarbox/1.0.0/radarbox-1.0.0.tar.gz

---
+++ packages/radarbox/radarbox-1.0.0.tar.gz/radarbox-1.0.0/radarbox/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from radarbox.api.aircraft_api import AircraftApi
+from radarbox.api.airports_api import AirportsApi
+from radarbox.api.airspace_api import AirspaceApi
+from radarbox.api.billing_api import BillingApi
+from radarbox.api.flights_api import FlightsApi
+from radarbox.api.statistics_api import StatisticsApi
+
