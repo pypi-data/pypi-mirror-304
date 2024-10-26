086205b2bd608fbc7fab694b2f322c748fb687c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:44 +0000

    Add package senderstats/2.0.6/senderstats-2.0.6.tar.gz

---
+++ packages/senderstats/senderstats-2.0.6.tar.gz/senderstats-2.0.6/src/senderstats/core/filters/__init__.py
@@ -0,0 +1,15 @@
+from .ExcludeDomainFilter import ExcludeDomainFilter
+from .ExcludeEmptySenderFilter import ExcludeEmptySenderFilter
+from .ExcludeIPFilter import ExcludeIPFilter
+from .ExcludeInvalidSizeFilter import ExcludeInvalidSizeFilter
+from .ExcludeSenderFilter import ExcludeSenderFilter
+from .RestrictDomainFilter import RestrictDomainFilter
+
+__all__ = [
+    'ExcludeDomainFilter',
+    'ExcludeEmptySenderFilter',
+    'ExcludeInvalidSizeFilter',
+    'ExcludeIPFilter',
+    'ExcludeSenderFilter',
+    'RestrictDomainFilter'
+]
