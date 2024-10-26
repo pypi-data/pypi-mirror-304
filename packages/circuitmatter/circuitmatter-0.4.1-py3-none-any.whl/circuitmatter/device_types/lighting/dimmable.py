8cb26e0eca558eddd2bd899667e15afc69f53aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:05:12 +0000

    Add package circuitmatter/0.4.1/circuitmatter-0.4.1-py3-none-any.whl

---
+++ packages/circuitmatter/circuitmatter-0.4.1-py3-none-any.whl/circuitmatter/device_types/lighting/dimmable.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Scott Shawcroft for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import abc
+
+from circuitmatter.clusters.general import level_control
+
+from .on_off import OnOffLight
+
+
+class DimmableLight(OnOffLight):
+    DEVICE_TYPE_ID = 0x0101
+    REVISION = 3
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        self._level_control = level_control.LevelControl()
+        self._level_control.feature_map |= level_control.FeatureBitmap.LIGHTING
+        self._level_control.min_level = 1
+        self._level_control.max_level = 254
+        self.servers.append(self._level_control)
+
+        self._level_control.move_to_level_with_on_off = self._move_to_level_with_on_off
+
+    def _move_to_level_with_on_off(self, session, value):
+        try:
+            self.brightness = value.Level / self._level_control.max_level
+        except Exception as e:
+            print(f"Error setting brightness: {e}")
+            return
+        if self._level_control.min_level == value.Level:
+            self._on_off.OnOff = False
+        else:
+            self._on_off.OnOff = True
+        self._level_control.CurrentLevel = value.Level
+
+    @property
+    def brightness(self):
+        """Set when the light is dimmed"""
+        return self._level_control.CurrentLevel / self._level_control.max_level
+
+    @brightness.setter
+    @abc.abstractmethod
+    def brightness(self, value):
+        raise NotImplementedError()
---
+++ packages/circuitmatter/circuitmatter-0.4.1-py3-none-any.whl/circuitmatter/device_types/lighting/on_off.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Scott Shawcroft for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import abc
+
+from circuitmatter.clusters.general import on_off
+from circuitmatter.clusters.general.identify import Identify
+
+from .. import simple_device
+
+
+class OnOffLight(simple_device.SimpleDevice):
+    """A light that can be turned `OnOffLight.on` and `OnOffLight.off`."""
+
+    DEVICE_TYPE_ID = 0x0100
+    REVISION = 3
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        self._identify = Identify()
+        self.servers.append(self._identify)
+
+        self._on_off = on_off.OnOff()
+        self._on_off.on = self._on
+        self._on_off.off = self._off
+        self._on_off.feature_map |= on_off.FeatureBitmap.LIGHTING
+        self.servers.append(self._on_off)
+
+    def _on(self, session):
+        try:
+            self.on()
+        except Exception as e:
+            print(f"Error turning on light: {e}")
+            return
+        self._on_off.OnOff = True
+
+    def _off(self, session):
+        try:
+            self.off()
+        except Exception as e:
+            print(f"Error turning off light: {e}")
+            return
+        self._on_off.OnOff = False
+
+    @abc.abstractmethod
+    def on(self):
+        """Called when the light is turned on"""
+        pass
+
+    @abc.abstractmethod
+    def off(self):
+        """Called when the light is turned off"""
+        pass
