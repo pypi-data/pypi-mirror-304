6fca213728045dd2445a93b61cd1e1c5973c99ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:27 +0000

    Add package spt/0.2.4/spt-0.2.4.tar.gz

---
+++ packages/spt/spt-0.2.4.tar.gz/spt-0.2.4/setup.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+THIS_DIR = Path(__file__).parent
+
+# with open(THIS_DIR / "docs/description.md", encoding="utf-8") as f:
+with open(THIS_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+def read_requirements(filepath: Path) -> list[str]:
+    with open(filepath, encoding="utf-8") as fd:
+        return [
+            package.strip("\n")
+            for package in fd.readlines()
+            if not package.startswith("#")
+        ]
+
+
+requirements = read_requirements(THIS_DIR / "requirements.txt")
+
+
+setup(
+    name="spt",
+    version="0.2.4",
+    description="Scientific matplotlib plot rcParams configuration template python package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "matplotlib",
+    ],
+    author="yangsl",
+    author_email="3304839708@qq.com",
+    maintainer="yangsl",
+    maintainer_email="330839708@qq.com",
+    url="https://github.com/Bit-Part-Young/spt",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+    extras_require={
+        "examples": [
+            "pandas",
+            "scikit-learn",
+        ]
+    },
+    python_requires=">=3.9",
+    platforms=["all"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
