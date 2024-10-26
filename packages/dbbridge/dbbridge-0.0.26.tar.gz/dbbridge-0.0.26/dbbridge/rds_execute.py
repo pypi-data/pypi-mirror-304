8714a90e0dfd7ece35be9cb9d24d4513baa3d891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 03:06:57 +0000

    Add package dbbridge/0.0.26/dbbridge-0.0.26.tar.gz

---
+++ packages/dbbridge/dbbridge-0.0.26.tar.gz/dbbridge-0.0.26/dbbridge/rds_execute.py
@@ -0,0 +1,44 @@
+import pymysql.cursors
+from .db_connection import get_db_connection
+
+def get_operation_from_query(query):
+    """
+    Determine the SQL operation (e.g., 'select', 'insert', 'update', 'delete') from the query.
+    """
+    return query.strip().split()[0].lower()
+
+def rds_execute(service, query, params=None, **config):
+    """
+    Execute a query on the RDS or local database.
+    """
+    connection = None
+    print("Query == > ",query)
+    print("params == > ", params)
+    try:
+        connection_obj = get_db_connection(service, **config)
+        connection = connection_obj.get_db_connection()
+
+        with connection.cursor() as cursor:
+            if isinstance(params, list) and all(isinstance(i, (tuple, list)) for i in params):
+                cursor.executemany(query, params)
+            else:
+                cursor.execute(query, params or {})
+
+            operation = get_operation_from_query(query)
+            if operation in ['insert', 'update', 'delete']:
+                connection.commit()
+
+            if operation == 'select':
+                return cursor.fetchall()
+
+    except pymysql.OperationalError as op_err:
+        print(f'Operational Error in DB query: {query}')
+        return op_err
+
+    except Exception as e:
+        print(f'Error while executing DB query: {query}')
+        return e
+
+    finally:
+        if connection:
+            connection_obj.close_connection()
