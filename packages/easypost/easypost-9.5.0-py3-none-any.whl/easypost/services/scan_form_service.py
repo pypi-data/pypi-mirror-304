dbec5417c7a3afd03f0bb2b6be7940d054d77eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:09 +0000

    Add package easypost/9.5.0/easypost-9.5.0-py3-none-any.whl

---
+++ packages/easypost/easypost-9.5.0-py3-none-any.whl/easypost/services/scan_form_service.py
@@ -0,0 +1,49 @@
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
+
+from easypost.models import ScanForm
+from easypost.services.base_service import BaseService
+
+
+class ScanFormService(BaseService):
+    def __init__(self, client):
+        self._client = client
+        self._model_class = ScanForm.__name__
+
+    def create(self, **params) -> ScanForm:
+        """Create a ScanForm."""
+        return self._create_resource(self._model_class, **params)
+
+    def all(self, **params) -> Dict[str, Any]:
+        """Retrieve a list of ScanForms."""
+        filters = {
+            "key": "scan_forms",
+        }
+
+        return self._all_resources(self._model_class, filters, **params)
+
+    def retrieve(self, id: str) -> ScanForm:
+        """Retrieve a ScanForm."""
+        return self._retrieve_resource(self._model_class, id)
+
+    def get_next_page(
+        self,
+        scan_forms: Dict[str, Any],
+        page_size: int,
+        optional_params: Optional[Dict[str, Any]] = None,
+    ) -> Dict[str, Any]:
+        """Retrieve the next page of the list ScanForm response."""
+        self._check_has_next_page(collection=scan_forms)
+
+        params = {
+            "before_id": scan_forms["scan_forms"][-1].id,
+            "page_size": page_size,
+        }
+
+        if optional_params:
+            params.update(optional_params)
+
+        return self.all(**params)
