0b63a9864a065a5b6fb4c3ff27189483cd88cb97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:05:08 +0000

    Add package qurrium/0.9.2/qurrium-0.9.2-cp39-abi3-macosx_11_0_arm64.whl

---
+++ packages/qurrium/qurrium-0.9.2-cp39-abi3-macosx_11_0_arm64.whl/qurry/recipe/__init__.py
@@ -0,0 +1,18 @@
+"""
+=============================================
+Position for `qurry.recipe`
+=============================================
+
+See https://github.com/harui2019/qurry/issues/82
+
+"""
+
+from .simple import (
+    Intracell,
+    Singlet,
+    TrivialParamagnet,
+    TopologicalParamagnet,
+    Cluster,
+    Cat,
+    GHZ,
+)
