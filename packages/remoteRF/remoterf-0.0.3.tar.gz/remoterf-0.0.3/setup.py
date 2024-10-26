1ef5b021695a341f4348c15bef3401d82a7ee21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:07:06 +0000

    Add package remoteRF/0.0.3/remoterf-0.0.3.tar.gz

---
+++ packages/remoteRF/remoterf-0.0.3.tar.gz/remoterf-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="remoteRF",
+    version="0.0.3",
+    author="Ethan Ge",
+    author_email="ethoGalaxy@gmail.com",
+    description="A python API to remotely access signal centric hardware. Client-side only!",
+    packages=find_packages(where="src"),  # Automatically finds subpackages like core, deviceA, deviceB
+    package_dir={"": "src"},
+    install_requires=[
+        "grpcio", "protobuf", "numpy", "prompt_toolkit", "python-dotenv"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'remoterf-login=remoteRF.core.acc_login:main',
+        ],
+    },
+)
