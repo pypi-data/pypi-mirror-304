f032cecab9219c37b4648c3c4444bed6675c62f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:34 +0000

    Add package ape-alchemy/0.8.3/ape-alchemy-0.8.3.tar.gz

---
+++ packages/ape-alchemy/ape-alchemy-0.8.3.tar.gz/ape-alchemy-0.8.3/ape_alchemy/__init__.py
@@ -0,0 +1,41 @@
+from ape import plugins
+
+from .provider import Alchemy
+
+NETWORKS = {
+    "ethereum": [
+        "mainnet",
+        "sepolia",
+    ],
+    "arbitrum": [
+        "mainnet",
+        "sepolia",
+    ],
+    "base": [
+        "mainnet",
+        "sepolia",
+    ],
+    "fantom": [
+        "opera",
+        "testnet",
+    ],
+    "optimism": [
+        "mainnet",
+        "sepolia",
+    ],
+    "polygon": [
+        "mainnet",
+        "amoy",
+    ],
+    "polygon-zkevm": [
+        "mainnet",
+        "cardona",
+    ],
+}
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for ecosystem_name in NETWORKS:
+        for network_name in NETWORKS[ecosystem_name]:
+            yield ecosystem_name, network_name, Alchemy
