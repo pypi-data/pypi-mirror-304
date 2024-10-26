27d6fec42021a4603f7c1a0028280883e5fe8129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:33 +0000

    Add package django-cad-inspector/0.1.0/django_cad_inspector-0.1.0-py3-none-any.whl

---
+++ packages/django-cad-inspector/django_cad_inspector-0.1.0-py3-none-any.whl/cadinspector/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic import RedirectView
+
+from .views import EntityDetailView, EntityListView, SceneDetailView, SceneListView
+
+app_name = "cadinspector"
+urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="cadinspector:scene_list"), name="home"),
+    path("entity/", EntityListView.as_view(), name="entity_list"),
+    path("entity/<pk>/", EntityDetailView.as_view(), name="entity_detail"),
+    path("scene/", SceneListView.as_view(), name="scene_list"),
+    path("scene/<pk>/", SceneDetailView.as_view(), name="scene_detail"),
+]
