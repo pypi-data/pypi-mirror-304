949c42b6e5fbc4e03662382c57b0ecccc29d3042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:04:36 +0000

    Add package lion-core/0.5.6/lion_core-0.5.6.tar.gz

---
+++ packages/lion-core/lion_core-0.5.6.tar.gz/lion_core-0.5.6/lion_core/action/base.py
@@ -0,0 +1,76 @@
+from typing import Any, NoReturn
+
+from lionabc import Action, EventStatus
+from lionabc.exceptions import LionAccessError
+from lionfuncs import to_dict
+from pydantic import PrivateAttr
+from typing_extensions import override
+
+from lion_core.generic import Element, Log
+from lion_core.setting import (
+    DEFAULT_TIMED_FUNC_CALL_CONFIG,
+    TimedFuncCallConfig,
+)
+
+
+class ObservableAction(Element, Action):
+
+    status: EventStatus = EventStatus.PENDING
+    execution_time: float | None = None
+    execution_response: Any = None
+    execution_error: str | None = None
+    _timed_config: TimedFuncCallConfig | None = PrivateAttr(None)
+    _content_fields: list = PrivateAttr(["execution_response"])
+
+    @override
+    def __init__(
+        self, timed_config: dict | TimedFuncCallConfig | None, **kwargs: Any
+    ) -> None:
+        super().__init__()
+        if timed_config is None:
+            self._timed_config = DEFAULT_TIMED_FUNC_CALL_CONFIG
+
+        else:
+            if isinstance(timed_config, TimedFuncCallConfig):
+                timed_config = timed_config.to_dict()
+            if isinstance(timed_config, dict):
+                timed_config = {**timed_config, **kwargs}
+            timed_config = TimedFuncCallConfig(**timed_config)
+            self._timed_config = timed_config
+
+    def to_log(self) -> Log:
+        """
+        Convert the action to a log entry.
+
+        Returns:
+            BaseLog: A log entry representing the action.
+        """
+        dict_ = self.to_dict()
+        content = {k: dict_[k] for k in self._content_fields if k in dict_}
+        loginfo = {k: dict_[k] for k in dict_ if k not in self._content_fields}
+        content = to_dict(
+            content,
+            use_model_dump=True,
+            recursive=True,
+            recursive_python_only=False,
+            max_recursive_depth=5,
+        )
+        loginfo = to_dict(
+            loginfo,
+            use_model_dump=True,
+            recursive=True,
+            recursive_python_only=False,
+            max_recursive_depth=5,
+        )
+        return Log(content=content, loginfo=loginfo)
+
+    @classmethod
+    def from_dict(cls, data: dict, /, **kwargs: Any) -> NoReturn:
+        """Event cannot be re-created."""
+        raise LionAccessError(
+            "An event cannot be recreated. Once it's done, it's done."
+        )
+
+
+__all__ = ["ObservableAction"]
+# File: lion_core/action/base.py
---
+++ packages/lion-core/lion_core-0.5.6.tar.gz/lion_core-0.5.6/lion_core/types.py
@@ -0,0 +1,3 @@
+from lionabc import Observable
+
+ID = str | Observable
