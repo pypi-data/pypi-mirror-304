9239e2767ff7b14cabaa62851067171d41c17ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:57 +0000

    Add package votoutils/0.0.1a2/votoutils-0.0.1a2-py3-none-any.whl

---
+++ packages/votoutils/votoutils-0.0.1a2-py3-none-any.whl/votoutils/upload/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Utility to rsync raw mission data to pipeline
+glider=$1
+mission=$2
+filesdir=$3
+echo send SEA$glider mission $mission data to pipeline
+tgtdir=/data/data_raw/complete_mission/SEA$glider/M$mission
+echo make directory on target if it does not already exist
+ssh pipeline@88.99.244.110 mkdir -p $tgtdir
+echo ""
+echo rsync data
+rsync -v --stats $filesdir/NAV/sea*$glider.$mission* "pipeline@88.99.244.110:$tgtdir"
+rsync -v --stats $filesdir/PLD_raw/sea*$glider.$mission* "pipeline@88.99.244.110:$tgtdir"
+echo Finished
