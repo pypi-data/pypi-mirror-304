bbce962e05a3e81079da755eaef56b9fcf4d508b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 07:04:59 +0000

    Add package scmkl/0.1.2/scmkl-0.1.2.tar.gz

---
+++ packages/scmkl/scmkl-0.1.2.tar.gz/scmkl-0.1.2/scmkl/approx_kernels.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from scmkl.data_processing import process_data
+
+
+def calculate_z(adata, n_features = 5000) -> tuple:
+    '''
+    Function to calculate approximate kernels.
+    Input:
+            adata- Adata obj as created by `Create_Adata`
+                Sigma can be calculated with Estimate_Sigma or a heuristic but must be positive.
+            n_features- Number of random feature to use when calculating Z- used for scalability
+    Output:
+            adata_object with Z matrices accessible with- adata.uns['Z_train'] and adata.uns['Z_test'] respectively
+
+    '''
+    assert np.all(adata.uns['sigma'] > 0), 'Sigma must be positive'
+
+    #Number of groupings taking from group_dict
+    N_pathway = len(adata.uns['group_dict'].keys())
+    D = adata.uns['D']
+
+    # Create Arrays to store concatenated group Z.  Each group of features will have a corresponding entry in each array
+    Z_train = np.zeros((len(adata.uns['train_indices']), 2 * adata.uns['D'] * N_pathway))
+    Z_test = np.zeros((len(adata.uns['test_indices']), 2 * adata.uns['D'] * N_pathway))
+
+    # Loop over each of the groups and creating Z for each
+    for m, group_features in enumerate(adata.uns['group_dict'].values()):
+        
+        #Extract features from mth group
+        num_group_features = len(group_features)
+
+        # Sample up to n_features features- important for scalability if using large groupings
+        # Will use all features if the grouping contains fewer than n_features
+        group_features = adata.uns['seed_obj'].choice(np.array(list(group_features)), np.min([n_features, num_group_features]), replace = False) 
+
+        # Create data arrays containing only features within this group
+        X_train = adata[adata.uns['train_indices'],:][:, group_features].X
+        X_test = adata[adata.uns['test_indices'],:][:, group_features].X
+
+        # Perform data filtering, and transformation according to given data_type
+        # Will remove low variance (< 1e5) features regardless of data_type
+        # If given data_type is 'counts' (like RNA) will log scale and z-score the data
+        X_train, X_test = process_data(X_train = X_train, X_test = X_test, data_type = adata.uns['data_type'], return_dense = True)
+
+        #Extract pre-calculated sigma used for approximating kernel
+        adjusted_sigma = adata.uns['sigma'][m]
+
+        #Calculates approximate kernel according to chosen kernel function- may add more functions in the future
+        #Distribution data comes from Fourier Transform of original kernel function
+        if adata.uns['kernel_type'].lower() == 'gaussian':
+
+            gamma = 1/(2*adjusted_sigma**2)
+            sigma_p = 0.5*np.sqrt(2*gamma)
+
+            W = adata.uns['seed_obj'].normal(0, sigma_p, X_train.shape[1]*D).reshape((X_train.shape[1]),D)
+
+        elif adata.uns['kernel_type'].lower() == 'laplacian':
+
+            gamma = 1/(2*adjusted_sigma)
+
+            W = gamma * adata.uns['seed_obj'].standard_cauchy(X_train.shape[1]*D).reshape((X_train.shape[1],D))
+
+        elif adata.uns['kernel_type'].lower() == 'cauchy':
+
+            gamma = 1/(2*adjusted_sigma**2)
+            b = 0.5*np.sqrt(gamma)
+
+            W = adata.uns['seed_obj'].laplace(0, b, X_train.shape[1]*D).reshape((X_train.shape[1],D))
+
+
+        train_projection = np.matmul(X_train, W)
+        test_projection = np.matmul(X_test, W)
+        
+
+        #Store group Z in whole-Z object.  Preserves order to be able to extract meaningful groups
+        Z_train[0:, np.arange( m * 2 * D , (m + 1) * 2 * D)] = np.sqrt(1/D)*np.hstack((np.cos(train_projection), np.sin(train_projection)))
+        Z_test[0:, np.arange( m * 2 * D , (m + 1) * 2 * D)] = np.sqrt(1/D)*np.hstack((np.cos(test_projection), np.sin(test_projection)))
+
+    adata.uns['Z_train'] = Z_train
+    adata.uns['Z_test'] = Z_test
+
+
+    return adata
---
+++ packages/scmkl/scmkl-0.1.2.tar.gz/scmkl-0.1.2/scmkl/train.py
@@ -0,0 +1,51 @@
+import numpy as np
+import celer
+
+
+
+def train_model(adata, group_size = 1, alpha = 0.9):
+    '''
+    Function to fit a grouplasso model to the provided data.
+    Inputs:
+            Adata with Z matrices in adata.uns
+            group_size- Argument describing how the features are grouped. 
+                    From Celer documentation:
+                    "groupsint | list of ints | list of lists of ints.
+                        Partition of features used in the penalty on w. 
+                            If an int is passed, groups are contiguous blocks of features, of size groups. 
+                            If a list of ints is passed, groups are assumed to be contiguous, group number g being of size groups[g]. 
+                            If a list of lists of ints is passed, groups[g] contains the feature indices of the group number g."
+                    If 1, model will behave identically to Lasso Regression
+            alpha- Group Lasso regularization coefficient. alpha is a floating point value controlling model solution sparsity. Must be a positive float.
+                        The smaller the value, the more feature groups will be selected in the trained model.
+    Outputs:
+
+            adata object with trained model in uns accessible with- adata.uns['model']
+                Specifics of model:
+                    model- The trained Celer Group Lasso model.  Used in Find_Selected_Pathways() function for interpretability.
+                        For more information about attributes and methods see the Celer documentation at https://mathurinm.github.io/celer/generated/celer.GroupLasso.html.
+
+    '''
+    assert alpha > 0, 'Alpha must be positive'
+
+    y_train = adata.obs['labels'].iloc[adata.uns['train_indices']]
+    X_train = adata.uns['Z_train']
+
+    cell_labels = np.unique(y_train)
+
+    # This is a regression algorithm. We need to make the labels 'continuous' for classification, but they will remain binary.
+    # Casts training labels to array of -1,1
+    train_labels = np.ones(y_train.shape)
+    train_labels[y_train == cell_labels[1]] = -1
+
+    # Alphamax is a calculation to regularize the effect of alpha (a sparsity parameter) across different data sets
+    alphamax = np.max(np.abs(X_train.T.dot(train_labels))) / X_train.shape[0] * alpha
+
+    # Instantiate celer Group Lasso Regression Model Object
+    model = celer.GroupLasso(groups = group_size, alpha = alphamax)
+
+    # Fit model using training data
+    model.fit(X_train, train_labels.ravel())
+
+    adata.uns['model'] = model
+    return adata
