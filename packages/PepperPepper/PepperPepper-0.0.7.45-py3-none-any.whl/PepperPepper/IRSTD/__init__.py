6d1be3977207c8d5258603ab0f79961df5643b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:04:44 +0000

    Add package PepperPepper/0.0.7.45/PepperPepper-0.0.7.45-py3-none-any.whl

---
+++ packages/PepperPepper/PepperPepper-0.0.7.45-py3-none-any.whl/PepperPepper/IRSTD/__init__.py
@@ -0,0 +1,9 @@
+# IRSTD包，专门设计为了解决红外图像小目标检测方向的需要。
+
+import evaluation
+import models
+import datasets
+
+
+__all__ = ['evaluation', 'models', 'datasets']
+
---
+++ packages/PepperPepper/PepperPepper-0.0.7.45-py3-none-any.whl/PepperPepper/environment.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib_inline import backend_inline
+import torch
+import torchvision
+import collections
+import random
+import cv2
+import re
+import os
+import hashlib
+import requests
+import collections
+import math
+from IPython import display
+import time
+import zipfile
+import tarfile
+import sklearn
+import threading
+import PIL
