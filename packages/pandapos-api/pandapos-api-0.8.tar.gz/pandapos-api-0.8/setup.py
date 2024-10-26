ae342f1d5f14cb470b53ec6d044a80a45858505e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:05:31 +0000

    Add package pandapos-api/0.8/pandapos-api-0.8.tar.gz

---
+++ packages/pandapos-api/pandapos-api-0.8.tar.gz/pandapos-api-0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandapos-api",
+    version="0.8",
+    packages=find_packages(),
+    description="PandaPos API",
+    author="Esat",
+    author_email="esatyilmaz3500@gmail.com",
+)
