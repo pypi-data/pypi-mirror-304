2a769eeeee0cadf764482c937ed1761cb4e1221e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:25 +0000

    Add package volttron-lib-auth/2.0.0rc1/volttron_lib_auth-2.0.0rc1-py3-none-any.whl

---
+++ packages/volttron-lib-auth/volttron_lib_auth-2.0.0rc1-py3-none-any.whl/volttron/auth/__init__.py
@@ -0,0 +1,17 @@
+from typing import List
+
+import volttron.auth.authz_manager
+# from volttron.auth.base import BaseAuthentication
+# from volttron.auth.base_client import BaseClientAuthorization
+# from volttron.auth.base_server import (BaseServerAuthentication,
+#                                        BaseServerAuthorization)
+
+# from volttron.platform.auth.credential_manager import FileBasedCredentialManager
+
+# __all__: List[str] = [
+#     "BaseClientAuthorization",
+#     "BaseServerAuthorization",
+#     "BaseServerAuthorization",
+#     "BaseAuthentication",
+#     #   "FileBasedCredentialManager"
+# ]    # noqa: WPS410 (the only __variable__ we use)
