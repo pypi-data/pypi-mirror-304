3a47601c022bf0a21b50109f6ef8dcdf16b0d9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:04:53 +0000

    Add package python-logging-filters/0.1.1/python_logging_filters-0.1.1.tar.gz

---
+++ packages/python-logging-filters/python_logging_filters-0.1.1.tar.gz/python_logging_filters-0.1.1/README.md
@@ -0,0 +1,2 @@
+# python-logging-filters
+Simple filters for standard python logging, e.g. for suppressing noisy 3rd party framework logging.
---
+++ packages/python-logging-filters/python_logging_filters-0.1.1.tar.gz/python_logging_filters-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from textwrap import dedent
+
+from setuptools import find_packages, setup
+
+setup(
+    name="python-logging-filters",
+    version="0.1.1",
+    description="standard python logging filters",
+    long_description=dedent(
+        """
+        Simple filters for standard python logging, e.g. for suppressing
+        noisy 3rd party framework logging.
+
+        # DjangoHttp404LogFilter
+
+        Suppresses Django's default 'Not Found: ...' logging.
+        See the `python_logging_filters.DjangoHttp404LogFilter`
+        and the official Django documentation for more details how
+        to use and configure that filter.
+        """
+    ),
+    author="puzzleYOU GmbH",
+    author_email="scrum@puzzleyou.de",
+    url="https://github.com/puzzleYOU/python-logging-filters/",
+    license="GPLv3",
+    platforms=["any"],
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=True,
+)
