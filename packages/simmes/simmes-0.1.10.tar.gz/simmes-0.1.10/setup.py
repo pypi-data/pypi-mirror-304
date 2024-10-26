2d28354c4b17b83f3f656405dd7606afe7467c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:21 +0000

    Add package simmes/0.1.10/simmes-0.1.10.tar.gz

---
+++ packages/simmes/simmes-0.1.10.tar.gz/simmes-0.1.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simmes',
+    version='0.1.10',
+    description='GRB measurement simulation packages',
+    packages=find_packages(include=["simmes","simmes.*"]),
+    package_data={"":["util_packages/files-det-ang-dependence/*","util_packages/files-swiftBAT-resp-mats/*"]},
+    license="MIT",
+    keywords="gamma-ray bursts",
+    author='Michael Moss',
+    author_email='mikejmoss3@gmail.com',
+    url='https://github.com/mikemoss3/simmes',
+)
