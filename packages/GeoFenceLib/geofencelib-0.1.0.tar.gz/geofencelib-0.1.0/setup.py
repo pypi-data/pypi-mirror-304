37ec9f87a8a6536b0941377c8b13559e632c8c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:05:03 +0000

    Add package GeoFenceLib/0.1.0/geofencelib-0.1.0.tar.gz

---
+++ packages/GeoFenceLib/geofencelib-0.1.0.tar.gz/geofencelib-0.1.0/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="GeoFenceLib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["shapely"],
+    author="Abhinav",
+    author_email="your.email@example.com",
+    description="A Python library for geofencing with dynamic shape and event handling.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/GeoFenceLib/geofencelib-0.1.0.tar.gz/geofencelib-0.1.0/tests/test_imports.py
@@ -0,0 +1,13 @@
+# tests/test_imports.py
+
+def test_core_import():
+    from geofence_lib import core
+
+def test_events_import():
+    from geofence_lib import events
+
+def test_manager_import():
+    from geofence_lib import manager
+
+def test_utils_import():
+    from geofence_lib import utils
