67c9fa6644195107eb66da2b68751cd141a1c76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 04:07:31 +0000

    Add package onedm/0.2.2/onedm-0.2.2.tar.gz

---
+++ packages/onedm/onedm-0.2.2.tar.gz/onedm-0.2.2/tests/sdf/test_references.py
@@ -0,0 +1,6 @@
+from onedm import sdf
+
+
+def test_reference(test_model: sdf.Document):
+    assert isinstance(test_model.data["Reference"], sdf.IntegerData)
+    assert test_model.data["Reference"].const == 0
