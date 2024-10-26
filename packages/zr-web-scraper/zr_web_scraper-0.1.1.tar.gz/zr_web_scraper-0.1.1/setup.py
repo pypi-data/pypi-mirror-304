504448339c066421fa442b9901ab9cd00e5de2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 03:07:35 +0000

    Add package zr-web-scraper/0.1.1/zr_web_scraper-0.1.1.tar.gz

---
+++ packages/zr-web-scraper/zr_web_scraper-0.1.1.tar.gz/zr_web_scraper-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zr_web_scraper',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'pandas'
+    ],
+    author='Oluwafemi Olasegiri',
+    author_email='olasegirioluwafemi@gmail.com',
+    description='A simple Web scraper package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/olasegirioluwa3/zr_web_scraper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
