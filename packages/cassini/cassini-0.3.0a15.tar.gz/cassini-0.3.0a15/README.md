d9b6a8b16189e1e344d89435e31a02a93d066c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:05:30 +0000

    Add package cassini/0.3.0a15/cassini-0.3.0a15.tar.gz

---
+++ packages/cassini/cassini-0.3.0a15.tar.gz/cassini-0.3.0a15/README.md
@@ -0,0 +1,50 @@
+# Cassini
+
+*Note Cassini is in it's alpha development stage, so things may not work perfectly - tell us about problems so we can fix them!*
+
+An electronic laboratory notebook (ELN), built inside Jupyter Lab.
+
+Cassini's goal is to help you explore, analyse and organise your data in an environment that's familiar.
+
+![Screenshot](demo.gif)
+
+## Features
+
+* **Structures your project** into an easy to navigate hierarchy of workpackages, experiments, samples and datasets.
+* Allows **retrieval of data by name** e.g. `project['WP3.2f-XRD'] / 'XRD_data.csv`.
+* Create new sample, experiment (etc.) notebooks through **custom dialogs**.
+* Create notebook templates for **standardised proceedures**.
+* **Browse and explore** your project through a custom browser, including previewing cell outputs, such as plots in-browser.
+* No magic! Everything is stored in regular **human navigatable folders**, cloud backups etc. work as expected.
+
+Check out the demo binder for a walkthrough of features (note this can take a little while to load):
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/0Hughman0/Cassini/HEAD?urlpath=lab/tree/Home.ipynb)
+
+## Installation and Setup
+
+    > pip install cassini
+
+Create a `project.py`:
+
+    # project.py
+    from cassini import Project, DEFAULT_TIERS
+    from cassini import jlgui
+
+    project = Project(DEFAULT_TIERS, __file__)
+    jlgui.extend_project(project)
+
+    if __name__ == '__main__':
+        project.launch()
+
+And launch it:
+
+    > python project.py
+
+Head to [Quickstart](https://0hughman0.github.io/Cassini/latest/quickstart.html) for more info.
+
+## Contributing
+
+Contributing guidelines are found [here](https://0hughman0.github.io/Cassini/latest/contributing.html).
+
+This includes development installation instructions and codebase orientation.
---
+++ packages/cassini/cassini-0.3.0a15.tar.gz/cassini-0.3.0a15/cassini/accessors.py
@@ -0,0 +1,141 @@
+import functools
+
+from typing import (
+    Callable,
+    Union,
+    Any,
+    cast,
+    Dict,
+    List,
+    Tuple,
+    TypeVar,
+    Generic,
+    Optional,
+    Type,
+    overload,
+)
+from typing_extensions import Self
+
+from .environment import env
+
+T = TypeVar("T")
+V = TypeVar("V")
+
+
+class _SoftProp(Generic[T, V]):
+    """
+    Like a property, except can be overwritten.
+    """
+
+    def __init__(self, func: Callable[[T], V]):
+        self.func = func
+        self.__wrapped__ = func
+
+    @overload
+    def __get__(self, instance: None, owner: Type[T]) -> Self:
+        pass
+
+    @overload
+    def __get__(self, instance: T, owner: Type[T]) -> V:
+        pass
+
+    def __get__(self, instance: Optional[T], owner: Type[T]) -> Union[V, Self]:
+        if instance:
+            return self.func(instance)
+        return self
+
+
+def soft_prop(wraps: Callable[[Any], V]) -> V:
+    """
+    Create an over-writable property
+    """
+    return cast(V, functools.wraps(wraps)(_SoftProp(wraps)))  # type: ignore[arg-type]
+
+
+class _CachedProp(Generic[T, V]):
+    """
+    Like a read only property, except it's only evaluated once.
+
+    Cached value is stored in the _CachedProp instance, which is maybe a bad idea, idk.
+    """
+
+    def __init__(self, func: Callable[[T], V]):
+        self.func = func
+        self.cache: Dict[T, V] = env.create_cache()
+
+        self.__wrapped__ = func
+
+    @overload
+    def __get__(self, instance: None, owner: Type[T]) -> Self:
+        pass
+
+    @overload
+    def __get__(self, instance: T, owner: Type[T]) -> V:
+        pass
+
+    def __get__(self, instance: Optional[T], owner: Type[T]) -> Union[V, Self]:
+        if instance is None:
+            return self
+
+        if instance in self.cache:
+            return self.cache[instance]
+
+        val = self.func(instance)
+        self.cache[instance] = val
+
+        return val
+
+    def __set__(self, instance: Optional[T], value: Any) -> None:
+        raise AttributeError("Trying to set a cached property - naughty!")
+
+    @property
+    def __isabstractmethod__(self):
+        return getattr(self.func, "__isabstractmethod__", False)
+
+
+def cached_prop(wrapped: Callable[[Any], V]) -> V:
+    """
+    Decorator for turning functions/ methods into `_CachedProp`s.
+    """
+    return cast(V, functools.wraps(wrapped)(_CachedProp(wrapped)))  # type: ignore[arg-type]
+
+
+class _CachedClassProp(Generic[T, V]):
+    """
+    Like a combination between `classmethod` and `property`.
+
+    The class instance is passed to the wrapped method upon calling.
+
+    Also performs same caching as `_CachedProp`.
+    """
+
+    def __init__(self, func: Callable[[Type[T]], V]):
+        self.func = func
+        self.cache: Dict[Type[T], V] = env.create_cache()
+
+        self.__wrapped__ = func
+
+    def __get__(self, instance: T, owner: Type[T]) -> V:
+        if owner in self.cache:
+            return self.cache[owner]
+
+        val = self.func(owner)
+        self.cache[owner] = val
+
+        return val
+
+    def __set__(self, instance: T, value: Any) -> Any:
+        raise AttributeError("Trying to set a cached class property - naughty!")
+
+    @property
+    def __isabstractmethod__(self):
+        return getattr(self.func, "__isabstractmethod__", False)
+
+
+def cached_class_prop(wrapped: Callable[[Any], V]) -> V:
+    """
+    Decorator for turning functions/ methods into `_CachedClassProp`s.
+
+    First argument of wrapped will be `self.__class__` rather than `self`.
+    """
+    return cast(V, functools.wraps(wrapped)(_CachedClassProp(wrapped)))  # type: ignore[arg-type]
