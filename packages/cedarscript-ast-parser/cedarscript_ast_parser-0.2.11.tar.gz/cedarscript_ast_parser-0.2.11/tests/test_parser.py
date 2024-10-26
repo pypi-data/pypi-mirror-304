159750a5f72da59a385ca8d09868edc19ec64bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:05:18 +0000

    Add package cedarscript-ast-parser/0.2.11/cedarscript_ast_parser-0.2.11.tar.gz

---
+++ packages/cedarscript-ast-parser/cedarscript_ast_parser-0.2.11.tar.gz/cedarscript_ast_parser-0.2.11/tests/test_parser.py
@@ -0,0 +1,7 @@
+import pytest
+from cedarscript_ast_parser import CEDARScriptASTParser
+
+
+def test_can_instantiate_parser():
+    """Test that we can create a CEDARScriptASTParser instance"""
+    CEDARScriptASTParser()
