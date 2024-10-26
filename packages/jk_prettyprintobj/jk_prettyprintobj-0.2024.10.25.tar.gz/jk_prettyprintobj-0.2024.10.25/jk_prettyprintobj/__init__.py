fab9c84928af4271379769a4a480860761434c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:44 +0000

    Add package jk_prettyprintobj/0.2024.10.25/jk_prettyprintobj-0.2024.10.25.tar.gz

---
+++ packages/jk_prettyprintobj/jk_prettyprintobj-0.2024.10.25.tar.gz/jk_prettyprintobj-0.2024.10.25/jk_prettyprintobj/__init__.py
@@ -0,0 +1,46 @@
+
+
+
+__author__ = "Jürgen Knauth"
+__version__ = "0.2024.10.25"
+__all__ = (
+	"DumperSettings",
+	"RawValue",
+	"DumpMixin",
+	"Dumper",
+	"DumpCtx",
+	"DEFAULT_DUMPER_SETTINGS",
+	"pprint",
+)
+
+
+
+
+
+from .DumperSettings import DumperSettings
+from .RawValue import RawValue
+from .dumper import DumpMixin, Dumper, DumpCtx, DEFAULT_DUMPER_SETTINGS
+
+from builtins import print as _print
+
+
+
+
+#
+# Print any value in a human readable way.
+#
+def pprint(something, printFunc = None):
+	if printFunc is None:
+		printFunc = _print
+	else:
+		assert callable(printFunc)
+
+	dumper = Dumper()
+	with dumper.createContext(None, "") as dumper2:
+		dumper2._dumpX("", something)
+	dumper.print(printFunc)
+#
+
+
+
+
