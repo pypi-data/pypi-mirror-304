968aed909b405ed4510b9f40614efaadf40a0d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:05:03 +0000

    Add package distancia/0.0.69/distancia-0.0.69-py3-none-any.whl

---
+++ packages/distancia/distancia-0.0.69-py3-none-any.whl/distancia/matrixDistance.py
@@ -0,0 +1,165 @@
+from .mainClass import *
+from .tools     import Vector,Matrix
+
+class Mahalanobis(Distance):
+	
+	def __init__(self)-> None:
+		super().__init__()
+		self.type='list_list'
+
+	def mean(self,data:list[list]):
+		"""
+		Calculate the mean of each dimension in the dataset.
+    
+		:param data: A dataset as a list of points (list of lists)
+		:return: Mean of each dimension as a list
+		"""
+		n = len(data)
+		d = len(data[0])
+		mean = [0] * d
+    
+		for point in data:
+			for i in range(d):
+				mean[i] += point[i]
+    
+		mean = [x / n for x in mean]
+		return mean
+
+	def covariance_matrix(self,data, mean):
+		"""
+		Calculate the covariance matrix of the dataset.
+    
+		:param data: A dataset as a list of points (list of lists)
+		:param mean: Mean of each dimension as a list
+		:return: Covariance matrix as a list of lists
+		"""
+		n = len(data)
+		d = len(data[0])
+		cov_matrix = [[0] * d for _ in range(d)]
+    
+		for point in data:
+			diff = [point[i] - mean[i] for i in range(d)]
+			for i in range(d):
+				for j in range(d):
+					cov_matrix[i][j] += diff[i] * diff[j]
+    
+		cov_matrix = [[x / (n - 1) for x in row] for row in cov_matrix]
+		return cov_matrix
+
+	def matrix_inverse(self,matrix):
+		"""
+		Calculate the inverse of a matrix using Gauss-Jordan elimination.
+    
+		:param matrix: A square matrix as a list of lists
+		:return: Inverse of the matrix as a list of lists
+		"""
+		n = len(matrix)
+		identity = [[float(i == j) for i in range(n)] for j in range(n)]
+		augmented = [row + identity_row for row, identity_row in zip(matrix, identity)]
+		for i in range(n):
+			pivot = augmented[i][i]
+			for j in range(2 * n):
+				augmented[i][j] /= pivot
+			for k in range(n):
+				if k != i:
+					factor = augmented[k][i]
+					for j in range(2 * n):
+						augmented[k][j] -= factor * augmented[i][j]
+    
+		inverse = [row[n:] for row in augmented]
+		return inverse
+
+	def compute(self,point :list, data :list[list]) -> float:
+		"""
+		Calculate the Mahalanobis distance between a point and a dataset.
+    
+		:param point: A point as a list of coordinates
+		:param data: A dataset as a list of points (list of lists)
+		:return: Mahalanobis distance between the point and the dataset
+		:raises ValueError: If the point dimensions do not match the dataset dimensions
+		! lever une execption si la matrice est singulière
+		"""
+    
+		mean_data = self.mean(data)
+		cov_matrix = self.covariance_matrix(data, mean_data)
+		cov_matrix_inv = self.matrix_inverse(cov_matrix)
+    
+		diff = [point[i] - mean_data[i] for i in range(len(point))]
+    
+		# Matrix multiplication: diff^T * cov_matrix_inv * diff
+		result = 0
+		for i in range(len(diff)):
+			for j in range(len(diff)):
+				result += diff[i] * cov_matrix_inv[i][j] * diff[j]
+    
+		return result**0.5
+		
+	def example(self):
+		self.obj1_example=Vector.vec_float_1
+		self.obj2_example = [[2, 1, 0],[2, 3, 4],[3, 4, 5],[4, 5, 6]]
+		#super().example()
+		
+
+
+
+
+class MahalanobisTaguchi(Distance):
+	
+    def __init__(self, reference_group :list[list])-> None:
+        """
+        Initialize the MahalanobisTaguchi class with a reference group.
+        
+        :param reference_group: A list of lists where each inner list is a data point in the reference group.
+        """
+        super().__init__()
+        self.type='vec_float'
+
+
+        self.reference_group = reference_group
+        self.mean_vector = self.calculate_mean_vector()
+        self.covariance_matrix = Matrix.covariance_matrix(reference_group)
+        self.inverse_covariance_matrix = Matrix.invert_matrix(self.covariance_matrix)
+
+    def calculate_mean_vector(self):
+        """
+        Calculate the mean vector of the reference group.
+
+        :return: A list representing the mean vector.
+        """
+        num_points = len(self.reference_group)
+        num_dimensions = len(self.reference_group[0])
+
+        mean_vector = [0] * num_dimensions
+
+        for point in self.reference_group:
+            for i in range(num_dimensions):
+                mean_vector[i] += point[i]
+
+        mean_vector = [x / num_points for x in mean_vector]
+        return mean_vector
+
+    def compute(self, data_point)-> float:
+        """
+        Calculate the Mahalanobis-Taguchi distance for a given data point.
+
+        :param data_point: A list representing the data point to be evaluated.
+        :return: The Mahalanobis-Taguchi distance as a float.
+        """
+        diff_vector = [data_point[i] - self.mean_vector[i] for i in range(len(self.mean_vector))]
+        
+        # Matrix multiplication with the inverse covariance matrix
+        temp_vector = [0] * len(diff_vector)
+        for i in range(len(diff_vector)):
+            for j in range(len(diff_vector)):
+                temp_vector[i] += diff_vector[j] * self.inverse_covariance_matrix[j][i]
+
+        # Final dot product to get the Mahalanobis-Taguchi distance
+        distance_squared = sum(temp_vector[i] * diff_vector[i] for i in range(len(diff_vector)))
+        return distance_squared ** 0.5
+        
+    def example(self):
+      self.obj1_exemple=Vector.vec_float_1
+      self.obj2_exemple = [[2, 1, 0],[2, 3, 4],[3, 4, 5],[4, 5, 6]]
+      #super().example()
+
+
