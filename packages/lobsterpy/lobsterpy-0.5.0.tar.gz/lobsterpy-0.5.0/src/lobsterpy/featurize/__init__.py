550c7e5fdafaecc73bf3d45816fad9540e141a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:06:00 +0000

    Add package lobsterpy/0.5.0/lobsterpy-0.5.0.tar.gz

---
+++ packages/lobsterpy/lobsterpy-0.5.0.tar.gz/lobsterpy-0.5.0/src/lobsterpy/featurize/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) lobsterpy development team
+# Distributed under the terms of a BSD 3-Clause "New" or "Revised" License
+
+"""This package provides the modules for featurzing Lobster data ready for ML studies."""
+
+from .utils import get_file_paths, get_structure_path
