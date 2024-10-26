d6338925566539280430270675f47c9a8a619157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 15:04:53 +0000

    Add package pywlf/0.0.6/pywlf-0.0.6.tar.gz

---
+++ packages/pywlf/pywlf-0.0.6.tar.gz/pywlf-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VNU = 6
+VERSION = f'0.0.{VNU}'
+README = ''
+with open("README.md", "r", encoding="utf-8") as fh:
+    README = fh.read()
+
+setup(name="pywlf",
+      version=VERSION,
+      keywords=["wlflog", "wlffile", "wlfdate", "wlfssh", "wlfcore", "wlfinfluxdb", "wlfmysql", "wlfredis", "wlfminio", "wlfkafka", "wlfhttp"],
+      description="Common utils for python3.",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      license="MIT Licence",
+      url="https://github.com/waisaa/tools-python3-wlf",
+      author="waisaa",
+      author_email="waisaa@qq.com",
+      packages=find_packages(),
+      include_package_data=True,
+      platforms="any",
+      python_requires='>=3.7',
+      install_requires=['colorlog', 'chardet'])
