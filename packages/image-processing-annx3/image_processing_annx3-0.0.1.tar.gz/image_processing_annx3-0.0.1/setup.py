b96d72ef668e32b049f6c8697042b8aa7d1fce89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:07:25 +0000

    Add package image-processing-annx3/0.0.1/image_processing_annx3-0.0.1.tar.gz

---
+++ packages/image-processing-annx3/image_processing_annx3-0.0.1.tar.gz/image_processing_annx3-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing-annx3",
+    version="0.0.1",
+    author="annx3",
+    author_email="ana.sara.sc@hotmail.com",
+    description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anascunha/image-processing-package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
