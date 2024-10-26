79ff2836403fa7cee561d68e4245d3404fa83413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 06:04:47 +0000

    Add package circuitmatter/0.4.0/circuitmatter-0.4.0-py3-none-any.whl

---
+++ packages/circuitmatter/circuitmatter-0.4.0-py3-none-any.whl/circuitmatter/utility/random.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Scott Shawcroft for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import os
+import secrets
+
+urandom = os.urandom
+
+randbelow = secrets.randbelow
