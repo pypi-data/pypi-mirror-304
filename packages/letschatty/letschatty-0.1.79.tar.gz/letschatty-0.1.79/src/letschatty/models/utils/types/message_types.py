16dd9650bf0a8f33209737b021ff264980923c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:47 +0000

    Add package letschatty/0.1.79/letschatty-0.1.79.tar.gz

---
+++ packages/letschatty/letschatty-0.1.79.tar.gz/letschatty-0.1.79/src/letschatty/models/utils/types/message_types.py
@@ -0,0 +1,27 @@
+# Tipos de mensajes disponibles
+from enum import StrEnum
+
+class MessageType(StrEnum):
+    TEXT = "text"
+    IMAGE = "image"
+    VIDEO = "video"
+    DOCUMENT = "document"
+    STICKER = "sticker"
+    AUDIO = "audio"
+    REACTION="reaction"
+    CENTRAL="central"
+    CONTACT = "contacts"
+    LOCATION = "location"
+    UNSUPPORTED = "unsupported"
+    INTERACTIVE = "interactive"
+    SYSTEM = "system"
+    ERROR = "error"
+    
+    @staticmethod
+    def values():
+        return [member.value for member in MessageType]
+    
+class MessageSubtype(StrEnum):
+    TEMPLATE = "template"
+    CHATTY_RESPONSE = "chatty_response"
+    NONE = ""
