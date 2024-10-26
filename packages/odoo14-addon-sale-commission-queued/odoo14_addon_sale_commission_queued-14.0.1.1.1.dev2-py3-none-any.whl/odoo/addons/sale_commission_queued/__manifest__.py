d1ea1d8362a65e0b6457e847cb2210237a53852d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:07:24 +0000

    Add package odoo14-addon-sale-commission-queued/14.0.1.1.1.dev2/odoo14_addon_sale_commission_queued-14.0.1.1.1.dev2-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-commission-queued/odoo14_addon_sale_commission_queued-14.0.1.1.1.dev2-py3-none-any.whl/odoo/addons/sale_commission_queued/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sales commissions queued",
+    "version": "14.0.1.1.0",
+    "author": "Ilyas, Ooops404," "Odoo Community Association (OCA)",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": ["sale_commission", "queue_job"],
+    "website": "https://github.com/OCA/commission",
+    "maintainers": ["renda-dev", "aleuffre"],
+    "data": [
+        "wizard/wizard_invoice.xml",
+    ],
+    "installable": True,
+}
