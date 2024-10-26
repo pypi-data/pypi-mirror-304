09879819569578dd885e676ff4ad4e6d15cd23a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 05:04:20 +0000

    Add package cloudflare-ddns-updater/0.1.30/cloudflare_ddns_updater-0.1.30.tar.gz

---
+++ packages/cloudflare-ddns-updater/cloudflare_ddns_updater-0.1.30.tar.gz/cloudflare_ddns_updater-0.1.30/cloudflare_ddns_updater/constants.py
@@ -0,0 +1,10 @@
+CONFIG_FILE = 'cloudflare_config.json'
+CONFIG_DIR = '/etc/ip_updater'
+LOG_FILE_PATH = '/var/log/ip_update.log'
+
+
+# These are for troubleshooting on dev machine
+# ______________________________________
+# CONFIG_DIR = 'ip_updater'           # |
+# LOG_FILE_PATH = 'ip_update.log'     # |
+# ______________________________________|
