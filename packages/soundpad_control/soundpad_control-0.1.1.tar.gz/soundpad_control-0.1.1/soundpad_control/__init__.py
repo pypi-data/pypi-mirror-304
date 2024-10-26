f8c3da972eaf4f0300663f9e0d209d6ed28ee4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-26 14:05:50 +0000

    Add package soundpad_control/0.1.1/soundpad_control-0.1.1.tar.gz

---
+++ packages/soundpad_control/soundpad_control-0.1.1.tar.gz/soundpad_control-0.1.1/soundpad_control/__init__.py
@@ -0,0 +1,20 @@
+"""
+Soundpad Control API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for controlling Soundpad remotely.
+
+:copyright: (c) 2024-present Ilya Kokhanovsky
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'soundpad_control'
+__author__ = 'Ilya Kokhanovsky'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2024-present Ilya Kokhanovsky'
+__version__ = '0.1.0'
+
+from .remote_control import SoundpadRemoteControl
+
+__all__ = ['SoundpadRemoteControl']
