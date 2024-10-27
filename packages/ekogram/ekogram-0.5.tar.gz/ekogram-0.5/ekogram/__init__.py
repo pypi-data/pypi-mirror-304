33cf480577da35941e119f45734400cd80b29827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:15 +0000

    Add package ekogram/0.5/ekogram-0.5.tar.gz

---
+++ packages/ekogram/ekogram-0.5.tar.gz/ekogram-0.5/ekogram/__init__.py
@@ -0,0 +1,3 @@
+from .ekogram import Bot, Markup, InputFile, InputMediaAnimation, InputMediaPhoto, InputMediaVideo
+
+__all__ = ['Bot', 'Markup', 'InputFile', 'InputMediaAnimation', 'InputMediaPhoto', 'InputMediaVideo']
