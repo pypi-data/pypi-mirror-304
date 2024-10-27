7ef08c7af57344d6e6114c62ad4e13644c73de69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:04 +0000

    Add package pidentity/0.1.2/pidentity-0.1.2-py3-none-any.whl

---
+++ packages/pidentity/pidentity-0.1.2-py3-none-any.whl/pidentity/__init__.py
@@ -0,0 +1,3 @@
+from pidentity.contract import Contract, Condition
+from pidentity.control import Control
+from pidentity.guard import Guard
---
+++ packages/pidentity/pidentity-0.1.2-py3-none-any.whl/pidentity/database.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+from sqlite3 import Cursor
+
+
+SQL = '''
+    CREATE TABLE IF NOT EXISTS contracts (
+        upon varchar(32) NOT NULL,
+        unto varchar(32) NOT NULL,
+        PRIMARY KEY (upon, unto)
+    );
+
+    CREATE TABLE IF NOT EXISTS conditions (
+        upon varchar(32) NOT NULL,
+        unto varchar(32) NOT NULL,
+        what varchar(32) NOT NULL CHECK(what in ('context', 'content', 'contact')),
+        condition JSON NOT NULL,
+        timestamped timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY(upon, unto, what),
+        FOREIGN KEY(upon, unto) references pidentity_contracts(upon, unto)
+    );
+'''
+
+
+INSERT_CONDITIONS_SQL = 'INSERT INTO conditions (upon, unto, what, condition) VALUES (:upon, :unto, :what, :condition);'
+DELETE_CONDITIONS_SQL = 'DELETE FROM conditions WHERE upon = :upon AND unto = :unto AND what = :what;'
+UPDATE_CONDITIONS_SQL = 'UPDATE conditions SET upon = :upon, unto = :unto, what = :what, conditions := condition;'
+SELECT_CONDITIONS_SQL = 'SELECT condition FROM conditions WHERE upon = :upon AND unto = :unto AND what = :what;'
