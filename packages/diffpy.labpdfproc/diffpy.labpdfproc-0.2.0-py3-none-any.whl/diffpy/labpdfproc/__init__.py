648b78bf56355ff35531f37626abcf56c5237bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:52 +0000

    Add package diffpy.labpdfproc/0.2.0/diffpy.labpdfproc-0.2.0-py3-none-any.whl

---
+++ packages/diffpy.labpdfproc/diffpy.labpdfproc-0.2.0-py3-none-any.whl/diffpy/labpdfproc/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.labpdfproc/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""Tools for processing x-ray powder diffraction data from laboratory sources."""
+
+# package version
+from diffpy.labpdfproc.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
