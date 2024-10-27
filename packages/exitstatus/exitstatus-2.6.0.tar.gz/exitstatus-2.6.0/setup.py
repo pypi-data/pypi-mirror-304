573e365eed5fb649b79e707692671f503010149b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:58 +0000

    Add package exitstatus/2.6.0/exitstatus-2.6.0.tar.gz

---
+++ packages/exitstatus/exitstatus-2.6.0.tar.gz/exitstatus-2.6.0/setup.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import setuptools
+
+project_dir = Path(__file__).parent
+
+setuptools.setup(
+    name="exitstatus",
+    version="2.6.0",
+    description="POSIX exit status codes",
+    long_description=project_dir.joinpath("README.rst").read_text(encoding="utf-8"),
+    keywords="exit status POSIX",
+    author="John Hagen",
+    author_email="johnthagen@gmail.com",
+    url="https://github.com/johnthagen/exitstatus",
+    packages=["exitstatus"],
+    package_data={"exitstatus": ["py.typed"]},
+    python_requires=">=3.9",
+    zip_safe=False,
+    license="MIT",
+    license_files=["LICENSE.txt"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Typing :: Typed",
+    ],
+)
