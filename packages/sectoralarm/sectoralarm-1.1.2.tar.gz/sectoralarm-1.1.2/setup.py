603c3f8cf7689605d9e0f87be42cd82c020d7dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:06:15 +0000

    Add package sectoralarm/1.1.2/sectoralarm-1.1.2.tar.gz

---
+++ packages/sectoralarm/sectoralarm-1.1.2.tar.gz/sectoralarm-1.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sectoralarm',
+    version='1.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Jonathan Petersson',
+    author_email='jpetersson@garnser.se',
+    description='A Python library for interacting with the Sector Alarm API.',
+    url='https://github.com/garnser/sector_alarm',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
