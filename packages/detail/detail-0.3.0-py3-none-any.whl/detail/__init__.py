99d1a47eed65d6a18a3e48c852281b123d3afa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:53 +0000

    Add package detail/0.3.0/detail-0.3.0-py3-none-any.whl

---
+++ packages/detail/detail-0.3.0-py3-none-any.whl/detail/__init__.py
@@ -0,0 +1,14 @@
+from detail.core import Commit, Note, NoteRange, Notes, Tag, detail, lint, log
+from detail.version import __version__
+
+__all__ = [
+    "Commit",
+    "Note",
+    "NoteRange",
+    "Notes",
+    "lint",
+    "log",
+    "detail",
+    "Tag",
+    "__version__",
+]
