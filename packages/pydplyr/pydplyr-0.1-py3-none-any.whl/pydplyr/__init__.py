2f19cdebef609ccf64a4f42e3e2d8dcf2e899650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:55 +0000

    Add package pydplyr/0.1/pydplyr-0.1-py3-none-any.whl

---
+++ packages/pydplyr/pydplyr-0.1-py3-none-any.whl/pydplyr/__init__.py
@@ -0,0 +1,7 @@
+# pydplyr/__init__.py
+
+from .arrange import *
+from .filter import *
+from .mutate import *
+from .select import *
+from .summarize import *
---
+++ packages/pydplyr/pydplyr-0.1-py3-none-any.whl/pydplyr/select.py
@@ -0,0 +1,9 @@
+def select():
+    """
+    A simple select function that prints a message when called.
+    """
+    return "This is a select function"
+
+
+if __name__ == "__main__":
+    select()
