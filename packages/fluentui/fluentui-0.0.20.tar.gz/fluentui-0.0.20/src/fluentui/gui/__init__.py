220a367240b9ae8aa51adec3b1cec90fe00c9498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:56 +0000

    Add package fluentui/0.0.20/fluentui-0.0.20.tar.gz

---
+++ packages/fluentui/fluentui-0.0.20.tar.gz/fluentui-0.0.20/src/fluentui/gui/__init__.py
@@ -0,0 +1,6 @@
+from .Action import Action
+from .Font import Font
+from .FontDatabase import FontDatabase
+# from .RowModel import RowModel
+# from .StdItem import StdItem
+# from .StdModel import StdModel
