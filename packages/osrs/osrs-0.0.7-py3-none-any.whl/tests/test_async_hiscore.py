a90fe58a21b6a3fed30335d6069ba2c71e40f6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:48 +0000

    Add package osrs/0.0.7/osrs-0.0.7-py3-none-any.whl

---
+++ packages/osrs/osrs-0.0.7-py3-none-any.whl/tests/test_async_hiscore.py
@@ -0,0 +1,35 @@
+import pytest
+from aiohttp import ClientSession
+
+from src.async_api.osrs.hiscores import Mode, PlayerStats, hiscore
+from src.exceptions import PlayerDoesNotExist
+
+
+@pytest.mark.asyncio
+async def test_get_valid():
+    hiscore_instance = hiscore()
+    async with ClientSession() as session:
+        player_stats = await hiscore_instance.get(
+            mode=Mode.OLDSCHOOL,
+            player="extreme4all",
+            session=session,
+        )
+
+        # Assertions to confirm the response is correct
+        assert isinstance(
+            player_stats, PlayerStats
+        ), "The returned object is not of type PlayerStats"
+        assert player_stats.skills, "Skills data should not be empty"
+        assert player_stats.activities, "Activities data should not be empty"
+
+
+@pytest.mark.asyncio
+async def test_get_invalid():
+    hiscore_instance = hiscore()
+    async with ClientSession() as session:
+        with pytest.raises(PlayerDoesNotExist):
+            _ = await hiscore_instance.get(
+                mode=Mode.OLDSCHOOL,
+                player="This_is_not_a_valid_name",
+                session=session,
+            )
