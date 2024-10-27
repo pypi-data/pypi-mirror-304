fc6004339b54a671066a005ac05564fe28f2b31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:45 +0000

    Add package blue-plugin/3.173.1/blue_plugin-3.173.1.tar.gz

---
+++ packages/blue-plugin/blue_plugin-3.173.1.tar.gz/blue_plugin-3.173.1/blue_plugin/.abcli/tests/help.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+function test_blue_plugin_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "blue_plugin" \
+        "blue_plugin browse" \
+        "blue_plugin leaf" \
+        "blue_plugin node" \
+        "blue_plugin node leaf" \
+        "blue_plugin task"; do
+        abcli_eval ,$options \
+            $module help
+        [[ $? -ne 0 ]] && return 1
+    done
+
+    return 0
+}
