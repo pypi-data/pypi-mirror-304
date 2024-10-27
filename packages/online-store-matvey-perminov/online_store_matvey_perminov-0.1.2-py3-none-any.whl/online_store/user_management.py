23bb655a9c61aea67f0220fd123699bbd98ea3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:06:04 +0000

    Add package online-store-matvey-perminov/0.1.2/online_store_matvey_perminov-0.1.2-py3-none-any.whl

---
+++ packages/online-store-matvey-perminov/online_store_matvey_perminov-0.1.2-py3-none-any.whl/online_store/user_management.py
@@ -0,0 +1,30 @@
+class UserManager:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, user_id, user_data):
+        if user_id in self.users:
+            print(f"Клиент с ID {user_id} уже существует")
+            return
+        self.users[user_id] = user_data
+        print(f"Клиент с ID {user_id} добавлен")
+
+    def remove_user(self, user_id):
+        if user_id not in self.users:
+            print(f"Клиент с ID {user_id} не найден")
+            return
+        del self.users[user_id]
+        print(f"Клиент с ID {user_id} удалён")
+
+    def update_user(self, user_id, user_data):
+        if user_id not in self.users:
+            print(f"Клиент с ID {user_id} не найден")
+            return
+        self.users[user_id] = user_data
+        print(f"Данные клиента с ID {user_id} обновлены")
+
+    def find_user(self, user_id):
+        if user_id not in self.users:
+            print(f"Клиент с ID {user_id} не найден")
+            return
+        return {user_id: self.users[user_id]}
