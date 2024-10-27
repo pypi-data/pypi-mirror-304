8e994dc03e4a884027ef9e35df37011f51ff94de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:40 +0000

    Add package nilesh-library/1.0.0/nilesh_library-1.0.0.tar.gz

---
+++ packages/nilesh-library/nilesh_library-1.0.0.tar.gz/nilesh_library-1.0.0/nilesh_library/__init__.py
@@ -0,0 +1,1 @@
+from .nilesh_library import get
---
+++ packages/nilesh-library/nilesh_library-1.0.0.tar.gz/nilesh_library-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='nilesh_library',
+    version='1.0.0',
+    description='A simple library to get numbers from 1 to 6',
+    author='Nilesh Patil',
+    author_email='patil53nilesh@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
