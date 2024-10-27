a1c058b5334b6daaa5002a5d67c479d3c026951d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:30 +0000

    Add package AcSecurity/0.4.13/acsecurity-0.4.13.tar.gz

---
+++ packages/AcSecurity/acsecurity-0.4.13.tar.gz/acsecurity-0.4.13/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AcSecurity',
+    version='0.4.13',
+    packages=find_packages(),
+    install_requires=[
+        'pip-audit',
+        'pylint',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'acsecurity=AcSecurity.run_scanner:main',
+        ],
+    },
+    description='A security scanner for applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Austin Cabler',
+    author_email='austin_cabler@icloud.com',
+    url='https://github.com/austincabler13/AcSecurity',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
