9c6311996763b36518b4bf1b503572bb50081e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:50 +0000

    Add package bgc-cloud/1.0.4/bgc_cloud-1.0.4.tar.gz

---
+++ packages/bgc-cloud/bgc_cloud-1.0.4.tar.gz/bgc_cloud-1.0.4/bgc_cloud/auth.py
@@ -0,0 +1,43 @@
+from .controller import BaseController
+
+
+class BGCAuth(BaseController):
+    def create_user_with_email_and_password(self, email, password, **kwargs):
+        return self._submit(
+            "auth",
+            "create_user_with_email_and_password", **{
+                "email": email, "password": password,
+                **kwargs
+            })
+
+    def create_user_with_phone_and_password(self, phone, password, **kwargs):
+        return self._submit(
+            "auth",
+            "create_user_with_phone_and_password", **{
+                "phone": phone, "password": password,
+                **kwargs
+            })
+
+    def login_with_phone_and_password(self, phone, password, **kwargs):
+        return self._submit(
+            "auth",
+            "login_with_phone_and_password", **{
+                "phone": phone, "password": password,
+                **kwargs
+            })
+
+    def login_with_email_and_password(self, email, password, **kwargs):
+        return self._submit(
+            "auth",
+            "login_with_email_and_password", **{
+                "email": email, "password": password,
+                **kwargs
+            })
+
+    def send_email_recovery_otp(self, email, **kwargs):
+        return self._submit(
+            "auth",
+            "send_email_recovery_otp", **{
+                "email": email,
+                **kwargs
+            })
