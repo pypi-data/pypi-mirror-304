3e867f8323bbbc1ce915143c861ac5d5866df7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 03:13:05 +0000

    Add package neojelll-url-shortener-api/0.1.9/neojelll_url_shortener_api-0.1.9-py3-none-any.whl

---
+++ packages/neojelll-url-shortener-api/neojelll_url_shortener_api-0.1.9-py3-none-any.whl/api/main.py
@@ -0,0 +1,9 @@
+from loguru import logger
+from .api import app
+import asyncio
+import uvicorn
+
+
+def run() -> None:
+    logger.info("Starting the server...")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
