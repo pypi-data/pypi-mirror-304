bb2399a2315435b52367b3731282a97453eb8b99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:46 +0000

    Add package pybalt/2024.10.27/pybalt-2024.10.27.tar.gz

---
+++ packages/pybalt/pybalt-2024.10.27.tar.gz/pybalt-2024.10.27/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='pybalt',
+    version='2024.10.27',
+    author='nichind',
+    author_email='nichinddev@gmail.com',
+    description='',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nichind/pybalt',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp',
+        'pytube',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.8'
+)
