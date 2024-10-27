5a4d544bc27b6faf846068893643451b713bd705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:06:07 +0000

    Add package ekogram/0.1/ekogram-0.1.tar.gz

---
+++ packages/ekogram/ekogram-0.1.tar.gz/ekogram-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+package_name = 'ekogram'
+packages = find_packages(include=[package_name, f'{package_name}.*'])
+
+setup(
+    name=package_name,
+    version='0.1',
+    description='Lightweight library for working with Telegram Bot Api version 7.10',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SiriRSST/Ekogram',
+    author='Siri-Team',
+    author_email='siriteamrs@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'],
+    keywords=['telebot', 'bot', 'gram', 'pytelegrambotapi', 'telegram'],
+    packages=packages,
+    install_requires=['requests'],
+    python_requires='>=3.7'
+)
