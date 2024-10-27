dd6870686479c232fd962e289b481fa28903c5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:18 +0000

    Add package CatScraper/0.1.8/catscraper-0.1.8.tar.gz

---
+++ packages/CatScraper/catscraper-0.1.8.tar.gz/catscraper-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CatScraper',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'Epate',
+        'panda'
+    ],
+    # 其他元数据
+    author='KitchenSweeper',
+    author_email='xiaoxiaogzs@outlook.com',
+    description='CatScraper is a Python library that automates tasks on the Codemao (编程猫) platform, helping users perform repetitive actions like liking, collecting, and more. With its easy-to-use code and customizable functions, CatScraper simplifies workflows and boosts productivity for developers and Codemao users alike.',
+    keywords='codemao',
+    url='https://github.com/kitswe/CatScraper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
