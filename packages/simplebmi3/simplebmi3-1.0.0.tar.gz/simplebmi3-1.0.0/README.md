ca49448bad107e6664d9039772ceeab10ba00bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 03:13:19 +0000

    Add package simplebmi3/1.0.0/simplebmi3-1.0.0.tar.gz

---
+++ packages/simplebmi3/simplebmi3-1.0.0.tar.gz/simplebmi3-1.0.0/README.md
@@ -0,0 +1,10 @@
+# SimpleBMI3
+
+SimpleBMI3 is a minimalistic BMI (Body Mass Index) calculator that allows users to quickly calculate their BMI based on weight and height.
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install simplebmi3
---
+++ packages/simplebmi3/simplebmi3-1.0.0.tar.gz/simplebmi3-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simplebmi3',  # Updated package name
+    version='1.0.0',
+    author='Muhammad Sohaib Hassan',
+    author_email='muhammadsohaibhassan3@gmail.com',
+    description='A minimal BMI calculator',
+    long_description=open('README.md').read(),  # Reads from README.md if it exists
+    long_description_content_type='text/markdown',  # Specify the format of long description
+    packages=find_packages(),  # Automatically find packages in the current directory
+    entry_points={
+        'console_scripts': [
+            'simple-bmi3 = simplebmi3.__main__:main',  # Updated entry point for command line
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the minimum Python version required
+)
