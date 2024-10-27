dbbac149149d6d528816e31995e06ace29b12694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:38 +0000

    Add package sdk-mocks/0.1.0/sdk_mocks-0.1.0-py3-none-any.whl

---
+++ packages/sdk-mocks/sdk_mocks-0.1.0-py3-none-any.whl/sdk_mocks/__init__.py
@@ -0,0 +1,10 @@
+"""
+PythonSDKMocks: A library of pre-built mocks for popular Python SDKs.
+"""
+
+__version__ = "0.1.0"
+__author__ = "BlaiseLabs"
+
+# Importing core classes/functions
+from .MockManager import MockManager
+# Add more imports as needed
