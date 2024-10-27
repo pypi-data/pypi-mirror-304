968e8bdc26f175878aea1ff1111366c4a45871f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:28 +0000

    Add package cartinize/0.1.1/cartinize-0.1.1-py3-none-any.whl

---
+++ packages/cartinize/cartinize-0.1.1-py3-none-any.whl/cartinize/leet.py
@@ -0,0 +1,10 @@
+import json
+
+
+def leet(text):
+    with open("cartinize/assets/leet_dict.json", "r") as file:
+        leet_dict = json.load(file)
+
+    leet_text = "".join(leet_dict.get(char, char) for char in text)
+
+    return leet_text
