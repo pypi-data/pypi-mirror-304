439235f9466858e708cdd1c39f1046a0cab09f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:24 +0000

    Add package ape-tokens/0.8.1/ape_tokens-0.8.1-py3-none-any.whl

---
+++ packages/ape-tokens/ape_tokens-0.8.1-py3-none-any.whl/ape_tokens/__init__.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+from ape import plugins
+
+
+@plugins.register(plugins.ConversionPlugin)
+def converters():
+    from ape.types.address import AddressType
+
+    from .converters import TokenAmountConverter, TokenSymbolConverter
+
+    yield int, TokenAmountConverter
+    yield AddressType, TokenSymbolConverter
+
+
+tokens = None  # NOTE: Initialized lazily
+
+
+def __getattr__(name: str) -> Any:
+    if name == "tokens":
+
+        global tokens
+        if tokens is None:
+            from .managers import TokenManager as _TokenManager
+
+            tokens = _TokenManager()
+
+        return tokens
+
+    else:
+        raise AttributeError(name)
+
+
+__all__ = [
+    "tokens",
+]
