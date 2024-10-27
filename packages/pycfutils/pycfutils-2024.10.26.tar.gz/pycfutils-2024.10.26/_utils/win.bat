95c26396add0d3a617f24da40bc62986306a8ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:09 +0000

    Add package pycfutils/2024.10.26/pycfutils-2024.10.26.tar.gz

---
+++ packages/pycfutils/pycfutils-2024.10.26.tar.gz/pycfutils-2024.10.26/_utils/win.bat
@@ -0,0 +1,34 @@
+@echo off
+
+setlocal enabledelayedexpansion enableextensions
+
+if defined NO_TEST (
+    goto :eof
+)
+
+:_test
+    echo Testing...
+    set PYTHONPATH=
+    :: @TODO - cfati: End with \ (if no pattern given)
+    if not defined TEST_VENV_PATTERN (
+        :: set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\"
+        set TEST_VENV_PATTERN="e:\Work\Dev\VEnvs\*test0"
+    )
+    if not defined TEST_WHEEL_DIR (
+        set TEST_WHEEL_DIR="e:\Work\Dev\Repos\GitHub\CristiFati\pycfutils\src\dist"
+    )
+    call bat_funcs dirname %TEST_VENV_PATTERN% _VENVS_DIR
+    set _VENVS_DIR=%_VENVS_DIR:"=%
+    for /f %%g in ('dir /b /ad /a-l %TEST_VENV_PATTERN:"=%') do (
+        echo Using environment: "%_VENVS_DIR%\%%g"
+        call "%_VENVS_DIR%\%%g\Scripts\activate.bat"
+        python -VV
+        python -m pip uninstall -y pycfutils
+        python -m pip -v install --no-index -f %TEST_WHEEL_DIR% pycfutils
+        python -m unittest discover -s "%_VENVS_DIR%\%%g\Lib\site-packages\pycfutils\tests"
+        python -c "import sys, pycfutils.gui as pg, pycfutils.io as pio, pycfutils.setup.command.build_clibdll as pscbcd;print('Press a key: ', pio.read_key(1));print(pg.message_box('MBox', sys.version, 320, 200))"
+        python -m pip uninstall -y pycfutils
+        call deactivate
+    )
+    goto :eof
+
