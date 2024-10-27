154e08d37fdd82db15a8db53a4eff864b872a88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:05:50 +0000

    Add package AnonyMate/0.1.2/AnonyMate-0.1.2.tar.gz

---
+++ packages/AnonyMate/AnonyMate-0.1.2.tar.gz/AnonyMate-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='AnonyMate',
+    version='0.1.2',
+    description='A comprehensive toolkit for data anonymization, masking, and encryption.',
+    long_description=long_description,  
+    long_description_content_type='text/markdown',  
+    author='Pasindu Bandara',
+    author_email='pasindubandara99@gmail.com',
+    url='https://github.com/yourusername/AnonyMate',  
+    packages=find_packages(include=["anonymate", "anonymate.*"]),
+    install_requires=[
+        'cryptography',
+        'faker',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
