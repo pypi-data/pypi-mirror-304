adbd0ab848354c794991e13da9fe9b5d5b9d1d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:06:09 +0000

    Add package StratLab/1.0.19/StratLab-1.0.19.tar.gz

---
+++ packages/StratLab/StratLab-1.0.19.tar.gz/StratLab-1.0.19/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='StratLab',
+    version='1.0.19',
+    packages=find_packages(where='/Users/bobbystickles/Desktop/StratLab', include=['StratLab']),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'yfinance',
+        'matplotlib'
+    ],
+    author='Robert Stickles',
+    author_email='rstickles16@outlook.com',
+    description='StratLab is a Python library designed to backtest stock market strategies.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rstickles16/StratLab',
+    license='MIT',
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
