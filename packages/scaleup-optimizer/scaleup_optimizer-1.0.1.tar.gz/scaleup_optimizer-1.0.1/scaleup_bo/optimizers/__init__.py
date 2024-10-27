5d47409df55fceb4c2b5edab0c81da7cbac273fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:07 +0000

    Add package scaleup-optimizer/1.0.1/scaleup_optimizer-1.0.1.tar.gz

---
+++ packages/scaleup-optimizer/scaleup_optimizer-1.0.1.tar.gz/scaleup_optimizer-1.0.1/scaleup_bo/optimizers/__init__.py
@@ -0,0 +1,5 @@
+from .small_scale_opt import SmallScaleBayesianOptimizer
+from .large_scale_opt import LargeScaleBayesianOptimizer
+
+
+__all__ = ["SmallScaleBayesianOptimizer", "LargeScaleBayesianOptimizer"]
