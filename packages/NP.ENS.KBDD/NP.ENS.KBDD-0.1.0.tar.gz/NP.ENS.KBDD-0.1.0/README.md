7b36f632b9dbbcf477f730cd2af3f564d85b3dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:05:50 +0000

    Add package NP.ENS.KBDD/0.1.0/NP.ENS.KBDD-0.1.0.tar.gz

---
+++ packages/NP.ENS.KBDD/NP.ENS.KBDD-0.1.0.tar.gz/NP.ENS.KBDD-0.1.0/README.md
@@ -0,0 +1,7 @@
+# NP.ENS.KBDD: a nonparametric ensemble knowledge-based and data-driven method for genetic network construction
+
+Updated: October 26 2024
+
+This repository contains code to reproduce the results of simulation and applications in
+
+Chen-Po Liao, Hung-Ching Chang, Chuhsing Kate Hsiao. "NP.ENS.KBDD: a nonparametric ensemble knowledge-based and data-driven method for genetic network construction" (2024)
---
+++ packages/NP.ENS.KBDD/NP.ENS.KBDD-0.1.0.tar.gz/NP.ENS.KBDD-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name='NP.ENS.KBDD',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    description='A package for NP.ENS.KBDD: a nonparametric ensemble knowledge-based and data-driven method for genetic network construction',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Chen-Po Liao',
+    author_email='liaochenpo@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'powerlaw'
+    ],
+    python_requires='>=3.6',
+)
