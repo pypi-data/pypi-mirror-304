d6ca6c2f6e1c4e5ad3f65f3ee622729197728ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:52 +0000

    Add package abadpour/6.65.1/abadpour-6.65.1-py3-none-any.whl

---
+++ packages/abadpour/abadpour-6.65.1-py3-none-any.whl/abadpour/.abcli/tests/build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+function test_CV_build() {
+    local options=$1
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+
+    abcli_eval dryrun=$do_dryrun \
+        "CV build \
+        ${@:2}"
+}
