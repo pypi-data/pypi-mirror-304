c60560664807032f377de0ccb0dc6046c29da17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:08 +0000

    Add package pycfutils/2024.10.26/pycfutils-2024.10.26-py3-none-win32.whl

---
+++ packages/pycfutils/pycfutils-2024.10.26-py3-none-win32.whl/pycfutils/exceptions.py
@@ -0,0 +1,23 @@
+import sys
+
+__all__ = ("CFUtilsException", "ModuleException", "NetworkException")
+
+
+class CFUtilsException(Exception):
+    def __str__(self):
+        if self.__cause__:
+            return f"{super().__str__()} ({self.__cause__})"
+        return super().__str__()
+
+
+class ModuleException(CFUtilsException):
+    pass
+
+
+class NetworkException(CFUtilsException):
+    pass
+
+
+if __name__ == "__main__":
+    print("This script is not meant to be run directly.\n")
+    sys.exit(-1)
