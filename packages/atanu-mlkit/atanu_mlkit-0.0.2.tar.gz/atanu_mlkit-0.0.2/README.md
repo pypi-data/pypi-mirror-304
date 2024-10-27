7143e615c5d4b289101a494bfe336a8c54fa59a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 03:12:38 +0000

    Add package atanu-mlkit/0.0.2/atanu_mlkit-0.0.2.tar.gz

---
+++ packages/atanu-mlkit/atanu_mlkit-0.0.2.tar.gz/atanu_mlkit-0.0.2/README.md
@@ -0,0 +1,15 @@
+# Atanu MLKit
+
+Welcome to the **Atanu MLKit**!
+
+
+[![PyPI version](https://badge.fury.io/py/atanu-mlkit.svg)](https://badge.fury.io/py/atanu-mlkit)
+
+#### Quick Installation
+```bash
+pip install atanu-mlkit
+```
+
+
+
+
---
+++ packages/atanu-mlkit/atanu_mlkit-0.0.2.tar.gz/atanu_mlkit-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atanu_mlkit',
+    version='0.0.2',
+    author="Atanu Debnath",
+    author_email="playatanu@gmail.com",
+    description="Machine Learning Kit",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/playatanu/atanu-mlkit",
+    project_urls={
+        "Documentation": "https://playatanu.github.io/atanu-mlkit/",
+        "Source": "https://github.com/playatanu/atanu-mlkit",
+        "Tracker": "https://github.com/playatanu/atanu-mlkit/issues",
+    },
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+)
