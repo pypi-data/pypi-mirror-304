c59b9952397a090b6df73fd2a916d85808c9ae57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:37 +0000

    Add package ekogram/0.1.8/ekogram-0.1.8-py3-none-any.whl

---
+++ packages/ekogram/ekogram-0.1.8-py3-none-any.whl/ekogram/_init_.py
@@ -0,0 +1,18 @@
+__all__ = [
+    'Bot',
+    'Markup',
+    'InputFile',
+    'InputMediaAnimation',
+    'InputMediaPhoto',
+    'InputMediaVideo',
+]
+__version__ = '0.1.8'
+
+from .ekogram import (
+    Bot,
+    Markup,
+    InputFile,
+    InputMediaAnimation,
+    InputMediaPhoto,
+    InputMediaVideo,
+)
