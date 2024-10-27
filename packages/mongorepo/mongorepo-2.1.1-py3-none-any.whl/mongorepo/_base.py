fac8da61675d9f73c3f1943e837f0ab52afa6e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:48 +0000

    Add package mongorepo/2.1.1/mongorepo-2.1.1-py3-none-any.whl

---
+++ packages/mongorepo/mongorepo-2.1.1-py3-none-any.whl/mongorepo/_base.py
@@ -0,0 +1,28 @@
+from dataclasses import Field, dataclass
+from enum import Enum
+from typing import Any, ClassVar, Protocol, TypeVar
+
+
+class DataclassInstance(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
+
+
+DTO = TypeVar('DTO', bound=DataclassInstance)
+_DTOField = TypeVar('_DTOField', bound=DataclassInstance)
+
+
+@dataclass(repr=False, slots=True, eq=False)
+class Index:
+    """Allows to specify extra information about mongodb index."""
+    field: str
+    name: str | None = None
+    desc: bool = True
+    unique: bool = False
+
+
+class Access(int, Enum):
+    """Use to indicate method access in a repository, see also
+    `mongorepo.utils.get_prefix` function."""
+    PUBLIC = 0
+    PROTECTED = 1
+    PRIVATE = 2
