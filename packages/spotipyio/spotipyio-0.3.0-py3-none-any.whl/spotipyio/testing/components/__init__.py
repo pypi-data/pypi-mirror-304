a6aeb5fcd07e539c53c56551faf8d54b3852af06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 03:13:22 +0000

    Add package spotipyio/0.3.0/spotipyio-0.3.0-py3-none-any.whl

---
+++ packages/spotipyio/spotipyio-0.3.0-py3-none-any.whl/spotipyio/testing/components/__init__.py
@@ -0,0 +1,44 @@
+from spotipyio.testing.components.albums import AlbumsInfoTestComponent
+from spotipyio.testing.components.artists import ArtistsInfoTestComponent, ArtistsTopTracksTestComponent
+from spotipyio.testing.components.chapters import ChaptersInfoTestComponent
+from spotipyio.testing.components.current_user import CurrentProfileTestComponent, TopItemsTestComponent
+from spotipyio.testing.components.episodes import EpisodesInfoTestComponent
+from spotipyio.testing.components.playlists import (
+    PlaylistsCoverUpdaterTestComponent,
+    PlaylistsCreatorTestComponent,
+    PlaylistsInfoTestComponent,
+    PlaylistItemsAdderTestComponent,
+    PlaylistsItemsRemoverTestComponent,
+)
+from spotipyio.testing.components.search import SearchItemTestComponent
+from spotipyio.testing.components.tracks import TracksAudioFeaturesTestComponent, TracksInfoTestComponent
+from spotipyio.testing.components.users import UserPlaylistsTestComponent
+
+
+__all__ = [
+    # Albums
+    "AlbumsInfoTestComponent",
+    # Artists
+    "ArtistsInfoTestComponent",
+    "ArtistsTopTracksTestComponent",
+    # Chapter
+    "ChaptersInfoTestComponent",
+    # Current User
+    "CurrentProfileTestComponent",
+    "TopItemsTestComponent",
+    # Episodes
+    "EpisodesInfoTestComponent",
+    # Playlists
+    "PlaylistsCoverUpdaterTestComponent",
+    "PlaylistsCreatorTestComponent",
+    "PlaylistsInfoTestComponent",
+    "PlaylistItemsAdderTestComponent",
+    "PlaylistsItemsRemoverTestComponent",
+    # Search
+    "SearchItemTestComponent",
+    # Tracks
+    "TracksAudioFeaturesTestComponent",
+    "TracksInfoTestComponent",
+    # Users
+    "UserPlaylistsTestComponent",
+]
---
+++ packages/spotipyio/spotipyio-0.3.0-py3-none-any.whl/spotipyio/testing/components/playlists/__init__.py
@@ -0,0 +1,17 @@
+from spotipyio.testing.components.playlists.playlist_cover_updater_test_component import (
+    PlaylistsCoverUpdaterTestComponent,
+)
+from spotipyio.testing.components.playlists.playlists_creator_test_component import PlaylistsCreatorTestComponent
+from spotipyio.testing.components.playlists.playlists_info_test_component import PlaylistsInfoTestComponent
+from spotipyio.testing.components.playlists.playlists_items_adder_test_component import PlaylistItemsAdderTestComponent
+from spotipyio.testing.components.playlists.playlists_items_remover_test_component import (
+    PlaylistsItemsRemoverTestComponent,
+)
+
+__all__ = [
+    "PlaylistsCoverUpdaterTestComponent",
+    "PlaylistsCreatorTestComponent",
+    "PlaylistsInfoTestComponent",
+    "PlaylistItemsAdderTestComponent",
+    "PlaylistsItemsRemoverTestComponent",
+]
