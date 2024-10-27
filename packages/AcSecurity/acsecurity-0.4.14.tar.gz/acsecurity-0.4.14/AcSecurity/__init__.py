11997197bcd0810fc30d8a649b28e2278ad0c173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:10 +0000

    Add package AcSecurity/0.4.14/acsecurity-0.4.14.tar.gz

---
+++ packages/AcSecurity/acsecurity-0.4.14.tar.gz/acsecurity-0.4.14/AcSecurity/__init__.py
@@ -0,0 +1,11 @@
+"""
+AcSecurity package.
+
+This package provides functionality to scan applications for vulnerabilities
+and code quality issues.
+"""
+
+from .scanner import AcSecurity
+__all__ = ['AcSecurity']
+__author__ = 'Austin Cabler'
+__version__ = '0.4.14'
