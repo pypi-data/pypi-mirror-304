74802540f096616c50b2a4af109615cc28b4814b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:07 +0000

    Add package Epate/1.5.0/epate-1.5.0.tar.gz

---
+++ packages/Epate/epate-1.5.0.tar.gz/epate-1.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+from pkg_resources import parse_requirements
+
+install_requires = ['requests']
+
+setup(name="Epate",
+      version="1.5.0",
+      author="xiaoxlh",
+      author_email="xiaoxiaogzs@outlook.com",
+      description="A python library for many API of the codemao's website.",
+      long_description="look at https://github.com/xiaoxat/Epate",
+      license="Apache License, Version 2.0",
+      url="https://xihl.cn",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          "Programming Language :: Python :: 3.12",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      packages=find_packages(),
+      install_requires=install_requires,
+      entry_points={'console_scripts': ['test = test.help:main']})
