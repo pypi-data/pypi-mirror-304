29b4ea894cb6b79b8aba0a75f0af7a14a89c47cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:06 +0000

    Add package cartinize/0.2.1/cartinize-0.2.1.tar.gz

---
+++ packages/cartinize/cartinize-0.2.1.tar.gz/cartinize-0.2.1/README.md
@@ -0,0 +1,9 @@
+# (ARt1nesE ^+slime 7raN5lA7OR ++💯-
+
+To-_XO TR4NSlaT3 !++~ 4-slatt+_ me554g3, *XO 2Un ^*💔 mA1n.P`/ _!!_ aND-+forever+ PaSS _woah_ 5TR1n9-+_100 as _!!\*-4n ok+^ 4RgumEn7 !ok🦋!
+
+```shell
+import 
+```
+
+## 7акЖ3 ^Lit*+ ПоЗbОлЯЕт-*xo!_ n3pевОДИт +vampire+ рycсКnй 💫!+ н4 +💀_ yaЗyk +👹+ кАР7n-+ok
---
+++ packages/cartinize/cartinize-0.2.1.tar.gz/cartinize-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.1'
+DESCRIPTION = 'Cartinese translator'
+
+setup(
+    name="cartinize",
+    version=VERSION,
+    author="ktkv419 (Kutikov Pasha)",
+    author_email="<kutikovpasha@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    keywords=['python', 'string', 'translate', 'carti', 'slatt'],
+)
