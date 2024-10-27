0b0b3ccda1fc69b0571d39015ec6cb0ab7dd3854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:05:25 +0000

    Add package illallangi-rdf/0.4.0/illallangi_rdf-0.4.0-py3-none-any.whl

---
+++ packages/illallangi-rdf/illallangi_rdf-0.4.0-py3-none-any.whl/illallangi/rdf/models/__init__.py
@@ -0,0 +1,18 @@
+from illallangi.rdf.models.airline import Airline, AirlineKey
+from illallangi.rdf.models.airport import Airport, AirportKey
+from illallangi.rdf.models.alliance import Alliance, AllianceKey
+from illallangi.rdf.models.course import Course, CourseKey
+from illallangi.rdf.models.residence import Residence, ResidenceKey
+
+__all__ = [
+    "Airline",
+    "AirlineKey",
+    "Airport",
+    "AirportKey",
+    "Alliance",
+    "AllianceKey",
+    "Course",
+    "CourseKey",
+    "Residence",
+    "ResidenceKey",
+]
