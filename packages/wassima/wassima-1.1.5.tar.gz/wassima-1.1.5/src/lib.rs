b2c8b33bf45b9329ec95abf36b04a55783e236d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:06:01 +0000

    Add package wassima/1.1.5/wassima-1.1.5.tar.gz

---
+++ packages/wassima/wassima-1.1.5.tar.gz/wassima-1.1.5/src/lib.rs
@@ -0,0 +1,26 @@
+use pyo3::{prelude::*, types::PyBytes};
+use pyo3::exceptions::PyRuntimeError;
+use rustls_native_certs;
+
+/// Retrieve a list of system DER root CAs
+#[pyfunction]
+fn root_der_certificates(py: Python) -> PyResult<Vec<Bound<'_, PyBytes, >>> {
+    let mut roots = Vec::new();
+    let certs = rustls_native_certs::load_native_certs();
+
+    if certs.is_err() {
+        return Err(PyRuntimeError::new_err("unable to extract root certificates"));
+    }
+
+    for cert in certs.unwrap() {
+        roots.push(PyBytes::new_bound(py, &cert.as_ref().to_vec()));
+    }
+
+    return Ok(roots);
+}
+
+#[pymodule]
+fn _rustls(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(root_der_certificates, m)?)?;
+    Ok(())
+}
