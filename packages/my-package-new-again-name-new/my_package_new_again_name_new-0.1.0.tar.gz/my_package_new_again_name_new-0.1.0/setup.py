0b54dbed917592e18ce1ea81b598c9fc34812eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:38 +0000

    Add package my-package-new-again-name-new/0.1.0/my_package_new_again_name_new-0.1.0.tar.gz

---
+++ packages/my-package-new-again-name-new/my_package_new_again_name_new-0.1.0.tar.gz/my_package_new_again_name_new-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='my-package-new-again-name-new',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'my-script=my_package.module:main_function',
+        ],
+    },
+)
