5689c155692d7392e70ac6216caec307ed3b216a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:20 +0000

    Add package neojelll-url-shortener-api/0.1.12/neojelll_url_shortener_api-0.1.12.tar.gz

---
+++ packages/neojelll-url-shortener-api/neojelll_url_shortener_api-0.1.12.tar.gz/neojelll_url_shortener_api-0.1.12/api/message_broker.py
@@ -0,0 +1,28 @@
+from aiokafka import AIOKafkaProducer
+from .logger import configure_logger
+from loguru import logger
+import os
+import json
+
+
+configure_logger()
+
+
+class MessageBroker(object):
+    def __init__(self):
+        self.producer = AIOKafkaProducer(
+            bootstrap_servers=f"{os.environ["BROKER_HOST"]}:{os.environ["BROKER_PORT"]}",
+            value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+        )
+
+    async def __aenter__(self):
+        await self.producer.start()
+        return self
+
+    async def send_data(self, topic: str, data):
+        logger.debug(f"Send data to message-broker... params: {repr(data)}")
+        await self.producer.send_and_wait(topic, data)
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.producer.flush()
+        await self.producer.stop()
