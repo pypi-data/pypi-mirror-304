7c6e5a93017ddb217bdecb8fb34818aced5103ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 15:05:26 +0000

    Add package lumtina/0.2.5/lumtina-0.2.5-py3-none-any.whl

---
+++ packages/lumtina/lumtina-0.2.5-py3-none-any.whl/lumtina/changelogs.py
@@ -0,0 +1,29 @@
+from .color import color
+from .logger import log
+
+def changelogs():
+# Log the latest update version
+
+    log(f"{color('Lumtina', 'red')} Latest Update: 0.2.5")
+
+# Log the changes made in this version
+    log(f'''{color('Lumtina', 'red')} Added:
+ [ + ] Added new file utils ( delete_file, copy_file, move_file )
+ [ + ] Added new Network Utils ( ping_host, get_ip_info, get_local_ip )
+ [ + ] Modified the README
+ [ - ] Fixed Bugs
+    ''')
+
+    log(f"{color('Lumtina', 'red')} Previous Update: 0.2.2")
+
+# Log the changes made in this version
+    log(f'''{color('Lumtina', 'red')} Added:
+ [ + ] Added Math Functions (add, subtract, multiply, divide, power, factorial, is_even, is_odd)
+ [ + ] Modified the README
+ [ - ] Fixed Bugs
+    ''')
+
+
+
+
+
