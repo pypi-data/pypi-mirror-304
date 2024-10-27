96da9165f036d78b51c740f68ab488b9542c5ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:54 +0000

    Add package abcli/9.367.1/abcli-9.367.1-py3-none-any.whl

---
+++ packages/abcli/abcli-9.367.1-py3-none-any.whl/abcli/.abcli/tests/help.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+function test_abcli_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "abcli" \
+        "abcli pytest"; do
+        abcli_eval ,$options \
+            $module help
+        [[ $? -ne 0 ]] && return 1
+    done
+
+    return 0
+}
