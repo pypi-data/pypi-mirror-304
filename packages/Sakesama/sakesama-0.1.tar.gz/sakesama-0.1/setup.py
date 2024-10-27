060cb1d8f0636ce941b869d8681d153810b193c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:53 +0000

    Add package Sakesama/0.1/sakesama-0.1.tar.gz

---
+++ packages/Sakesama/sakesama-0.1.tar.gz/sakesama-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="Sakesama",
+    version="0.1",
+    authour= "KaijaKuri",
+    author_email="pr4een3@gmail.com",
+    description="This is a speech to text package created by your Kaijakuri"
+
+)
+install_requirements=[
+    'selenium',
+    'webdriver_manager',
+]
+
