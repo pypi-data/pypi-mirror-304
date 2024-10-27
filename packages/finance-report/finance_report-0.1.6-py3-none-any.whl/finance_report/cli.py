e13aaa10c3b6642cc7101d5282ec438202d9086f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:08 +0000

    Add package finance-report/0.1.6/finance_report-0.1.6-py3-none-any.whl

---
+++ packages/finance-report/finance_report-0.1.6-py3-none-any.whl/finance_report/cli.py
@@ -0,0 +1,19 @@
+import argparse
+from finance_report.report_generator import load_transactions, calculate_totals, generate_report
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Генератор отчёта по доходам и расходам.")
+    parser.add_argument("--input-file", required=True, help="Путь к входному CSV-файлу")
+    parser.add_argument("--output-file", help="Путь к выходному TXT-файлу (необязательный)")
+
+    args = parser.parse_args()
+    data = load_transactions(args.input_file)
+    totals = calculate_totals(data)
+    report = generate_report(totals, args.output_file)
+
+    print(report)
+
+
+if __name__ == "__main__":
+    main()
