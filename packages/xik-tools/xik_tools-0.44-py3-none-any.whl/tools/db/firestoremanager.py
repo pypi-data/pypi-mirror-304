24b7e0758f8800cb8342cfec1d9807e750c3f48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:08 +0000

    Add package xik-tools/0.44/xik_tools-0.44-py3-none-any.whl

---
+++ packages/xik-tools/xik_tools-0.44-py3-none-any.whl/tools/db/firestoremanager.py
@@ -0,0 +1,72 @@
+from google.cloud import firestore
+from google.oauth2 import service_account
+from datetime import datetime
+import logging
+
+class FirestoreManager:
+    def __init__(self, credentials_file=None):
+        # 로깅 설정
+        logging.basicConfig(level=logging.INFO)
+        
+        if credentials_file is not None:
+            self.credentials = service_account.Credentials.from_service_account_file(credentials_file)
+            logging.info("Credentials loaded successfully.")
+            self.db = firestore.Client(credentials=self.credentials)
+        else:
+            self.db = firestore.Client()
+        
+        logging.debug("Firestore client initialized successfully.")
+
+    def save_dataframe(self, dict_data: dict, collection_name=None):
+        def _save_dataframes_to_firestore(date_doc_ref, dict_data):                
+                for data_name, dataframe in dict_data.items():
+                    data_doc_ref = date_doc_ref.collection(data_name)
+                    logging.debug(f"Processing collection: {data_name} in Firestore.")
+                    
+                    for index, row in dataframe.iterrows():
+                        doc_key = f'doc_{index}'
+                        doc_ref = data_doc_ref.document(doc_key)
+                        doc_ref.set(row.to_dict())
+                        logging.debug(f"Document {doc_key} added successfully under {date_doc_ref.parent.id}/{date_doc_ref.id}/{data_name}.")
+
+        date_key = datetime.now().strftime("%Y-%m-%d")  # 예: "2024-10-19"
+        logging.debug(f"Date key set to: {date_key}")
+        
+        collection_doc_ref = self.db.collection(collection_name) if collection_name else self.db.collection("default_collection")
+        date_doc_ref = collection_doc_ref.document(date_key)
+        logging.debug(f"Saving data under collection: {collection_name if collection_name else 'default_collection'} with date key: {date_key}")
+
+        _save_dataframes_to_firestore(date_doc_ref, dict_data)
+        logging.info(f"Data saved successfully under collection {collection_name if collection_name else 'default_collection'}.")
+   
+
+    def read_data(self, collection_name):
+        """Firestore에서 데이터를 읽어옵니다."""
+        docs = self.db.collection(collection_name).stream()
+        data = {}
+        for doc in docs:
+            data[doc.id] = doc.to_dict()
+            logging.info(f"Document {doc.id} retrieved successfully from {collection_name}.")
+        return data
+
+    def save_list(self, dict_list: dict, collection_name="sample_collection"):
+        """
+        dict_list = {doc_key: data_list}
+        """
+        for doc_key, data_list in dict_list.items():
+            # Firestore 컬렉션 및 문서 경로 지정
+            doc_ref = self.db.collection(collection_name).document(doc_key)
+            doc_ref.set({'data_list': data_list})
+
+
+    def read_list(self, doc_key, collection_name="sample_collection"):
+        """
+        Firestore에서 리스트 데이터를 읽어옵니다.
+        """
+        doc_ref = self.db.collection(collection_name).document(doc_key)
+        doc = doc_ref.get()
+        
+        if doc.exists:
+            return doc.to_dict().get('data_list')
+        else:
+            return None
