991e52b549c3152a0d1769451c921bc4a58e1e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:04 +0000

    Add package alibaba-aop/1.0.0/alibaba_aop-1.0.0.tar.gz

---
+++ packages/alibaba-aop/alibaba_aop-1.0.0.tar.gz/alibaba_aop-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alibaba_aop',  # 需要发布的模块名称
+    version='1.0.0',  # 版本号
+    description='Alibaba AOP SDK',  # 模块描述
+    author='Fhhhhh',  # 作者
+    author_email='1729219134@qq.com',  # 作者邮箱
+    py_modules=find_packages()  # 需要发布的模块
+)
