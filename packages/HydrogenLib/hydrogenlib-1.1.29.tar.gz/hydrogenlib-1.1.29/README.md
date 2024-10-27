84842f9dfb5ba73c46ccebc23983f373eca88cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:35 +0000

    Add package HydrogenLib/1.1.29/hydrogenlib-1.1.29.tar.gz

---
+++ packages/HydrogenLib/hydrogenlib-1.1.29.tar.gz/hydrogenlib-1.1.29/README.md
@@ -0,0 +1,41 @@
+# HydrogenLib
+
+[![PyPI - Version](https://img.shields.io/pypi/v/hydrogenlib.svg)](https://pypi.org/project/hydrogenlib)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/hydrogenlib.svg)](https://pypi.org/project/hydrogenlib)
+[![Github Stars](https://img.shields.io/github/stars/SongzqInChina/HydrogenLib.svg)](https://github.com/SongzqInChina/HydrogenLib)
+[![Action Status](https://github.com/SongzqInChina/HydrogenLib/actions/workflows/python-publish.yml/badge.svg)](
+    https://github.com/SongzqInChina/HydrogenLib/actions)
+
+## 当前为测试版本，功能不完整，详细请见 [注意事项](#注意事项)
+
+-----
+
+## 目录
+
+- [安装](#安装)
+- [许可证](#许可证)
+- [注意事项](#注意事项)
+
+## 安装
+
+```console
+pip install HydrogenLib
+```
+
+## 注意事项
+
+- 模块标注的是Python版本需大于`1.12.0`，实际可能允许运行在`Python 1.12.0`以下版本（可能的最低版本`Python 3.7`）。
+- `.Database` 只是一个简单的基于单JSON文件的轻量级数据库，可能不适用于所有使用场景。
+- `.Namedpipe` 为非Windows系统的支持未完成。
+- `.REPlus` 实现较为简单，可能无法完全满足需求。
+- `.InteractiveCLI` 及其子模块暂时停止开发。
+- `.HyConfigLanguage` 未完成，且无法使用
+- `.TestManager` 将会在未来版本移除。
+- 对于有关IO功能的函数和类，大多使用了异步模式，请注意兼容性。
+
+- 代码实现中有关序列化和反序列化的操作，均使用`jsonpickle`模块，可能有安全风险。
+- **所有模块均未进行测试。**
+
+## 许可证
+
+`HydrogenLib` 使用 [GPL](https://spdx.org/licenses/GPL-3.0.html)许可证([Open LICENSE.md](LICENSE.md)).
---
+++ packages/HydrogenLib/hydrogenlib-1.1.29.tar.gz/hydrogenlib-1.1.29/upload.py
@@ -0,0 +1,132 @@
+import argparse
+import os
+import subprocess
+import sys
+import time
+
+import rich.traceback
+from rich import print
+
+rich.traceback.install()
+
+
+def run_command(command):
+    ps = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return ps.returncode, ps
+
+
+def init_parser(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--install", '-i',
+        help="Install HydrogenLib",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--skip-check", '-c',
+        help="Skip check HydrogenLib wheel",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--skip-upload", '-d',
+        help="Skip upload HydrogenLib wheel",
+        action="store_true"
+    )
+    parser.add_argument(
+        '--skip-build', '-s',
+        help="Skip building HydrogenLib wheel",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--clean", '-k',
+        help="Clean HydrogenLib wheel",
+        action="store_true"
+    )
+    parser.add_argument(  # 接收一个参数，version
+        "--version", '-v',
+        help="Set HydrogenLib version",
+        default="None",
+        type=str
+    )
+
+
+version_path = r"src/HydrogenLib/Resource\version"
+args = sys.argv[1::]
+
+if __name__ == '__main__':
+    spinner = "aesthetic"
+    parser = argparse.ArgumentParser()
+    console = rich.console.Console(force_terminal=True)
+    init_parser(parser)
+    args = parser.parse_args(args)
+    if args.version != "None":
+        with open(version_path, "r") as f:
+            f.seek(0)
+            cur_version = tuple(map(int, f.read().strip().split('.')))
+        now_version = tuple(map(int, args.version.strip().split('.')))
+        if cur_version >= now_version:
+            console.print("[bold red]The version you set is lower than the current version!")
+        else:
+            with console.status("Setting HydrogenLib version...", spinner=spinner):
+
+                rt_code, ps = run_command(["hatch", "version", args.version])
+                with open(version_path, "w") as f:
+                    f.write(args.version)
+            time.sleep(0.1)
+            if rt_code != 0:
+                console.print("[bold red]Setting HydrogenLib version failed!")
+                console.print(ps.stderr.decode())
+                console.print(ps.stdout.decode())
+                sys.exit(rt_code)
+            print("[bold green]success!")
+    if args.clean:
+        if os.name == 'nt':
+            command = ["powershell.exe", "-Command", "rm", r".\dist\*"]
+        elif os.name == 'posix':
+            command = ["rm", "-rf", r"./dist/*"]
+        else:
+            console.print(f"[bold red]Unsupported OS({os.name})!")
+            sys.exit(1)
+
+        with console.status("Cleaning HydrogenLib wheel...", spinner=spinner):
+            rt_code, ps = run_command(command)
+        time.sleep(0.1)
+        if rt_code != 0:
+            console.print("[bold red]Cleaning HydrogenLib wheel failed!")
+            console.print(ps.stderr)
+            console.print(ps.stdout)
+            sys.exit(rt_code)
+        print("[bold green]success!")
+
+    if not args.skip_build:
+        # 播放工作动画
+        with console.status("Building HydrogenLib wheel...", spinner=spinner):
+            rt_code, ps = run_command(["hatch", "build"])
+        time.sleep(0.1)
+        if rt_code != 0:
+            console.print("[bold red]Building HydrogenLib wheel failed!")
+            console.print(ps.stderr)
+            sys.exit(rt_code)
+        print("[bold green]success!")
+    # console.console.print('\n')
+    if not args.skip_check:
+        with console.status("Checking HydrogenLib wheel...", spinner=spinner):
+            rt_code, ps = run_command(["twine", "check", "dist/*"])
+        time.sleep(0.1)
+        if rt_code != 0:
+            console.print("[bold red]Checking HydrogenLib wheel failed!")
+            console.print(ps.stderr)
+            sys.exit(rt_code)
+        print("[bold green]success!")
+    # console.console.print('\n')
+    if not args.skip_upload:
+        with console.status("Uploading HydrogenLib wheel...", spinner=spinner):
+            rt_code, ps = run_command(["twine", "upload", r".\dist\*", '--disable-progress-bar'])
+        time.sleep(0.1)
+        if rt_code != 0:
+            console.print(f"[bold red]failed!({rt_code})")
+            console.print(ps.stdout.decode())
+            console.print(ps.stderr.decode())
+            sys.exit(rt_code)
+        print("[bold green]success!")
+
+    print("[bold green]all steps are success, you can run `pip install HydrogenLib -U` to update HydrogenLib")
