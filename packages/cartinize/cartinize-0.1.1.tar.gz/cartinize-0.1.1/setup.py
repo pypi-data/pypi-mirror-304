0c8381e34633de50e1e1ec1e3347287b3396b8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 04:05:28 +0000

    Add package cartinize/0.1.1/cartinize-0.1.1.tar.gz

---
+++ packages/cartinize/cartinize-0.1.1.tar.gz/cartinize-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Cartinese translator'
+
+setup(
+    name="cartinize",
+    version=VERSION,
+    author="ktkv419 (Kutikov Pasha)",
+    author_email="<kutikovpasha@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'string', 'translate', 'carti', 'slatt'],
+)
