59721e7cc6aca6227b68d88aee21c27c944925ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 14:05:09 +0000

    Add package TESTXMZAPI/1.0.0/testxmzapi-1.0.0.tar.gz

---
+++ packages/TESTXMZAPI/testxmzapi-1.0.0.tar.gz/testxmzapi-1.0.0/TESTXMZAPI/HWNLP/__init__.py
@@ -0,0 +1,7 @@
+from .mt_service import MTService
+from .nlg_service import NLGService
+from .nlpf_service import NLPFService
+from .nlu_service import NLUService
+from .. import KVS
+
+__all__ = ["MTService", "NLGService", "NLPFService", "NLUService"]
