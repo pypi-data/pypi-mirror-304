a81a85df42a7fffb4143142086f9d2e7efb97fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-27 05:05:20 +0000

    Add package open-webui/0.3.35/open_webui-0.3.35-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.3.35-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.DQzwzqk1.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Messages.Bkfcraz2.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.DQzwzqk1.js.map
---
+++ packages/open-webui/open_webui-0.3.35-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.8SWFMS1E.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.sCzT-hjY.js";export{t as start};
+//# sourceMappingURL=start.8SWFMS1E.js.map
