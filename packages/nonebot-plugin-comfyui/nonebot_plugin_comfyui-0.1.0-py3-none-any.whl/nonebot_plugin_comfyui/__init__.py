8d80548e87f5b494a1c118d26805a86a08987f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:04:49 +0000

    Add package nonebot-plugin-comfyui/0.1.0/nonebot_plugin_comfyui-0.1.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-comfyui/nonebot_plugin_comfyui-0.1.0-py3-none-any.whl/nonebot_plugin_comfyui/__init__.py
@@ -0,0 +1,45 @@
+from nonebot import logger
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters, require
+from nonebot.rule import ArgumentParser
+from nonebot.plugin.on import on_shell_command
+
+from .config import Config
+
+from .handler import comfyui_handler
+
+comfyui_parser = ArgumentParser()
+
+comfyui_parser.add_argument("prompt", nargs="*", help="标签", type=str)
+comfyui_parser.add_argument("-u", "-U", nargs="*", dest="negative_prompt", type=str, help="Negative prompt")
+comfyui_parser.add_argument("--ar", "-ar", dest="accept_ratio", type=str, help="Accept ratio")
+comfyui_parser.add_argument("--s", "-s", dest="seed", type=int, help="Seed")
+comfyui_parser.add_argument("--steps", "-steps", "-t", dest="steps", type=int, help="Steps")
+comfyui_parser.add_argument("--cfg", "-cfg", dest="cfg_scale", type=float, help="CFG scale")
+comfyui_parser.add_argument("-n", "--n", dest="denoise_strength", type=float, help="Denoise strength")
+comfyui_parser.add_argument("-height", dest="height", type=int, help="Height")
+comfyui_parser.add_argument("-width", dest="width", type=int, help="Width")
+comfyui_parser.add_argument("-v", dest="video", action="store_true", help="Video output flag")
+comfyui_parser.add_argument("-wf", "--work-flows", dest="work_flows", type=str, help="Workflows")
+comfyui_parser.add_argument("-sp", "--sampler", dest="sampler", type=str, help="采样器")
+comfyui_parser.add_argument("-sch", "--scheduler", dest="scheduler", type=str, help="调度器")
+comfyui_parser.add_argument("-b", "--batch_size", dest="batch_size", type=int, help="每批数量")
+comfyui_parser.add_argument("-m", "--model", dest="model", type=str, help="模型")
+
+
+__plugin_meta__ = PluginMetadata(
+    name="Comfyui绘图插件",
+    description="专门适配Comfyui的绘图插件",
+    usage="/prompt",
+    config=Config,
+    type="application",
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    extra={"author": "DiaoDaiaChan", "email": "437012661@qq.com"},
+)
+
+comfyui = on_shell_command(
+    "prompt",
+    parser=comfyui_parser,
+    priority=5,
+    block=True,
+    handlers=[comfyui_handler]
+)
