9c5ced3f16a6926a1eee5eb275434b8d499f7f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 03:13:11 +0000

    Add package spectrum-io/0.6.4/spectrum_io-0.6.4-py3-none-any.whl

---
+++ packages/spectrum-io/spectrum_io-0.6.4-py3-none-any.whl/spectrum_io/search_result/__init__.py
@@ -0,0 +1,9 @@
+"""Initialize seach result."""
+
+from .mascot import Mascot
+from .maxquant import MaxQuant
+from .msamanda import MSAmanda
+from .msfragger import MSFragger
+from .sage import Sage
+from .scout import Scout
+from .xisearch import Xisearch
