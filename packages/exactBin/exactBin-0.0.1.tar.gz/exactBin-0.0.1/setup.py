c630797804ce74903204fbd7fe5ea7fb82383ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 03:13:27 +0000

    Add package exactBin/0.0.1/exactBin-0.0.1.tar.gz

---
+++ packages/exactBin/exactBin-0.0.1.tar.gz/exactBin-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="exactBin",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A Python module for binary floats.",
+)
