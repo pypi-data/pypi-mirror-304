6d6a97482dbb4ff34d5dfad9e145cb3175c72e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:30 +0000

    Add package embykeeper/5.0.13/embykeeper-5.0.13-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-5.0.13-py3-none-any.whl/embykeeper/telechecker/monitor/test_pornemby_nohp.py
@@ -0,0 +1,8 @@
+from .pornemby_nohp import PornembyNoHPMonitor
+
+
+class TestPornembyNoHPMonitor(PornembyNoHPMonitor):
+    name = "Pornemby 血量耗尽停止发言 测试"
+    chat_name = "api_group"
+    chat_user = "embykeeper_test_bot"
+    chat_allow_outgoing = True
