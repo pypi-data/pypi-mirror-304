667b8d19decb31f48eb89ab3c874af41899e0d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:58 +0000

    Add package atom/0.11.0/atom-0.11.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/atom/atom-0.11.0-cp310-cp310-macosx_11_0_arm64.whl/atom/api.py
@@ -0,0 +1,124 @@
+# --------------------------------------------------------------------------------------
+# Copyright (c) 2013-2024, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# --------------------------------------------------------------------------------------
+"""Module exporting the public interface to atom."""
+
+from .atom import Atom
+from .catom import (
+    CAtom,
+    ChangeType,
+    DefaultValue,
+    GetAttr,
+    GetState,
+    Member,
+    PostGetAttr,
+    PostSetAttr,
+    PostValidate,
+    SetAttr,
+    Validate,
+    atomclist,
+    atomdict,
+    atomlist,
+    atomref,
+    atomset,
+    defaultatomdict,
+)
+from .coerced import Coerced
+from .containerlist import ContainerList
+from .delegator import Delegator
+from .dict import DefaultDict, Dict
+from .enum import Enum
+from .event import Event
+from .instance import ForwardInstance, Instance
+from .list import List
+from .meta import (
+    AtomMeta,
+    MissingMemberWarning,
+    add_member,
+    clone_if_needed,
+    observe,
+    set_default,
+)
+from .property import Property, cached_property
+from .scalars import (
+    Bool,
+    Bytes,
+    Callable,
+    Constant,
+    Float,
+    FloatRange,
+    Int,
+    Range,
+    ReadOnly,
+    Str,
+    Value,
+)
+from .set import Set
+from .signal import Signal
+from .subclass import ForwardSubclass, Subclass
+from .tuple import FixedTuple, Tuple
+from .typed import ForwardTyped, Typed
+from .typing_utils import ChangeDict
+
+__all__ = [
+    "Atom",
+    "AtomMeta",
+    "MissingMemberWarning",
+    "add_member",
+    "observe",
+    "set_default",
+    "clone_if_needed",
+    "CAtom",
+    "ChangeType",
+    "DefaultValue",
+    "GetAttr",
+    "GetState",
+    "Member",
+    "PostGetAttr",
+    "PostSetAttr",
+    "PostValidate",
+    "SetAttr",
+    "Validate",
+    "atomclist",
+    "atomdict",
+    "defaultatomdict",
+    "atomlist",
+    "atomref",
+    "atomset",
+    "Coerced",
+    "ContainerList",
+    "Delegator",
+    "Dict",
+    "DefaultDict",
+    "Enum",
+    "Event",
+    "ForwardInstance",
+    "Instance",
+    "List",
+    "Property",
+    "cached_property",
+    "Bool",
+    "Bytes",
+    "Callable",
+    "Constant",
+    "Float",
+    "FloatRange",
+    "Int",
+    "Range",
+    "ReadOnly",
+    "Str",
+    "Value",
+    "Set",
+    "Signal",
+    "ForwardSubclass",
+    "Subclass",
+    "Tuple",
+    "ForwardTyped",
+    "Typed",
+    "FixedTuple",
+    "ChangeDict",
+]
