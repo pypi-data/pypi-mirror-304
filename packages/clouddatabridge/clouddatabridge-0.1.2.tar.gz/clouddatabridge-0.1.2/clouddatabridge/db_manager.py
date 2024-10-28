5a7e275aa3db2371d3e5a4873a0ad5c46572825d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:24 +0000

    Add package clouddatabridge/0.1.2/clouddatabridge-0.1.2.tar.gz

---
+++ packages/clouddatabridge/clouddatabridge-0.1.2.tar.gz/clouddatabridge-0.1.2/clouddatabridge/db_manager.py
@@ -0,0 +1,21 @@
+from .local_db import LocalDBConnection
+from .aws_db import AWSRDSConnection
+from .azure_db import AzureSQLConnection 
+from .gcp_db import GCPMySQLConnection
+
+def create_db_connection(service, project_id=None, host=None, user=None, password=None, database=None, port=3306, region=None):
+    if service not in ['local', 'aws', 'azure', 'gcp']:
+        return ValueError(f"Unsupported cloud provider: '{service}'. Supported providers: aws, azure, gcp, local")
+
+    if service == "local":
+        return LocalDBConnection(host, user, password, database, port)
+    elif service == "aws":
+        if not region:
+            return ValueError("Region is required for AWS connection.")
+        return AWSRDSConnection(host, user, database, region, port)
+    elif service == "azure":
+        return AzureSQLConnection(host, user, database, password, port)
+    elif service == "gcp":
+        return GCPMySQLConnection(host, user, password, database, port)
+    else:
+        raise ValueError("Unsupported service type. Use 'local', 'aws', 'azure', 'gcp'.")
