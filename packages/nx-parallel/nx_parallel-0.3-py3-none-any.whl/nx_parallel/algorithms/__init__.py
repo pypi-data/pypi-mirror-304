4fcfa87c630fe6123f4e783511f2c46176c24df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:44 +0000

    Add package nx-parallel/0.3/nx_parallel-0.3-py3-none-any.whl

---
+++ packages/nx-parallel/nx_parallel-0.3-py3-none-any.whl/nx_parallel/algorithms/__init__.py
@@ -0,0 +1,13 @@
+# subpackages
+from .bipartite import *
+from .centrality import *
+from .shortest_paths import *
+from .approximation import *
+from .connectivity import *
+
+# modules
+from .efficiency_measures import *
+from .isolate import *
+from .tournament import *
+from .vitality import *
+from .cluster import *
