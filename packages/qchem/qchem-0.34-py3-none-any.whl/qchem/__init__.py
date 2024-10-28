62a6fcf019c3727e051547d4b44c93c2843cd217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:06:08 +0000

    Add package qchem/0.34/qchem-0.34-py3-none-any.whl

---
+++ packages/qchem/qchem-0.34-py3-none-any.whl/qchem/__init__.py
@@ -0,0 +1,7 @@
+from . import Parser
+from .Data.Enums import OrcaBasisSet, OrcaDensityFunctional, OrcaCalculationType
+from .OrcaCalculation import OrcaCalculation
+from .XYZFile import XYZFile
+from .Molecule import Molecule
+from .ClusterCalculation import ClusterCalculation
+from .Data.Constants import CovalentRadiiConstants, AtomicMassConstants
