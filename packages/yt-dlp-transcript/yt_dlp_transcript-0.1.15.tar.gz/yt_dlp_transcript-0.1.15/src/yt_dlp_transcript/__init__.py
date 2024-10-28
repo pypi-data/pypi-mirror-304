4c8af0547a162bdf5a6023c828b787f7e5848b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:27 +0000

    Add package yt-dlp-transcript/0.1.15/yt_dlp_transcript-0.1.15.tar.gz

---
+++ packages/yt-dlp-transcript/yt_dlp_transcript-0.1.15.tar.gz/yt_dlp_transcript-0.1.15/src/yt_dlp_transcript/__init__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError  # noqa
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from configargparse import ArgumentParser, ArgumentDefaultsRawHelpFormatter
+import srt
+
+opts = {
+    "no_warnings": True,
+    "noprogress": True,
+    "postprocessors": [
+        {"format": "srt", "key": "FFmpegSubtitlesConvertor", "when": "before_dl"},
+    ],
+    "quiet": True,
+    "retries": 10,
+    "skip_download": True,
+    "writeautomaticsub": True,
+}
+
+
+def yt_dlp_transcript(url=None, language="en", verbose=False, **kwargs):
+    with TemporaryDirectory() as temp_dir:
+        path = Path(temp_dir)
+        opts["outtmpl"] = {"default": str(path / "res")}
+        opts["subtitleslangs"] = [language]
+        opts.update(kwargs)
+        if verbose:
+            del opts["quiet"]
+            print(opts)
+        ydl = YoutubeDL(opts)
+        ydl.download(url)
+        srt_files = list(path.glob("*"))
+        if len(srt_files) < 1:
+            raise DownloadError(f"Error: cannot download subtitles for {url}, probably the video has no subtitles.")
+        subtitles = srt.parse(open(srt_files[0]).read())
+        res = " ".join([s.content.replace(r"\h", "") for s in subtitles])
+        return res
+
+
+def main():
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsRawHelpFormatter)
+    parser.add_argument("-l", "--language", default="en", help="subtitles language")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
+    parser.add_argument("url", help="Youtube URL")
+    args = parser.parse_args()
+    print(yt_dlp_transcript(**args.__dict__))
+
+
+if __name__ == "__main__":
+    main()
