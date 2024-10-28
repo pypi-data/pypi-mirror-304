16bbab1ceb48a0072880f0fe7966528581509995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:16 +0000

    Add package panasonic-programmable-keys/0.1.2/panasonic_programmable_keys-0.1.2-py3-none-any.whl

---
+++ packages/panasonic-programmable-keys/panasonic_programmable_keys-0.1.2-py3-none-any.whl/panasonic_programmable_keys/rpc/client.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Iterator
+
+import Pyro5.api
+
+from ..input.models import KeyPressEvent
+from ..util import logger
+from ..util import settings
+from .server import KeyService
+
+
+class KeyClient(object):
+    def __init__(self) -> None:
+        self.socket = Path(settings.rpc.get("socket", "/run/panasonic/keys.sock"))
+
+    def connectable(self) -> bool:
+        with Pyro5.api.Proxy(f"PYRO:keyservice@./u:{self.socket}") as server:
+            try:
+                server._pyroValidateHandshake(True)
+            except Exception:
+                return False
+        return True
+
+    def yield_keys(self) -> Iterator[KeyPressEvent]:
+        logger.debug("Receiving keys")
+        server: KeyService = Pyro5.api.Proxy(f"PYRO:keyservice@./u:{self.socket}")  # type: ignore
+        for key_event in server.yield_keys():
+            yield KeyPressEvent(**key_event)
