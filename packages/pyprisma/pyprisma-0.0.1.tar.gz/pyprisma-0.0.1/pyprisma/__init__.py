521d2b97fc9b317f886da9224361516c3db34095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:33 +0000

    Add package pyprisma/0.0.1/pyprisma-0.0.1.tar.gz

---
+++ packages/pyprisma/pyprisma-0.0.1.tar.gz/pyprisma-0.0.1/pyprisma/__init__.py
@@ -0,0 +1,23 @@
+# type: ignore[attr-defined]
+"""A simple way to use langchain framework."""
+
+import sys
+
+from pyprisma.chat import chat
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
+
+__all__ = ["chat"]
