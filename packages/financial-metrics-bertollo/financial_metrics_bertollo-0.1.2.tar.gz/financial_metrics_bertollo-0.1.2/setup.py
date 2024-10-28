720b3553f0f3f89307eeb969225eadbcc74957c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:46 +0000

    Add package financial-metrics-bertollo/0.1.2/financial_metrics_bertollo-0.1.2.tar.gz

---
+++ packages/financial-metrics-bertollo/financial_metrics_bertollo-0.1.2.tar.gz/financial_metrics_bertollo-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='financial_metrics_bertollo',
+    version='0.1.2',
+    description='A package for calculating financial metrics such as net profit and ROI.',
+    author='Victor Bertollo',
+    author_email='bertollo.victor@gmail.com',
+    packages=['financial_metrics'],
+    entry_points={
+        'console_scripts': [
+            'financial_metrics=financial_metrics.__main__:main',
+        ],
+    },
+    install_requires=[],
+)
