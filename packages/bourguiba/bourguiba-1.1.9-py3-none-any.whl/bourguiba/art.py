16b30a6bbadc47fb650e6c476590849c879de3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:34 +0000

    Add package bourguiba/1.1.9/bourguiba-1.1.9-py3-none-any.whl

---
+++ packages/bourguiba/bourguiba-1.1.9-py3-none-any.whl/bourguiba/art.py
@@ -0,0 +1,19 @@
+# bourguiba/art.py
+
+import os
+
+def display_ascii_art():
+    # Specify the path to ASCII_ART.txt directly
+    art_path = os.path.join(os.path.dirname(__file__), 'ASCII_ART.txt')  # This is correct
+
+    print(f"Looking for ASCII art at: {art_path}")  # Debugging line
+    
+    try:
+        with open(art_path, 'r') as f:
+            art = f.read()
+            print(art)  # Print the ASCII art
+    except Exception as e:
+        print(f"Error reading ASCII art: {e}")
+
+if __name__ == "__main__":
+    display_ascii_art()
