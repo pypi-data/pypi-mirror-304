07752ac3fafe773c2b90f5121c4e838fdd1c9416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 03:13:10 +0000

    Add package chatweather/0.1.3/chatweather-0.1.3.tar.gz

---
+++ packages/chatweather/chatweather-0.1.3.tar.gz/chatweather-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="chatweather",
+    version="0.1.3",
+    author="SanghyunPark",
+    author_email="hirvahapjh@gmail.com",
+    description="A package for weather forecasting and ChatGPT integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daisybum/pyWeather",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['config.py', 'config.yaml']},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests",
+        "xmltodict",
+        "openai",
+        "pytest"
+    ],
+)
