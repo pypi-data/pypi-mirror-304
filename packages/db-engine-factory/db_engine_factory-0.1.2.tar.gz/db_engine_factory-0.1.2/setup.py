3a05245a590c9563475e48104a1c2a0da27eb78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:31 +0000

    Add package db-engine-factory/0.1.2/db_engine_factory-0.1.2.tar.gz

---
+++ packages/db-engine-factory/db_engine_factory-0.1.2.tar.gz/db_engine_factory-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='db-engine-factory',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dotenv',
+        'SQLAlchemy>=2.0',
+        'pyodbc',
+        'pymysql',
+        'psycopg2-binary',
+    ],
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A utility for creating SQLAlchemy database engines from config files.',
+    url='https://github.com/skyblue-will/db_engine_factory',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'test_db_factory=tests.test:test_connections',
+        ],
+    },
+)
