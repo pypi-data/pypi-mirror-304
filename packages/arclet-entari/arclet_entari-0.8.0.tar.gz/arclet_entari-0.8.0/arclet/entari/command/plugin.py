a4e00abe5e9c6b8c6528a916c0c2e72c72044bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 15:05:58 +0000

    Add package arclet-entari/0.8.0/arclet_entari-0.8.0.tar.gz

---
+++ packages/arclet-entari/arclet_entari-0.8.0.tar.gz/arclet_entari-0.8.0/arclet/entari/command/plugin.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import Any
+
+from arclet.alconna import Alconna, command_manager
+from arclet.letoderea import BaseAuxiliary, Provider, ProviderFactory
+
+from ..event import MessageCreatedEvent
+from ..plugin.model import Plugin, PluginDispatcher
+from .model import Match, Query
+from .provider import AlconnaProviderFactory, AlconnaSuppiler, Assign, MessageJudger, _seminal
+
+
+class AlconnaPluginDispatcher(PluginDispatcher):
+
+    def __init__(
+        self,
+        plugin: Plugin,
+        command: Alconna,
+        need_tome: bool = False,
+        remove_tome: bool = True,
+    ):
+        self.supplier = AlconnaSuppiler(command, need_tome, remove_tome)
+        super().__init__(plugin, MessageCreatedEvent)
+
+        self.bind(MessageJudger(), self.supplier)
+        self.bind(AlconnaProviderFactory())
+
+    def assign(
+        self,
+        path: str,
+        value: Any = _seminal,
+        or_not: bool = False,
+        priority: int = 16,
+        auxiliaries: list[BaseAuxiliary] | None = None,
+        providers: list[Provider | type[Provider] | ProviderFactory | type[ProviderFactory]] | None = None,
+    ):
+        _auxiliaries = auxiliaries or []
+        _auxiliaries.append(Assign(path, value, or_not))
+        return self.register(priority, _auxiliaries, providers)
+
+    def dispose(self):
+        super().dispose()
+        command_manager.delete(self.supplier.cmd)
+        del self.supplier.cmd
+        del self.supplier
+
+    Match = Match
+    Query = Query
+
+
+def mount(cmd: Alconna, need_tome: bool = False, remove_tome: bool = True):
+    if not (plugin := Plugin.current()):
+        raise LookupError("no plugin context found")
+    disp = AlconnaPluginDispatcher(plugin, cmd, need_tome, remove_tome)
+    if disp.id in plugin.dispatchers:
+        return plugin.dispatchers[disp.id]
+    plugin.dispatchers[disp.id] = disp
+    return disp
---
+++ packages/arclet-entari/arclet_entari-0.8.0.tar.gz/arclet_entari-0.8.0/arclet/entari/core.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+import asyncio
+from contextlib import suppress
+
+from arclet.letoderea import BaseAuxiliary, Contexts, EventSystem, Param, Provider, ProviderFactory, global_providers
+from launart import Launart
+from loguru import logger
+from satori import LoginStatus
+from satori.client import App
+from satori.client.account import Account
+from satori.client.protocol import ApiProtocol
+from satori.config import Config
+from satori.model import Event
+from tarina.generic import get_origin
+
+from .command import _commands
+from .event import MessageCreatedEvent, event_parse
+from .plugin.service import plugin_service
+from .session import Session
+
+
+class ApiProtocolProvider(Provider[ApiProtocol]):
+    async def __call__(self, context: Contexts):
+        if account := context.get("$account"):
+            return account.protocol
+
+
+class SessionProvider(Provider[Session]):
+    def validate(self, param: Param):
+        return get_origin(param.annotation) == Session
+
+    async def __call__(self, context: Contexts):
+        if "$origin_event" in context and "$account" in context:
+            return Session(context["$account"], context["$event"])
+
+
+global_providers.extend([ApiProtocolProvider(), SessionProvider()])
+
+
+class Entari(App):
+    id = "entari.service"
+
+    def __init__(self, *configs: Config, ignore_self_message: bool = True):
+        super().__init__(*configs)
+        self.ignore_self_message = ignore_self_message
+        self.event_system = EventSystem()
+        self.event_system.register(_commands.publisher)
+        self.register(self.handle_event)
+        self.lifecycle(self.account_hook)
+        self._ref_tasks = set()
+
+        @self.on_message(priority=0)
+        def log(event: MessageCreatedEvent):
+            logger.info(
+                f"[{event.channel.name or event.channel.id}] "
+                f"{event.member.nick if event.member else (event.user.name or event.user.id)}"
+                f"({event.user.id}) -> {event.message.content!r}"
+            )
+
+    def on(
+        self,
+        *events: type,
+        priority: int = 16,
+        auxiliaries: list[BaseAuxiliary] | None = None,
+        providers: list[Provider | type[Provider] | ProviderFactory | type[ProviderFactory]] | None = None,
+    ):
+        return self.event_system.on(*events, priority=priority, auxiliaries=auxiliaries, providers=providers)
+
+    def on_message(
+        self,
+        priority: int = 16,
+        auxiliaries: list[BaseAuxiliary] | None = None,
+        providers: list[Provider | type[Provider] | ProviderFactory | type[ProviderFactory]] | None = None,
+    ):
+        return self.event_system.on(
+            MessageCreatedEvent, priority=priority, auxiliaries=auxiliaries, providers=providers
+        )
+
+    def ensure_manager(self, manager: Launart):
+        self.manager = manager
+        manager.add_component(plugin_service)
+
+    async def handle_event(self, account: Account, event: Event):
+        async def event_parse_task(connection: Account, raw: Event):
+            loop = asyncio.get_running_loop()
+            with suppress(NotImplementedError):
+                ev = event_parse(connection, raw)
+                if self.ignore_self_message and isinstance(ev, MessageCreatedEvent) and ev.user.id == account.self_id:
+                    return
+                self.event_system.publish(ev)
+                for plugin in plugin_service.plugins.values():
+                    for disp in plugin.dispatchers.values():
+                        if not disp.validate(ev):
+                            continue
+                        if disp._run_by_system:
+                            continue
+                        task = loop.create_task(disp.publish(ev))
+                        self._ref_tasks.add(task)
+                        task.add_done_callback(self._ref_tasks.discard)
+                return
+
+            logger.warning(f"received unsupported event {raw.type}: {raw}")
+
+        await event_parse_task(account, event)
+
+    async def account_hook(self, account: Account, state: LoginStatus):
+        _connected = []
+        _disconnected = []
+        for plug in plugin_service.plugins.values():
+            _connected.extend([func(account) for func in plug._connected])
+            _disconnected.extend([func(account) for func in plug._disconnected])
+        if state == LoginStatus.CONNECT:
+            await asyncio.gather(*_connected, return_exceptions=True)
+        elif state == LoginStatus.DISCONNECT:
+            await asyncio.gather(*_disconnected, return_exceptions=True)
