345a97b7ac7da94b37add940f9d17c7f5efb34b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 03:13:22 +0000

    Add package raintools-colors/0.1.0/raintools_colors-0.1.0.tar.gz

---
+++ packages/raintools-colors/raintools_colors-0.1.0.tar.gz/raintools_colors-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raintools_colors",
+    version="0.1.0",
+    description="A simple module to print colored text in the terminal",
+    author="Plingenn",
+    author_email="raintool@engineer.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+)
