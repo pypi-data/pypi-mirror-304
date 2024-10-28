cf0d2ebc05230937980b3d97b56b62fd7da299be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:10 +0000

    Add package geo-parameters/0.9.3/geo_parameters-0.9.3.tar.gz

---
+++ packages/geo-parameters/geo_parameters-0.9.3.tar.gz/geo_parameters-0.9.3/tests/test_names.py
@@ -0,0 +1,27 @@
+from geo_parameters.wave import Hs
+
+
+def test_name_of_class():
+    assert Hs.name == "hs"
+    assert Hs.long_name() == "significant_wave_height"
+    assert Hs.standard_name() == "sea_surface_wave_significant_height"
+    assert Hs.standard_name(alias=True) == "significant_height_of_wind_and_swell_waves"
+
+
+def test_units_of_class():
+    assert str(Hs.units()) == "m"
+
+
+def test_name_of_instance():
+    assert Hs().name == "hs"
+    assert Hs(name="hsig").name == "hsig"
+    assert Hs("hsig").name == "hsig"
+    assert Hs().long_name() == "significant_wave_height"
+    assert Hs().standard_name() == "sea_surface_wave_significant_height"
+    assert (
+        Hs().standard_name(alias=True) == "significant_height_of_wind_and_swell_waves"
+    )
+
+
+def test_units_of_instance():
+    assert str(Hs().units()) == "m"
