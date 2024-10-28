617e55f4bffe9f5cbe31fdd0fa013b09189b6b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 15:06:14 +0000

    Add package evox/0.9.0rc2/evox-0.9.0rc2-py3-none-any.whl

---
+++ packages/evox/evox-0.9.0rc2-py3-none-any.whl/evox/monitors/eval_monitor.py
@@ -0,0 +1,176 @@
+import warnings
+
+import jax
+import jax.numpy as jnp
+
+from evox import Monitor, dataclass, pytree_field
+from evox.vis_tools import plot
+
+
+@dataclass
+class EvalMonitorState:
+    first_step: bool = pytree_field(static=True)
+    latest_solution: jax.Array
+    latest_fitness: jax.Array
+    topk_solutions: jax.Array
+    topk_fitness: jax.Array
+
+
+@dataclass
+class EvalMonitor(Monitor):
+    """Evaluation monitor.
+    Used for both single-objective and multi-objective workflow.
+    Hooked around the evaluation process,
+    can monitor the offspring, their corresponding fitness and keep track of the evaluation count.
+    Moreover, it can also record the best solution or the pareto front on-the-fly.
+
+    Parameters
+    ----------
+    multi_obj
+        Whether the optimization is multi-objective.
+        Default to False.
+    full_fit_history
+        Whether to record the full history of fitness value.
+        Default to True. Setting it to False may reduce memory usage.
+    full_sol_history
+        Whether to record the full history of solutions.
+        Default to False.
+        Setting it to True may increase memory usage,
+        and adding a significant overhead of transfering the entire solutions set from GPU to CPU.
+    topk
+        Only affect Single-objective optimization. The number of elite solutions to record.
+        Default to 1, which will record the best individual.
+    calc_pf
+        Only affect Multi-objective optimization.
+        Whether to keep updating the pareto front during the run. (The Archive)
+        Default to False.
+        Setting it to True will cause the monitor to
+        maintain a pareto front of all the solutions with unlimited size,
+        which may hurt performance.
+    parallel_monitor
+        Enable the use of parallel monitor,
+        that is monitoring multiple optimization process in parallel.
+        Typically used in meta-optimization settings where the use of vmap of a workflow is needed.
+    """
+
+    multi_obj: bool = pytree_field(default=False, static=True)
+    full_fit_history: bool = pytree_field(default=True, static=True)
+    full_sol_history: bool = pytree_field(default=False, static=True)
+    topk: int = pytree_field(default=1, static=True)
+
+    fitness_history: list = pytree_field(static=True, init=False)
+    solution_history: list = pytree_field(static=True, init=False)
+    opt_direction: int = pytree_field(static=True, init=False)
+
+    def __post_init__(self):
+        self.set_frozen_attr("opt_direction", 1)
+        self.set_frozen_attr("fitness_history", [])
+        self.set_frozen_attr("solution_history", [])
+
+    def hooks(self):
+        return ["post_ask", "post_eval"]
+
+    def set_opt_direction(self, opt_direction):
+        self.set_frozen_attr("opt_direction", opt_direction)
+
+    def setup(self, _key):
+        return EvalMonitorState(
+            first_step=True,
+            latest_solution=None,
+            latest_fitness=None,
+            topk_solutions=None,
+            topk_fitness=None,
+        )
+
+    def post_ask(self, state, _workflow_state, candidate):
+        return state.replace(latest_solution=candidate)
+
+    def post_eval(self, state, _workflow_state, fitness):
+        if fitness.ndim == 1:
+            # single-objective
+            self.multi_obj = False
+            if state.first_step:
+                topk_solutions = state.latest_solution
+                topk_fitness = fitness
+                state = state.replace(first_step=False)
+            else:
+                topk_solutions = jnp.concatenate(
+                    [state.topk_solutions, state.latest_solution]
+                )
+                topk_fitness = jnp.concatenate([state.topk_fitness, fitness])
+            rank = jnp.argsort(topk_fitness)
+            topk_solutions = topk_solutions[rank[: self.topk]]
+            topk_fitness = topk_fitness[rank[: self.topk]]
+            state = state.replace(
+                topk_solutions=topk_solutions,
+                topk_fitness=topk_fitness,
+                latest_fitness=fitness,
+            )
+        else:
+            # multi-objective
+            self.multi_obj = True
+            state = state.replace(latest_fitness=fitness)
+
+        if self.full_fit_history or self.full_sol_history:
+            return state.register_callback(
+                self._record_history,
+                state.latest_solution if self.full_sol_history else None,
+                fitness if self.full_fit_history else None,
+            )
+        else:
+            return state
+
+    def _record_history(self, solution, fitness):
+        # since history is a list, which doesn't have a static shape
+        # we need to use register_callback to record the history
+        if self.full_sol_history:
+            self.solution_history.append(solution)
+        if self.full_fit_history:
+            self.fitness_history.append(fitness)
+
+    def get_latest_fitness(self, state):
+        return self.opt_direction * state.latest_fitness, state
+
+    def get_latest_solution(self, state):
+        return state.latest_solution, state
+
+    def get_topk_fitness(self, state):
+        return self.opt_direction * state.topk_fitness, state
+
+    def get_topk_solutions(self, state):
+        return state.topk_solutions, state
+
+    def get_best_solution(self, state):
+        return state.topk_solutions[..., 0, :], state
+
+    def get_best_fitness(self, state):
+        if self.multi_obj:
+            raise ValueError(
+                "Multi-objective optimization does not have a single best fitness."
+            )
+        return self.opt_direction * state.topk_fitness[..., 0], state
+
+    def get_fitness_history(self, state):
+        return [self.opt_direction * fit for fit in self.fitness_history], state
+
+    def get_solution_history(self, state):
+        return self.solution_history, state
+
+    def plot(self, problem_pf=None, **kwargs):
+        if not self.fitness_history:
+            warnings.warn("No fitness history recorded, return None")
+            return
+
+        if self.fitness_history[0].ndim == 1:
+            n_objs = 1
+        else:
+            n_objs = self.fitness_history[0].shape[1]
+
+        if n_objs == 1:
+            return plot.plot_obj_space_1d(self.get_history(), **kwargs)
+        elif n_objs == 2:
+            return plot.plot_obj_space_2d(self.get_history(), problem_pf, **kwargs)
+        elif n_objs == 3:
+            return plot.plot_obj_space_3d(self.get_history(), problem_pf, **kwargs)
+        else:
+            warnings.warn("Not supported yet.")
---
+++ packages/evox/evox-0.9.0rc2-py3-none-any.whl/evox/problems/neuroevolution/reinforcement_learning/brax.py
@@ -0,0 +1,198 @@
+from typing import Callable, Any, Optional
+from brax import envs
+from brax.io import html, image
+import jax
+from jax import jit, vmap
+import jax.numpy as jnp
+import jax.tree_util as jtu
+from evox import Problem, State, jit_method
+
+
+def vmap_rng_split(key: jax.Array, num: int = 2) -> jax.Array:
+    # batched_key [B, 2] -> batched_keys [num, B, 2]
+    return jax.vmap(jax.random.split, in_axes=(0, None), out_axes=1)(key, num)
+
+
+class Brax(Problem):
+    def __init__(
+        self,
+        policy: Callable,
+        env_name: str,
+        max_episode_length: int,
+        num_episodes: int,
+        stateful_policy: bool = False,
+        initial_state: Any = None,
+        reduce_fn: Callable[[jax.Array, int], jax.Array] = jnp.mean,
+        backend: str = "generalized",
+    ):
+        """Contruct a brax-based problem
+
+        Parameters
+        ----------
+        policy
+            A callable if stateful: ``fn(state, weight, obs) -> action, state`` otherwise ``fn(weights, obs) -> action``
+        env_name
+            The environment name.
+        batch_size
+            The number of brax environments to run in parallel.
+            Usually this should match the population size at the algorithm side.
+        max_episode_length
+            The maximum number of timesteps of an episode.
+        num_episodes
+            Evaluating the number of episodes for each individual.
+        stateful_policy
+            Whether the policy is stateful (for example, RNN).
+            Default to False.
+            If False, the policy should be a pure function with signature (weights, obs) -> action.
+            If True, the policy should be a stateful function with signature (state, weights, obs) -> (action, state).
+        initial_state
+            The initial state of the stateful policy.
+            Default to None.
+            Only used when stateful_policy is True.
+        reduce_fn
+            The function to reduce the rewards of multiple episodes.
+            Default to jnp.mean.
+        backend
+            Brax's backend, one of "generalized", "positional", "spring".
+            Default to "generalized".
+        """
+        if stateful_policy:
+            self.batched_policy = jit(vmap(vmap(policy, in_axes=(0, None, 0))))
+        else:
+            self.batched_policy = jit(vmap(vmap(policy, in_axes=(None, 0))))
+        self.policy = policy
+        self.env_name = env_name
+        self.backend = backend
+        self.env = envs.wrappers.training.VmapWrapper(
+            envs.get_environment(env_name=env_name, backend=backend)
+        )
+        self.stateful_policy = stateful_policy
+        self.initial_state = initial_state
+        self.max_episode_length = max_episode_length
+        self.num_episodes = num_episodes
+        self.reduce_fn = reduce_fn
+
+        self.jit_reset = jit(vmap(self.env.reset))
+        self.jit_env_step = jit(vmap(self.env.step))
+
+    def setup(self, key):
+        return State(key=key)
+
+    @jit_method
+    def evaluate(self, state, weights):
+        pop_size = jtu.tree_leaves(weights)[0].shape[0]
+        key, eval_key = jax.random.split(state.key)
+
+        def _cond_func(carry):
+            counter, _state, done, _total_reward = carry
+            return (counter < self.max_episode_length) & (~done.all())
+
+        def _body_func(carry):
+            counter, rollout_state, done, total_reward = carry
+            if self.stateful_policy:
+                state, brax_state = rollout_state
+                action, state = self.batched_policy(state, weights, brax_state.obs)
+                rollout_state = (state, brax_state)
+            else:
+                (brax_state,) = rollout_state
+                action = self.batched_policy(weights, brax_state.obs)
+                rollout_state = (brax_state,)
+            brax_state = self.jit_env_step(brax_state, action)
+            done = brax_state.done * (1 - done)
+            total_reward += (1 - done) * brax_state.reward
+            return counter + 1, rollout_state, done, total_reward
+
+        brax_state = self.jit_reset(
+            vmap_rng_split(jax.random.split(eval_key, self.num_episodes), pop_size)
+        )
+
+        if self.stateful_policy:
+            initial_state = jax.tree.map(
+                lambda x: jnp.broadcast_to(x, (pop_size, self.num_episodes, *x.shape)),
+                self.initial_state,
+            )
+            rollout_state = (initial_state, brax_state)
+        else:
+            rollout_state = (brax_state,)
+
+        # [pop_size, num_episodes]
+        _, _, _, total_reward = jax.lax.while_loop(
+            _cond_func,
+            _body_func,
+            (
+                0,
+                rollout_state,
+                jnp.zeros((pop_size, self.num_episodes)),
+                jnp.zeros((pop_size, self.num_episodes)),
+            ),
+        )
+
+        total_reward = self.reduce_fn(total_reward, axis=-1)
+
+        return total_reward, state.replace(key=key)
+
+    def visualize(
+        self,
+        key,
+        weights,
+        output_type: str = "HTML",
+        respect_done: bool = False,
+        num_episodes: Optional[int] = None,
+        *args,
+        **kwargs,
+    ):
+        """Visualize the brax environment with the given policy and weights.
+
+        Parameters
+        ----------
+        key
+            The random key.
+        weights
+            The weights of the policy.
+        output_type
+            The output type, either "HTML" or "rgb_array".
+        respect_done
+            Whether to respect the done signal.
+        num_episodes
+            The number of episodes to visualize, used to override the num_episodes in the constructor.
+            If None, use the num_episodes in the constructor.
+        """
+        assert output_type in [
+            "HTML",
+            "rgb_array",
+        ], "output_type must be either HTML or rgb_array"
+
+        env = envs.get_environment(env_name=self.env_name, backend=self.backend)
+        brax_state = jax.jit(env.reset)(key)
+        jit_env_step = jit(env.step)
+        trajectory = [brax_state.pipeline_state]
+        episode_length = 1
+
+        if self.stateful_policy:
+            rollout_state = (self.initial_state, brax_state)
+        else:
+            rollout_state = (brax_state,)
+
+        num_episodes = num_episodes or self.num_episodes
+        for _ in range(num_episodes):
+            if self.stateful_policy:
+                state, brax_state = rollout_state
+                action, state = self.policy(state, weights, brax_state.obs)
+                rollout_state = (state, brax_state)
+            else:
+                (brax_state,) = rollout_state
+                action = self.policy(weights, brax_state.obs)
+                rollout_state = (brax_state,)
+
+            trajectory.append(brax_state.pipeline_state)
+            brax_state = jit_env_step(brax_state, action)
+            trajectory.append(brax_state.pipeline_state)
+            episode_length += 1 - brax_state.done
+
+            if respect_done and brax_state.done:
+                break
+
+        if output_type == "HTML":
+            return html.render(env.sys, trajectory, *args, **kwargs)
+        else:
+            return image.render_array(env.sys, trajectory, **kwargs)
