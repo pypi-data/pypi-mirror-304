d8bc384de7d63c6fa942b949e4c9fb44842dde5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:23 +0000

    Add package clouddatabridge/0.1.1/clouddatabridge-0.1.1-py3-none-any.whl

---
+++ packages/clouddatabridge/clouddatabridge-0.1.1-py3-none-any.whl/clouddatabridge/rds_execute.py
@@ -0,0 +1,46 @@
+import pymysql.cursors
+from .db_connection import get_db_connection
+
+def get_operation_from_query(query):
+    """
+    Determine the SQL operation (e.g., 'select', 'insert', 'update', 'delete') from the query.
+    """
+    return query.strip().split()[0].lower()
+
+def rds_execute(service, query, params=None, **config):
+    """
+    Execute a query on the RDS or local database.
+    """
+    connection = None
+    try:
+        connection_obj = get_db_connection(service, **config)
+        connection = connection_obj.get_db_connection()
+        with connection.cursor() as cursor:
+            if isinstance(params, list) and all(isinstance(i, (tuple, list)) for i in params):
+                cursor.executemany(query, params)
+            else:
+                if params:
+                    cursor.execute(query, params)
+                else:
+                    cursor.execute(query)
+
+            operation = get_operation_from_query(query)
+            if operation in ['insert', 'update', 'delete']:
+                connection.commit()
+
+            if operation == 'select':
+                return cursor.fetchall()
+
+    except pymysql.OperationalError as op_err:
+        print(f'Operational Error in DB query: {query}')
+        print(op_err)
+        return op_err
+
+    except Exception as e:
+        print(f'Error while executing DB query: {query}')
+        print(e)
+        return e
+
+    finally:
+        if connection:
+            connection_obj.close_connection()
