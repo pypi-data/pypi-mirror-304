254d4c35740655cd5be63b673d43fe0709d778bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:41 +0000

    Add package streamlit-tree-select-dark/0.0.13/streamlit_tree_select_dark-0.0.13.tar.gz

---
+++ packages/streamlit-tree-select-dark/streamlit_tree_select_dark-0.0.13.tar.gz/streamlit_tree_select_dark-0.0.13/streamlit_tree_select/frontend/build/precache-manifest.9918972cec8eb26a907ae1add7807b3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9211815f0c58e22f90692d137718a64b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a16aeab14a683d1bbb8",
+    "url": "./static/js/2.35ac2ee5.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.35ac2ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c53a489e013fcd685e",
+    "url": "./static/js/main.8786dc3a.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
