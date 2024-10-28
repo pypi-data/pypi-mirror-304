5e73fb759b4bad7c14a9c38e8a73ef9ccf32f34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:05:19 +0000

    Add package torchic/0.3.0/torchic-0.3.0-py3-none-any.whl

---
+++ packages/torchic/torchic-0.3.0-py3-none-any.whl/torchic/physics/__init__.py
@@ -0,0 +1,18 @@
+from torchic.physics.calibration import (
+    bethe_bloch_calibration,
+    cluster_size_calibration,
+    BetheBloch,
+    pyBetheBloch,
+    pySimilBetheBloch,
+)
+
+from torchic.physics import ITS
+
+__all__ = [
+    'bethe_bloch_calibration',
+    'cluster_size_calibration',
+    'BetheBloch',
+    'pyBetheBloch',
+    'pySimilBetheBloch',
+    'ITS',
+]
