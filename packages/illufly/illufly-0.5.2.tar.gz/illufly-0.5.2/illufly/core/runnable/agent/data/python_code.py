4beecc83de8126d8afaf922e9038a12097ddda1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:34 +0000

    Add package illufly/0.5.2/illufly-0.5.2.tar.gz

---
+++ packages/illufly/illufly-0.5.2.tar.gz/illufly-0.5.2/illufly/core/runnable/agent/data/python_code.py
@@ -0,0 +1,130 @@
+from typing import Dict, Any
+
+from .....utils import extract_segments, raise_invalid_params
+from .....io import EventBlock
+from ....dataset import Dataset
+from ...prompt_template import PromptTemplate
+from ..base import BaseAgent
+
+import textwrap
+import pandas as pd
+import numpy as np
+
+class PandasAgent(BaseAgent):
+    """
+    对所提供的 pandas 数据集做分析和处理。
+    """
+    @classmethod
+    def allowed_params(cls):
+        return {
+            "datasets": "数据集清单, 类型为 Dataset 实例，或者提供 `{name: {description, df}}` 的键值对来创建 Dataset 实例",
+            "agent": "生成代码的 ChatAgent 实例",
+            "template_id": "生成代码提示语模板ID",
+            **BaseAgent.allowed_params(),
+        }
+
+    def __init__(self, datasets: Dataset, agent: "ChatAgent", template_id: str=None, **kwargs):
+        self.datasets = datasets if isinstance(datasets, Dataset) else Dataset(datasets)
+        self.template_id = template_id or "CODE/Pandas"
+
+        self.agent = agent
+        template = PromptTemplate(self.template_id, binding_map={
+            "registered_global": lambda: list(self.registered_global.keys()),
+            "safe_builtins": lambda: list(self.safe_builtins.keys()),
+            "names": lambda: self.datasets.names,
+            "description": lambda: self.datasets.description,
+            "summary": lambda: self.datasets.summary
+        })
+        self.agent.reset_init_memory(template)
+        self.agent.start_marker = "```python"
+        self.agent.end_marker = "```"
+
+        # 生成作为工具被使用时的功能描述
+        description = kwargs.pop(
+            "description",
+            f"回答关于[{self.datasets.names}]等数据集的问题。\n具体包括：{self.datasets.description}"
+        )
+        super().__init__(description=description, **kwargs)
+        self._last_code = None
+    @property
+    def safe_builtins(self):
+        return {
+            "abs": abs,
+            "all": all,
+            "any": any,
+            "bin": bin,
+            "bool": bool,
+            "chr": chr,
+            "complex": complex,
+            "divmod": divmod,
+            "enumerate": enumerate,
+            "filter": filter,
+            "hex": hex,
+            "isinstance": isinstance,
+            "issubclass": issubclass,
+            "iter": iter,
+            "len": len,
+            "map": map,
+            "max": max,
+            "min": min,
+            "next": next,
+            "oct": oct,
+            "ord": ord,
+            "pow": pow,
+            "range": range,
+            "reversed": reversed,
+            "round": round,
+            "sorted": sorted,
+            "sum": sum,
+            "zip": zip,
+    }
+
+    @property
+    def registered_global(self):
+        return {
+            "__builtins__": self.safe_builtins,
+            "pd": pd,  # 允许 pandas 模块
+            "np": np,  # 允许 numpy 模块
+            "math": __import__('math'),
+            "random": __import__('random'),
+            "datetime": __import__('datetime'),
+            "collections": __import__('collections'),
+            "itertools": __import__('itertools'),
+            "functools": __import__('functools'),
+            "operator": __import__('operator'),
+            "data": self.datasets.df,
+            "pandas_code_exe_result": None,
+        }
+
+    @property
+    def last_code(self):
+        return self._last_code
+
+    def call(self, question: str, *args, **kwargs):
+        new_chat = kwargs.pop("new_chat", True)
+        yield from self.agent.call(question, *args, **kwargs, new_chat=new_chat)
+
+        self._last_code = self.agent.last_output
+        if self.last_code:
+            self._last_output = self.execute_code(self.last_code)
+            yield EventBlock("text", self._last_output)
+        else:
+            yield EventBlock("warn", "没有正确生成python代码失败。")
+
+    def execute_code(self, code: str):
+        """
+        执行代码，并返回执行结果。
+        """
+
+        # 禁止代码中执行引入其他库的操作
+        safety_code = '\n'.join([line for line in code.split('\n') if not line.strip().startswith('import')])
+        code_to_exec = f"{safety_code}\n\npandas_code_exe_result = main()\n"
+
+        # 创建一个新的无污染空间
+        exec_namespace = self.registered_global.copy()
+        try:
+            exec(code_to_exec, exec_namespace)
+        except Exception as e:
+            return f"执行代码时发生错误: {e}"
+
+        return exec_namespace.get('pandas_code_exe_result', "生成的代码已经执行，但返回了空结果。")
---
+++ packages/illufly/illufly-0.5.2.tar.gz/illufly-0.5.2/illufly/hub/prompt.py
@@ -0,0 +1,184 @@
+"""
+    提示语模板采用了`mustache`语法，这是一种逻辑无关的模板语法，包括以下特性：
+    1. **变量替换**：使用`{{variable}}`来插入变量的值。
+    2. **注释**：使用`{{! comment }}`来添加不会在渲染结果中显示的注释。
+    3. **条件判断**：使用`{{#condition}}{{/condition}}`来进行条件判断，如果条件为真，则渲染内部的内容。
+    4. **部分模板**：使用`{{> partial}}`来包含另一个模板文件。
+    ... 还有其他一些可用的特性
+"""
+
+from typing import List, Dict, Any, Union
+from importlib.resources import read_text, is_resource, contents
+from chevron.renderer import render as mustache_render
+from chevron.tokenizer import tokenize as mustache_tokenize
+from ..config import get_env
+
+import os
+import re
+import json
+
+PROMPT_WRITING_BASE = 'illufly.__PROMPT_TEMPLATES__'
+
+def find_resource_template(*seg_path):
+    """
+    过滤出提示语模板所在的目录清单。
+    """
+
+    all_resources = contents(".".join([PROMPT_WRITING_BASE, *seg_path]))
+    return [r for r in all_resources if not is_resource(f'{PROMPT_WRITING_BASE}', r)]
+
+def load_resource_template(template_id: str):
+    """
+    从python包资源文件夹加载提示语模板。
+    """
+    template_id = template_id.replace(os.sep, '.')
+    parts = template_id.split('.')
+    if parts[-1] not in find_resource_template(*parts[:-1]):
+        raise ValueError(f"<{template_id}> template_id not exist !")
+
+    def _get_template_str(res_file: str):
+        if (res_folder := f'{PROMPT_WRITING_BASE}.{template_id}') and is_resource(res_folder, res_file):
+            return read_text(res_folder, res_file)
+        elif (res_folder := f'{PROMPT_WRITING_BASE}') and is_resource(res_folder, res_file):
+            return read_text(res_folder, res_file)
+        else:
+            return ''
+
+    prompt_str = _get_template_str('main.mu')
+
+    # 替换 {{>include_name}} 变量
+    include_dict = {}
+    matches = re.findall(r'{{>(.*?)}}', prompt_str)
+    for part_name in matches:
+        prmopt_str = _get_template_str(f'{part_name.strip()}.mu')
+        if prmopt_str:
+            include_dict[part_name] = prmopt_str
+        else:
+            raise RuntimeError(f"Can't find {part_name}.mu in {template_id} !")
+    for part_name, part_str in include_dict.items():
+        prompt_str = prompt_str.replace("{{>" + part_name + "}}", part_str)
+
+    return prompt_str
+
+def _find_prompt_file(template_id: str, file_name, template_folder: str=None, sep: str=None, all_path: List[str]=[], force: bool=False):
+    sep = sep or os.sep
+    template_id = template_id.strip(f'{sep}| ')
+
+    prompt_folder = os.path.join(template_folder or "", template_id)
+    if sep != os.sep:
+        prompt_folder = prompt_folder.replace(os.sep, sep).strip(sep)
+
+    for ext in ['mu', 'mustache', 'txt']:
+        if (file_path := os.path.join(prompt_folder, f'{file_name}.{ext}')) and os.path.exists(file_path):
+            return file_path
+
+    all_path.append(prompt_folder)
+    if not template_id:
+        if force:
+            raise ValueError(f"Can't find {file_name}(.mu, .mustache, .txt) in [ {', '.join(all_path)} ]!")
+        else:
+            return None
+    return _find_prompt_file(template_id.rpartition(sep)[0], file_name, template_folder, sep, all_path)
+
+def load_prompt_template(template_id: str, template_folder: str=None,):
+    """
+    从模板文件夹加载提示语模板。
+
+    提示语模板中的约定：
+    1. 加载模板时，从模板路径开始寻找，如果找不到会向上查找文件，直至模板根文件夹
+    2. 提示语模板的主文件是 main.mu
+    3. 预处理 main.mu 中的 {{>include_name}} 语法
+    4. 如果找不到 main.mu 文件，则从资源文件夹中加载
+    """
+    template_folder = template_folder or get_env("ILLUFLY_PROMPT_TEMPLATE_LOCAL_FOLDER")
+    main_prompt = _find_prompt_file(template_id, 'main', template_folder)
+
+    if main_prompt:
+        with open(main_prompt, 'r') as f:
+            prompt_str = f.read()
+
+            # 替换 {{>include_name}} 变量
+            include_dict = {}
+            matches = re.findall(r'{{>(.*?)}}', prompt_str)
+            for part_name in matches:
+                part_file = _find_prompt_file(template_id, part_name.strip(), template_folder)
+                with open(part_file, 'r') as f:
+                    include_dict[part_name] = f.read()
+            for part_name, part_str in include_dict.items():
+                prompt_str = prompt_str.replace("{{>" + part_name + "}}", part_str)
+
+            return prompt_str
+    else:
+        prompt_str = load_resource_template(template_id)
+        if prompt_str:
+            return prompt_str
+
+    raise ValueError(f'无法构建模板：{template_id}')
+
+def get_template_variables(template_text: str):
+    vars: Set[str] = set()
+    section_depth = 0
+    for token_type, key in mustache_tokenize(template_text):
+        if token_type == "end":
+            section_depth -= 1
+        elif (
+            token_type in ("variable", "section", "inverted section", "no escape")
+            and key != "."
+            and section_depth == 0
+        ):
+            vars.add(key.split(".")[0])
+        if token_type in ("section", "inverted section"):
+            section_depth += 1
+    return vars
+
+def clone_prompt_template(template_id: str, template_folder: str=None):
+    """
+    克隆提示语模板。
+    根据指定 template_id 将文件夹和文件拷贝到本地 template_folder 位置。
+    
+    如果已经存在，就不再覆盖已修改的模板成果。
+    """
+    template_path = os.path.join(
+        template_folder or get_env("ILLUFLY_PROMPT_TEMPLATE_LOCAL_FOLDER"),
+        template_id.replace('.', os.sep)
+    )
+
+    # 如果已经存在，就不再覆盖已修改的模板成果。
+    if os.path.exists(template_path):
+        files = os.listdir(template_path)
+        if files:
+            raise ValueError(f"template_folder [{template_path}] not empty !")
+
+    # 确保 template_id 是有效的
+    template_id = template_id.replace(os.sep, '.')
+    parts = template_id.split('.')
+    if parts[-1] not in find_resource_template(*parts[:-1]):
+        raise ValueError(f"<{template_id}> template_id not exist !")
+
+    # 如果模板不存在，则创建目录
+    os.makedirs(template_path, exist_ok=True)
+
+    def _get_template_str(res_file: str):
+        if (res_folder := f'{PROMPT_WRITING_BASE}.{template_id}') and is_resource(res_folder, res_file):
+            return read_text(res_folder, res_file)
+        elif (res_folder := f'{PROMPT_WRITING_BASE}') and is_resource(res_folder, res_file):
+            return read_text(res_folder, res_file)
+        else:
+            return ''
+
+    prompt_str = _get_template_str('main.mu')
+    if not prompt_str:
+        raise ValueError(f'main.mu MUST exist in {template_id} !')
+    
+    with open(os.path.join(template_path, 'main.mu'), 'w') as f:
+        f.write(prompt_str)
+
+        # 保存替换 {{>include_name}} 变量文件
+        matches = re.findall(r'{{>(.*?)}}', prompt_str)
+        for part_name in matches:
+            prmopt_str = _get_template_str(f'{part_name.strip()}.mu')
+            if prmopt_str:
+                with open(os.path.join(template_path, f'{part_name}.mu'), 'w') as f:
+                    f.write(prmopt_str)
+
+    return template_path
