1a830409bf2bf49f96be1f6f1f4dd3648abb2a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:17 +0000

    Add package JunoAccessManager/1.0.1/JunoAccessManager-1.0.1.tar.gz

---
+++ packages/JunoAccessManager/JunoAccessManager-1.0.1.tar.gz/JunoAccessManager-1.0.1/junoaccessmanager/src/core/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from junoaccessmanager.src.apps.routers.user import user_router
+from junoaccessmanager.src.apps.routers.permission import permission_router
+from junoaccessmanager.src.apps.routers.pulsar_manage import pulsar_router
+
+from junoaccessmanager.src.settings import settings
+
+if settings.API_PREFIX:
+    base_url = f"/{settings.API_PREFIX}"
+else:
+    base_url = ""
+
+
+router = APIRouter(prefix=base_url)
+
+router.include_router(user_router, tags=["用户"], prefix='/user')
+router.include_router(permission_router, tags=["权限"], prefix='/permission')
+router.include_router(pulsar_router, tags=["操作"], prefix='/pulsar')
