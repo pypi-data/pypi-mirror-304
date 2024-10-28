fc948ecd78f8d080f42ae06a3152e2e3d04cd251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:36 +0000

    Add package python-logging-filters/0.1.5/python_logging_filters-0.1.5.tar.gz

---
+++ packages/python-logging-filters/python_logging_filters-0.1.5.tar.gz/python_logging_filters-0.1.5/python_logging_filters/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import re
+
+
+class DjangoHttp404LogFilter(logging.Filter):
+    """Suppresses Django's default 'Not Found: ...' log messages.
+
+    This filter can be integrated into the django `LOGGING` setting,
+    see the following excerpt as an example:
+
+    LOGGING = {
+        ...,
+        "handlers": {
+            "console": {
+                ...,
+                "filters": ["http404"],
+            },
+        },
+        "filters": {
+            "http404": {
+                "()": "python_logging_filters.DjangoHttp404LogFilter",
+            }
+        },
+        ...,
+    }
+
+    NOTE: At the moment, this filter does only take the desired effect with
+    production settings and using a mature WSGI-compliant server (not
+    `./manage.py runserver`). Using the Django debug server or setting
+    `DEBUG = True` issues a "Not Found" log warning which cannot be trivially
+    filtered.
+    """
+    PATTERN = re.compile("^Not Found:")
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        is_not_found_record = (
+            self.PATTERN.match(record.msg) is not None
+            and record.levelno == logging.WARNING
+            and record.name.startswith("django")
+        )
+        return not is_not_found_record
