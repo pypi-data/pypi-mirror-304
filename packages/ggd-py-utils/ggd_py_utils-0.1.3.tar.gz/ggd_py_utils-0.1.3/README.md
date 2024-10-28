8b3da5fded021eab9d7382b5a9f85ae3e729ace9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:04:47 +0000

    Add package ggd-py-utils/0.1.3/ggd_py_utils-0.1.3.tar.gz

---
+++ packages/ggd-py-utils/ggd_py_utils-0.1.3.tar.gz/ggd_py_utils-0.1.3/README.md
@@ -0,0 +1,38 @@
+**Package Purpose**
+
+The `ggd-py-utils` package is a collection of reusable utilities for Python projects. The package is designed to provide a set of useful tools for various tasks, including tracing, machine learning, and formatting.
+
+**Dependencies**
+
+The package requires the following dependencies:
+
+* `faiss-cpu==1.8.0`
+* `unidecode==1.3.8`
+* `colorama==0.4.6`
+* `chime==0.7.0`
+* `fasttext-wheel==0.9.2`
+* `plotly==5.22.0`
+* `nltk==3.8.1`
+* `tf_keras==2.18.0`
+* `numpy==1.26.4`
+* `torch==2.2.2`
+* `sentence-transformers==3.0.1`
+
+**Installation**
+
+To install the package, run the following command:
+```bash
+pip uninstall ggd-py-utils -y; pip install ggd-py-utils
+```
+**Updating**
+
+To update the package, run the following command:
+```bash
+pip install --upgrade ggd-py-utils
+```
+**Uninstallation**
+
+To uninstall the package, run the following command:
+```bash
+pip uninstall ggd-py-utils -y
+```
---
+++ packages/ggd-py-utils/ggd_py_utils-0.1.3.tar.gz/ggd_py_utils-0.1.3/ggd_py_utils/machine_learning/data/cleaning.py
@@ -0,0 +1,38 @@
+from nltk.stem import SnowballStemmer
+
+snowball_stemmer = SnowballStemmer("spanish")
+
+def clean_text(text: str) -> str:
+    """
+    Clean a given text by removing punctuation, converting to lowercase, and removing Spanish stopwords.
+
+    :param text: The text to clean.
+    :return: The cleaned text.
+    """
+    spanish_stopwords: list[str] = ['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'sí', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'también', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mí', 'antes', 'algunos', 'qué', 'unos', 'yo', 'otro', 'otras', 'otra', 'él', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tú', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosotros', 'vosotras', 'os', 'mío', 'mía',
+                         'míos', 'mías', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estás', 'está', 'estamos', 'estáis', 'están', 'esté', 'estés', 'estemos', 'estéis', 'estén', 'estaré', 'estarás', 'estará', 'estaremos', 'estaréis', 'estarán', 'estaría', 'estarías', 'estaríamos', 'estaríais', 'estarían', 'estaba', 'estabas', 'estábamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuviéramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviésemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad']
+    text = text.lower()
+    
+    from unidecode import unidecode
+    
+    text = unidecode(text)
+    
+    from string import punctuation
+    
+    text = text.translate(str.maketrans(punctuation, ' ' * len(punctuation)))
+    
+    from regex import sub
+    
+    text = sub(r'\d+', '<N>', text)
+
+    words: list[str] = text.split()
+    text = [snowball_stemmer.stem(word) for word in words]
+    text = " ".join(text)
+
+    words: list[str] = text.split()
+    words = [word for word in words if word not in spanish_stopwords]
+    text = " ".join(words)
+    
+    text = sub(r'\s+', ' ', text).strip()
+    
+    return text
