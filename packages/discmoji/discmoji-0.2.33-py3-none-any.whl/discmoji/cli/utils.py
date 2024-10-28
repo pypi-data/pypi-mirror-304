7d56d091745e6fcdd776abda64295896ab7ca188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:19 +0000

    Add package discmoji/0.2.33/discmoji-0.2.33-py3-none-any.whl

---
+++ packages/discmoji/discmoji-0.2.33-py3-none-any.whl/discmoji/cli/utils.py
@@ -0,0 +1,7 @@
+"""
+    # discmoji.cli.utils
+    contains useful utilities that can be used for checks. Most checks that are not integrated into the 
+    structures themselves are here.
+"""
+
+...
