d0abd2d55d298778330842fbabccebcb68005202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 15:05:45 +0000

    Add package pythonhardmode/0.0.1/pythonhardmode-0.0.1-py3-none-any.whl

---
+++ packages/pythonhardmode/pythonhardmode-0.0.1-py3-none-any.whl/hardmode-py/hardmode.py
@@ -0,0 +1,11 @@
+def hardmode():
+    # pythonhardmode © 2024 by Nguyen Hoang Quoc Anh is licensed under CC BY-SA 4.0 
+    global str
+    global int
+    global print
+    global range
+    str = None
+    int = None
+    print = None
+    range = None
+
