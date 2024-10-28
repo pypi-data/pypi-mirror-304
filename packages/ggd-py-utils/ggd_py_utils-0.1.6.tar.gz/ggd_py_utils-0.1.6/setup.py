da53976c035c5d5e11ff2308a0b8aca5d56ace35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:04:55 +0000

    Add package ggd-py-utils/0.1.6/ggd_py_utils-0.1.6.tar.gz

---
+++ packages/ggd-py-utils/ggd_py_utils-0.1.6.tar.gz/ggd_py_utils-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ggd-py-utils",
+    version="0.1.6",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A collection of utility functions for my projects.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Gerardo Ignacio Galdames Díaz",
+    author_email="gerardogaldames@gmail.com",
+    url="https://github.com/ggaldamesd/ggd-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "faiss-cpu==1.8.0",
+        "unidecode==1.3.8",
+        "colorama==0.4.6",
+        "chime==0.7.0",
+        "fasttext-wheel==0.9.2",
+        "plotly==5.22.0",
+        "nltk==3.8.1",
+        "tf_keras==2.18.0",
+        "torch==2.2.2",
+        "numpy==1.26.4",
+        "sentence-transformers==3.0.1",
+    ],
+)
