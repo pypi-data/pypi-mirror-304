0f036d1210911ac81cd1bd891796e9000d9ee192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:22 +0000

    Add package pyinapp-purchase/0.0.3/pyinapp_purchase-0.0.3.tar.gz

---
+++ packages/pyinapp-purchase/pyinapp_purchase-0.0.3.tar.gz/pyinapp_purchase-0.0.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+long_description = ""
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name= "pyinapp_purchase",
+    packages= ["google_purchase"],
+    version= "0.0.3",
+    license= "MIT",
+    description= "pyinapp_purchase is an open-source Python library designed to simplify and securely validate in-app purchase tokens server side.",
+    author= "Michael Jalloh",
+    long_description=long_description,
+    long_description_content_type= "text/markdown",
+    author_email= "michaeljalloh19@gmail.com",
+    url= "https://github.com/Michael-Jalloh/pyinapp_purchase",
+    keywords= ["inapp purchase","inapp","purchase","google inapp purchase"],
+    install_requires=[
+        "PyJWT",
+        "requests",
+        "cryptography"
+    ],
+    classifiers= [
+        "Development Status :: 3 - Alpha",      
+        "Intended Audience :: Developers",      
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",   
+        "Programming Language :: Python :: 3",   
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    platforms=["any"],
+    project_urls={
+        "issues": "https://github.com/Michael-Jalloh/pyinapp_purchase/issues",
+        "source": "https://github.com/Michael-Jalloh/pyinapp_purchase"
+    },
+    package_dir={"google_purchase": "src/google_purchase"},
+)
---
+++ packages/pyinapp-purchase/pyinapp_purchase-0.0.3.tar.gz/pyinapp_purchase-0.0.3/src/google_purchase/google_purchase.py
@@ -0,0 +1,104 @@
+import time
+import json
+import requests
+import jwt
+from google_purchase.responses import GoggleResponse, GogglePurrchaseResponse, GoogleVerificationError
+
+
+class GogglePurchaseProduct(object):
+
+    def __init__(self, google_api_file_path: str) -> None:
+        with open(google_api_file_path) as fh:
+            json_data = json.load(fh)
+            self.key = json_data.get("private_key")
+            
+            self.claims = {
+            "iss": json_data.get("client_email"),
+            "scope": "https://www.googleapis.com/auth/androidpublisher",
+            "aud": json_data.get("token_uri"),
+            }
+        self.header = {"alg":"RS256", "typ":"JWT"}
+
+    def verify_purchase(self, package_name: str, product_id: str, purchase_token: str, expiration: int =120) -> GogglePurrchaseResponse:
+        claim_start = time.time()
+        self.claims["exp"] = claim_start + expiration
+        self.claims["iat"] = claim_start
+
+        token = jwt.encode(self.claims, self.key, headers=self.header, algorithm="RS256")
+        params = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            "assertion": token
+        }
+
+        r = requests.post("https://www.googleapis.com/oauth2/v4/token", data=params)
+        r.raise_for_status()
+        access_token = r.json()["access_token"]
+
+        headers = {
+            "Authorization": "Bearer " + access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = f"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{package_name}/purchases/products/{product_id}/tokens/{purchase_token}" #"{google_verify_purchase_endpoint}/{product_id}/tokens/{subscription_token}"
+        response = requests.get(url=url, headers=headers)
+
+        if response.status_code == 200:
+            return GogglePurrchaseResponse(response.json())
+        
+        raise GoogleVerificationError(response.json())
+
+    def consume_purchase(self, package_name: str, product_id: str, purchase_token: str, expiration: int = 120) -> GoggleResponse:
+        claim_start = time.time()
+        self.claims["exp"] = claim_start + expiration
+        self.claims["iat"] = claim_start
+
+        token = jwt.encode(self.claims, self.key, headers=self.header, algorithm="RS256")
+        params = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            "assertion": token
+        }
+
+        r = requests.post("https://www.googleapis.com/oauth2/v4/token", data=params)
+        r.raise_for_status()
+        access_token = r.json()["access_token"]
+
+        headers = {
+            "Authorization": "Bearer " + access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = f"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{package_name}/purchases/products/{product_id}/tokens/{purchase_token}:consume"
+        response = requests.post(url=url, headers=headers)
+
+        if response.status_code == 200:
+            return GoggleResponse({"message":"purchase consumed"})
+        
+        raise GoogleVerificationError(response.json())
+    
+    def acknowledge_purchase(self, package_name: str, product_id: str, purchase_token: str, payload: str = "", expiration: int = 120) -> GoggleResponse:
+        claim_start = time.time()
+        self.claims["exp"] = claim_start + expiration
+        self.claims["iat"] = claim_start
+
+        token = jwt.encode(self.claims, self.key, headers=self.header, algorithm="RS256")
+        params = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            "assertion": token
+        }
+
+        r = requests.post("https://www.googleapis.com/oauth2/v4/token", data=params)
+        r.raise_for_status()
+        access_token = r.json()["access_token"]
+
+        headers = {
+            "Authorization": "Bearer " + access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = f"https://androidpublisher.googleapis.com/androidpublisher/v3/applications/{package_name}/purchases/products/{product_id}/tokens/{purchase_token}:acknowledge"
+        response = requests.post(url=url, headers=headers, data=json.dumps({"developerPayload":payload}))
+
+        if response.status_code == 200:
+            return GoggleResponse({"message":"purchase acknowledge"})
+        
+        raise GoogleVerificationError(response.json())
