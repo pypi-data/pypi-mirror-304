a6dfac50ce512c9baa6520a0c0e59c2c02ae466b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 03:13:20 +0000

    Add package picofun/0.4.0/picofun-0.4.0.tar.gz

---
+++ packages/picofun/picofun-0.4.0.tar.gz/picofun-0.4.0/tests/test_template.py
@@ -0,0 +1,27 @@
+"""Test the template module."""
+
+import jinja2
+import pytest
+
+import picofun.errors
+import picofun.template
+
+
+def test_template_load() -> None:
+    """Test loading a template."""
+    template = picofun.template.Template("tests/data/templates")
+    file = template.get("empty.j2")
+    assert isinstance(file, jinja2.environment.Template)
+
+
+def test_template_load_missing() -> None:
+    """Test loading a missing template."""
+    template = picofun.template.Template("tests/data")
+    with pytest.raises(FileNotFoundError):
+        template.get("missing.j2")
+
+
+def test_template_render() -> None:
+    """Test rendering a template."""
+    template = picofun.template.Template("tests/data/templates")
+    assert template.render("empty.j2") == ""
