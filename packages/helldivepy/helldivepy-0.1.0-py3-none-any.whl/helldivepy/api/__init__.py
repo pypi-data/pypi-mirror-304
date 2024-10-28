ea9dfa706483b7d3d45ed671d6d5a7977f5d14f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:05:42 +0000

    Add package helldivepy/0.1.0/helldivepy-0.1.0-py3-none-any.whl

---
+++ packages/helldivepy/helldivepy-0.1.0-py3-none-any.whl/helldivepy/api/__init__.py
@@ -0,0 +1,15 @@
+from .dispatches import DispatchModule
+from .war import WarModule
+from .steam import SteamModule
+from .assignments import AssignmentsModule
+from .planets import PlanetsModule
+from .campaigns import CampaignModule
+
+__all__ = [
+    "DispatchModule",
+    "WarModule",
+    "SteamModule",
+    "AssignmentsModule",
+    "PlanetsModule",
+    "CampaignModule",
+]
