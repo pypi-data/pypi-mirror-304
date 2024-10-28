c931b14a2bf6c197b0e656a1ad5ea590b43f50a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:05:19 +0000

    Add package chatnoir-pyterrier/3.0.0/chatnoir_pyterrier-3.0.0.tar.gz

---
+++ packages/chatnoir-pyterrier/chatnoir_pyterrier-3.0.0.tar.gz/chatnoir_pyterrier-3.0.0/chatnoir_pyterrier/__init__.py
@@ -0,0 +1,13 @@
+from importlib_metadata import version
+
+__version__ = version("chatnoir-pyterrier")
+
+from logging import getLogger
+
+from chatnoir_pyterrier import retrieve, feature
+
+logger = getLogger("chatnoir-pyterrier")
+
+# Re-export from child modules.
+Feature = feature.Feature
+ChatNoirRetrieve = retrieve.ChatNoirRetrieve
