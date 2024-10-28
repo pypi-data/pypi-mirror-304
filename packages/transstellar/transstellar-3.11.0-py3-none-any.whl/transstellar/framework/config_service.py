e094aa9024ae94808379ab758ebc668cac7ca9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 15:06:03 +0000

    Add package transstellar/3.11.0/transstellar-3.11.0-py3-none-any.whl

---
+++ packages/transstellar/transstellar-3.11.0-py3-none-any.whl/transstellar/framework/config_service.py
@@ -0,0 +1,16 @@
+import yaml
+
+class ConfigService():
+    _instance = None
+    config = {}
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ConfigService, cls).__new__(cls)
+            with open('config.yml') as config_file:
+                cls._instance.config = yaml.safe_load(config_file)
+
+        return cls._instance
+
+    def get_config(self):
+        return self._instance.config
