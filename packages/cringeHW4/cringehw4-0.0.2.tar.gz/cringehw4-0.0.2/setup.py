bd0d5b10b3196cff556cb9218efa5ca833386e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:40 +0000

    Add package cringeHW4/0.0.2/cringehw4-0.0.2.tar.gz

---
+++ packages/cringeHW4/cringehw4-0.0.2.tar.gz/cringehw4-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='cringeHW4',
+    version='0.0.2',
+    py_modules=["order_receipt"],  # Модули, которые вы хотите включить
+    entry_points={
+        'console_scripts': [
+            'generate_receipt=generate_receipt:main',  # Команда для запуска скрипта
+        ],
+    },
+    author= 'osisochka',
+    author_email= 'a.onufrienko@edu.centraluniversity.ru',
+    description='generation of ordered receipts',
+    python_requires='>=3.6',
+)
+
