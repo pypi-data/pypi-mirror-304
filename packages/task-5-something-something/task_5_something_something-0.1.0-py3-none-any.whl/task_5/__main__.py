6260ce0307a8136b56b01aea863585414aef8072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:09 +0000

    Add package task-5-something-something/0.1.0/task_5_something_something-0.1.0-py3-none-any.whl

---
+++ packages/task-5-something-something/task_5_something_something-0.1.0-py3-none-any.whl/task_5/__main__.py
@@ -0,0 +1,11 @@
+import task_5
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-file', type=str, required=True)
+parser.add_argument('--output-file', type=str, required=True)
+
+args = parser.parse_args()
+
+task_5.otchet(args.input_file, args.output_file)
