a292938e049f22e06384826edccc17f15395247a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:32 +0000

    Add package dhondtxai/0.5.3/dhondtxai-0.5.3.tar.gz

---
+++ packages/dhondtxai/dhondtxai-0.5.3.tar.gz/dhondtxai-0.5.3/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dhondtxai',
+    version='0.5.3',
+    author='Türker Berk DÖNMEZ',
+    author_email='turkerberkdonmez@gmail.com',
+    description="An explainability tool for machine learning models using the D'Hondt method.",
+    url='https://github.com/turkerbdonmez/dhondtxai',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'catboost'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
