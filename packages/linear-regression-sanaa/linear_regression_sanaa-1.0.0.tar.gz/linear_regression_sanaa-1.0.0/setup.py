31908452c7c3e80f2b0e188e0de97bd70d368856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 05:04:41 +0000

    Add package linear-regression-sanaa/1.0.0/linear_regression_sanaa-1.0.0.tar.gz

---
+++ packages/linear-regression-sanaa/linear_regression_sanaa-1.0.0.tar.gz/linear_regression_sanaa-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='linear-regression-sanaa',
+   version='1.0.0',
+   author='sanaa bensaid',
+   author_email='s.bensaid@esi-sba.dz',
+   packages=[''],
+   url='http://pypi.python.org/pypi/linear-regression/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['']
+)
