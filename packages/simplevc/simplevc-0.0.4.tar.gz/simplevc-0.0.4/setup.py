111683b5a6713c005ea27cda2ce8639abd00dada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:08 +0000

    Add package simplevc/0.0.4/simplevc-0.0.4.tar.gz

---
+++ packages/simplevc/simplevc-0.0.4.tar.gz/simplevc-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+	readme = readme_file.read()
+
+requirements = []
+
+setup(
+	name="simplevc",
+	version="0.0.4",
+	author="Alden Leung",
+	author_email="alden.leung@gmail.com",
+	description="A simple Python version control package",
+	long_description=readme,
+	long_description_content_type="text/markdown",
+	url="https://github.com/aldenleung/simplevc/",
+	packages=find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		"Programming Language :: Python :: 3.9",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+	],
+)
