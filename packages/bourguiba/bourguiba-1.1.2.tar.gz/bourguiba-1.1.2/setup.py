ff772d8404a8d218aa81b793a8bdee61d1066a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 16:05:21 +0000

    Add package bourguiba/1.1.2/bourguiba-1.1.2.tar.gz

---
+++ packages/bourguiba/bourguiba-1.1.2.tar.gz/bourguiba-1.1.2/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bourguiba",
+    version="1.1.2",
+    author="si aziz bahloul",
+    author_email="azizbahloul3@gmail.com",
+    description="sidkom bourguiba ne7elkom il 9mal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AzizBahloul/bourguiba",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "bourguiba=bourguiba.art:display_ascii_art",
+        ],
+    },
+)
