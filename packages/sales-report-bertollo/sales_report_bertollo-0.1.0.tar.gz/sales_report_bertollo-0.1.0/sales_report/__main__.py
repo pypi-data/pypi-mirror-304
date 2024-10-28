aae672278ef0817154ce69c6ee7e6e6f2ae5b6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 04:05:40 +0000

    Add package sales-report-bertollo/0.1.0/sales_report_bertollo-0.1.0.tar.gz

---
+++ packages/sales-report-bertollo/sales_report_bertollo-0.1.0.tar.gz/sales_report_bertollo-0.1.0/sales_report/__main__.py
@@ -0,0 +1,16 @@
+import argparse
+
+from .report_generator import generate_report
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate sales report from CSV file.')
+    parser.add_argument('--input-file', required=True, help='Path to the input CSV file')
+    parser.add_argument('--output-file', required=True, help='Path to the output CSV file')
+
+    args = parser.parse_args()
+
+    generate_report(args.input_file, args.output_file)
+
+if __name__ == '__main__':
+    main()
