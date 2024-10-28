0cd98a7329b033fcf88a6099c5386a16e6c40b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-28 14:05:21 +0000

    Add package paravastu/1.8/paravastu-1.8-py3-none-any.whl

---
+++ packages/paravastu/paravastu-1.8-py3-none-any.whl/paravastu/__init__.py
@@ -0,0 +1,2 @@
+# paravastulab/__init__.py
+from .core import double_click, double_click_button, fix_dropbox_location, translate_path, change_directory, do_nothing, display_graphic_file, list_plot, interactive_list_plot
