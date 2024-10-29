6bdcbec38189ba630fa2d44ae0210fd19fbd04a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:10:58 +0000

    Add package pretix-sepadebit/2.5.2/pretix_sepadebit-2.5.2.tar.gz

---
+++ packages/pretix-sepadebit/pretix_sepadebit-2.5.2.tar.gz/pretix_sepadebit-2.5.2/pretix_sepadebit/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class SepaExport(models.Model):
+    event = models.ForeignKey(
+        "pretixbase.Event",
+        related_name="sepa_exports",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    organizer = models.ForeignKey(
+        "pretixbase.Organizer",
+        related_name="sepa_exports",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+    xmldata = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    testmode = models.BooleanField(default=False)
+    currency = models.CharField(max_length=9, blank=True)
+
+
+class SepaExportOrder(models.Model):
+    export = models.ForeignKey(SepaExport, on_delete=models.CASCADE)
+    order = models.ForeignKey("pretixbase.Order", on_delete=models.CASCADE)
+    payment = models.ForeignKey(
+        "pretixbase.OrderPayment", on_delete=models.CASCADE, null=True
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class SepaDueDate(models.Model):
+    payment = models.OneToOneField(
+        "pretixbase.OrderPayment",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="sepadebit_due",
+    )
+    date = models.DateField()
+    remind_after = models.DateTimeField()
+    reminded = models.BooleanField(default=False)
