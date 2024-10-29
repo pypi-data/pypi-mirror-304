ffdafeb950e54e6d50bfce76ad08971feea7506f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:10:57 +0000

    Add package pysedm/0.41.0/pysedm-0.41.0-py3-none-any.whl

---
+++ packages/pysedm/pysedm-0.41.0-py3-none-any.whl/pysedm/background.py
@@ -0,0 +1,325 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""  """
+import warnings
+import numpy as np
+import matplotlib.pyplot as mpl
+
+from astropy.io import fits as pf
+from astropy.utils.console import ProgressBar
+
+
+from propobject   import BaseObject
+from .utils       import tools
+from .sedm        import SEDM_CCD_SIZE
+
+DEGREE   = 5
+LEGENDRE = True
+
+NGAUSS = 1
+CONTDEGREE_GAUSS = 4
+LEGENDRE_GAUSS= False
+
+
+# Background uses multiprocess and not dask. #
+
+# ------------------ #
+#  Builder           #
+# ------------------ #
+def build_background(ccd, client=None,
+                    smoothing=[0,5], start=2, jump=10,
+                    savefile=None, ):
+    """ """
+    from .io import is_stdstars, filename_to_background_name
+    ccd.fit_background(start=start, jump=jump, smoothing=smoothing,
+                       set_it=False,
+                       is_std = is_stdstars(ccd.header), 
+                       client=client)
+    
+    ccd._background.writeto( filename_to_background_name(ccd.filename) )
+    if savefile is not None:
+        ccd._background.show(savefile=savefile)
+        mpl.close("all")
+
+
+# ------------------ #
+#  Main Functions    #
+# ------------------ #
+def get_background(contvalues, size=SEDM_CCD_SIZE, smoothing=[0,5]):
+    """ """
+    back = Background()
+    back.create(contvalues)
+    back.build(size[0],size[1], smoothing=smoothing)
+    return back
+
+def load_background(filename):
+    """ """
+    back = Background()
+    back.load(filename)
+    return back
+
+# ------------------- #
+#   MultiProcessing   #
+# ------------------- #
+def get_contvalue(spec):
+    """ """
+    spec.fit_continuum(DEGREE, legendre=LEGENDRE)
+    return spec.contmodel.fitvalues
+
+def get_contvalue_sdt(spec):
+    """ """
+    spec.fit_continuum(CONTDEGREE_GAUSS, legendre=LEGENDRE_GAUSS, ngauss=NGAUSS)
+    return spec.contmodel.fitvalues
+
+def fit_background(ccd, client, 
+                   start=2, jump=10, is_std=False):
+    """ calling `get_contvalue` for each ccd column (xslice).
+    This uses astropy's ProgressBar.map 
+
+    Parameters
+    ----------
+    ccd: pysedm.CCD
+        ccd for which we have to fit a background.
+
+    client: dask.distributed.Client
+        client use for computed distribution. None means simple for-loop
+
+    start, jump: int
+        decide which column to fit (from {start} every {jump})
+
+    is_std: bool
+        is it a standard star ? (much brighter)
+
+    Returns 
+    -------
+    dictionary 
+    """
+    # Running from ipython notebook
+    
+    index_column = range(ccd.width)[start::jump]
+
+    # - which function
+    apply_func = get_contvalue_sdt if is_std else get_contvalue
+    # - delayed or not ?
+    if client is not None:
+        import dask
+        apply_func = dask.delayed(apply_func)
+        
+    # - looping over dask.
+    res = {}
+    for index_ in index_column:
+        slice_ = ccd.get_xslice(index_)
+        res[index_] = apply_func(slice_) # dasked or not
+        
+    if client is not None: # is dasked
+        res = client.gather( client.compute(res) )
+
+    return res
+
+def _get_xaxis_polynomial_(xyv, degree=DEGREE, legendre=LEGENDRE,
+                         xmodel=None, clipping = [5,5]):
+    """ """
+    from modefit.basics import get_polyfit
+    x,y,v = xyv
+    flagin = ((np.nanmean(y) - clipping[0] * np.nanstd(y)) < y) *  (y< (np.nanmean(y) + clipping[1] * np.nanstd(y)))
+    
+    contmodel = get_polyfit(x[flagin], y[flagin], v[flagin], degree=degree, legendre=legendre)
+    contmodel.fit(a0_guess=np.nanmedian(y[flagin]))
+    
+    if xmodel is not None:
+        return contmodel.fitvalues, contmodel.model.get_model(x=xmodel)#, contmodel
+    return contmodel.fitvalues#, None, contmodel
+
+        
+class Background( BaseObject ):
+    """ """
+    PROPERTIES      = ["contvalues" ]
+    SIDE_PROPERTIES = ['filename',"header","y"]
+    DERIVED_PROPERTIES = ['input_columns', "input_background","background"]
+    
+    def load(self, filename):
+        """ """
+        data_ = pf.open(filename)
+        
+        # - background
+        self._derived_properties['background'] = data_[0].data
+        self._side_properties['header'] = data_[0].header
+        # - contvalues
+        contheader = data_["POLYVALUES"].header
+        params = [int(k.replace("VALUE","")) for k,v in contheader.items() if "VALUE" in k]
+        columns = data_["COLUMNS"].data
+        convalues = {col:{contheader['VALUE%d'%i]: c_ for i,c_ in zip(params, cval_i)}
+                         for col, cval_i in zip(columns, data_["POLYVALUES"].data)}
+        self.create(convalues) 
+        
+    def create(self, contvalues):
+        """ setup the instance based on the input 'contvalue' """
+        self._properties['contvalues']    = contvalues
+        self._derived_properties['input_columns'] = None
+        
+    def build(self, width, height, smoothing= [0,5]):
+        """ """
+        self._derived_properties['background'] = self.get_background(height, width, smoothing=smoothing)
+        
+    def contvalue_to_polynome(self, contvalue_):
+        """ """
+        from modefit.basics import polynomial_model, normal_and_polynomial_model
+        if "mu0" in contvalue_.keys():
+            poly = normal_and_polynomial_model( CONTDEGREE_GAUSS, NGAUSS)
+            poly.use_legendre=LEGENDRE_GAUSS
+        else:
+            poly = polynomial_model( DEGREE )
+            poly.use_legendre=LEGENDRE
+            
+        poly.setup([contvalue_[k] for k in poly.FREEPARAMETERS])
+        self._side_properties["y"] = np.linspace(0, SEDM_CCD_SIZE[1], self.n_inputcolumns*2)
+        model = poly.get_model(self._y)
+        del(poly)
+        return model
+
+
+    def get_background(self, width, height, smoothing = [0,5]):
+        """ """
+        from scipy import ndimage, interpolate
+        # get the blured image
+        self._filtered = ndimage.filters.gaussian_filter( self.input_background, smoothing)
+        # resample
+        orig_shape = np.shape(self.input_background)
+        x = np.linspace(0,1, orig_shape[0])
+        y = np.linspace(0,1, orig_shape[1])
+        nk = interpolate.RectBivariateSpline(x,y, self._filtered, kx=3,ky=3)
+        
+        return nk(np.linspace(0,1,width),np.linspace(0,1,height))
+
+
+    # ----------------- #
+    #   I/O and Plots   #
+    # ----------------- #
+    def writeto(self, savefile, overwrite=True, **header_kwargs):
+        """ save the background as a .fits file
+        
+        The object will be structured as follows:
+        0 PrimaryHDU: Background image 
+        1 POLYVALUES: list of values associated to the best fits of column continuum
+                      [parameter names in the header]
+        2 COLUMNS: list of the fitted columns
+        
+        Use load() to read a .fits file created by this method.
+        
+        Parameters
+        ----------
+        savefile: [string]
+            Full path of the .fits file to be created
+            
+        overwrite: [bool] -optional-
+            Shall this overwrite an existing file if any.
+
+        **header_kwargs additional information to be saved in the Primary header.
+        
+        Returns
+        -------
+        Void
+        """
+        img_shape = np.shape(self.background)
+        
+        self.header["NAXIS"] = len(img_shape)
+        self.header['NAXIS1'] = img_shape[0]
+        self.header['NAXIS2'] = img_shape[1]
+        self.header['TYPE']   = "background"
+        for k,v in header_kwargs:
+            self.header[k] = v
+            
+        # --- Build the HDU
+        hdu = [pf.PrimaryHDU(self.background, self.header)] # Background
+        
+        params = np.sort(list(list(self._contvalues.values())[0].keys()))
+        header_POLY = pf.Header()
+        for i, p in enumerate(params): 
+            header_POLY['VALUE%s'%i] = p
+            
+        hdu.append(pf.ImageHDU( [[self._contvalues[i][p] for p in params] for i in self.input_columns], name='POLYVALUES', header=header_POLY))
+        hdu.append(pf.ImageHDU(self.input_columns, name='COLUMNS'))
+        
+        hdulist = pf.HDUList(hdu)
+        hdulist.writeto(savefile, overwrite =overwrite)
+
+        
+    def show(self, savefile=None, vmin=None, vmax=None, show=True, **kwargs):
+        """ """
+        from .utils.mpl import figout
+        
+        fig = mpl.figure(figsize=[9,4])
+        space = 0.02
+        width = 0.38
+        axs  = fig.add_axes([0.1,0.1,width,0.8])
+        ax   = fig.add_axes([0.1+width+space,0.1,width,0.8])
+        axc  = fig.add_axes([0.1+(width+space)*2,0.1,0.02,0.8])
+        if vmin is None:
+            vmin = "2"
+        if vmax is None:
+            vmax = "98"
+        prop = dict(origin="lower", aspect="auto", 
+            vmax=np.percentile(self.input_background, float(vmax)) if type(vmax) == str else vmax,
+            vmin=np.percentile(self.input_background, float(vmin)) if type(vmin) == str else vmin)
+                          
+        axs.imshow(self.input_background,        **prop)
+        cl = ax.imshow(self.background, **prop)
+        
+        axs.set_title("column fit [source]")
+        ax.set_title("background")
+        ax.set_yticks([])
+        fig.colorbar(cl, axc)
+        
+        fig.figout(savefile=savefile, show=show)
+        
+    # -------------------- #
+    #    Properites        #
+    # -------------------- #
+    @property
+    def n_inputcolumns(self):
+        """ Number of column for which the background has been estimated"""
+        return len(self._contvalues)
+
+    @property
+    def input_columns(self):
+        """ index of the ccd column where the background has been estimated """
+        if self._derived_properties['input_columns'] is None:
+            self._derived_properties['input_columns'] = np.sort(list(self._contvalues.keys()))
+            
+        return self._derived_properties['input_columns']
+    
+    @property
+    def _contvalues(self):
+        """ """
+        return self._properties['contvalues']
+
+    @property
+    def _y(self):
+        """ """
+        if self._side_properties['y'] is None:
+            self._side_properties["y"] = np.linspace(0, 1, self.n_inputcolumns)
+        return self._side_properties['y']
+    
+
+    # -- Background
+    @property
+    def background(self):
+        """ """
+        return self._derived_properties["background"]
+    @property
+    def input_background(self):
+        """ """
+        if self._derived_properties["input_background"] is None:
+            self._derived_properties["input_background"] = \
+              np.asarray([self.contvalue_to_polynome(self._contvalues[i])
+                            for i in self.input_columns]).T
+        return self._derived_properties["input_background"]
+    
+    # -- Fits tools
+    @property
+    def header(self):
+        """ """
+        if self._side_properties["header"] is None:
+            self._side_properties["header"] = pf.Header()
+        return self._side_properties["header"]
---
+++ packages/pysedm/pysedm-0.41.0-py3-none-any.whl/pysedm/dask/daskes.py
@@ -0,0 +1,240 @@
+""" Dasked version of pysedm/bin/extractstars.py """
+
+import os
+import numpy as np
+from dask import delayed
+from .base import DaskCube
+
+
+from .. import get_sedmcube, fluxcalibration, io
+from ..sedm import SEDMExtractStar, flux_calibrate_sedm
+from astropy.io import fits
+
+
+def get_extractstar(cube, step1range=[4500,7000], step1bins=6,
+                   centroid="auto", **kwargs):
+    """ """
+    es = SEDMExtractStar(cube)
+    es.set_lbdastep1(lbdarange=step1range, bins=step1bins)
+    es.set_centroid(centroid=centroid)
+    return es
+
+def get_fluxcalfile(cubefile):
+    """ """
+    return io.fetch_nearest_fluxcal(mjd=fits.getval(cubefile,"MJD_OBS"))
+
+def run_extractstar(es, spaxelbuffer = 10,
+                    spaxels_to_use=None,
+                    spaxels_to_avoid=None,
+                    psfmodel="NormalMoffatTilted",
+                    slice_width = 1, fwhm_guess=None, 
+                    verbose=False,
+                    store_fig=True, store_data=True,
+                    **kwargs):
+    """ """
+    
+    if spaxels_to_use is not None: # You fixed which you want
+        if len(spaxels_to_use)<4:
+            warnings.warn("ExtractStar: You provided less than 4 spaxel to fit")
+        es.set_fitted_spaxels(spaxels_to_use)
+
+    elif es.fitted_spaxels is None: # Automatic selections (with or without spaxels)
+        es.get_spaxels_tofit(buffer=spaxelbuffer, update=True,
+                             spaxels_to_avoid=spaxels_to_avoid)
+
+    if verbose: print("* Starting extractstar.run")
+    es.run(slice_width=slice_width, psfmodel=psfmodel,
+           fwhm_guess=fwhm_guess, verbose=verbose, **kwargs)
+    
+    rawspec = es.get_spectrum("raw", persecond=True)
+    rawspec.set_filename(es.cube.filename.replace("e3d", "esspec"))
+    #
+    # - Output
+    if store_fig and es is not None:
+        basename = es.basename
+        es.show_mla(savefile = basename.replace("{placeholder}","esmla")+".pdf")
+        if es._es_headerkey["posok"]:
+            es.show_adr(savefile = basename.replace("{placeholder}","esadr")+".pdf")
+            es.show_psf(savefile = basename.replace("{placeholder}","espsf")+".pdf")
+        rawspec.show(savefile = basename.replace("{placeholder}","esspec")+".pdf")
+        
+    if store_data:
+        rawspec.writeto(rawspec.filename)
+
+    return rawspec
+
+def calibrate_spec(spec, fluxcalfile, default_airmass=1.1,
+                       store_fig=True, store_data=True):
+    """ """
+    if fluxcalfile is None:
+        fluxcal = None
+    else:
+        fluxcal = fluxcalibration.load_fluxcal_spectrum(fluxcalfile)
+        
+    if fluxcal is None:
+        spec.header["FLUXCAL"] = (False,"has the spectra been flux calibrated")
+        spec.header["CALSRC"] = (None, "Flux calibrator filename")
+        spec.header["BUNIT"]  = (spec.header.get('BUNIT',""),"Flux Units")
+        specout = spec.copy()
+    else:
+        spec.scale_by(fluxcal.get_inversed_sensitivity(spec.header.get("AIRMASS",default_airmass)),
+                      onraw=False)
+        spec.header["FLUXCAL"] = (True, "has the spectra been flux calibrated")
+        spec.header["CALSRC"] = (os.path.basename(fluxcalfile), "Flux calibrator filename")
+        spec.header["BUNIT"]  = ("erg/s/A/cm^2", "Flux Units")
+        specout = spec.copy()
+
+    specout.set_filename( spec.filename.replace("esspec","spec") )
+    if store_fig:
+        specout.show( savefile=specout.filename.replace(".fits",".pdf") )
+                         
+    if store_data:
+        specout.writeto(specout.filename)
+        
+    return specout
+        
+def build_fluxcalibrator(rawspec_std,
+                             store_plot=True, store_data=True):
+    """ 
+    
+
+    Returns
+    -------
+    speccal filename
+    """
+    speccal, fl = fluxcalibration.get_fluxcalibrator(rawspec_std, fullout=True)
+    speccal.header.set("SOURCE", os.path.basename(rawspec_std.filename),
+                           "This object has been derived from this file")
+    speccal.header.set("PYSEDMT","Flux Calibration Spectrum", "Object to use to flux calibrate")
+
+    speccal.set_filename( rawspec_std.filename.replace("esspec","fluxcal") )
+    if store_plot:
+        _ = fl.show( savefile=speccal.filename.replace(".fits",".pdf") )
+        
+    if store_data:
+        speccal.writeto( speccal.filename )
+
+    # Returning the filename because dask has serialization issue 
+    return speccal.filename
+
+
+
+class DaskES( DaskCube ):
+    """
+    1. Get Files
+    cubefiles
+
+    2. Get Cubes
+    cube = get_sedmcube(filecube)
+    cube.header['REDUCER'] = args.reducer
+    
+    3. remove cubes for CR
+
+    4. Extract point source from the cubes 
+    es_out = cube.extract_pointsource(**es_options)
+    
+    5. Get flux calibration file
+
+    6. flux calibrate the results
+
+    7. store the result
+    """
+
+
+    # =============== #
+    #    Methods      #
+    # =============== #
+
+
+    # -------- #
+    # Running  #
+    # -------- #
+    def compute(self, stdbasenames=None, get_delayed=False, **kwargs):
+        """ """
+        if stdbasenames is None:
+            data = self.get_datafiles(add_stdcalib=True, index_per_calib=True, **kwargs)
+            stdbasenames = np.unique(data.index.levels[0])
+        
+        spectra = [self.stdconnected_extractstars(std_basename=stdbasename)
+                    for stdbasename in stdbasenames]
+        
+        if get_delayed:
+            return spectra
+        
+        return self.client.compute(spectra)
+
+    def stdconnected_extractstars(self, std_basename=None, **kwargs):
+        """ """
+        #
+        # Get the cube paths
+        datafile_std  = self.get_datafiles(**kwargs).xs(std_basename).set_index("basename")
+        
+        cubefile_std = datafile_std.loc[std_basename]["filepath"]
+        cubefiles    = datafile_std.drop(std_basename)["filepath"].values
+
+        #
+        # Build the graph
+        raw_std = self.extract_rawstar(cubefile_std, centroid="brightest")
+        fluxcalfile = self.build_fluxcalibrator(raw_std)
+        f_spec = [raw_std]
+        for cubefile in cubefiles:
+            rawspec = self.extract_rawstar(cubefile)
+            spec = self.calibrate_rawspec(rawspec, fluxcalfile=fluxcalfile)
+            f_spec.append(spec)
+            
+        return f_spec
+    
+    @classmethod
+    def single_extractstars(cls, cube_filename, fluxcalfile=None, calibrate=True, **kwargs):
+        """ """
+        rawspec = cls.extract_rawstar(cube_filename, **kwargs)
+        if fluxcalfile is None and calibrate:
+            fluxcalfile = delayed(get_fluxcalfile)(cube_filename)
+            
+        return cls.calibrate_rawspec(rawspec, fluxcalfile)
+
+    # -------- #
+    # Static   #
+    # -------- #
+    @staticmethod
+    def extract_rawstar(cube_filename, centroid='auto', store_fig=True, store_data=True, **kwargs):
+        """ Extract the pointsource from the given filename 
+        
+        Parameters
+        ----------
+        cube_filename: [sting]
+            Path to the e3d cube.
+
+        Returns
+        -------
+        Spectrum (not flux calibrated)
+        """
+        # 1. Get cube
+        cube = delayed(get_sedmcube)(cube_filename)
+                
+        # 3. Get ExtractStar
+        estar = delayed(get_extractstar)(cube, centroid=centroid)
+        
+        # 4. Run ExtractStar
+        rawspec = delayed(run_extractstar)(estar, store_fig=store_fig, store_data=store_data,
+                                               **kwargs)
+        return rawspec
+
+    @staticmethod
+    def calibrate_rawspec(rawspec, fluxcalfile):
+        """ """
+        return delayed(calibrate_spec)(rawspec, fluxcalfile)
+
+    @staticmethod
+    def build_fluxcalibrator(rawspec):
+        """ """
+        return delayed(build_fluxcalibrator)(rawspec)
+        
+
+
+    # -------- #
+    # Checks   #
+    # -------- #
+    def compute_check(self, futures):
+        """ """
+        
