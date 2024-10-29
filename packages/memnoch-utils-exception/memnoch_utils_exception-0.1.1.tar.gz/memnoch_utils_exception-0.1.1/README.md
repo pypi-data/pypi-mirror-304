05d5eb9ed74e133085a412a9f1d1b379c776b171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:11:58 +0000

    Add package memnoch-utils-exception/0.1.1/memnoch_utils_exception-0.1.1.tar.gz

---
+++ packages/memnoch-utils-exception/memnoch_utils_exception-0.1.1.tar.gz/memnoch_utils_exception-0.1.1/README.md
@@ -0,0 +1,13 @@
+# memnoch_utils_exception
+Une courte description de mon package.
+
+## Installation
+
+```bash
+pip install memnoch_utils_exception
+```
+
+## Utilisation
+
+from memnoch_utils_exception import *
+ma_fonction()
---
+++ packages/memnoch-utils-exception/memnoch_utils_exception-0.1.1.tar.gz/memnoch_utils_exception-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="memnoch_utils_exception",
+    version="0.1.1",
+    author="Guillaume LEFEBVRE",
+    author_email="guillaume@ldmail.fr",
+    description="",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Memnoch9178/mem-utils",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['memnoch_utils_texte'],
+    include_package_data=True,
+)
