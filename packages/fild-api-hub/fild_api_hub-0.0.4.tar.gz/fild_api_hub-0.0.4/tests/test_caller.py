d17353d806fb4aafab94327c7061b814c0104463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:10:58 +0000

    Add package fild-api-hub/0.0.4/fild_api_hub-0.0.4.tar.gz

---
+++ packages/fild-api-hub/fild_api_hub-0.0.4.tar.gz/fild_api_hub-0.0.4/tests/test_caller.py
@@ -0,0 +1,46 @@
+import json
+
+from fild.sdk import Dictionary
+from requests import Response
+
+from fildapi import ApiCaller
+from tests.data.dummy_api import CheckCall
+
+
+class CheckCallCaller(ApiCaller):
+    method = CheckCall
+
+
+def test_verify_response():
+    caller = CheckCallCaller()
+    caller.response = Response()
+    caller.response.status_code = 200
+    caller.verify_response()
+
+
+def test_verify_error_response():
+    caller = CheckCallCaller()
+    caller.response = Response()
+    caller.response.status_code = 300
+    caller.verify_response(error_code=300)
+
+
+def test_verify_response_body():
+    caller = CheckCallCaller()
+    caller.response = Response()
+    caller.response.status_code = 200
+    caller.response._content = str.encode(json.dumps({}))  # pylint: disable=protected-access
+    caller.verify_response(resp_body=Dictionary())
+
+
+def test_verify_response_text():
+    caller = CheckCallCaller()
+    caller.response = Response()
+    caller.response.status_code = 200
+    caller.response.headers = {'Content-Type': 'text/csv'}
+    caller.response._content = str.encode('test')  # pylint: disable=protected-access
+    caller.verify_response(
+        resp_body='test_parsed',
+        parse_response=lambda x: f'{x}_parsed',
+        normalize=True
+    )
