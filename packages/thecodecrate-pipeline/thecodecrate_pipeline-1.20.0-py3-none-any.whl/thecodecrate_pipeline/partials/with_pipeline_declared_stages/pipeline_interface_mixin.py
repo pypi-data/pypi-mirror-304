00f6debced2db576c023921c259c727a4b983fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:04:41 +0000

    Add package thecodecrate-pipeline/1.20.0/thecodecrate_pipeline-1.20.0-py3-none-any.whl

---
+++ packages/thecodecrate-pipeline/thecodecrate_pipeline-1.20.0-py3-none-any.whl/thecodecrate_pipeline/partials/with_pipeline_declared_stages/pipeline_interface_mixin.py
@@ -0,0 +1,18 @@
+from typing import Protocol
+
+from ..with_pipeline_as_list.pipeline_interface_mixin import (
+    PipelineInterfaceMixin as WithPipelineAsListInterface,
+)
+from ..with_base.pipeline_interface import (
+    PipelineInterface as WithPipelineBaseInterface,
+)
+
+
+class PipelineInterfaceMixin(
+    WithPipelineAsListInterface,
+    WithPipelineBaseInterface,
+    Protocol,
+):
+    def _should_instantiate_stages(self) -> bool: ...
+
+    def _instantiate_stages(self) -> None: ...
