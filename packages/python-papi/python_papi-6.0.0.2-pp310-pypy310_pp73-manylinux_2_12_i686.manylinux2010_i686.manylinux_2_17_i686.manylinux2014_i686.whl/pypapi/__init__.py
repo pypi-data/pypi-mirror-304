488c2fd7a2c60ed68fe2f3dcb695af72fe51c4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:11:10 +0000

    Add package python-papi/6.0.0.2/python_papi-6.0.0.2-pp310-pypy310_pp73-manylinux_2_12_i686.manylinux2010_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/python-papi/python_papi-6.0.0.2-pp310-pypy310_pp73-manylinux_2_12_i686.manylinux2010_i686.manylinux_2_17_i686.manylinux2014_i686.whl/pypapi/__init__.py
@@ -0,0 +1,15 @@
+from . import papi_high
+from . import papi_low
+from . import events
+from . import consts
+from . import exceptions
+from . import structs
+
+__all__ = [
+    "papi_high",
+    "papi_low",
+    "events",
+    "consts",
+    "exceptions",
+    "structs",
+]
